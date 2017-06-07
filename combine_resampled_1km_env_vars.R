3b02510573cea7bdcdb36b028155b07d2dec1889
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-06-07 15:24:48 +0100

    removed cell field; reset row names

--- combine_resampled_1km_env_vars.R
+++ combine_resampled_1km_env_vars.R
@@ -6,6 +6,7 @@ my_resources <- c(
   file.path("R", "prepare_datasets", "filter_and_resample.r"),
   file.path("R", "prepare_datasets", "grid_up_foi_dataset.r"),
   file.path("R", "prepare_datasets", "average_up.r"),
+  file.path("R", "prepare_datasets", "sub_n_sample.r"),
   file.path("R", "utility_functions.r"))
 
 my_pkgs <- c("data.table", "dplyr")
@@ -52,25 +53,21 @@ all_pixel_df <- do.call("rbind", pxl_job)
 # check duplicate cell values - it is OK to have duplicate! 
 # sum(duplicated(all_pixel_df[,1:4]))
 
-# all_pixel_df$cell <- seq_len(nrow(all_pixel_df))
+all_pixel_df <- all_pixel_df[setdiff(names(all_pixel_df), "cell")]
 
 data_points <- all_pixel_df[all_pixel_df$type != "pseudoAbsence", ]
 psAb <- all_pixel_df[all_pixel_df$type == "pseudoAbsence", ]
 
-psAb_spl <- split(psAb, list(psAb$data_id, psAb$ADM_0, psAb$ADM_1), drop = TRUE)
+psAb_spl <- unname(split(psAb, list(psAb$data_id, psAb$ADM_0, psAb$ADM_1), drop = TRUE))
 
 lng <- lapply(psAb_spl, nrow)
        
-sub_n_sample <- function(i, a){
-  x <- ifelse(nrow(i) >= a, a, nrow(i))
-  ids <- sample(seq_len(nrow(i)), x)
-  i[ids,]
-}
-  
 psAb_spl_smp <- lapply(psAb_spl, sub_n_sample, 215)
 
 psAbs_square <- do.call("rbind", psAb_spl_smp)
 
 all_pixel_df_2 <- rbind(data_points, psAbs_square)
   
+rownames(all_pixel_df_2) <- seq_len(nrow(all_pixel_df_2))
+
 write_out_rds(all_pixel_df_2, out_pt, out_fl_nm)
