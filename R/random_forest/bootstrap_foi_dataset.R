9362010cfd1912facc292804065a7b0ed81a387e
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-06-07 15:32:37 +0100

    changed to output the bootstrapped indices rather than the bootstrapped dataset

--- R/random_forest/bootstrap_foi_dataset.R
+++ R/random_forest/bootstrap_foi_dataset.R
@@ -1,5 +1,5 @@
 do_boostrap <- function(dataset){
   idx <- unname(split(seq_len(nrow(dataset)), dataset$cell))
-  pick <- sample(idx, size = length(idx), replace = TRUE)
-  dataset[unlist(pick), ]
+  sample(idx, size = length(idx), replace = TRUE)
+  #dataset[unlist(pick), ]
 }
