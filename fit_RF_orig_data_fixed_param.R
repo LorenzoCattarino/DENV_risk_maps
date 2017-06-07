608efc8f3a8d71c0a1e282f6072603ff6e1225a2
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-06-07 16:33:12 +0100

    changes

--- fit_RF_orig_data_fixed_param.R
+++ fit_RF_orig_data_fixed_param.R
@@ -8,10 +8,7 @@ my_resources <- c(
   file.path("R", "random_forest", "make_RF_predictions.r"),
   file.path("R", "random_forest", "get_1_0_point_position.r"),
   file.path("R", "random_forest", "calculate_sum_squared_errors.r"),
-  file.path("R", "random_forest", "spatial_sampK_cv_rng3.r"),
-  file.path("R", "random_forest", "grid_up_foi_dataset.r"),
-  file.path("R", "random_forest", "bootstrap_foi_dataset.r"),
-  file.path("R", "random_forest", "wrapper_to_spatial_sampK_cv_rng3.r"))
+  file.path("R", "random_forest", "spatial_sampK_cv_rng3.r"))
 
 my_pkgs <- "ranger"
 
@@ -38,11 +35,7 @@ pAbs_wgt <- 0.25
 
 out_path <- file.path("output", "model_objects")
 
-out_name <- "list_of_boot_models.rds"   
-          
-gr_size <- 5
-
-no_fits <- 200
+out_name <- "best_model_admin.rds"   
 
 
 # ---------------------------------------- Are you using the cluster?
@@ -88,9 +81,6 @@ my_predictors <- predictor_rank$variable[1:9]
 # ---------------------------------------- pre process the original foi dataset
 
 
-# remove outliers 
-foi_data <- subset(foi_data, ISO != "PYF" & ISO != "HTI")
-
 # set pseudo absence value
 foi_data[foi_data$type == "pseudoAbsence", y_var] <- pseudoAbs_value
 
@@ -99,19 +89,22 @@ foi_data$new_weight <- all_wgt
 foi_data[foi_data$type == "pseudoAbsence", "new_weight"] <- pAbs_wgt
 
 # add an ID for each data point (used by get_training_point_positions() and get_validating_point_positions())
-foi_data <- cbind(id = seq_len(nrow(foi_data)), foi_data)
+training_dataset <- cbind(id = seq_len(nrow(foi_data)), foi_data)
 
 
 # ---------------------------------------- create objects needed for run
 
 
-# no_data <- nrow(foi_data)
-# 
-# # get the position (1/0) of the points in the validating dataset
-# valid_point_pos <- get_validating_point_positions(no_data, training_dataset)
-# 
-# # get training dataset (full dataset - no bootstrap)
-# training_dataset <- foi_data[, c(y_var, my_predictors)]
+no_data <- nrow(foi_data)
+
+# get the position (1/0) of the points in the validating dataset
+valid_point_pos <- get_validating_point_positions(no_data, training_dataset)
+
+# get weights 
+my_weights <- training_dataset$new_weight 
+
+# get training dataset (full dataset - no bootstrap)
+training_dataset <- foi_data[, c(y_var, my_predictors)]
 
 y_data <- foi_data[, y_var]
 
@@ -123,44 +116,31 @@ x_data <- foi_data[, my_predictors]
 
 # run one job
 
-# RF_fit <- obj$enqueue(
-#   wrapper_to_core_fun(seq_len(no_fits)[1],
-#                       model_dataset = foi_data, 
-#                       grid_size = gr_size, 
-#                       predictors = my_predictors,
-#                       dependent_variable = y_var, 
-#                       no_trees = no_trees, 
-#                       min_node_size = min_node_size, 
-#                       x_data = x_data, 
-#                       y_data = y_data))
-
 if (CLUSTER) {
   
-  RF_fit <- queuer::qlapply(
-    seq_len(no_fits), 
-    wrapper_to_core_fun, 
-    obj,
-    model_dataset = foi_data, 
-    grid_size = gr_size, 
-    predictors = my_predictors,
-    dependent_variable = y_var, 
+  RF_fit <- obj$enqueue(
+    spatial.cv.rf(preds = my_predictors, 
+                  y_var = y_var, 
+                  train_set = training_dataset, 
+                  no_trees = no_trees, 
+                  min_node_size = min_node_size, 
+                  x_data = x_data, 
+                  y_data = y_data, 
+                  valid_points = valid_point_pos, 
+                  my_weights = my_weights))
+  
+} else {
+  
+  RF_fit <- spatial.cv.rf(
+    preds = my_predictors, 
+    y_var = y_var, 
+    train_set = training_dataset, 
     no_trees = no_trees, 
     min_node_size = min_node_size, 
     x_data = x_data, 
-    y_data = y_data)
-
-} else {
-  
-  RF_fit <- lapply(seq_len(no_fits), 
-                   wrapper_to_core_fun, 
-                   model_dataset = foi_data, 
-                   grid_size = gr_size, 
-                   predictors = my_predictors,
-                   dependent_variable = y_var, 
-                   no_trees = no_trees, 
-                   min_node_size = min_node_size, 
-                   x_data = x_data, 
-                   y_data = y_data)
+    y_data = y_data, 
+    valid_points = valid_point_pos, 
+    my_weights = my_weights)
   
 }
 
@@ -172,8 +152,4 @@ if (!CLUSTER) {
 # ---------------------------------------- save model
 
 
-res <- RF_fit$results()
-
-model_obj_lst <- lapply(res, "[[", "obj")
-
-write_out_rds(model_obj_lst, out_path, out_name)
+write_out_rds(RF_fit$obj, out_path, out_name)
