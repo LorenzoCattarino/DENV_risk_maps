a6e24da8129d5c13b731d4d97a8e130538c29fee
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-06-08 09:47:39 +0100

    added indexing of prediction and model rds objects for multiple EM runs

--- R/random_forest/Exp_Max_algorithm_with_boot.r
+++ R/random_forest/Exp_Max_algorithm_with_boot.r
@@ -2,20 +2,11 @@ exp_max_algorithm_boot <- function(
   i, pxl_dataset, gr_size,
   niter, foi_data, no_trees, 
   min_node_size, my_predictors, grp_flds,
-  out_md_nm, out_prd_nm, md_out_pth, 
-  prd_out_pth){
+  out_model_name, out_pred_name,
+  model_out_path, pred_out_path){
   
-  
-  ### 1. get a bootstrapped sample of the pxl dataset
-  
-  # overlay squared grid on data points 
-  gridded_dataset <- grid_up(pxl_dataset, gr_size, rnd_dist = TRUE)
-  
-  # get a bootstrapped sample of the df indices  
-  bt_ids <- do_boostrap(gridded_dataset)
-  
-  
-  ### 2. run the EM algorithm 
+  a <- out_model_name[i]
+  b <- out_pred_name[i]
   
   exp_max_algorithm(
     niter = niter, 
@@ -25,10 +16,9 @@ exp_max_algorithm_boot <- function(
     min_node_size = min_node_size,
     my_predictors = my_predictors, 
     grp_flds = grp_flds,
-    out_model_name = out_md_nm,
-    out_pred_name = out_prd_nm,
-    model_out_path = md_out_pth,
-    pred_out_path = prd_out_pth,
-    boot_inds = bt_ids)
+    out_model_name = a,
+    out_pred_name = a,
+    model_out_path = model_out_path,
+    pred_out_path = pred_out_path)
   
 }
