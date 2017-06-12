f33bfdae097d5a170c01eb333e71025e2aa88c6c
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-06-12 14:46:29 +0100

    added browser

--- R/random_forest/bootstrap_foi_dataset.R
+++ R/random_forest/bootstrap_foi_dataset.R
@@ -1,4 +1,5 @@
 do_boostrap <- function(dataset){
+  #browser()
   idx <- unname(split(seq_len(nrow(dataset)), dataset$cell))
   pick <- sample(idx, size = length(idx), replace = TRUE)
   dataset[unlist(pick), ]
