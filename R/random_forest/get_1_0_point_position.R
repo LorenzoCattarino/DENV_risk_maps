19feee4d0c1e0e9228c7c711ffebea7549fe53ae
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-06-14 09:21:37 +0100

    made the name of the unique field ID an argument

--- R/random_forest/get_1_0_point_position.R
+++ R/random_forest/get_1_0_point_position.R
@@ -1,21 +1,21 @@
-get_training_point_positions <- function(no_data, training_dataset){
+get_training_point_positions <- function(no_data, training_dataset, id_fld){
   
   training.set <- rep(0, no_data)
   
   # get the unique index of the data points in the training set
-  model.training.set <- unique(training_dataset$id) 
+  model.training.set <- unique(training_dataset[, id_fld]) 
   
   training.set[model.training.set] <- 1
   
   training.set
 }
 
-get_validating_point_positions <- function(no_data, training_dataset){
+get_validating_point_positions <- function(no_data, training_dataset, id_fld){
   
   training.set <- rep(0, no_data)
   
   # get the unique index of the data points in the training set
-  model.training.set <- unique(training_dataset$id) 
+  model.training.set <- unique(training_dataset[, id_fld]) 
   
   training.set[model.training.set] <- 1
   
