b77cf595f6b4bd34fc5c9c653cbf4122e79b5965
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-06-14 09:19:23 +0100

    renamed file

--- create_bootstrap_samples.r
+++ create_bootstrap_samples.r
@@ -3,7 +3,7 @@ options(didehpc.cluster = "fi--didemrchnb")
 CLUSTER <- TRUE
 
 my_resources <- c(
-  file.path("R", "random_forest", "grid_and_boot.r"),
+  file.path("R", "random_forest", "grid_and_bootstrap_multi_fits.r"),
   file.path("R", "random_forest", "grid_up_foi_dataset.r"),
   file.path("R", "random_forest", "bootstrap_foi_dataset.r"),
   file.path("R", "utility_functions.r"))
@@ -16,26 +16,34 @@ ctx <- context::context_save(path = "context",
                              packages = my_pkgs)
 
 
+# ---------------------------------------- define parameters
+
+
+no_fits <- 200
+
+dependent_variable <- "FOI"
+
+grid_size <- 5
+
+pseudoAbs_value <- 0
+
+all_wgt <- 1
+
+pAbs_wgt <- 0.25
+
+
 # ---------------------------------------- are you using the cluster? 
 
 
 if (CLUSTER) {
   
-  config <- didehpc::didehpc_config(template = "24Core")
-  obj <- didehpc::queue_didehpc(ctx, config = config)
+  obj <- didehpc::queue_didehpc(ctx)
   
 } else {
   
   context::context_load(ctx)
-  context::parallel_cluster_start(8, ctx)
   
 }
-
-
-# ---------------------------------------- define parameters
-
-
-no_fits <- 200
 
 
 # ---------------------------------------- load data
@@ -46,6 +54,18 @@ foi_data <- read.csv(
   stringsAsFactors = FALSE) 
 
 
+# ---------------------------------------- pre process the original foi dataset
+
+
+foi_data[foi_data$type == "pseudoAbsence", dependent_variable] <- pseudoAbs_value
+
+foi_data$new_weight <- all_wgt
+
+foi_data[foi_data$type == "pseudoAbsence", "new_weight"] <- pAbs_wgt
+
+names(foi_data)[names(foi_data) == dependent_variable] <- "o_j"
+
+
 # ---------------------------------------- submit job
 
 
@@ -53,16 +73,17 @@ if (CLUSTER) {
   
   get_boot_samples <- queuer::qlapply(
     seq_len(no_fits),
-    grid_and_boot()) 
-
+    grid_and_boot_multi,
+    obj,
+    a = foi_data, 
+    b = grid_size) 
+  
 } else {
-
+  
   get_boot_samples <- lapply(
-    seq_len(no_fits),
-    
-  )
+    seq_len(no_fits)[1:2],
+    grid_and_boot_multi,
+    a = foi_data, 
+    b = grid_size)
   
 }   
-    
-    
-    
