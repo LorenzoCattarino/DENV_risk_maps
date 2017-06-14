624c36e58b827f590945efcf040708b14b675951
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-06-14 09:20:03 +0100

    saves list of bootstrap samples

---
+++ save_bootstrap_samples.r
@@ -0,0 +1,53 @@
+options(didehpc.cluster = "fi--didemrchnb")
+
+CLUSTER <- TRUE
+
+my_resources <- c(
+  file.path("R", "random_forest", "grid_and_bootstrap_multi_fits.r"),
+  file.path("R", "random_forest", "grid_up_foi_dataset.r"),
+  file.path("R", "random_forest", "bootstrap_foi_dataset.r"),
+  file.path("R", "utility_functions.r"))
+
+my_pkgs <- c()
+
+context::context_log_start()
+ctx <- context::context_save(path = "context",
+                             sources = my_resources,
+                             packages = my_pkgs)
+
+
+# ---------------------------------------- define parameters
+
+
+out_fl_nm <- "bootstrap_samples.rds"
+
+out_pt <- file.path("output", "foi")
+
+
+# ---------------------------------------- rebuild the queue obj
+
+
+if (CLUSTER) {
+  
+  obj <- didehpc::queue_didehpc(ctx)
+  
+} else {
+  
+  context::context_load(ctx)
+  
+}
+
+task_b_name <- "preglacial_urubu"
+
+get_boot_samples_t <- obj$task_bundle_get(task_b_name)
+
+
+# ---------------------------------------- get the results 
+
+
+boot_samples <- get_boot_samples_t$results()
+
+
+# ---------------------------------------- save
+
+write_out_rds(boot_samples, out_pt, out_fl_nm)
