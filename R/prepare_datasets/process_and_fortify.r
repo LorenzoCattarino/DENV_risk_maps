11d46df8b2263a4c21e41461dc339728060f7ef7
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-07-03 17:29:58 +0100

    removed erase operation

--- R/prepare_datasets/process_and_fortify.r
+++ R/prepare_datasets/process_and_fortify.r
@@ -1,14 +1,10 @@
-process_and_fortify <- function(shp_fl_1, shp_fl_2, out_path, out_name){
+fortify_and_save <- function(shp_fl, out_path, out_name){
   
-  shp_fl_1 <- shp_fl_1[!shp_fl_1@data$NAME_ENGLI == "Antarctica" | shp_fl_1@data$NAME_ENGLI == "Caspian Sea", ]
+  shp_fl <- shp_fl[!shp_fl@data$NAME_ENGLI == "Antarctica" | shp_fl@data$NAME_ENGLI == "Caspian Sea", ]
   
-  shp_fl_1_erased <- shp_fl_1 - shp_fl_2
-  
-  a <- fortify(shp_fl_1_erased, region = "OBJECTID")
+  a <- fortify(shp_fl, region = "OBJECTID")
   
   dir.create(file.path("output", "shapefiles"), FALSE, TRUE)
-  
-  writeOGR(shp_fl_1_erased, file.path("output", "shapefiles"), "gadm28_adm0_eras", driver = "ESRI Shapefile")
   
   write_out_rds(a, out_path, out_name)
 
