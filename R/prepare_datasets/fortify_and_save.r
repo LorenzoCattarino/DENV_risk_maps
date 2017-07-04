83199d4588c7a5bf35ab5af5f8fdf443f143245a
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-07-04 09:05:38 +0100

    Fixed brackets

--- R/prepare_datasets/fortify_and_save.r
+++ R/prepare_datasets/fortify_and_save.r
@@ -1,6 +1,6 @@
 fortify_and_save <- function(shp_fl, out_path, out_name){
   
-  shp_fl <- shp_fl[!shp_fl@data$NAME_ENGLI == "Antarctica" | shp_fl@data$NAME_ENGLI == "Caspian Sea", ]
+  shp_fl <- shp_fl[!(shp_fl@data$NAME_ENGLI == "Antarctica" | shp_fl@data$NAME_ENGLI == "Caspian Sea"), ]
   
   a <- fortify(shp_fl)
   
