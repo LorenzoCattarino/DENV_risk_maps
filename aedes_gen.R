30a318c496a193e5873336f48d302a89add63c31
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-07-17 16:28:45 +0100

    Removed superfluous lines

--- aedes_gen.R
+++ aedes_gen.R
@@ -1,60 +1,40 @@
-rm(list = ls())
-
-# load packages 
 library(raster)
 library(maptools)
 library(fields)
 
 # load data 
-Global.mod.results2005to2010 <- raster(file.path("data", 
-                                                 "aedes_generations", 
-                                                 "eggsgen_ck_2005-2010_Global_arg.grd"))
+aedes_gen <- raster(file.path("data", 
+                              "aedes_generations", 
+                              "eggsgen_ck_2005-2010_Global_arg.grd"))
+
 country_border_shp <- readShapePoly(file.path("data", 
                                               "shapefiles", 
                                               "gadm28_levels.shp", 
                                               "gadm28_adm0.shp"))
+
 admin_shp <- readShapePoly(file.path("data", 
                                      "shapefiles", 
                                      "gadm28_levels.shp", 
                                      "gadm28_adm1.shp"))  
 
-# define projection
-geograhic_CRS <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84")
+lands_pop <- raster(file.path("data",
+                              "Landscan_2015",
+                              "lspop2015.flt"))
 
-proj4string(country_border_shp) <- geograhic_CRS
-
-?projectRaster
 
 # ---------------------------------------- plot original raster
 
 
 map_colours <- terrain.colors(50)
 
-lon <- seq(150, 150, by = 50)
-lat <- seq(-60, 60, by = 20)
-
-tiff(file.path("figures", "dengue_dataset", 
-              "aedes_gen_original_res.tiff"), 
-    width = 10, height = 6, units = "in", 
-    compression = "lzw",
+png(file.path("figures", 
+              "aedes_gen_original_res.png"), 
+    width = 14, 
+    height = 6, 
+    units = "in", 
     res = 300)
 
-print(plot(
-  Global.mod.results2005to2010,
-  ext = extent(-180, 180, -80, 80),
-  axes = TRUE, 
-  main = "number of aedes generations at 25 x 25 pixel res", 
-  xlab = "Longitude",
-  ylab = "Latitude",
-  useRaster = FALSE)) 
-
-axis(1, at = seq(-150, 150, 50), lab = seq(-150, 150, 50))
-
-axis(2, at = seq(-60, 60, 20), lab = seq(-60, 60, 20))
-
-box()
-
-plot(country_border_shp, lwd = 0.5, border = "gray30", add = TRUE)
+plot(aedes_gen)
 
 dev.off()
 
