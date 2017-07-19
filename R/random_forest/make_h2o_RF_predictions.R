67fdccb862ff864a9b4ee9c644ea234b4780055f
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-07-19 14:42:03 +0100

    new function

---
+++ R/random_forest/make_h2o_RF_predictions.R
@@ -0,0 +1,16 @@
+make_h2o_predictions <- function(mod_obj, dataset, sel_preds){
+  
+  #browser()
+  
+  x_data <- subset(dataset,, sel_preds, drop = FALSE)
+  
+  x_data <- as.h2o(x_data)
+  
+  preds <- predict(mod_obj, x_data)
+  
+  preds <- as.vector(preds)
+  
+  preds [preds < 0] <- 0
+  
+  preds
+}
