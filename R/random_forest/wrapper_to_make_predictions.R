67f84ee82d8c16c759cf1b7c8988bc30676e9433
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-07-22 17:36:42 +0100

    Introduced back the loop over bootstrap samples and made the script h2o compatible

--- R/random_forest/wrapper_to_make_predictions.R
+++ R/random_forest/wrapper_to_make_predictions.R
@@ -1,29 +1,22 @@
 wrapper_to_make_preds <- function(
   dataset, predictors, model_in_path,
   parallel, cut_off, base_info, 
-  var_names){
+  var_names, no_fits){
   
   #browser()
-  
-  model_lst <- readRDS(model_in_path)
   
   # Remove records with at least one NA predictor value
   dataset_2 <- remove_NA_rows(dataset, predictors)
-  
-  preds_all_models <- make_predictions(
-    model_lst,
-    dataset = dataset_2,
-    sel_preds = predictors)
-
-  #browser()
-  
-  # preds_all_models <- loop_simplify(
-  #   model_lst,
-  #   make_predictions,
-  #   dataset = dataset_2,
-  #   sel_preds = predictors,
-  #   parallel = parallel,
-  #   what = numeric(nrow(dataset_2)))
+    
+  preds_all_models <- loop_simplify(
+    seq_len(no_fits),
+    function(i){
+      RF_obj_nm <- paste0("RF_obj_sample_", i, ".rds")
+      RF_obj <- h2o.loadModel(file.path(model_in_path, RF_obj_nm))
+      make_h2o_predictions(RF_obj, dataset_2, predictors)
+    },
+    parallel = parallel,
+    what = numeric(nrow(dataset_2)))
   
   preds_all_models[preds_all_models < cut_off] <- 0
   
@@ -31,44 +24,35 @@ wrapper_to_make_preds <- function(
   
   if(is.null(dim(preds_all_models))){
     
-    mean_pred <- preds_all_models
-      
-    #mean_pred <- mean(preds_all_models)
+    mean_pred <- mean(preds_all_models)
     
-    #sd_pred <- sd(preds_all_models)
+    percentiles <- quantile(preds_all_models, probs = c(0.025, 0.975))
     
-    #percentiles <- quantile(preds_all_models, probs = c(0.025, 0.975))
-    
-    #a <- percentiles[1]
-    #b <- percentiles[2]
+    a <- percentiles[1]
+    b <- percentiles[2]
     
   }else{
     
-    mean_pred <- preds_all_models
+    mean_pred <- rowMeans(preds_all_models)
     
-    #mean_pred <- rowMeans(preds_all_models)
+    percentiles <- apply(preds_all_models, 1, FUN = quantile, probs = c(0.025, 0.975))
     
-    #sd_pred <- apply(preds_all_models, 1, FUN = sd)
+    percentiles <- t(percentiles)
     
-    #percentiles <- apply(preds_all_models, 1, FUN = quantile, probs = c(0.025, 0.975))
-    
-    #percentiles <- t(percentiles)
-    
-    #a <- percentiles[, 1]
-    #b <- percentiles[, 2]
+    a <- percentiles[, 1]
+    b <- percentiles[, 2]
     
   }
   
   zero_logical <- mean_pred == 0
   
   out <- setNames(data.frame(dataset_2[!zero_logical, base_info],
-                             mean_pred[!zero_logical]),
-                             #sd_pred[!zero_logical],
-                             #a[!zero_logical],
-                             #b[!zero_logical]),
+                             mean_pred[!zero_logical],
+                             a[!zero_logical],
+                             b[!zero_logical]),
                   nm = c(base_info, var_names))
   
-  #if(sum(is.na(out$mean_pred)) > 0) stop ("NA predictions")
+  if(sum(is.na(out$mean_pred)) > 0) stop ("NA predictions")
     
   out  
 
