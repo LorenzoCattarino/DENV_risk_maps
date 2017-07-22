4928675cd2a2499b0b6efdf7729cb7a6ada80ba2
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-07-22 18:41:38 +0100

    Made a bit more explicit the differences between predicting at pixel level (1 km) and predicting at square level (>1 km); style edits

--- predict_pixel_level.R
+++ predict_pixel_level.R
@@ -1,4 +1,4 @@
-options(didewin.cluster = "fi--didemrchnb")
+options(didehpc.cluster = "fi--didemrchnb")
 
 CLUSTER <- TRUE
 
@@ -6,42 +6,51 @@ my_resources <- c(
   file.path("R", "utility_functions.R"),
   file.path("R", "random_forest", "wrapper_to_load_tile_dataset.R"),
   file.path("R", "random_forest", "wrapper_to_make_predictions.R"),
-  file.path("R", "random_forest", "make_RF_predictions.R"),
+  file.path("R", "random_forest", "make_h2o_RF_predictions.R"),
   file.path("R", "prepare_datasets", "remove_NA_rows.R"))
 
-my_pkgs <- c("ranger", "data.table")
+my_pkgs <- c("h2o", "data.table")
 
 context::context_log_start()
-ctx <- context::context_save(packages = my_pkgs,
+ctx <- context::context_save(path = "context",
                              sources = my_resources,
-                             root = "context")
+                             packages = my_pkgs)
 
 
 # ---------------------------------------- define parameters
 
 
-cut_off <- 0
-
-bs_inf <- c("cell", "lat.grid", "long.grid", "population") 
-#c("pixel_id", "latitude", "longitude", "population", ADM_0", "ADM_1", "ADM_2")
-
-var_names <- "mean_pred" #, "sd_pred", "low_perc", "up_perc")
+var_names <- c("mean_pred" , "low_perc", "up_perc")
 
 RF_obj_path <- file.path(
   "output",
-  "model_objects",
-  "best_model_20km_cw.RDS")
+  "EM_algorithm",
+  "optimized_model_objects",
+  "boot_samples")
 
-in_path <- file.path(
-  "output",
-  "env_variables",
-  "all_sets_0_1667_deg")
+no_fits <- 200
 
-out_path <- file.path(
-  "output", 
-  "predictions", 
-  "best_model_20km_cw",
-  "tile_sets_0_1667_deg")
+cut_off <- 0
+
+gr_size <- 1 # km 
+
+if (gr_size == 1) {
+  bs_inf <- c("pixel_id", "latitude", "longitude", "population", "ADM_0", "ADM_1", "ADM_2")
+  in_path <- file.path("data", "env_variables", "all_sets_gadm_codes")
+  out_pth <- file.path("output", 
+                       "predictions", 
+                       "boot_model_20km_cw",
+                       "tile_sets_0_0083_deg")
+} 
+
+if (gr_size == 20) {
+  bs_inf <- c("cell", "lat.grid", "long.grid", "population")
+  in_path <- file.path("output", "env_variables", "all_sets_0_1667_deg")
+  out_pth <- file.path("output", 
+                       "predictions", 
+                       "boot_model_20km_cw",
+                       "tile_sets_0_1667_deg")
+}
 
 
 # ---------------------------------------- are you using the cluster?
@@ -49,8 +58,8 @@ out_path <- file.path(
 
 if (CLUSTER) {
   
-  #config <- didewin::didewin_config(template = "12and16Core")
-  obj <- didewin::queue_didewin(ctx)
+  config <- didehpc::didehpc_config(template = "12and16Core")
+  obj <- didehpc::queue_didehpc(ctx, config = config)
 
 } else {
   
@@ -99,51 +108,23 @@ best_predictors <- predictor_rank$variable[1:9]
 # get tile ids
 tile_ids <- tile_summary$tile.id
 
-#NA_pixel_tile_ids <- NA_pixel_tiles$tile_id
-NA_pixel_tile_ids <- c(217, 254, 288, 321, 326, 402, 442, 458, 485, 486, 492, 493, 494, 519, 520, 523, 530, 539, 284, 289, 290, 291, 406, 432, 433, 529)
+if (gr_size == 1) {
+  NA_pixel_tile_ids <- NA_pixel_tiles$tile_id
+}
+
+if (gr_size == 20){
+  NA_pixel_tile_ids <- c(217, 254, 288, 321, 326, 402, 
+                         442, 458, 485, 486, 492, 493, 
+                         494, 519, 520, 523, 530, 539, 
+                         284, 289, 290, 291, 406, 432, 
+                         433, 529)
+}
 
 tile_ids_2 <- tile_ids[!tile_ids %in% NA_pixel_tile_ids]  
 
 
-# ---------------------------------------- submit jobs
+# ---------------------------------------- submit one job 
 
-
-if (CLUSTER) {
-  
-  all_tiles <- queuer::qlapply(
-    seq_along(tile_ids_2), 
-    wrapper_to_load_tile_dataset, 
-    obj,
-    ids_vec = tile_ids_2,
-    sel_preds = best_predictors,
-    in_path = in_path,
-    model_in_path = RF_obj_path,
-    out_path = out_path,
-    cut_off = cut_off,
-    var_names = var_names,
-    base_info = bs_inf,
-    parallel = FALSE)
-  
-} else {
-  
-  all_tiles <- lapply(
-    seq_along(tile_ids_2)[61],
-    wrapper_to_load_tile_dataset,
-    ids_vec = tile_ids_2,
-    sel_preds = best_predictors,
-    in_path = in_path,
-    model_in_path = RF_obj_path,
-    out_path = out_path,
-    cut_off = cut_off,
-    var_names = var_names,
-    base_info = bs_inf,
-    parallel = FALSE)
-  
-}
-
-if (!CLUSTER) {
-  context:::stop_parallel_cluster()
-}
 
 # t <- obj$enqueue(
 #   wrapper_to_load_tile_dataset(
@@ -156,4 +137,48 @@ if (!CLUSTER) {
 #     cut_off = cut_off,
 #     var_names = var_names,
 #     base_info = bs_inf,
-#     parallel = FALSE))
+#     parallel = FALSE,
+#     no_fits = no_fits))
+
+
+# ---------------------------------------- submit all jobs
+
+
+if (CLUSTER) {
+  
+  all_tiles <- queuer::qlapply(
+    seq_along(tile_ids_2), 
+    wrapper_to_load_tile_dataset, 
+    obj,
+    ids_vec = tile_ids_2,
+    sel_preds = best_predictors,
+    in_path = in_path,
+    model_in_path = RF_obj_path,
+    out_path = out_pth,
+    cut_off = cut_off,
+    var_names = var_names,
+    base_info = bs_inf,
+    parallel = FALSE,
+    no_fits = no_fits)
+  
+} else {
+  
+  all_tiles <- lapply(
+    seq_along(tile_ids_2)[61],
+    wrapper_to_load_tile_dataset,
+    ids_vec = tile_ids_2,
+    sel_preds = best_predictors,
+    in_path = in_path,
+    model_in_path = RF_obj_path,
+    out_path = out_pth,
+    cut_off = cut_off,
+    var_names = var_names,
+    base_info = bs_inf,
+    parallel = FALSE,
+    no_fits = no_fits)
+  
+}
+
+if (!CLUSTER) {
+  context:::stop_parallel_cluster()
+}
