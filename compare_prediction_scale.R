0664d00d15ba031878b81f88540b1889ebfd6497
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-07-24 16:42:09 +0100

    Modified after running 200 bootsamples

--- compare_prediction_scale.R
+++ compare_prediction_scale.R
@@ -9,13 +9,16 @@ source(file.path("R", "prepare_datasets", "average_up.R"))
 
 res <- 20 
 
-out_name <- paste0("prediction_scale_comparison_best_", res, "km_cw.csv")
+mod_type <- "boot"
+  
+out_name <- paste0("prediction_scale_comparison_", mod_type, "_", res, "km_cw.csv")
 
 var_names_pxl_dts <- "mean_pred" #, "sd_pred", "low_perc", "up_perc")
 
-var_names_sqr_dts <- "p_i" 
+var_names_sqr_dts_train <- "mean_train" 
+var_names_sqr_dts_test <- "mean_test" 
 
-tgt_dir_name <- paste0("best_model_", res, "km_cw")
+tgt_dir_name <- paste0(mod_type, "_model_", res, "km_cw")
 
 out_path <- file.path(
   "output",
@@ -27,40 +30,42 @@ out_path <- file.path(
 
 
 foi_data <- read.csv(
-  file.path("output", "All_FOI_estimates_linear_env_var.csv"),
+  file.path("output", "foi", "All_FOI_estimates_linear_env_var.csv"),
   header = TRUE)
 
 adm_preds <- readRDS(  
   file.path("output",
             "predictions",
             tgt_dir_name,
-            "adm_1_predictions.RDS"))
+            "adm_1_predictions.rds"))
 
 square_preds <- readRDS(
   file.path("output",
             "predictions",
             tgt_dir_name,
-            paste0("square_predictions_best_model_", res, "km_cw.RDS")))
+            "square_predictions.rds"))
 
 pxl_preds <- readRDS(
   file.path("output",
             "predictions",
             tgt_dir_name,
-            paste0("pxl_predictions_best_model_", res, "km_cw.RDS")))
+            "pred_0_0083_deg_long.rds"))
   
-  
+full_square_dts <- readRDS(
+  file.path("output", 
+            "EM_algorithm", 
+            "env_variables", 
+            "aggreg_pixel_level_env_vars_20km.rds"))  
+
+
 # ---------------------------------------- pre process the data
 
-
-# remove outliers 
-foi_data <- subset(foi_data, ISO != "PYF" & ISO != "HTI")
 
 # remove pseudo absences 
 foi_data <- subset(foi_data, type != "pseudoAbsence")
 
-ad_adm <- foi_data %>% group_by_(.dots = c("ID_0", "ID_1"))
-
-o_j <- ad_adm %>% summarise(o_j = mean(FOI))
+# rename FOI field
+names(foi_data)[names(foi_data) == "FOI"] <- "o_j"
 
 
 # ---------------------------------------- pre process admin predictions
@@ -71,12 +76,20 @@ adm_df <- adm_preds[!duplicated(adm_preds[, c("ID_0", "ID_1")]), ]
 names(adm_df)[names(adm_df) == "mean_pred"] <- "adm_pred"#, "adm_sd", "adm_low_perc", "adm_up_perc")
 
 
+# ---------------------------------------- pre process square predictions 
+
+
+square_preds_train <- cbind(full_square_dts, square_preds[, 1:3])
+
+square_preds_test <- cbind(full_square_dts, square_preds[, 4:6])
+
+
 # ---------------------------------------- average up 
 
 
 average_pxl <- average_up(
   pxl_df = pxl_preds, 
-  grp_flds = c("ADM_0", "ADM_1"), 
+  grp_flds = c("data_id", "ADM_0", "ADM_1"), 
   var_names = var_names_pxl_dts)
 
 names(average_pxl)[names(average_pxl) == "ADM_0"] <- "ID_0"
@@ -84,36 +97,53 @@ names(average_pxl)[names(average_pxl) == "ADM_1"] <- "ID_1"
 names(average_pxl)[names(average_pxl) == "mean_pred"] <- "mean_pxl_pred" 
 # "mean_pxl_sd", "mean_pxl_lp", "mean_pxl_up"
 
-average_sqr <- average_up(
-  pxl_df = square_preds,
-  grp_flds = c("ID_0", "ID_1"),
-  var_names = var_names_sqr_dts)
+average_sqr_train <- average_up(
+  pxl_df = square_preds_train,
+  grp_flds = c("data_id", "ADM_0", "ADM_1"),
+  var_names = var_names_sqr_dts_train)
 
-names(average_sqr)[names(average_sqr) == "p_i"] <- "mean_square_pred" 
+names(average_sqr_train)[names(average_sqr_train) == "ADM_0"] <- "ID_0"
+names(average_sqr_train)[names(average_sqr_train) == "ADM_1"] <- "ID_1" 
+
+average_sqr_test <- average_up(
+  pxl_df = square_preds_test,
+  grp_flds = c("data_id", "ADM_0", "ADM_1"),
+  var_names = var_names_sqr_dts_test)
+
+names(average_sqr_test)[names(average_sqr_test) == "ADM_0"] <- "ID_0"
+names(average_sqr_test)[names(average_sqr_test) == "ADM_1"] <- "ID_1" 
+
+#names(average_sqr)[names(average_sqr) == "p_i"] <- "mean_square_pred" 
 
 
 # ---------------------------------------- join 
 
 
 m_1 <- merge(
-  o_j[, c("ID_0", "ID_1", "o_j")],
-  adm_preds[, c("ID_0", "ID_1", "mean_pred")],
-  by = c("ID_0", "ID_1"), 
+  foi_data[, c("data_id", "ID_0", "ID_1", "o_j")],
+  adm_df[, c("ID_0", "ID_1", "mean_pred")],
+  by = c("ID_0", "ID_1"),
   all.y = FALSE)
 
 m_2 <- merge(
   m_1,
-  average_pxl[, c("ID_0", "ID_1", "mean_pxl_pred")],
-  by = c("ID_0", "ID_1"), 
+  average_pxl[, c("data_id", "ID_0", "ID_1", "mean_pxl_pred")],
+  by = c("data_id", "ID_0", "ID_1"),
   all.y = FALSE)
 
 m_3 <- merge(
   m_2,
-  average_sqr,
-  by = c("ID_0", "ID_1"),
+  average_sqr_train[, c("data_id", "ID_0", "ID_1", "mean_train")],
+  by = c("data_id", "ID_0", "ID_1"),
   all.y = FALSE)
   
-write_out_csv(m_3, out_path, out_name)
+m_4 <- merge(
+  m_3,
+  average_sqr_test[, c("data_id", "ID_0", "ID_1", "mean_test")],
+  by = c("data_id", "ID_0", "ID_1"),
+  all.y = FALSE)
+
+write_out_csv(m_4, out_path, out_name)
 
 
 # ---------------------------------------- write out dataframe
