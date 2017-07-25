9f2a162787af63e1953be5ae32396968fedc02ef
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-07-25 10:22:14 +0100

    Fixed to allow using h2o

--- R/random_forest/spatial_sampK_cv_rng3.R
+++ R/random_forest/spatial_sampK_cv_rng3.R
@@ -1,29 +1,34 @@
 spatial.cv.rf <- function(
   preds, y_var, train_set, 
-  no_trees, min_node_size, x_data, 
-  y_data, valid_points, my_weights){
+  no_trees, min_node_size, my_weights, 
+  model_name){
+  
+  h2o.init()
   
   # fit a RF model
-  ranger_obj <- fit_RF(
+  RF_obj <- fit_h2o_RF(
     dependent_variable = y_var, 
+    predictors = preds, 
     training_dataset = train_set, 
     no_trees = no_trees, 
     min_node_size = min_node_size,
-    my_weights = my_weights)
+    my_weights = my_weights,
+    model_nm = model_name)
   
   # make predictions
-  predictions <- make_predictions(
-    mod_obj = ranger_obj, 
-    dataset = x_data, 
+  predictions <- make_h2o_predictions(
+    mod_obj = RF_obj, 
+    dataset = train_set, 
     sel_preds = preds)
     
-  # calculate sum of squared errors
-  sse <- calc_SSE(
-    y.data = y_data,
-    valid.set = valid_points,
-    predictions = predictions)
+  # # calculate sum of squared errors
+  # sse <- calc_SSE(
+  #   y.data = y_data,
+  #   valid.set = valid_points,
+  #   predictions = predictions)
   
-  list(obj = ranger_obj,
-       predictions = predictions,
-       sse = sse)
+  h2o.shutdown(prompt = FALSE)
+  
+  predictions
+
 }
