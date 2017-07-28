b346067bcb5a4394b77e60ba421c6e85da6a33e5
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-07-28 10:05:37 +0100

    Modified script after introducing h2o

--- EM_algorithm_5.R
+++ EM_algorithm_5.R
@@ -1,19 +1,22 @@
 # Runs the EM algorithm on the entire original foi dataset
 
-options(didewin.cluster = "fi--didemrchnb")
+options(didehpc.cluster = "fi--didemrchnb")
 
 CLUSTER <- TRUE
 
 my_resources <- c(
   file.path("R", "utility_functions.R"),
-  file.path("R", "random_forest", "Exp_Max_algorithm.R"))  
+  file.path("R", "random_forest", "Exp_Max_algorithm.R"),
+  file.path("R", "random_forest", "fit_h2o_random_forest_model.r"),
+  file.path("R", "random_forest", "make_h2o_RF_predictions.r"),
+  file.path("R", "random_forest", "quick_raster_map.r"))  
 
-my_pkgs <- c("ranger", "dplyr")
+my_pkgs <- c("h2o", "dplyr", "fields")
 
 context::context_log_start()
-ctx <- context::context_save(packages = my_pkgs,
+ctx <- context::context_save(path = "context",
                              sources = my_resources,
-                             root = "context")
+                             packages = my_pkgs)
 
 
 # ---------------------------------------- are you using the cluster? 
@@ -21,8 +24,8 @@ ctx <- context::context_save(packages = my_pkgs,
 
 if (CLUSTER) {
   
-  config <- didewin::didewin_config(template = "24Core")
-  obj <- didewin::queue_didewin(ctx, config = config)
+  config <- didehpc::didehpc_config(template = "24Core")
+  obj <- didehpc::queue_didehpc(ctx, config = config)
 
 } else {
   
@@ -34,17 +37,7 @@ if (CLUSTER) {
 # ---------------------------------------- define parameters
 
 
-pxl_dts_name <- "All_FOI_estimates_disaggreg_20km.RDS"
-
-out_md_nm <- "best_model_20km_cw.RDS"
-
-out_prd_nm <- "square_predictions_best_model_20km_cw.RDS"
-
-prd_out_pth <- file.path("output", "predictions", "best_model_20km_cw")
-
-md_out_pth <- file.path("output", "model_objects")
-
-grp_flds <- c("ID_0", "ID_1")
+dependent_variable <- "o_j"
 
 no_trees <- 500
 
@@ -54,14 +47,37 @@ all_wgt <- 1
 
 pAbs_wgt <- 0.25
 
-niter <- 35
+niter <- 200
 
-  
+grp_flds <- c("ID_0", "ID_1", "data_id")
+
+pxl_dts_name <- "All_FOI_estimates_disaggreg_20km.rds"
+
+out_md_nm <- "best_model_20km_cw.rds"
+
+md_out_pth <- file.path("output", "EM_algorithm", "optimized_model_objects")
+
+diag_t_pth <- file.path("output", "EM_algorithm", "diagnostics", "best_model_20km_cw")
+
+diag_t_nm <- "diagno_table.rds"
+
+map_pth <- file.path("figures", "EM_algorithm", "best_model_20km_cw", "maps")
+
+map_nm <- "map"
+
+sq_pred_pth <- file.path("output", "EM_algorithm", "square_predictions", "best_model_20km_cw")
+
+sq_pred_nm <- "dd_debug.rds"
+
+wgt_ftcr <- 1
+#wgt_ftcr <- 1 / 10000
+
+
 # ---------------------------------------- load data
 
 
 pxl_dataset <- readRDS(
-  file.path("output", pxl_dts_name))
+  file.path("output", "EM_algorithm", "env_variables_foi", pxl_dts_name))
 
 predictor_rank <- read.csv(
   file.path("output", 
@@ -72,7 +88,7 @@ predictor_rank <- read.csv(
   stringsAsFactors = FALSE)
 
 foi_data <- read.csv(
-  file.path("output", "All_FOI_estimates_linear_env_var.csv"),
+  file.path("output", "foi", "All_FOI_estimates_linear_env_var.csv"),
   stringsAsFactors = FALSE) 
 
 
@@ -85,19 +101,11 @@ my_predictors <- predictor_rank$variable[1:9]
 # ---------------------------------------- pre process the original foi dataset
 
 
-foi_data <- subset(foi_data, ISO != "PYF" & ISO != "HTI")
-
 foi_data$new_weight <- all_wgt
 
 foi_data[foi_data$type == "pseudoAbsence", "new_weight"] <- pAbs_wgt
 
-ad_adm <- foi_data %>% group_by_(.dots = grp_flds)
-
-o_j <- ad_adm %>% summarise(o_j = mean(FOI))
-
-new_weight_j <- ad_adm %>% summarise(new_weight = unique(new_weight))
-
-admin_dataset <- left_join(o_j, new_weight_j)
+names(foi_data)[names(foi_data) == "FOI"] <- dependent_variable
 
 
 # ---------------------------------------- pre process the prediction dataset
@@ -105,18 +113,6 @@ admin_dataset <- left_join(o_j, new_weight_j)
 
 names(pxl_dataset)[names(pxl_dataset) == "ADM_0"] <- grp_flds[1]
 names(pxl_dataset)[names(pxl_dataset) == "ADM_1"] <- grp_flds[2]
-
-pxl_dataset[, my_predictors][pxl_dataset[, my_predictors] == 0] <- NA
-
-na_rows <- apply(pxl_dataset, 1, anyNA)
-
-pxl_dataset <- pxl_dataset[!na_rows, ]
-
-px_adm <- pxl_dataset %>% group_by_(.dots = grp_flds)
-
-adm_pop <- px_adm %>% summarise(adm_pop = sum(population))
-  
-pxl_dataset <- left_join(pxl_dataset, adm_pop)
 
 pxl_dataset$pop_weight <- pxl_dataset$population / pxl_dataset$adm_pop
 
@@ -129,7 +125,7 @@ pxl_dataset[pxl_dataset$type == "pseudoAbsence", "new_weight"] <- pAbs_wgt
 
 
 # drop values in pxl_dataset which are not in o_j (PYF and HTI) 
-pxl_dataset <- inner_join(pxl_dataset, o_j)
+pxl_dataset <- inner_join(pxl_dataset, foi_data[, c(grp_flds, dependent_variable)])
 
 
 # ---------------------------------------- submit job
@@ -140,31 +136,43 @@ if (CLUSTER) {
   EM_alg_run <- obj$enqueue(
     exp_max_algorithm(
       niter = niter, 
-      adm_dataset = admin_dataset,
-      pxl_dataset = pxl_dataset, 
+      adm_dataset = foi_data,
+      pxl_dataset = pxl_dataset,
+      pxl_dataset_full = pxl_dataset,
       no_trees = no_trees, 
       min_node_size = min_node_size,
       my_predictors = my_predictors, 
       grp_flds = grp_flds,
-      out_model_name = out_md_nm,
-      out_pred_name = out_prd_nm,
-      model_out_path = md_out_pth,
-      pred_out_path = prd_out_pth))
+      RF_obj_path = md_out_pth,
+      RF_obj_name = out_md_nm,
+      diagn_tab_path = diag_t_pth, 
+      diagn_tab_name = diag_t_nm,
+      map_path = map_pth, 
+      map_name = map_nm,
+      sq_pr_path = sq_pred_pth, 
+      sq_pr_name = sq_pred_nm,
+      wgt_factor = wgt_ftcr))
   
 } else {
   
   EM_alg_run <- exp_max_algorithm(
     niter = niter, 
-    adm_dataset = admin_dataset,
+    adm_dataset = foi_data,
     pxl_dataset = pxl_dataset, 
+    pxl_dataset_full = pxl_dataset,
     no_trees = no_trees, 
     min_node_size = min_node_size,
     my_predictors = my_predictors, 
     grp_flds = grp_flds,
-    out_model_name = out_md_nm,
-    out_pred_name = out_prd_nm,
-    model_out_path = md_out_pth,
-    pred_out_path = prd_out_pth)
+    RF_obj_path = md_out_pth,
+    RF_obj_name = out_md_nm,
+    diagn_tab_path = diag_t_pth, 
+    diagn_tab_name = diag_t_nm,
+    map_path = map_pth, 
+    map_name = map_nm,
+    sq_pr_path = sq_pred_pth, 
+    sq_pr_name = sq_pred_nm,
+    wgt_factor = wgt_ftcr)
 
 }
 
