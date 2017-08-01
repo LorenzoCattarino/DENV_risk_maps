3b858daeebe61fa38a81ecdd9a38c98f72d482c3
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-08-01 14:10:36 +0100

    Removed unused code

--- EM_algorithm_with_bootstrap_8.r
+++ EM_algorithm_with_bootstrap_8.r
@@ -41,8 +41,10 @@ if (CLUSTER) {
 # ---------------------------------------- define parameters
 
 
+model_type <- "boot_model_20km_vw"
+
 out_fl_nm <- "square_predictions_all_data.rds"
-out_pt <- file.path("output", "predictions", "boot_model_20km_cw")
+out_pt <- file.path("output", "predictions", model_type)
 
 
 # ---------------------------------------- get results
@@ -55,63 +57,9 @@ EM_alg_run_t <- obj$task_bundle_get(my_task_id)
 EM_alg_run <- EM_alg_run_t$results()
 
 
-# ---------------------------------------- get mean predictions for train and test set
+# ---------------------------------------- combine all results together
 
 
 prediction_sets <- do.call("cbind", EM_alg_run)
 
 write_out_rds(prediction_sets, out_pt, out_fl_nm)
-
-
-# #train_sets <- vapply(EM_alg_run, "[[", numeric(len), 2)
-# #test_sets <- vapply(EM_alg_run, "[[", numeric(len), 3)
-# 
-# 
-# 
-# # ===================================================================
-# # ===================================================================
-# 
-# # recorded train and test sets are wrong - you have to recalculate them
-# 
-# full_pxl_dts <- readRDS(file.path("output", "EM_algorithm", "env_variables", "aggreg_pixel_level_env_vars_20km.rds"))
-# 
-# no_squares <- nrow(full_pxl_dts)
-# 
-# no_fits <- 200
-# 
-# train_sets <- matrix(0, nrow = no_squares, ncol = no_fits)
-# 
-# for (i in seq_len(no_fits)){
-# 
-#   pxl_dts_nm <- paste0("All_FOI_estimates_disaggreg_20km_sample_", i, ".rds")
-# 
-#   pxl_dts_boot <- readRDS(file.path("output", "EM_algorithm", "env_variables_foi", "boot_samples", pxl_dts_nm))
-# 
-#   test <- semi_join(full_pxl_dts, pxl_dts_boot, by = c("data_id", "latitude", "longitude"))
-# 
-#   ids <- test$square[order(test$square)]
-# 
-#   train_sets[ids, i] <- 1
-# 
-# }
-# 
-# test_sets <- 1 - train_sets
-# 
-# # ===================================================================
-# # ===================================================================
-# 
-# 
-# 
-# train_sets_n <- rowSums(train_sets)
-# test_sets_n <- rowSums(test_sets)
-# 
-# produc_train <- prediction_sets * train_sets
-# produc_test <- prediction_sets * test_sets
-# 
-# mean_prediction_train <- rowSums(produc_train) / train_sets_n
-# mean_prediction_test <- rowSums(produc_test) / test_sets_n
-# 
-# percentiles_train <- t(apply(produc_train, 1, quantile, probs = c(0.025, 0.975)))
-# percentiles_test <- t(apply(produc_test, 1, quantile, probs = c(0.025, 0.975)))
-# colnames(percentiles_train) <- c("low_perc_train", "up_perc_train")
-# colnames(percentiles_test) <- c("low_perc_test", "up_perc_test")
