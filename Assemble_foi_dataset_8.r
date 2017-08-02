235d0ab8b574d5fed81aaf565ef10d9624ae5b24
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-08-02 16:43:36 +0100

    New script

---
+++ Assemble_foi_dataset_8.r
@@ -0,0 +1,71 @@
+# Creates a map of the dataset point and dengue presence-absence mask 
+
+# load packages
+library(maptools) 
+library(dplyr)
+library(colorRamps)
+
+
+# ---------------------------------------- load data
+
+
+All_FOI_estimates <- read.table(
+  file.path("output", 
+            "foi", 
+            "All_FOI_estimates_linear.txt"), 
+  header = TRUE, 
+  sep = ",")
+
+pseudoAbsences <- read.csv(
+  file.path("output", 
+            "datasets", 
+            "pseudo_absence_points_NUM_CODES_sub.csv"), 
+  header = TRUE)
+
+world_shp_admin_1_dengue <- readShapePoly(
+  file.path("data", 
+            "shapefiles", 
+            "gadm28_levels.shp", 
+            "gadm28_adm1_dengue.shp"))
+
+
+# ---------------------------------------- pre processing
+
+
+data_points <- SpatialPoints(All_FOI_estimates[, c("longitude", "latitude")])
+pseudoAbsence_points <- SpatialPoints(pseudoAbsences[, c("longitude","latitude")])
+
+data_points_list <- list(
+  "sp.points",
+  data_points,
+  pch = 21, fill = "black", col = NA, cex = 1)
+
+pseudoAbsence_points_list <- list(
+  "sp.points", 
+  pseudoAbsence_points,
+  pch = 21, fill = "yellow", col = NA, cex = 1)
+
+
+# ---------------------------------------- plot
+
+
+png(file.path("figures", "dengue_points_and_absence_mask.png"), 
+     width = 18, 
+    height = 10, 
+    units = "in", 
+    pointsize = 12,
+    bg = "white", 
+    res = 200)
+
+spplot(world_shp_admin_1_dengue, "dengue", lwd = 0.5,
+       scales = list(x = list(draw = TRUE, 
+                              at = seq(-150, 150, 50)), 
+                     y = list(draw = TRUE)),
+       xlab = "Longitude",
+       ylab = "Latitude", 
+       col.regions = c("palegreen3","red2"),
+       colorkey = FALSE,
+       sp.layout = list(data_points_list,
+                        pseudoAbsence_points_list))
+
+dev.off()
