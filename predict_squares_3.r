6ceca3b6a4b0026051c0828b4ef8d3bc30453c2d
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-08-02 16:58:31 +0100

    Renamed scripts

--- predict_squares_3.r
+++ predict_squares_3.r
@@ -1,14 +1,17 @@
-# Binds all square tiles together 
+# Makes predictions for all squares in each tile 
 
 options(didehpc.cluster = "fi--didemrchnb")
 
 CLUSTER <- TRUE
 
 my_resources <- c(
-  file.path("R", "prepare_datasets", "load_and_bind.r"),
-  file.path("R", "utility_functions.r"))
+  file.path("R", "utility_functions.r"),
+  file.path("R", "random_forest", "wrapper_to_load_tile_dataset.R"),
+  file.path("R", "random_forest", "wrapper_to_make_predictions.R"),
+  file.path("R", "random_forest", "make_h2o_RF_predictions.R"),
+  file.path("R", "prepare_datasets", "remove_NA_rows.R"))
 
-my_pkgs <- "data.table"
+my_pkgs <- c("h2o", "data.table")
 
 context::context_log_start()
 ctx <- context::context_save(path = "context",
@@ -16,23 +19,30 @@ ctx <- context::context_save(path = "context",
                              packages = my_pkgs)
 
 
-# ---------------------------------------- define parameters 
+# ---------------------------------------- define parameters
 
 
-model_tp <- "boot_model_20km_cw" 
+model_tp <- "boot_model_20km_cw"
 
-in_pt <- file.path(
-  "output", 
-  "predictions",
-  model_tp,
-  "tile_sets_0_1667_deg")
+bs_inf <- c("cell", "lat.grid", "long.grid", "population")
 
-out_pt <- file.path(
-  "output", 
-  "predictions",
-  model_tp)
+in_path <- file.path("output", "env_variables", "all_sets_0_1667_deg")
 
-out_fl_nm <- "pred_0_1667_deg_long.rds"
+out_pth <- file.path("output", 
+                     "predictions", 
+                     model_tp,
+                     "tile_sets_0_1667_deg")
+
+# var_names <- "mean_pred"
+var_names <- c("mean_pred" , "low_perc", "up_perc")
+
+RF_obj_path <- file.path(
+  "output",
+  "EM_algorithm",
+  "optimized_model_objects",
+  "boot_samples")
+
+no_fits <- 200
 
 
 # ---------------------------------------- are you using the cluster?
@@ -40,33 +50,124 @@ out_fl_nm <- "pred_0_1667_deg_long.rds"
 
 if (CLUSTER) {
   
-  config <- didehpc::didehpc_config(template = "24Core")
+  config <- didehpc::didehpc_config(template = "12and16Core")
   obj <- didehpc::queue_didehpc(ctx, config = config)
   
-}else{
+} else {
   
   context::context_load(ctx)
+  #context::start_parallel_cluster(8, ctx)
   
 }
+
+
+# ---------------------------------------- load data
+
+
+# predicting variable rank
+predictor_rank <- read.csv(
+  file.path("output", 
+            "variable_selection", 
+            "metropolis_hastings", 
+            "exp_1", 
+            "variable_rank_final_fits_exp_1.csv"),
+  stringsAsFactors = FALSE)
+
+# tiles
+tile_summary <- read.csv(
+  file.path("data", 
+            "env_variables", 
+            "plus60minus60_tiles.csv"), 
+  header = TRUE, 
+  sep = ",", 
+  stringsAsFactors = FALSE)
+
+# NA pixel tiles 
+NA_pixel_tiles <- read.table(
+  file.path("output", 
+            "datasets", 
+            "NA_pixel_tiles.txt"), 
+  sep = ",", 
+  header = TRUE)
 
 
 # ---------------------------------------- pre processing
 
 
-fi <- list.files(in_pt, 
-                 pattern = "^tile",
-                 full.names = TRUE)
+# get the vector of best predictors (from MH variable selection routine)
+best_predictors <- predictor_rank$variable[1:9]
+
+# get tile ids
+tile_ids <- tile_summary$tile.id
+
+NA_pixel_tile_ids <- c(217, 254, 288, 321, 326, 402, 
+                         442, 458, 485, 486, 492, 493, 
+                         494, 519, 520, 523, 530, 539, 
+                         284, 289, 290, 291, 406, 432, 
+                         433, 529)
+
+tile_ids_2 <- tile_ids[!tile_ids %in% NA_pixel_tile_ids]  
 
 
-# ---------------------------------------- submit job
+# ---------------------------------------- submit one job 
+
+
+# t <- obj$enqueue(
+#   wrapper_to_load_tile_dataset(
+#     seq_along(tile_ids_2)[1],
+#     ids_vec = tile_ids_2,
+#     sel_preds = best_predictors,
+#     in_path = in_path,
+#     model_in_path = RF_obj_path,
+#     out_path = out_pth,
+#     var_names = var_names,
+#     base_info = bs_inf,
+#     parallel = FALSE,
+#     no_fits = no_fits,
+#     average = TRUE,
+#     model_type = model_tp))
+
+
+# ---------------------------------------- submit all jobs
 
 
 if (CLUSTER) {
   
-  all_tiles <- obj$enqueue(load_and_bind(fi, out_pt, out_fl_nm))
-
+  all_tiles <- queuer::qlapply(
+    seq_along(tile_ids_2), 
+    wrapper_to_load_tile_dataset, 
+    obj,
+    ids_vec = tile_ids_2,
+    sel_preds = best_predictors,
+    in_path = in_path,
+    model_in_path = RF_obj_path,
+    out_path = out_pth,
+    var_names = var_names,
+    base_info = bs_inf,
+    parallel = FALSE,
+    no_fits = no_fits,
+    average = TRUE,
+    model_type = model_tp)
+  
 } else {
   
-  all_tiles <- load_and_bind(fi, out_pt, out_fl_nm)
+  all_tiles <- lapply(
+    seq_along(tile_ids_2)[61],
+    wrapper_to_load_tile_dataset,
+    ids_vec = tile_ids_2,
+    sel_preds = best_predictors,
+    in_path = in_path,
+    model_in_path = RF_obj_path,
+    out_path = out_pth,
+    var_names = var_names,
+    base_info = bs_inf,
+    parallel = FALSE,
+    no_fits = no_fits,
+    average = TRUE,
+    model_type = model_tp)
+  
+}
 
+if (!CLUSTER) {
+  context:::stop_parallel_cluster()
 }
--- predict_squares_4.r
+++ predict_squares_4.r
@@ -1,62 +1,72 @@
-# Reshapes the combined df of square predictions from long to wide 
+# Binds all square tiles together 
 
-library(data.table)
-library(fields)
+options(didehpc.cluster = "fi--didemrchnb")
+
+CLUSTER <- TRUE
+
+my_resources <- c(
+  file.path("R", "prepare_datasets", "load_and_bind.r"),
+  file.path("R", "utility_functions.r"))
+
+my_pkgs <- "data.table"
+
+context::context_log_start()
+ctx <- context::context_save(path = "context",
+                             sources = my_resources,
+                             packages = my_pkgs)
 
 
 # ---------------------------------------- define parameters 
 
 
-model_tp <- "boot_model_20km_cw"
+model_tp <- "boot_model_20km_cw" 
 
-x <- file.path(
-  "output",
+in_pt <- file.path(
+  "output", 
   "predictions",
   model_tp,
-  "pred_0_1667_deg_long.rds")
+  "tile_sets_0_1667_deg")
 
 out_pt <- file.path(
   "output", 
   "predictions",
   model_tp)
 
-out_fl_nm <- "pred_0_1667_deg_wide.txt"
-
-gr_size <- 20
-
-res <- (1 / 120) * gr_size
-
-lats <- seq(-90, 90, by = res)
-lons <- seq(-180, 180, by = res)
+out_fl_nm <- "pred_0_1667_deg_long.rds"
 
 
-# ---------------------------------------- load data 
+# ---------------------------------------- are you using the cluster?
 
 
-all_preds <- readRDS(x)
-				   
-all_preds$lat.int=floor(all_preds$lat.grid*6+0.5)
-all_preds$long.int=floor(all_preds$long.grid*6+0.5)
-#all_preds$foi=ifelse(all_preds$mean_pred<0.01,0,all_preds$mean_pred)
-all_preds$foi=all_preds$mean_pred
+if (CLUSTER) {
+  
+  config <- didehpc::didehpc_config(template = "24Core")
+  obj <- didehpc::queue_didehpc(ctx, config = config)
+  
+}else{
+  
+  context::context_load(ctx)
+  
+}
 
-lats.int=lats*6
-lons.int=lons*6
 
-mat <- matrix(NA, nrow = length(lons), ncol = length(lats)) # subtract 1 from nrow and ncol if using image.plot
+# ---------------------------------------- pre processing
 
-i.lat <- findInterval(all_preds$lat.int, lats.int)
-i.lon <- findInterval(all_preds$long.int, lons.int)
 
-mat[cbind(i.lon, i.lat)] <- all_preds$foi
+fi <- list.files(in_pt, 
+                 pattern = "^tile",
+                 full.names = TRUE)
 
-write.table(mat, 
-            file.path(out_pt, out_fl_nm),
-            row.names = FALSE,
-            sep = ",")
 
-# png(file.path(out_pt, "map.png"), type = "cairo", antialias = "none", width = 7, height = 3, units = "in", res = 300)
-# 
-# image.plot(lons, lats, mat, asp = 1)
-# 
-# dev.off()
+# ---------------------------------------- submit job
+
+
+if (CLUSTER) {
+  
+  all_tiles <- obj$enqueue(load_and_bind(fi, out_pt, out_fl_nm))
+
+} else {
+  
+  all_tiles <- load_and_bind(fi, out_pt, out_fl_nm)
+
+}
