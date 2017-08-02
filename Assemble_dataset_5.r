37612d9c1b6d59c6c9a0a0a0fadb4b4ca6cb98bb
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-08-02 15:45:33 +0100

    Style edits

--- Assemble_dataset_5.r
+++ Assemble_dataset_5.r
@@ -1,17 +1,20 @@
+# load packages
 library(maptools)
 
 pap <- read.csv(
   file.path("output", 
             "datasets", 
             "pseudo_absence_points_NUM_CODES.csv"), 
-  header = TRUE, sep = ",", stringsAsFactors = FALSE)
+  header = TRUE, 
+  sep = ",", 
+  stringsAsFactors = FALSE)
 
 dup <- duplicated(pap[, c("ID_0", "ID_1")])
 
 pap_2 <- pap[!dup, ]
 
 write.csv(pap_2,
-            file.path("output", 
-                      "datasets", 
-                      "pseudo_absence_points_NUM_CODES_sub.csv"), 
-            row.names = FALSE)
+          file.path("output", 
+                    "datasets", 
+                    "pseudo_absence_points_NUM_CODES_sub.csv"), 
+          row.names = FALSE)
