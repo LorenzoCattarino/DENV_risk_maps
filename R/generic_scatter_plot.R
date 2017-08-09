6823c1d20e4c19a7b7297fc1801c7d26edd25661
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-08-09 13:32:10 +0100

    Major changes to plotting function - now it works fine

--- R/generic_scatter_plot.R
+++ R/generic_scatter_plot.R
@@ -1,73 +1,74 @@
 generic_scatter_plot <- function(
-  df_to_plot, x, y, 
-  my_path, file_name, 
-  x_axis_tag, y_axis_tag, ttl, 
-  alpha, reverse_x_axis, mirror){
+  df, x, y, 
+  file_name, file_path,  
+  alpha = NULL, mirror = NULL, reverse_x_axis = NULL){
   
-  if(!is.null(alpha)){
-    
-    df_to_plot[, x] <- df_to_plot[, x] * alpha    
-  
-  }
-  
-  x_values <- pretty(df_to_plot[, x], n = 5) 
-  
-  if(mirror){
-    y_values <- x_values
-  }else{
-    y_values <- pretty(df_to_plot[, y], n = 5)
-  }
-  
-  dir.create(my_path, FALSE, TRUE)
-  
-  jpeg(filename = file.path(my_path, file_name), 
-       width = 6, 
-       height = 4, 
-       units = "in", 
-       res = 200)
-
-  p1 <- ggplot(df_to_plot, aes(x = get(x), y = get(y))) +
-    geom_point(aes(x = get(x), y = get(y)), size = 1) +
-    geom_abline(intercept = 0, slope = 1, linetype = "dashed")
-  
-  # if(fit_line){
-  #   p2 <- p1 + geom_smooth(method = "lm", formula = y ~ x)
-  #   eq <- ddply(df_to_plot, as.formula(paste0("~", facet_var)), lm_eqn, y = y, x = x)
-  #   p3 <- p2 + geom_text(data = eq, aes(x = 0.02, y = 0.06, label = V1), parse = TRUE, inherit.aes = FALSE) + 
-  #     facet_wrap(as.formula(paste0("~", facet_var)))
-  # }else{
-  #   p3 <- p1
-  # }
+  # if(!is.null(alpha)){
+  #   
+  #   df[, x] <- df[, x] * alpha    
   # 
-  # if(facet){
-  #   p4 <- p3 + facet_wrap(as.formula(paste0("~", facet_var)))
-  # }else{
-  #   p4 <- p3
   # }
+  # x_values <- pretty(df[, x], n = 5) 
+  # if(mirror){
+  #   y_values <- x_values
+  # }else{
+  #   y_values <- pretty(df[, y], n = 5)
+  # }
+  # if(reverse_x_axis){
+  #   p2 <- p1 + scale_x_reverse(x_axis_tag,
+  #                              limits = c(max(x_values), min(x_values)),
+  #                              breaks = rev(x_values),
+  #                              labels = rev(x_values))
+  # }else{
+  #   p2 <- p1 + scale_x_continuous(x_axis_tag,
+  #                                 limits = c(min(x_values), max(x_values)),
+  #                                 breaks = x_values,
+  #                                 labels = x_values)
+  # }
+
+  #browser()
   
-  if(reverse_x_axis){
-    p2 <- p1 + scale_x_reverse(x_axis_tag,
-                               limits = c(max(x_values), min(x_values)),
-                               breaks = rev(x_values),
-                               labels = rev(x_values))
-  }else{
-    p2 <- p1 + scale_x_continuous(x_axis_tag,
-                                  limits = c(min(x_values), max(x_values)),
-                                  breaks = x_values,
-                                  labels = x_values)
-  }
+  x_values <- pretty(df[, x], n = 5)
+  y_values <- pretty(df[, y], n = 5)
+  min_x_value <- min(x_values)
+  max_x_value <- max(x_values)
+  min_y_value <- min(y_values)
+  max_y_value <- max(y_values)
+  
+  eq <- lm_eqn(df = df, y = y, x = x)
+  
+  eq_df <- data.frame(eq) 
     
-  p3 <- p2 + scale_y_continuous(y_axis_tag, 
-                                limits = c(min(y_values), max(y_values)), 
-                                breaks = y_values) +
-    ggtitle(ttl) +
-    theme(axis.title.x = element_text(hjust = 0.5, vjust = 1, size = 15, margin = margin(t = 10)),
-          axis.title.y = element_text(hjust = 0.5, vjust = 0, size = 15, margin = margin(r = 10)),
-          axis.text.x = element_text(angle = 0, hjust = 0.5, vjust = 1, size = 15),
-          axis.text.y = element_text(size = 15),
-          plot.title = element_text(lineheight = 1, face = "bold", hjust = 0.5))
+  p <- ggplot(df, aes_string(x = x, y = y)) +
+    geom_point(aes_string(x = x, y = y), size = 1) + 
+    scale_x_continuous("Observations",
+                       #limits = c(min_x_value, max_x_value),
+                       breaks = x_values,
+                       labels = x_values) + 
+    scale_y_continuous("Predictions", 
+                       #limits = c(min_y_value, max_y_value), 
+                       breaks = y_values,
+                       labels = y_values) +
+    geom_smooth(method = "lm", formula = y ~ x - 1, se = FALSE) + 
+    coord_cartesian(xlim = c(min_x_value, max_x_value),
+                    ylim = c(min_y_value, max_y_value)) + 
+    geom_text(data = eq_df, aes(x = 0.02, y = max_y_value, label = eq), parse = TRUE) +
+    theme(axis.title.x = element_text(hjust = 0.5, vjust = 1, size = 15, margin = margin(t = 20)),
+          axis.title.y = element_text(hjust = 0.5, vjust = 0, size = 15, margin = margin(r = 20)),
+          axis.text.x = element_text(size = 11),
+          axis.text.y = element_text(size = 11),
+          plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm"))
   
-  print(p3)
+  dir.create(file_path, FALSE, TRUE)
+  
+  png(filename = file.path(file_path, file_name), 
+      width = 6, 
+      height = 4, 
+      units = "in", 
+      res = 200)
+  
+  print(p)
   
   dev.off()
+  
 }
