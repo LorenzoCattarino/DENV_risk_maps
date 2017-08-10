7633b50a4f27e45ad8d46eac6a444f66c7c7e4f2
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-08-10 15:09:32 +0100

    Added output paths and names inside the function

--- R/random_forest/load_predict_and_save.R
+++ R/random_forest/load_predict_and_save.R
@@ -1,10 +1,12 @@
 load_predict_and_save <- function(
-  i, pxl_dts_path, RF_obj_path, 
-  my_preds, out_file_path, out_file_name){
+  i, RF_obj_path, 
+  my_preds, no_fits){
+  
+  pxl_dts_path <- file.path("output", "EM_algorithm", "env_variables", "boot_samples")
   
   pxl_dts_nm <- paste0("aggreg_pixel_level_env_vars_20km_sample_", i, ".rds")
   
-  RF_obj_nm <- paste0("RF_obj_sample_", i, ".rds")
+  RF_obj_nm <- paste0("RF_model_object_", i, ".rds")
   
   pxl_dts_boot <- readRDS(file.path(pxl_dts_path, pxl_dts_nm))
   
@@ -19,6 +21,14 @@ load_predict_and_save <- function(
     sel_preds = my_preds)
   
   pxl_dts_boot$p_i <- p_i
+  
+  
+  # ---------------------------------------- 
+  
+  
+  out_file_name <- paste0("covariates_and_foi_20km_", seq_len(no_fits), ".rds")
+  
+  out_file_path <- file.path("output", "EM_algorithm", "env_variables_foi", "boot_samples")
   
   a <- out_file_name[i]
   
