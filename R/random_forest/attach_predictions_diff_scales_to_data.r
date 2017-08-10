4dc942da1dc70b3fed65430e6687df9182d30284
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-08-10 09:30:35 +0100

    Added `group fields` argument

--- R/random_forest/attach_predictions_diff_scales_to_data.r
+++ R/random_forest/attach_predictions_diff_scales_to_data.r
@@ -1,8 +1,8 @@
 attach_pred_different_scale_to_data <- function(
   i, model_path, foi_data,
   adm_dts, predictors, all_sqr_preds,
-  sqr_dts, tile_ids, in_path, 
-  bt_samples, out_path){
+  sqr_dts, tile_ids,
+  bt_samples, out_path, grp_fields){
   
   
   #browser()
@@ -29,21 +29,18 @@ attach_pred_different_scale_to_data <- function(
   
   # -------------------------------------- process the original bootstrap sample  
   
-  
-  # # remove pseudo absences 
-  # foi_data <- subset(foi_data, type != "pseudoAbsence")
-  
+
   # rename FOI field
   names(foi_data)[names(foi_data) == "FOI"] <- "o_j"
-  names(foi_data)[names(foi_data) == "ID_0"] <- "ADM_0"
-  names(foi_data)[names(foi_data) == "ID_1"] <- "ADM_1" 
+  names(foi_data)[names(foi_data) == "ID_0"] <- grp_fields[1]
+  names(foi_data)[names(foi_data) == "ID_1"] <- grp_fields[2]
   
   
   # -------------------------------------- process admin predictions
   
   
-  names(adm_dts)[names(adm_dts) == "ID_0"] <- "ADM_0"
-  names(adm_dts)[names(adm_dts) == "ID_1"] <- "ADM_1" 
+  names(adm_dts)[names(adm_dts) == "ID_0"] <- grp_fields[1]
+  names(adm_dts)[names(adm_dts) == "ID_1"] <- grp_fields[2] 
   
   adm_dts_2 <- remove_NA_rows(adm_dts, predictors)
   
@@ -51,64 +48,64 @@ attach_pred_different_scale_to_data <- function(
   
   fltr_adm <- inner_join(
     adm_dts_2, 
-    foi_data[, c("data_id", "ADM_0", "ADM_1")])
+    foi_data[, grp_fields])
 
     
-  # -------------------------------------- process square predictions 
-  
-  
+  # -------------------------------------- process square predictions
+
+
   sqr_preds <- all_sqr_preds[, i]
-  
-  sqr_dts <- cbind(sqr_dts[, c("data_id", "ADM_0", "ADM_1", "population")], 
+
+  sqr_dts <- cbind(sqr_dts[, c(grp_fields, "population")],
                    pred = sqr_preds)
-  
+
   average_sqr <- average_up(
     pxl_df = sqr_dts,
-    grp_flds = c("data_id", "ADM_0", "ADM_1"),
+    grp_flds = grp_fields,
     var_names = "pred")
-  
-  names(average_sqr)[names(average_sqr) == "pred"] <- "mean_square_pred" 
 
-    
-  # -------------------------------------- process 1 km predictions 
-  
-  
+  names(average_sqr)[names(average_sqr) == "pred"] <- "mean_square_pred"
+
+
+  # -------------------------------------- process 1 km predictions
+
+
   #[c(140, 141, 170, 171)]
-  
+
   tile_prds <- loop(
     seq_along(tile_ids),
     load_predict_filter,
-    ids_vec = tile_ids, 
-    in_path = in_path,
-    predictors = predictors, 
-    RF_obj = RF_obj, 
+    ids_vec = tile_ids,
+    predictors = predictors,
+    RF_obj = RF_obj,
     foi_dts = foi_data,
+    grp_flds = grp_fields,
     parallel = FALSE)
-  
+
   tile_prds_rb <- do.call("rbind", tile_prds)
-  
+
   average_pxl <- average_up(
-    pxl_df = tile_prds_rb, 
-    grp_flds = c("data_id", "ADM_0", "ADM_1"), 
+    pxl_df = tile_prds_rb,
+    grp_flds = grp_fields,
     var_names = "pred")
-  
-  names(average_pxl)[names(average_pxl) == "pred"] <- "mean_pxl_pred" 
-  
-  
-  # -------------------------------------- join admin, square and pixel level predictions 
+
+  names(average_pxl)[names(average_pxl) == "pred"] <- "mean_pxl_pred"
+
+
+  # -------------------------------------- join admin, square and pixel level predictions
   
   
   m_1 <- left_join(
-    foi_data[, c("data_id", "ADM_0", "ADM_1", "o_j")],
-    fltr_adm[, c("data_id", "ADM_0", "ADM_1", "adm_pred")])
+    foi_data[, c(grp_fields, "o_j")],
+    fltr_adm[, c(grp_fields, "adm_pred")])
 
   m_2 <- left_join(
     m_1,
-    average_sqr[, c("data_id", "ADM_0", "ADM_1", "mean_square_pred")])
+    average_sqr[, c(grp_fields, "mean_square_pred")])
 
   m_final <- left_join(
     m_2,
-    average_pxl[, c("data_id", "ADM_0", "ADM_1", "mean_pxl_pred")])
+    average_pxl[, c(grp_fields, "mean_pxl_pred")])
   
   
   # --------------------------------------
