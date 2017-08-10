feed254ab296a6a7e31a8538d45f1953b05b16b8
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-08-10 14:40:49 +0100

    Deleted unused line

--- R/random_forest/make_h2o_RF_predictions.R
+++ R/random_forest/make_h2o_RF_predictions.R
@@ -2,14 +2,12 @@ make_h2o_predictions <- function(mod_obj, dataset, sel_preds){
   
   #browser()
   
-  x_data <- subset(dataset,, sel_preds, drop = FALSE)
+  x_data <- subset(dataset, , sel_preds, drop = FALSE)
   
   x_data <- as.h2o(x_data)
   
   preds <- predict(mod_obj, x_data)
   
   as.vector(preds)
-  
-  #preds [preds < 0] <- 0
   
 }
