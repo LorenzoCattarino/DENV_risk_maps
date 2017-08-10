4fee4991bb3904e1373008a35774d27261404abe
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-08-10 09:31:22 +0100

    Style edits

--- EM_algorithm_with_bootstrap_8.r
+++ EM_algorithm_with_bootstrap_8.r
@@ -7,19 +7,30 @@ options(didehpc.cluster = "fi--didemrchnb")
 CLUSTER <- TRUE
 
 my_resources <- c(
+  file.path("R", "utility_functions.r"),
   file.path("R", "random_forest", "wrapper_to_Exp_Max_algorithm.r"),
   file.path("R", "random_forest", "fit_h2o_random_forest_model.r"),
   file.path("R", "random_forest", "make_h2o_RF_predictions.r"),
   file.path("R", "random_forest", "Exp_Max_algorithm.r"),
   file.path("R", "random_forest", "quick_raster_map.r"),
-  file.path("R", "utility_functions.r"))
+  file.path("R", "random_forest", "get_lm_equation.r"),
+  file.path("R", "generic_scatter_plot.r"))
 
-my_pkgs <- c("h2o", "dplyr", "fields")
+my_pkgs <- c("h2o", "dplyr", "fields", "ggplot2")
 
 context::context_log_start()
 ctx <- context::context_save(path = "context",
                              sources = my_resources,
                              packages = my_pkgs)
+
+
+# ---------------------------------------- define parameters
+
+
+model_type <- "boot_model_20km_cw"
+
+out_fl_nm <- "square_predictions_all_data.rds"
+out_pt <- file.path("output", "predictions_world", model_type)
 
 
 # ---------------------------------------- rebuild the queue object?
@@ -37,19 +48,10 @@ if (CLUSTER) {
 }
 
 
-# ---------------------------------------- define parameters
-
-
-model_type <- "boot_model_20km_vw"
-
-out_fl_nm <- "square_predictions_all_data.rds"
-out_pt <- file.path("output", "predictions", model_type)
-
-
 # ---------------------------------------- get results
 
 
-my_task_id <- "browny_vireo"
+my_task_id <- "garish_senegalpython"
 
 EM_alg_run_t <- obj$task_bundle_get(my_task_id)
 
