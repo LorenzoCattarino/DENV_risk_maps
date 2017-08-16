19b4cd19ec99efcdc2e956acdc71aefb60f76eb9
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-08-16 16:02:13 +0100

    Some changes

--- R/prepare_datasets/find_NA_pixel_tiles.R
+++ R/prepare_datasets/find_NA_pixel_tiles.R
@@ -1,15 +1,19 @@
 find_tiles_with_all_NA_pred_values <- function(
-  i, ids_vec, sel_preds){
+  i, ids_vec, in_path){
+  
+  #browser()
   
   one_id <- ids_vec[i]
   cat("tile id = ", one_id, "\n")
   
   file_name <- paste0("tile_", one_id, ".txt")
   
-  tile <- fread(file.path("output",
-                          "env_variables",
-                          "all_sets_0_1667_deg",
-                          file_name))  
+  tile <- fread(file.path(in_path, file_name),
+                header = TRUE, 
+                sep = ",",              
+                na.strings = c("NA", "-1.#IND", "Peipsi", "Moskva", "IJsselmeer", "Zeeuwse meren"),
+                fill = TRUE, 
+                data.table = FALSE)  
   
   tile_dim <- dim(tile)
   
