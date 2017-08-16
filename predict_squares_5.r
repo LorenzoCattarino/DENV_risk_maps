8730d9e00ba47c176eab84d9805a103a8ff381d4
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-08-16 13:46:57 +0100

    Changed script names

--- predict_squares_5.r
+++ predict_squares_5.r
@@ -1,12 +1,14 @@
-# Binds all square tiles together 
+# Takes the averaged value across models, and confidence intervals, 
+# of different measured variables (foi, R0, burden...), for each tile. 
 
 options(didehpc.cluster = "fi--didemrchnb")
 
 CLUSTER <- TRUE
 
 my_resources <- c(
-  file.path("R", "prepare_datasets", "load_and_bind.r"),
-  file.path("R", "utility_functions.r"))
+  file.path("R", "utility_functions.r"),
+  file.path("R", "prepare_datasets", "calculate_mean_across_fits.r"),
+  file.path("R", "prepare_datasets", "wrapper_to_mean_across_fits.r"))
 
 my_pkgs <- "data.table"
 
@@ -16,27 +18,37 @@ ctx <- context::context_save(path = "context",
                              packages = my_pkgs)
 
 
-# ---------------------------------------- define parameters 
+# ---------------------------------------- define parameters
 
 
-model_tp <- "boot_model_20km_cw" 
+model_tp <- "boot_model_20km_cw"
 
-out_fl_nm <- "pred_0_1667_deg_long.rds"
+no_fits <- 50
 
+NA_tile_fl_name <- "NA_pixel_tiles_20km.txt"
 
-# ---------------------------------------- define variables 
+bs_inf <- c("cell", "lat.grid", "long.grid", "population")
 
+orig_in_pth  <- file.path("output", "env_variables", "all_sets_0_1667_deg")
 
-in_pt <- file.path(
+in_pth <- file.path(
   "output", 
-  "predictions_world",
+  "predictions_world", 
   model_tp,
   "tile_sets_0_1667_deg")
 
-out_pt <- file.path(
+var_names <- "foi"
+
+
+# ---------------------------------------- define variables
+
+
+out_pth <- file.path(
   "output", 
-  "predictions_world",
-  model_tp)
+  "predictions_world", 
+  model_tp,
+  "tile_sets_0_1667_deg",
+  "averages")
 
 
 # ---------------------------------------- are you using the cluster?
@@ -44,33 +56,96 @@ out_pt <- file.path(
 
 if (CLUSTER) {
   
-  config <- didehpc::didehpc_config(template = "24Core")
+  config <- didehpc::didehpc_config(template = "12and16Core")
   obj <- didehpc::queue_didehpc(ctx, config = config)
   
-}else{
+} else {
   
   context::context_load(ctx)
+  context::parallel_cluster_start(8, ctx)
   
 }
+
+
+# ---------------------------------------- load data
+
+
+# tiles
+tile_summary <- read.csv(
+  file.path("data", 
+            "env_variables", 
+            "plus60minus60_tiles.csv"), 
+  header = TRUE, 
+  sep = ",", 
+  stringsAsFactors = FALSE)
+
+# NA pixel tiles 
+NA_pixel_tiles <- read.table(
+  file.path("output", 
+            "datasets", 
+            NA_tile_fl_name), 
+  sep = ",", 
+  header = TRUE)
 
 
 # ---------------------------------------- pre processing
 
 
-fi <- list.files(in_pt, 
-                 pattern = "^tile",
-                 full.names = TRUE)
+# get tile ids
+tile_ids <- tile_summary$tile.id
+
+NA_pixel_tile_ids <- NA_pixel_tiles$tile_id
+
+tile_ids_2 <- tile_ids[!tile_ids %in% NA_pixel_tile_ids]  
 
 
-# ---------------------------------------- submit job
+# ---------------------------------------- submit one job 
 
 
-if (CLUSTER) {
-  
-  all_tiles <- obj$enqueue(load_and_bind(fi, out_pt, out_fl_nm))
+t <- obj$enqueue(
+  wrapper_to_mean_across_fits(
+    seq_along(tile_ids_2)[1],
+    ids_vec = tile_ids_2,
+    in_path = in_pth,
+    orig_in_path = orig_in_pth,
+    var_names = var_names,
+    parallel = TRUE,
+    base_info = bs_inf,
+    out_path = out_pth))
 
-} else {
-  
-  all_tiles <- load_and_bind(fi, out_pt, out_fl_nm)
 
-}
+# ---------------------------------------- submit all jobs
+
+
+# if (CLUSTER) {
+# 
+#   pred_tiles <- queuer::qlapply(
+#     seq_along(tile_ids_2),
+#     wrapper_to_mean_across_fits,
+#     obj,
+#     ids_vec = tile_ids_2,
+#     in_path = in_pth,
+#     orig_in_path = orig_in_pth,
+#     var_names = var_names,
+#     parallel = TRUE,
+#     base_info = bs_inf,
+#     out_path = out_pth)
+# 
+# } else {
+# 
+#   pred_tiles <- lapply(
+#     seq_along(tile_ids_2)[1],
+#     wrapper_to_mean_across_fits,
+#     ids_vec = tile_ids_2,
+#     in_path = in_pth,
+#     orig_in_path = orig_in_pth,
+#     var_names = var_names,
+#     parallel = TRUE,
+#     base_info = bs_inf,
+#     out_path = out_pth)
+# 
+# }
+# 
+# if (!CLUSTER) {
+#   context::parallel_cluster_stop()
+# }
---
+++ predict_squares_7.r
@@ -0,0 +1,151 @@
+# Makes a pretty map of the square predictions  
+
+options(didehpc.cluster = "fi--didemrchnb")
+
+CLUSTER <- TRUE
+
+my_resources <- c(
+  file.path("R", "random_forest", "plot_map_pixel_level_ggplot.r"))
+  
+my_pkgs <- c("data.table", "ggplot2", "colorRamps", "raster", "rgdal", "scales")
+
+context::context_log_start()
+ctx <- context::context_save(path = "context",
+                             sources = my_resources,
+                             packages = my_pkgs)
+
+
+# ---------------------------------------- define parameters 
+
+
+model_tp <- "boot_model_20km_cw"
+
+gr_size <- 20
+
+res <- (1 / 120) * gr_size
+
+cut_off <- 0.009
+
+lats <- seq(-90, 90, by = res)
+lons <- seq(-180, 180, by = res)
+
+out_fl_nm <- "pred_0_1667_deg.png"
+
+
+# ---------------------------------------- define variables
+
+
+x <- file.path(
+  "output",
+  "predictions_world",
+  model_tp,
+  "pred_0_1667_deg_long.rds")
+
+out_pt <- file.path(
+  "figures", 
+  "predictions_world",
+  model_tp)
+
+
+# ---------------------------------------- are you using the cluster?
+
+
+if (CLUSTER) {
+  
+  config <- didehpc::didehpc_config(template = "24Core")
+  obj <- didehpc::queue_didehpc(ctx, config = config)
+  
+}else{
+  
+  context::context_load(ctx)
+  
+}
+
+
+# ---------------------------------------- load data 
+
+
+all_preds <- readRDS(x)
+
+country_shp <- readOGR(dsn = file.path("output", "shapefiles"), layer = "gadm28_adm0_eras")
+
+
+# ---------------------------------------- remove the Caspian Sea
+
+
+country_shp <- country_shp[!country_shp@data$NAME_ENGLI == "Caspian Sea", ]
+
+
+# ---------------------------------------- create matrix of foi values 
+
+
+all_preds$lat.int=floor(all_preds$lat.grid*6+0.5)
+all_preds$long.int=floor(all_preds$long.grid*6+0.5)
+all_preds$foi=ifelse(all_preds$mean_pred < cut_off, 0,all_preds$mean_pred)
+
+lats.int=lats*6
+lons.int=lons*6
+
+mat <- matrix(0, nrow = length(lons), ncol = length(lats))
+
+i.lat <- findInterval(all_preds$lat.int, lats.int)
+i.lon <- findInterval(all_preds$long.int, lons.int)
+
+mat[cbind(i.lon, i.lat)] <- all_preds$foi
+
+
+# ---------------------------------------- convert matrix to raster object
+
+
+mat_ls <- list(x = lons,
+               y = lats,
+               z = mat)
+
+r_mat <- raster(mat_ls)
+
+
+#----------------------------------------- get raster extent 
+
+
+my_ext <- matrix(r_mat@extent[], nrow = 2, byrow = TRUE) 
+
+
+# ---------------------------------------- apply same extent to the shape file 
+
+
+country_shp@bbox <- my_ext
+
+
+# ---------------------------------------- mask the raster to the shape file
+
+
+r_mat_msk <- mask(r_mat, country_shp)
+
+
+# ---------------------------------------- convert to ggplot-friendly objects 
+
+
+r_spdf <- as(r_mat_msk, "SpatialPixelsDataFrame")
+
+r_df <- as.data.frame(r_spdf)
+
+shp_fort <- fortify(country_shp)
+
+
+# ---------------------------------------- submit job
+
+
+if (CLUSTER) {
+  
+  foi_map <- obj$enqueue(map_data_pixel_ggplot(df = r_df, 
+                                               shp = shp_fort, 
+                                               out_path = out_pt, 
+                                               out_file_name = out_fl_nm))
+}  else {
+  
+  foi_map <- map_data_pixel_ggplot(df = r_df, 
+                                   shp = shp_fort, 
+                                   out_path = out_pt, 
+                                   out_file_name = out_fl_nm)
+  
+}
