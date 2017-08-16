b48dd44849c09518a0217a9367fd565c7a9130e8
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-08-16 09:49:27 +0100

    Works fine

--- R/random_forest/wrapper_to_load_tile_dataset.R
+++ R/random_forest/wrapper_to_load_tile_dataset.R
@@ -1,15 +1,16 @@
 wrapper_to_load_tile_dataset <- function(
   i, ids_vec, in_path, 
-  predictors, model_in_path, parallel,
-  base_info, var_names, no_fits,
-  average, model_type, out_path){
+  no_fits, model_in_path, predictors, 
+  parallel, out_path, out_name){
   
   #browser()
   
   one_id <- ids_vec[i]
   cat("tile id = ", one_id, "\n")
 
-  file_name <- paste0("tile_", one_id, ".txt")
+  tile_nm <- paste0("tile_", one_id)
+         
+  file_name <- paste0(tile_nm, ".txt")
 
   tile <- fread(file.path(in_path, file_name),
                 header = TRUE,
@@ -18,21 +19,17 @@ wrapper_to_load_tile_dataset <- function(
                 fill = TRUE,
                 data.table = FALSE)
   
-  out <- wrapper_to_make_preds(tile, 
-                               predictors, 
-                               model_in_path,
-                               parallel, 
-                               base_info, 
-                               var_names, 
-                               no_fits, 
-                               average, 
-                               model_type)
+  foi <- wrapper_to_make_preds(
+    no_fits = no_fits,
+    model_in_path = model_in_path, 
+    dataset = tile, 
+    predictors = predictors, 
+    parallel = parallel)
+    
+  out_pt_one_tile <- out_path[i]  
+
+  out_nm <- paste0(out_name, ".rds")
   
-  dir.create(out_path, FALSE, TRUE)
-  
-  write.table(out, 
-              file.path(out_path, file_name),
-              row.names = FALSE,
-              sep = ",")
-  
+  write_out_rds(foi, out_pt_one_tile, out_nm)
+              
 }  
