906e0e3b46689a074f342e44721fa57cd965b79b
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-08-16 13:41:58 +0100

    Removed subsetting because added in script run before this one

--- R/prepare_datasets/find_NA_pixel_tiles.R
+++ R/prepare_datasets/find_NA_pixel_tiles.R
@@ -11,18 +11,16 @@ find_tiles_with_all_NA_pred_values <- function(
                           "all_sets_0_1667_deg",
                           file_name))  
   
-  # Remove records with at least one NA predictor value
-  tile_2 <- remove_NA_rows(tile, sel_preds)
+  tile_dim <- dim(tile)
   
-  tile_dim <- dim(tile_2)
-  
-  if(tile_dim[1] == 0){
+  if(tile_dim[1] == 0) {
     
     out <- TRUE
   
-  }else{
+  } else {
     
     out<- FALSE
+  
   }
   
   out
