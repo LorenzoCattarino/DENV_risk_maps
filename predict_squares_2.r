a88332daa7d6c82a41dc8a97f7b0b5f8fc989747
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-08-17 11:49:54 +0100

    Added local package source

--- predict_squares_2.r
+++ predict_squares_2.r
@@ -2,20 +2,27 @@
 
 options(didehpc.cluster = "fi--didemrchnb")
 
-CLUSTER <- TRUE
+CLUSTER <- FALSE
 
 my_resources <- c(
   file.path("R", "prepare_datasets", "remove_NA_rows.r"),
   file.path("R", "prepare_datasets", "find_NA_pixel_tiles.R"))
 
-my_pkgs <- c("data.table")
+my_pkgs <- "data.table"
 
+lcf <- provisionr::package_sources(local = file.path("R_binaries", "data.table_1.10.5.zip"))
 context::context_log_start()
 ctx <- context::context_save(path = "context",
                              sources = my_resources,
-                             packages = my_pkgs)
+                             package_sources = lcf)
 
 
+# ---------------------------------------- define parameters 
+
+
+in_pth <- file.path("output", "env_variables", "all_sets_0_1667_deg")
+
+          
 # ------------------------------------- Are you using the cluster?
 
 
@@ -33,30 +40,17 @@ if(CLUSTER) {
 # ---------------------------------------- Load data
 
 
-# predicting variable rank
-predictor_rank <- read.csv(
-  file.path("output", 
-            "variable_selection", 
-            "metropolis_hastings", 
-            "exp_1", 
-            "variable_rank_final_fits_exp_1.csv"),
-  stringsAsFactors = FALSE)
-
 # tiles info 
 tile_summary <- read.csv(
   file.path("data", 
             "env_variables", 
             "plus60minus60_tiles.csv"), 
   header = TRUE, 
-  sep = ",", 
   stringsAsFactors = FALSE)
 
 
 # ---------------------------------------- pre processing
 
-
-# get the vector of best predictors (from MH variable selection routine)
-best_predictors <- predictor_rank$variable[1:9]
 
 # get tile ids
 tile_ids <- tile_summary$tile.id
@@ -65,35 +59,35 @@ tile_ids <- tile_summary$tile.id
 # ----------------------------------------  submit one job
  
 
-t <- obj$enqueue(find_tiles_with_all_NA_pred_values(
-  seq_along(tile_ids)[1],
-  ids_vec = tile_ids,
-  sel_preds = best_predictors))
+# t <- obj$enqueue(find_tiles_with_all_NA_pred_values(
+#   seq_along(tile_ids)[185],
+#   ids_vec = tile_ids,
+#   in_path = in_pth))
 
 
 # ---------------------------------------- submit all jobs
 
 
-# if (CLUSTER) {
-#   
-#   find_NA_tls <- queuer::qlapply(
-#     seq_along(tile_ids), 
-#     find_tiles_with_all_NA_pred_values, 
-#     obj,
-#     ids_vec = tile_ids,
-#     sel_preds = best_predictors)
-#   
-# } else {
-#   
-#   find_NA_tls <- lapply(
-#     seq_along(tile_ids)[245],
-#     find_tiles_with_all_NA_pred_values,
-#     ids_vec = tile_ids,
-#     sel_preds = best_predictors)
-#   
-# }
+if (CLUSTER) {
 
-NA_jobs <- which(all_tiles$results() == TRUE)
+  find_NA_tls <- queuer::qlapply(
+    seq_along(tile_ids),
+    find_tiles_with_all_NA_pred_values,
+    obj,
+    ids_vec = tile_ids,
+    in_path = in_pth)
+
+} else {
+
+  find_NA_tls <- lapply(
+    seq_along(tile_ids)[185],
+    find_tiles_with_all_NA_pred_values,
+    ids_vec = tile_ids,
+    in_path = in_pth)
+
+}
+
+NA_jobs <- which(find_NA_tls$results() == TRUE)
 
 NA_pixel_tiles <- tile_summary$tile.id[NA_jobs]
 
