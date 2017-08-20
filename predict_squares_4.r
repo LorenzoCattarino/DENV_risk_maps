61468fda2a8ff5dd68eb5a9b0254e039acaeb15d
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-08-20 10:57:08 +0100

    Works fine

--- predict_squares_4.r
+++ predict_squares_4.r
@@ -1,27 +1,31 @@
-# Calculates R0 values (4 different assumptions) from each foi value, 
-# for each tile and model fit
+# Calculates, for each tile, model fit and R0-Wolbachia effect assumption combinations
+# R0 values and burden, for different foi value (squares).
 
 options(didehpc.cluster = "fi--didemrchnb")
 
-CLUSTER <- FALSE
+CLUSTER <- TRUE
 
 my_resources <- c(
   file.path("R", "utility_functions.r"),
-  file.path("R", "burden_and_interventions", "get_age_band_bounds.R"),
-  file.path("R", "burden_and_interventions", "wrapper_to_get_R0.r"),  
-  file.path("R", "burden_and_interventions", "calculate_probabilities_and_R0.r"),
+  file.path("R", "burden_and_interventions", "wrapper_to_burden_and_wolbachia_one_tile.r"),
+  file.path("R", "burden_and_interventions", "wrapper_to_multi_factor_burden.r"),
+  file.path("R", "burden_and_interventions", "wrapper_to_get_multi_foi_R0.r"),
+  file.path("R", "burden_and_interventions", "wrapper_to_get_R0.r"),             
+  file.path("R", "burden_and_interventions", "get_age_band_bounds.r"),
   file.path("R", "burden_and_interventions", "calculate_infection_probability_and_number.r"),
   file.path("R", "burden_and_interventions", "calculate_average_infect_probab.r"),
-  file.path("R", "burden_and_interventions", "calculate_R0.r"),
-  file.path("R", "burden_and_interventions", "load_tile_get_burden_and_wolbachia.r"))
+  file.path("R", "burden_and_interventions", "calculate_total_incidence.r"),
+  file.path("R", "burden_and_interventions", "calculate_incidence_of_infections.r"),
+  file.path("R", "burden_and_interventions", "calculate_R0.r"))
 
-my_pkgs <- c("data.table", "approximate")
+my_pkgs <- c("data.table", "approximate", "dplyr")
 
+gth <- provisionr::package_sources(github = "richfitz/approximate")
 context::context_log_start()
 ctx <- context::context_save(path = "context",
                              sources = my_resources,
                              packages = my_pkgs,
-                             package_sources = context::package_sources(github = "richfitz/approximate"))
+                             package_sources = gth)
 
 
 # ---------------------------------------- define parameters
@@ -33,26 +37,18 @@ out_fl_nm <- "pred_0_1667_deg_long.rds"
 
 NA_tile_fl_name <- "NA_pixel_tiles_20km.txt"
 
-sympt_weights <- c(0.45, 0.85, 0.15)
+# percentage reduction in R0 caused by wolbachia
+sf_vals <- 1
 
-phi_vals <- c(1, 2, 3, 4)
+# id of the combinations of infectiousness weights (i.e., how many infections you can have before immunity)
+phi_set_id <- c(1, 2, 3, 4)
 
-sf_vals <- c(1, 0.3)
+phi_set_id_tag <- "phi_set_id"
 
-
-# ---------------------------------------- define variables 
-
-
-in_pth <- file.path(
-  "output", 
-  "predictions_world", 
-  model_tp,
-  "tile_sets_0_1667_deg")
-
-out_pth <- file.path(
-  "output", 
-  "burden",
-  model_tp)
+# relative simptomaticity of prim second and tert infections 
+w_1 <- 0.45
+w_2 <- 0.85
+w_3 <- 0.15
 
 
 # ---------------------------------------- are you using the cluster?
@@ -61,12 +57,12 @@ out_pth <- file.path(
 if (CLUSTER) {
   
   config <- didehpc::didehpc_config(template = "12and16Core")
-  obj <- didehpc::queue_didehpc(ctx)
+  obj <- didehpc::queue_didehpc(ctx, config = config)
   
 } else {
   
   context::context_load(ctx)
-  #context::start_parallel_cluster(8, ctx)
+  #context::parallel_cluster_start(8, ctx)
   
 }
 
@@ -98,6 +94,23 @@ age_struct <- read.csv(
   header = TRUE) 
 
 
+# ---------------------------------------- create df with different combinations of infectiousness weigths (phi)  
+
+
+v1 <- c(1, 1, 0, 0) # Up to 2 infections
+v2 <- c(1, 1, 1, 0) # Up to 3 infections
+v3 <- c(1, 1, 1, 1) # Up to 4 infections 
+
+phi_2 <- 1
+phi_1 <- (w_1 * 2 + (1 - w_1)) / (w_2 * 2 + (1 - w_2)) 
+phi_3 <- phi_4 <- (w_3 * 2 + (1 - w_3)) / (w_2 * 2 + (1 - w_2))
+
+v4 <- c(phi_1, phi_2, phi_3, phi_4) # up to 4, with symptomatic infections twice as infectious as asymptomatic ones
+
+phi_combs <- setNames(data.frame(seq_len(4), rbind(v1, v2, v3, v4)),
+                      nm = c(phi_set_id_tag, "phi1", "phi2", "phi3", "phi4"))
+
+
 # ---------------------------------------- extract info from age structure 
 
 
@@ -108,14 +121,6 @@ age_band_bnds <- get_age_band_bounds(age_band_tgs)
 age_band_L_bounds <- age_band_bnds[, 1]
 
 age_band_U_bounds <- age_band_bnds[, 2] + 1
-
-
-# ---------------------------------------- pre process age structure
-
-
-zero_age_str_countries <- apply(age_struct[, age_band_tgs], 1, sum) == 0
-
-age_struct <- age_struct[!zero_age_str_countries, ]
 
 
 # ---------------------------------------- get tile ids
@@ -129,55 +134,107 @@ NA_pixel_tile_ids <- NA_pixel_tiles$tile_id
 tile_ids_2 <- tile_ids[!tile_ids %in% NA_pixel_tile_ids]  
 
 
-# ---------------------------------------- create combinations of R0 assumptions and wolbachia effects
+# ---------------------------------------- create combinations of phi sets and wolbachia effect
 
 
-fctr_combs <- expand.grid(
-  phi = phi_vals,
-  scaling_factor = sf_vals)
+fct_c <- setNames(expand.grid(phi_set_id, sf_vals),
+                       nm = c(phi_set_id_tag, "scaling_factor"))
 
-fctr_combs <- fctr_combs[order(fctr_combs$phi,
-                               -fctr_combs$scaling_factor), ]
+fct_c <- cbind(id = seq_len(nrow(fct_c)), fct_c)
 
-fctr_combs <- cbind(ID_run = seq_len(nrow(fctr_combs)), fctr_combs)
+fct_c_2 <- left_join(fct_c, phi_combs, by = phi_set_id_tag)
 
-fctr_combs_ls <- df_to_list(x = fctr_combs, use_names = TRUE)
+fctr_combs <- df_to_list(fct_c_2, use_names = TRUE)
+
+
+# ---------------------------------------- define variables 
+
+
+orig_in_pth  <- file.path(
+  "output", 
+  "env_variables", 
+  "all_sets_0_1667_deg", 
+  "gadm")
+
+in_pth <- file.path(
+  "output", 
+  "predictions_world", 
+  model_tp,
+  "tile_sets_0_1667_deg")
+
+out_pth_all <- file.path(
+  "output", 
+  "predictions_world", 
+  model_tp,
+  "tile_sets_0_1667_deg",
+  paste0("tile_", tile_ids_2))
 
 
 # ---------------------------------------- submit one job 
 
 
-t <- obj$enqueue(load_tile_get_burden_and_wolbachia(
-  seq_along(tile_ids_2)[1], 
-  ids_vec = tile_ids_2, 
-  in_path = in_pth, 
-  model_in_path, parallel,
-  base_info, var_names, no_fits,
-  average, model_type, out_path, xx))
+# t <- obj$enqueue(
+#   burden_and_wolbachia_one_tile(
+#     seq_along(tile_ids_2)[1],
+#     ids_vec = tile_ids_2,
+#     in_path = in_pth,
+#     orig_in_path = orig_in_pth,
+#     xx = fctr_combs,
+#     age_band_tgs = age_band_tgs,
+#     age_band_lower_bounds = age_band_L_bounds,
+#     age_band_upper_bounds = age_band_U_bounds,
+#     w_1 = w_1,
+#     w_2 = w_2,
+#     w_3 = w_3,
+#     age_struct = age_struct,
+#     out_path = out_pth_all,
+#     parallel = TRUE))
 
 
 # ---------------------------------------- submit all jobs
 
 
 if (CLUSTER) {
-  
-  
-} else {
-  
-  R_0_tiles <- lapply(
-    seq_along(tile_ids_2)[1],
-    wrapper_to_load_tile_dataset,
+
+  R_0_tiles <- queuer::qlapply(
+    seq_along(tile_ids_2),
+    burden_and_wolbachia_one_tile,
+    obj,
     ids_vec = tile_ids_2,
     in_path = in_pth,
-    out_path = out_pth,
-    my_fun = wrapper_to_make_preds,
-    predictors = best_predictors,
-    model_in_path = RF_obj_path,
-    parallel = FALSE,
-    base_info = bs_inf,
-    var_names = var_names,
-    no_fits = no_fits,
-    average = TRUE,
-    model_type = model_tp)
-  
+    orig_in_path = orig_in_pth,
+    xx = fctr_combs,
+    age_band_tgs = age_band_tgs,
+    age_band_lower_bounds = age_band_L_bounds,
+    age_band_upper_bounds = age_band_U_bounds,
+    w_1 = w_1,
+    w_2 = w_2,
+    w_3 = w_3,
+    age_struct = age_struct,
+    out_path = out_pth_all,
+    parallel = TRUE)
+
+} else {
+
+  R_0_tiles <- lapply(
+    seq_along(tile_ids_2)[1],
+    burden_and_wolbachia_one_tile,
+    ids_vec = tile_ids_2,
+    in_path = in_pth,
+    orig_in_path = orig_in_pth,
+    xx = fctr_combs,
+    age_band_tgs = age_band_tgs,
+    age_band_lower_bounds = age_band_L_bounds,
+    age_band_upper_bounds = age_band_U_bounds,
+    w_1 = w_1,
+    w_2 = w_2,
+    w_3 = w_3,
+    age_struct = age_struct,
+    out_path = out_pth_all,
+    parallel = FALSE)
+
+}
+
+if (!CLUSTER) {
+  context::parallel_cluster_stop()
 }
