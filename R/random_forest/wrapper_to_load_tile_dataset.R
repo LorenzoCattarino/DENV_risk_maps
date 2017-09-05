39b9cb01724ff6baca5f92d82780d70209064a11
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-09-05 14:45:28 +0100

    Added R0 and burden calculation, and averaging across fits

--- R/random_forest/wrapper_to_load_tile_dataset.R
+++ R/random_forest/wrapper_to_load_tile_dataset.R
@@ -1,12 +1,16 @@
 wrapper_to_load_tile_dataset <- function(
   i, ids_vec, in_path, 
   no_fits, model_in_path, predictors, 
-  parallel, out_path, out_name){
+  burden, age_struct, var_names,
+  fctr_combs, age_band_tgs, 
+  age_band_lower_bounds, age_band_upper_bounds,
+  w_1, w_2, w_3, base_info,
+  out_path){
   
   #browser()
   
   one_id <- ids_vec[i]
-  cat("tile id = ", one_id, "\n")
+  cat("tile id =", one_id, "\n")
 
   tile_nm <- paste0("tile_", one_id)
          
@@ -24,14 +28,70 @@ wrapper_to_load_tile_dataset <- function(
     model_in_path = model_in_path, 
     dataset = tile, 
     predictors = predictors, 
-    parallel = parallel)
+    parallel = FALSE)
     
   foi[foi < 0] <- 0
   
-  out_pt_one_tile <- out_path[i]  
-
-  out_nm <- paste0(out_name, ".rds")
+  look_up <- inner_join(
+    age_struct,
+    tile[, c("cell", "ADM_0")], 
+    by = c("ID_0" = "ADM_0"))
   
-  write_out_rds(foi, out_pt_one_tile, out_nm)
-              
+  browser()
+  
+  if(burden & nrow(look_up)) {
+    
+    R0_and_burden <- loop(
+      fctr_combs,
+      burden_multi_factor_wrapper,
+      foi_data = foi, 
+      orig_data = tile,
+      age_band_tags = age_band_tgs,
+      age_band_lower_bounds = age_band_lower_bounds,
+      age_band_upper_bounds = age_band_upper_bounds,
+      w_1 = w_1, 
+      w_2 = w_2, 
+      w_3 = w_3,
+      look_up = look_up,
+      var_names = var_names,
+      parallel = TRUE)
+    
+    out_ls <- c(list(foi), unlist(R0_and_burden, recursive = FALSE))
+    
+    no_runs <- length(fctr_combs)
+    burden_tags <- vapply(var_names[2:length(var_names)], paste, character(4), 1:no_runs, sep = "_")
+    burden_tags <- as.vector(burden_tags)
+  
+  } else {
+    
+    out_ls <- list(foi)
+    
+    burden_tags <- NULL
+  
+  }
+  
+  exp_v_nms <- c(var_names[1], burden_tags)  
+  
+  ret <- loop(
+    seq_along(exp_v_nms),
+    wrapper_to_mean_across_fits,
+    exp_v_nms,
+    out_ls,
+    parallel = TRUE)
+  
+  all_averaged_vars <- do.call("cbind", ret)
+  
+  out <- cbind(tile[, base_info], all_averaged_vars)
+  
+  zero_logic <- out$foi_mean == 0
+  
+  out_mz <- out[!zero_logic, ] 
+  
+  dir.create(out_path, FALSE, TRUE)
+  
+  write.table(out_mz,
+              file.path(out_path, file_name),
+              row.names = FALSE,
+              sep = ",")
+  
 }  
