97d4bb1dc882a9f15367ce76868710297e24d948
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-09-05 14:44:57 +0100

    Simplified

--- R/prepare_datasets/wrapper_to_mean_across_fits.r
+++ R/prepare_datasets/wrapper_to_mean_across_fits.r
@@ -1,49 +1,11 @@
-wrapper_to_mean_across_fits <- function(
-  i, ids_vec, in_path, orig_in_path,
-  var_names, parallel, base_info,
-  out_path){
+wrapper_to_mean_across_fits <- function(i, var_names, out_list){
   
+  fl <- out_list[[i]] 
   
-  #browser()
+  ret <- mean_across_fits(fl)
   
-  # ----------------------------------------
+  col_nms <- paste0(var_names[i], "_", colnames(ret))
   
-  
-  one_id <- ids_vec[i]
-  cat("tile id = ", one_id, "\n")
-  
-  tile_nm <- paste0("tile_", one_id)
-  
-  file_name <- paste0(tile_nm, ".txt")
-  
-  original_tile <- fread(file.path(orig_in_path, file_name),
-                         header = TRUE,
-                         sep = ",",
-                         na.strings = c("NA", "-1.#IND", "Peipsi", "Moskva", "IJsselmeer", "Zeeuwse meren"),
-                         fill = TRUE,
-                         data.table = FALSE)
-  
-  a <- file.path(in_path, tile_nm)
-  
-  ret <- loop(seq_along(var_names), 
-              mean_across_fits, 
-              var_names = var_names, 
-              fl_pth = a, 
-              parallel = parallel)
-  
-  all_averaged_vars <- do.call("cbind", ret)
-  
-  out <- cbind(original_tile[, base_info], all_averaged_vars)
-  
-  zero_logic <- out$foi_mean == 0
-  
-  out_mz <- out[!zero_logic, ] 
-  
-  dir.create(out_path, FALSE, TRUE)
-  
-  write.table(out_mz,
-              file.path(out_path, file_name),
-              row.names = FALSE,
-              sep = ",")
-              
+  setNames(ret, col_nms)
+
 }
