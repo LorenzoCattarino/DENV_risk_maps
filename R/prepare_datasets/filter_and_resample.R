01dd31772f4239545747caf0034b4139e5f244e6
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-09-06 15:47:44 +0100

    Replaced repeated code with a function

--- R/prepare_datasets/filter_and_resample.R
+++ R/prepare_datasets/filter_and_resample.R
@@ -2,44 +2,40 @@ filter_and_resample <- function(x, foi_dts, env_var_names, grp_flds, grid_size){
   
   #browser()
   
-  xx <- fread(x,
+  cat("tile path =", x, "\n")
+  
+  tile <- fread(x,
               header = TRUE, 
               sep = ",",              
               na.strings = c("NA", "-1.#IND", "Peipsi", "Moskva", "IJsselmeer", "Zeeuwse meren"),
               fill = TRUE, 
               data.table = FALSE)
-  
+ 
   #"Peipsi", "Moskva", "IJsselmeer", "Zeeuwse meren"
-  if(is.character(xx$ADM_0)) stop("ADM_0 is a character")
+  if(is.character(tile$ADM_0)) stop("ADM_0 is a character")
   
   aa <- inner_join(
-    xx, 
+    tile, 
     foi_dts[, c("unique_id", "data_id", "ADM_0", "ADM_1")])
   
-  yy <- grid_up(
-    dataset = aa, 
-    grid_size = grid_size, 
-    rnd_dist = FALSE)
-
-  bb <- subset(yy, population != 0)
+  if(nrow(aa) > 0) {
+    
+    cc <- clean_and_resample(aa, env_var_names, grid_size, grp_flds)
+    
+    dd <- inner_join(
+      cc,
+      foi_dts[, c("unique_id", "data_id", "ADM_0", "ADM_1", "type", "adm_pop")])
+    
+    names(dd)[names(dd) == "lat.grid"] <- "latitude"
+    
+    names(dd)[names(dd) == "long.grid"] <- "longitude"
+    
+  } else {
+    
+    dd <- NULL
   
-  cc <- average_up(
-    pxl_df = bb, 
-    grp_flds = grp_flds, 
-    var_names = env_var_names)
+  }
   
-  dd <- inner_join(
-    cc,
-    foi_dts[, c("unique_id", "data_id", "ADM_0", "ADM_1", "type", "adm_pop")])
-  
-  dd$lat.grid <- dd$lat.grid * grid_size
-  
-  dd$long.grid <- dd$long.grid * grid_size
-  
-  names(dd)[names(dd) == "lat.grid"] <- "latitude"
-  
-  names(dd)[names(dd) == "long.grid"] <- "longitude"
-  
-  dd    
+  dd
 
 }
