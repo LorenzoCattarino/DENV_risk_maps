122b6c844dd0856588e7edf004f7ee6774040df6
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-09-06 15:48:41 +0100

    Replaced repeated code with a function

--- R/prepare_datasets/resample.R
+++ R/prepare_datasets/resample.R
@@ -11,29 +11,7 @@ resample <- function(x, grp_flds, grid_size, env_var_names, out_path){
   
   if(is.character(tile$ADM_0)) stop("ADM_0 is a character")
   
-  xx <- remove_NA_rows(tile, env_var_names)
-  
-  bb <- xx[!is.na(xx$population), ]
-
-  if (nrow(bb) > 0) {
-    
-    bb[bb$population == 0, "population"] <- 1
-  
-  }
-  
-  yy <- grid_up(
-    dataset = bb, 
-    grid_size = grid_size, 
-    rnd_dist = FALSE)
-  
-  cc <- average_up(
-    pxl_df = yy, 
-    grp_flds = grp_flds, 
-    var_names = env_var_names)
-  
-  cc$lat.grid <- cc$lat.grid * grid_size
-  
-  cc$long.grid <- cc$long.grid * grid_size
+  cc <- clean_and_resample(tile, env_var_names, grid_size, grp_flds)
   
   file_name <- basename(x) 
   
