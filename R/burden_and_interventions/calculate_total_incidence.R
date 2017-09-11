4f03029b4929144bf1f974b211a08ccc96a59ce1
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-09-11 11:06:19 +0100

    Changed multiple arguments to one list

--- R/burden_and_interventions/calculate_total_incidence.R
+++ R/burden_and_interventions/calculate_total_incidence.R
@@ -1,6 +1,11 @@
 calculate_total_incidence_rate <- function(
-  I1_rate, I2_rate, I3_rate, I4_rate, 
+  inc_rates_list, 
   rho, gamma_1, gamma_3){
+  
+  I1_rate <- inc_rates_list[[1]] 
+  I2_rate <- inc_rates_list[[2]]
+  I3_rate <- inc_rates_list[[3]] 
+  I4_rate <- inc_rates_list[[4]]
   
   rho * I2_rate + (gamma_1 * I1_rate) + (gamma_3 * (I3_rate + I4_rate))
   
