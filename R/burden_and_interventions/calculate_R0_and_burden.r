30e3431c2ee2d14a6a5325c7f547c21e05fb4c8e
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-09-12 16:25:44 +0100

    Moved out calculation of look up table

--- R/burden_and_interventions/calculate_R0_and_burden.r
+++ R/burden_and_interventions/calculate_R0_and_burden.r
@@ -1,76 +1,36 @@
-calculate_R0_and_burden <- function(
-  FOI, N, age_struct, 
-  age_band_lower_bounds, age_band_upper_bounds, age_band_tags,
+wrapper_to_R0_and_burden <- function(
+  FOI, n_j, 
+  age_band_lower_bounds, age_band_upper_bounds, 
   vec_phis, scaling_factor,
-  w_1, w_2, w_3, var_names){
-  
-  #browser()
-  
-  fun_ls <- list(
-    "calculate_primary_infection_prob",
-    "calculate_secondary_infection_prob",
-    "calculate_tertiary_infection_prob",
-    "calculate_quaternary_infection_prob")
+  rho, gamma_1, gamma_3, 
+  var_names, R0_to_FOI){
+
+  browser()
   
   out <- rep(0, length(var_names))
   
   cat("FOI =", FOI, "\n")
-  cat("phi 1 =", vec_phis[1], "\n")  
-  cat("phi 2 =", vec_phis[2], "\n") 
-  cat("phi 3 =", vec_phis[3], "\n") 
-  cat("phi 4 =", vec_phis[4], "\n") 
+  cat("infectiousness =", vec_phis, "\n")  
   cat("scaling factor =", scaling_factor, "\n")
   
   if (FOI > 0) {
     
-    # calculate n people in age group
-    n_j <- age_struct * N  
-    
-    
-    # ---------------------------------------- calculate incidence of infections
-    
-    
-    inf_probs <- lapply(
-      fun_ls, 
-      do.call,
-      list(FOI, age_band_lower_bounds, age_band_upper_bounds))
-    
-    inc_rates <- lapply(
-      inf_probs,
-      calc_average_prob_infect,
-      age_band_upper_bounds, 
-      age_band_lower_bounds) 
-    
-    infec_j <- lapply(inc_rates, calculate_case_number, n_j)
-    
-    total_infec <- vapply(infec_j, sum, numeric(1))
-    
+
+    N <- sum(n_j)
     
     # ---------------------------------------- calculate R0
     
     
     R0 <- calculate_R0(
-      FOI = FOI,
-      N = N,
-      vec_infections = total_infec, 
-      vec_phis = vec_phis)
+      FOI = FOI, 
+      N = N, 
+      n_j = n_j, 
+      age_band_lower_bounds = age_band_lower_bounds, 
+      age_band_upper_bounds = age_band_upper_bounds,
+      vec_phis = vec_phis, 
+      prob_fun = prob_fun)
     
     cat("R0 =", R0, "\n")
-    
-    
-    # ---------------------------------------- create look up function to transform R0 back to FOI
-    
-    
-    R0_to_FOI <- approximate(
-      target = calculate_R0,
-      a = 0,
-      b = 2,
-      N = N,
-      vec_infections = total_infec,
-      vec_phis = vec_phis,
-      tol = 1e-5,
-      target_vectorised = FALSE,
-      inverse = TRUE)
     
     
     # ---------------------------------------- calculate reduced R0 and back transform R0
@@ -87,7 +47,7 @@ calculate_R0_and_burden <- function(
     
     
     inf_probs <- lapply(
-      fun_ls, 
+      prob_fun, 
       do.call,
       list(red_FOI, age_band_lower_bounds, age_band_upper_bounds))
     
@@ -108,12 +68,13 @@ calculate_R0_and_burden <- function(
     # total number of infections in the population N
     a <- sum(total_infec)
     
+    I1_rate <- inc_rates[[1]] 
+    I2_rate <- inc_rates[[2]]
+    I3_rate <- inc_rates[[3]] 
+    I4_rate <- inc_rates[[4]]    
+    
     # total incidence of cases per age group
-    tot_incid_rate_j <- calculate_total_incidence_rate(
-      inc_rates_list = inc_rates,
-      rho = w_2,
-      gamma_1 = w_1,
-      gamma_3 = w_3)
+    tot_incid_rate_j <- rho * I2_rate + (gamma_1 * I1_rate) + (gamma_3 * (I3_rate + I4_rate))  
     
     # total number of cases per age group
     case_number_j <- calculate_case_number(tot_incid_rate_j, n_j)
@@ -121,13 +82,18 @@ calculate_R0_and_burden <- function(
     # total number of cases in the population N
     b <- sum(case_number_j)
     
-    c <- calculate_incidence_of_infections(a, N, 1000)
+    c <- (a / N) * 1000
     
-    d <- calculate_incidence_of_infections(b, N, 1000)
+    d <- (b / N) * 1000
     
     
     # ----------------------------------------
     
+    
+    cat("number of infections =", a, "\n")
+    cat("number of cases =", b, "\n")
+    cat("incidence of infections =", c, "\n")
+    cat("incidence of infections =", d, "\n")
     
     out <- c(red_R0, a, b, c, d)
     
