a2fd755fb88f1248451c9309638734310a760b86
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-09-12 16:32:49 +0100

    Changes

--- R/burden_and_interventions/wrapper_to_multi_factor_burden.r
+++ R/burden_and_interventions/wrapper_to_multi_factor_burden.r
@@ -1,12 +1,13 @@
-burden_multi_factor_wrapper <- function(
+wrapper_to_multi_factor_R0_and_burden <- function(
   x, foi_data, orig_data,
   age_band_tags, age_band_lower_bounds, 
   age_band_upper_bounds, 
-  w_1, w_2, w_3,
+  rho, gamma_1, gamma_3,
   look_up, parallel_2, var_names){
   
   
   #browser()
+  
   
   # ---------------------------------------- Extract factor values
   
@@ -20,30 +21,55 @@ burden_multi_factor_wrapper <- function(
   phi_4 <- x$phi4
   
   sf <- x$scaling_factor
+    
+    
+  # ---------------------------------------- define parameters 
+  
+  
+  prob_fun <- list("calculate_primary_infection_prob",
+                   "calculate_secondary_infection_prob",
+                   "calculate_tertiary_infection_prob",
+                   "calculate_quaternary_infection_prob")
+  
   
   # ---------------------------------------- define variables
  
 
   vec_phis <- c(phi_1, phi_2, phi_3, phi_4)
 
+  max_FOI <- max(foi_data$mean_pred)
+  
+  
+  # ---------------------------------------- create look up function to back map foi from R0
+
+  
+  R0_to_FOI_list <- apply(seq_len(nrow(look_up)), 
+                          get_all_look_up_functions, 
+                          look_up = look_up, 
+                          age_band_tags = age_band_tags, 
+                          vec_phis = vec_phis, 
+                          prob_fun = prob_fun, 
+                          max_FOI = max_FOI, 
+                          age_band_lower_bounds = age_band_lower_bounds, 
+                          age_band_upper_bounds = age_band_upper_bounds)
+  
   
   # ---------------------------------------- calculates R0 values for different pixels  
   
 
   burden_estimates <- loop(
     seq_len(nrow(foi_data)),
-    wrapper_to_get_multi_foi_R0, 
+    wrapper_to_replicate_R0_and_burden, 
     foi_data = foi_data, 
-    orig_data = orig_data,
-    age_data = look_up, 
+    look_up = look_up,
+    age_band_tags = age_band_tags,
     age_band_lower_bounds = age_band_lower_bounds, 
     age_band_upper_bounds = age_band_upper_bounds, 
-    age_band_tags = age_band_tags,
     vec_phis = vec_phis,
     scaling_factor = sf,
-    w_1 = w_1, 
-    w_2 = w_2, 
-    w_3 = w_3,
+    rho = rho, 
+    gamma_1 = gamma_1, 
+    gamma_3 = gamma_3,
     var_names = var_names,
     parallel = parallel_2)
   
