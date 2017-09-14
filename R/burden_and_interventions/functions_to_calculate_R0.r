fe0717bdd0b12956e7d3e35c9efb06c03211604c
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-09-14 09:47:09 +0100

    Added other functions

--- R/burden_and_interventions/functions_to_calculate_R0.r
+++ R/burden_and_interventions/functions_to_calculate_R0.r
@@ -46,9 +46,9 @@ calc_average_prob_infect <- function(
 }
 
 calculate_case_number <- function(
-  incidence_rate, age_band_pop){
+  incidence, age_band_pop){
   
-  incidence_rate * age_band_pop
+  incidence * age_band_pop
   
 }
 
@@ -66,8 +66,8 @@ get_age_band_bounds <- function(tags) {
 
 calculate_R0 <- function(
   FOI, N, n_j, 
-  age_band_lower_bounds, age_band_upper_bounds,
-  vec_phis, prob_fun){
+  prob_fun, age_band_lower_bounds, age_band_upper_bounds,
+  vec_phis){
   
   infection_probabilities <- lapply(
     prob_fun, 
@@ -86,4 +86,77 @@ calculate_R0 <- function(
   
   FOI * N / (sum(total_infection_numbers * vec_phis))
   
+}
+
+calculate_infections <- function(
+  FOI, n_j, 
+  prob_fun, age_band_lower_bounds, age_band_upper_bounds){
+  
+  infection_probabilities <- lapply(
+    prob_fun, 
+    do.call,
+    list(FOI, age_band_lower_bounds, age_band_upper_bounds))
+  
+  infection_incidences <- lapply(
+    infection_probabilities,
+    calc_average_prob_infect,
+    age_band_upper_bounds, 
+    age_band_lower_bounds) 
+  
+  infection_numbers_j <- lapply(infection_incidences, calculate_case_number, n_j)
+  
+  total_infection_number <- vapply(infection_numbers_j, sum, numeric(1))
+  
+  sum(total_infection_number)
+  
+}
+
+calculate_cases <- function(
+  FOI, n_j, 
+  prob_fun, age_band_lower_bounds, age_band_upper_bounds,
+  rho, gamma_1, gamma_3){
+  
+  infection_probabilities <- lapply(
+    prob_fun, 
+    do.call,
+    list(FOI, age_band_lower_bounds, age_band_upper_bounds))
+  
+  infection_incidences <- lapply(
+    infection_probabilities,
+    calc_average_prob_infect,
+    age_band_upper_bounds, 
+    age_band_lower_bounds) 
+  
+  I1_rate <- infection_incidences[[1]] 
+  I2_rate <- infection_incidences[[2]]
+  I3_rate <- infection_incidences[[3]] 
+  I4_rate <- infection_incidences[[4]]    
+  
+  tot_incid_rate_j <- rho * I2_rate + (gamma_1 * I1_rate) + (gamma_3 * (I3_rate + I4_rate))  
+  
+  case_number_j <- calculate_case_number(tot_incid_rate_j, n_j)
+  
+  sum(case_number_j)
+  
+}
+
+wrapper_to_lookup <- function(i, age_struct, tags, FOI_values, my_fun, ...){
+  
+  m_j <- age_struct[i, tags]
+  vapply(
+    FOI_values,
+    my_fun,
+    numeric(1),
+    n_j = m_j,
+    ...)
+  
+}
+
+calculate_infectiousness_wgts_for_sym_asym_assumption <- function(w_1, w_2, w_3){
+  
+  phi_2 <- 1
+  phi_1 <- (w_1 * 2 + (1 - w_1)) / (w_2 * 2 + (1 - w_2)) 
+  phi_3 <- phi_4 <- (w_3 * 2 + (1 - w_3)) / (w_2 * 2 + (1 - w_2))
+  
+  c(phi_1, phi_2, phi_3, phi_4)
 }
