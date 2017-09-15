4e7bedbf0d04044a99e3393ea4e0a8f1608ce232
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-09-15 11:57:48 +0100

    Works fine

--- R/burden_and_interventions/wrapper_to_replicate_R0_and_burden.r
+++ R/burden_and_interventions/wrapper_to_replicate_R0_and_burden.r
@@ -1,31 +1,39 @@
 wrapper_to_replicate_R0_and_burden <- function(
   i, 
-  foi_data, look_up, age_band_tags,
-  age_band_lower_bounds, age_band_upper_bounds, 
-  vec_phis, scaling_factor,
-  rho, gamma_1, gamma_3,
-  var_names, R0_to_FOI_list){
+  foi_data, age_struct, 
+  scaling_factor, var_names, 
+  FOI_to_R0_list,
+  FOI_to_Inf_list,
+  FOI_to_C_list,
+  age_band_lower_bounds, age_band_upper_bounds, age_band_tags,
+  vec_phis, prob_fun){
   
   
   #browser()
   
   
-  idx <- foi_data$fun_idx
+  # ---------------------------------------- get the right look up table for each square 
   
-  adm0 <- foi_data$ADM_0
+  
+  idx <- foi_data[i, "age_id"]
     
-  R0_to_FOI <- R0_to_FOI_list[[idx]]
+  FOI_to_R0 <- FOI_to_R0_list[[idx]]
+  FOI_to_Inf <- FOI_to_Inf_list[[idx]]
+  FOI_to_C <- FOI_to_C_list[[idx]]
   
   
-  # ---------------------------------------- get vector of foi values and number of people in each age group
+  # ---------------------------------------- get vector of foi values and number of people in each square
   
   
   #FOI_values <- foi_data[i, ]
   FOI_values <- foi_data[i, "mean_pred"]
   
-  n_j <- look_up[look_up$ADM_0 == adm0, age_band_tags]
+  sqr_pop <- foi_data[i, "population"]
+  #cat("square population =", sqr_pop, "\n")
   
+  m_j <- age_struct[age_struct$ADM_0 == foi_data[i, "ADM_0"], age_band_tags]
   
+    
   # ---------------------------------------- calculates R0 values for different replicates of the same pixel  
   
   
@@ -44,14 +52,15 @@ wrapper_to_replicate_R0_and_burden <- function(
   #        w_3 = w_3)
   
   wrapper_to_R0_and_burden(FOI_values,
-                           n_j = n_j, 
+                           n_j = m_j,
                            age_band_lower_bounds = age_band_lower_bounds, 
-                           age_band_upper_bounds = age_band_upper_bounds, 
-                           vec_phis = vec_phis,
+                           age_band_upper_bounds = age_band_upper_bounds,
+                           vec_phis = vec_phis, 
+                           prob_fun = prob_fun,
                            scaling_factor = scaling_factor,
-                           rho = rho, 
-                           gamma_1 = gamma_1,
-                           gamma_3 = gamma_3,
                            var_names = var_names,
-                           R0_to_FOI = R0_to_FOI)  
+                           FOI_to_R0 = FOI_to_R0, 
+                           FOI_to_Inf = FOI_to_Inf, 
+                           FOI_to_C = FOI_to_C,
+                           N = sqr_pop)  
 }
