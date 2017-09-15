e502897a2e88c505c37d7c791fa3257b657793fa
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-09-15 15:31:50 +0100

    Added function

--- R/random_forest/wrapper_to_make_predictions.R
+++ R/random_forest/wrapper_to_make_predictions.R
@@ -1,3 +1,17 @@
+make_h2o_predictions <- function(mod_obj, dataset, sel_preds){
+  
+  #browser()
+  
+  x_data <- subset(dataset, , sel_preds, drop = FALSE)
+  
+  x_data <- as.h2o(x_data)
+  
+  preds <- predict(mod_obj, x_data)
+  
+  as.vector(preds)
+  
+}
+
 wrapper_to_make_preds <- function(
   no_fits, model_in_path, dataset, 
   predictors, parallel){
