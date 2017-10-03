21fc7d85d87c4afaff8d0f68096d6a63684e9643
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-10-03 11:41:14 +0100

    Removed unique ID - you do not need it here

--- assemble_foi_dataset_6.r
+++ assemble_foi_dataset_6.r
@@ -97,10 +97,9 @@ pop_col_names <- grep("pop", colnames(foi_data_with_env_variables), value = FALS
 
 colnames(foi_data_with_env_variables)[pop_col_names] <- "population"
 
-## Add ID fields - you will need them when doing the spatial disaggregation with bootstrapping
+## Add unique ID to each data point - you will need them when doing spatial disaggregation and bootstrapping
 
-foi_data_with_env_variables <- cbind(unique_id = seq_len(nrow(foi_data_with_env_variables)),
-                                     data_id = seq_len(nrow(foi_data_with_env_variables)), 
+foi_data_with_env_variables <- cbind(data_id = seq_len(nrow(foi_data_with_env_variables)), 
                                      foi_data_with_env_variables)
 
 FT_vars <- apply(expand.grid(c(fourier_transform_elem), FTs_dt), 
