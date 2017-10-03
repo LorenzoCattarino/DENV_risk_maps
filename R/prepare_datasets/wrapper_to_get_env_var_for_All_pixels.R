b628df55b30c2447742a2ae670ea2557003153f4
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-10-03 13:55:01 +0100

    Added all the functions used for this routine

--- R/prepare_datasets/wrapper_to_get_env_var_for_All_pixels.R
+++ R/prepare_datasets/wrapper_to_get_env_var_for_All_pixels.R
@@ -40,3 +40,203 @@ wrapper_to_get_env_var_for_pixels <- function (x, a, b, c, d, e, pop_raster){
               row.names = FALSE, sep = ",")
   
 }
+
+wrapper_to_load_env_var <- function(x, ...){
+  
+  a <- x$set_id
+  #cat("set id =", a, "\n")    
+  
+  out <- load_env_var(set_id = a, ...)
+  
+}
+
+# load Altitude, all fourier elements for DayTemp, EVI, MIR, NightTemp and RFE, 
+# for ONE country,
+# as elements of a list
+
+load_env_var <- function(set_id, my_path, FTs_data, txt_file_roots) {
+  
+  result_folders <- c("altitude", "FTs", "LandCover")
+  
+  data_list <- vector("list", length = length(result_folders) + length(FTs_data) - 1)
+  
+  for (i in 1:length(result_folders))
+  {
+    
+    result_folder <- result_folders[i]
+    #cat("result folder = ", result_folder, "\n")
+    
+    if(result_folder == "altitude") {
+      
+      root_file_name <- sprintf("%s%s%s", txt_file_roots[i], set_id, ".txt")
+      #cat("root file = ", root_file_name, "\n") 
+      
+      file_ex <- file.exists(file.path(my_path, result_folder, root_file_name))
+      
+      if(file_ex == TRUE)
+      {
+        
+        txt_file <- fread(file.path(my_path, result_folder, root_file_name), 
+                          header = TRUE, 
+                          sep = "\t", 
+                          na.strings = c("NA", "-1.#IND"), 
+                          quote = "", 
+                          fill = TRUE, 
+                          data.table = FALSE)
+        
+      }else{
+        
+        txt_file <- NA 
+        
+      }
+      
+      data_list[[1]] <- txt_file
+      
+    }
+    
+    if(result_folder == "FTs") {
+      
+      file_names <- sprintf(paste(txt_file_roots[i], FTs_data, "%s%s", sep="_"), 
+                            set_id, ".txt")  
+      
+      for (j in 1:length(file_names))
+      {
+        
+        root_file_name <- file_names[j] 
+        #cat("root file = ", root_file_name, "\n")      
+        
+        file_ex <- file.exists(file.path(my_path, result_folder, root_file_name))
+        
+        if(file_ex == TRUE)
+        {
+          
+          txt_file <- fread(file.path(my_path, result_folder, root_file_name), 
+                            header = TRUE, 
+                            sep = "\t", 
+                            na.strings = c("NA", "-1.#IND"),
+                            quote = "", 
+                            fill = TRUE,
+                            data.table = FALSE)
+          
+        }else{
+          
+          txt_file <- NA
+          
+        }
+        
+        data_list[[1 + j]] <- txt_file
+        
+      }
+      
+    }     
+    
+    if(result_folder == "LandCover") {
+      
+      root_file_name <- sprintf("%s%s%s", txt_file_roots[i], set_id, ".txt")
+      #cat("root file = ", root_file_name, "\n") 
+      
+      file_ex <- file.exists(file.path(my_path, result_folder, root_file_name))
+      
+      if(file_ex == TRUE)
+      {
+        
+        txt_file <- fread(file.path(my_path, result_folder, root_file_name), 
+                          header = TRUE, 
+                          sep = "\t", 
+                          na.strings = c("NA", "-1.#IND"),
+                          quote = "", 
+                          fill = TRUE,
+                          data.table = FALSE)
+        
+      }else{
+        
+        txt_file <- NA
+        
+      }
+      
+      data_list[[1 + j + 1]] <- txt_file
+      
+    }
+    
+  }
+  
+  data_list
+  
+}
+
+append_env_var_by_tile <- function(x, a, b){
+  
+  out <- x[[1]]
+  
+  names(out) <- b[[1]]
+  
+  for (i in seq(2, length(x))){
+    
+    cat("dataset no =", i, "\n")
+    
+    dataset <- x[i]
+    
+    if(!is.na(dataset))
+    {
+      
+      vars <- a[[i]]
+      cat("variables =", vars, "\n")
+      
+      sub_dataset <- dataset[[1]][, vars]
+      
+      out <- cbind(out, sub_dataset)
+      
+      var_names <- b[[i]] 
+      
+      names(out)[names(out) %in% vars] <- var_names
+      
+    }
+    
+  }
+  
+  out
+  
+}
+
+assign_NA <- function(i, var_list) {
+  
+  year.i <- 2007
+  year.f <- 2014
+  ppyear <- 64
+  n.years <- year.f - year.i + 1
+  ntimes <- ppyear * n.years 
+  
+  dat <- var_list[[i]]
+  
+  if (i != 1 | i != 7) {
+    
+    if(i == 2 | i == 3) {
+      
+      # DayTemp or NightTemp
+      
+      na.val <- -273 * ntimes
+      na.rows <- which(dat$const_term < na.val)
+      dat[na.rows, -(1:3)] <- NA
+      
+    } else if(i == 4) {
+      
+      # EVI
+      
+      na.val <- -0.2 * ntimes
+      na.rows <- which(dat$const_term < na.val)
+      dat[na.rows, -(1:3)] <- NA
+      
+    } else if(i == 5) {
+      
+      # MIR
+      
+      na.val <- 0 * ntimes
+      na.rows <- which(dat$const_term < na.val)
+      dat[na.rows, -(1:3)] <- NA
+    }    
+    
+  }
+  
+  dat
+  
+}
