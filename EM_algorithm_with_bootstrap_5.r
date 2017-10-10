bdb6ac981220dc1ee735761302b4e7f1d38ec420
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-10-10 14:02:08 +0100

    Works fine

--- EM_algorithm_with_bootstrap_5.r
+++ EM_algorithm_with_bootstrap_5.r
@@ -60,6 +60,8 @@ predictor_rank <- read.csv(
 
 my_predictors <- predictor_rank$variable[1:9]
 
+my_predictors <- c(my_predictors, "RFE_const_term")
+
 
 # ---------------------------------------- submit one job 
 
