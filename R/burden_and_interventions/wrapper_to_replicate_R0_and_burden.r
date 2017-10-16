6e82ea38c6a3b76da6992c43e22fefe78dcff344
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-10-16 15:52:49 +0100

    Added argument; did calculation for each fit FOI

--- R/burden_and_interventions/wrapper_to_replicate_R0_and_burden.r
+++ R/burden_and_interventions/wrapper_to_replicate_R0_and_burden.r
@@ -6,7 +6,7 @@ wrapper_to_replicate_R0_and_burden <- function(
   FOI_to_Inf_list,
   FOI_to_C_list,
   age_band_lower_bounds, age_band_upper_bounds, age_band_tags,
-  vec_phis, prob_fun, reverse){
+  vec_phis, prob_fun, no_fits, reverse){
   
   
   #browser()
@@ -25,8 +25,10 @@ wrapper_to_replicate_R0_and_burden <- function(
   # ---------------------------------------- get vector of foi values and number of people in each square
   
   
-  #FOI_values <- foi_data[i, ]
-  FOI_values <- foi_data[i, "mean_pred"]
+  col_ids <- as.character(seq_len(no_fits))
+  
+  FOI_values <- foi_data[i, col_ids]
+  #FOI_values <- foi_data[i, "mean_pred"]
   
   sqr_pop <- foi_data[i, "population"]
   #cat("square population =", sqr_pop, "\n")
@@ -37,31 +39,35 @@ wrapper_to_replicate_R0_and_burden <- function(
   # ---------------------------------------- calculates R0 values for different replicates of the same pixel  
   
   
-  # vapply(FOI_values,
-  #        calculate_R0_and_burden,
-  #        numeric(3),
-  #        N = N, 
-  #        age_struct = age_struct, 
-  #        age_band_lower_bounds = age_band_lower_bounds, 
-  #        age_band_upper_bounds = age_band_upper_bounds, 
-  #        age_band_tags = age_band_tags,
-  #        vec_phis = vec_phis,
-  #        scaling_factor = scaling_factor,
-  #        w_1 = w_1, 
-  #        w_2 = w_2, 
-  #        w_3 = w_3)
+  n <- length(var_names)
   
-  wrapper_to_R0_and_burden(FOI_values,
-                           n_j = m_j,
-                           age_band_lower_bounds = age_band_lower_bounds, 
-                           age_band_upper_bounds = age_band_upper_bounds,
-                           vec_phis = vec_phis, 
-                           prob_fun = prob_fun,
-                           scaling_factor = scaling_factor,
-                           var_names = var_names,
-                           FOI_to_R0 = FOI_to_R0, 
-                           FOI_to_Inf = FOI_to_Inf, 
-                           FOI_to_C = FOI_to_C,
-                           N = sqr_pop,
-                           reverse = reverse)  
+  vapply(FOI_values,
+         wrapper_to_R0_and_burden,
+         numeric(n),
+         n_j = m_j,
+         age_band_lower_bounds = age_band_lower_bounds, 
+         age_band_upper_bounds = age_band_upper_bounds,
+         vec_phis = vec_phis, 
+         prob_fun = prob_fun,
+         scaling_factor = scaling_factor,
+         var_names = var_names,
+         FOI_to_R0 = FOI_to_R0, 
+         FOI_to_Inf = FOI_to_Inf, 
+         FOI_to_C = FOI_to_C,
+         N = sqr_pop,
+         reverse = reverse)
+  
+  # wrapper_to_R0_and_burden(FOI_values,
+  #                          n_j = m_j,
+  #                          age_band_lower_bounds = age_band_lower_bounds, 
+  #                          age_band_upper_bounds = age_band_upper_bounds,
+  #                          vec_phis = vec_phis, 
+  #                          prob_fun = prob_fun,
+  #                          scaling_factor = scaling_factor,
+  #                          var_names = var_names,
+  #                          FOI_to_R0 = FOI_to_R0, 
+  #                          FOI_to_Inf = FOI_to_Inf, 
+  #                          FOI_to_C = FOI_to_C,
+  #                          N = sqr_pop,
+  #                          reverse = reverse)  
 }
