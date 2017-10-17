d60d7064d0135593cb7b7d9dc75a042d6f7130b6
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-10-17 16:50:48 +0100

    Removed R2 equation from plots

--- R/plotting/plot_RF_preds_vs_obs_by_cv_dataset.r
+++ R/plotting/plot_RF_preds_vs_obs_by_cv_dataset.r
@@ -21,6 +21,7 @@ RF_preds_vs_obs_plot_stratif <- function (
   p <- ggplot(df, aes_string(x = x, y = y)) +
     facet_grid(as.formula(paste0("dataset ~", facet_var))) + 
     geom_point(aes_string(x = x, y = y), size = 1) +
+    geom_abline(slope = 1, intercept = 0, linetype = 2) +
     scale_x_continuous("Observations",  
                        #limits = c(min_x_value, max_x_value), 
                        breaks = x_values,
@@ -37,28 +38,28 @@ RF_preds_vs_obs_plot_stratif <- function (
           strip.text.x = element_text(size = 12),
           strip.text.y = element_text(size = 12))
 
-  p2 <- p + geom_smooth(method = "lm", formula = y ~ x - 1, se = FALSE) +    
-    coord_cartesian(xlim = c(min_x_value, max_x_value),
-                    ylim = c(min_y_value, max_y_value))
-    
-  eq <- ddply(df, as.formula(paste0("dataset ~", facet_var)), lm_eqn, y = y, x = x)
-  
-  p3 <- p2 + 
-    geom_text(data = eq, aes(x = 0.04, y = max_y_value, label = V1), 
-              parse = TRUE, 
-              inherit.aes = FALSE) +
-    facet_grid(as.formula(paste0("dataset ~", facet_var)))
+  # p2 <- p + geom_smooth(method = "lm", formula = y ~ x - 1, se = FALSE) +    
+  #   coord_cartesian(xlim = c(min_x_value, max_x_value),
+  #                   ylim = c(min_y_value, max_y_value))
+  #   
+  # eq <- ddply(df, as.formula(paste0("dataset ~", facet_var)), lm_eqn, y = y, x = x)
+  # 
+  # p3 <- p2 + 
+  #   geom_text(data = eq, aes(x = 0.04, y = max_y_value, label = V1), 
+  #             parse = TRUE, 
+  #             inherit.aes = FALSE) +
+  #   facet_grid(as.formula(paste0("dataset ~", facet_var)))
   
   dir.create(file_path, FALSE, TRUE)
   
   png(filename = file.path(file_path, file_name), 
       width = 10, 
-      height = 6, 
+      height = 7, 
       units = "in", 
       pointsize = 12,
       res = 200)
   
-  print(p3)
+  print(p)
   
   dev.off()
 }
