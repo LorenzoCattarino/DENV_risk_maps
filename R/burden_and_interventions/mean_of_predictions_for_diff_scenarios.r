45edc80b460600f777dca17b4462260f6798c03b
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-10-20 13:30:43 +0100

    Fixed output names

--- R/burden_and_interventions/mean_of_predictions_for_diff_scenarios.r
+++ R/burden_and_interventions/mean_of_predictions_for_diff_scenarios.r
@@ -5,9 +5,9 @@ average_foi_and_burden_predictions <- function(
   
   my_var <- vars[j]
   
-  root_name <- paste0(my_var, "_all_squares_0_1667_deg")
-  
   if(j == 1) {
+    
+    root_name <- paste0(my_var, "_all_squares_0_1667_deg")
     
     dat <- readRDS(file.path(in_path, paste0(root_name, ".rds")))
     
@@ -22,7 +22,7 @@ average_foi_and_burden_predictions <- function(
     loop(seq_len(no_scenarios),
          calculate_mean_of_burden_predictions_for_different_scenarios,
          in_path = in_path, 
-         root_name = root_name,
+         my_var = my_var,
          col_names = col_names, 
          base_info = base_info, 
          out_path = out_path,
@@ -33,14 +33,16 @@ average_foi_and_burden_predictions <- function(
 }
 
 calculate_mean_of_burden_predictions_for_different_scenarios <- function(
-  i, in_path, root_name,
+  i, in_path, my_var,
   col_names, base_info, out_path){
+  
+  root_name <- paste0(my_var, "_all_squares_0_1667_deg")
   
   dat <- readRDS(file.path(in_path, sprintf("%s_%s%s", root_name, i, ".rds")))
   
   ret <- average_boot_samples_dim2(dat[, col_names])
   
-  out_name <- sprintf("%s_%s%s", root_name, i, ".rds")
+  out_name <- sprintf("%s_%s_%s%s", my_var, "mean_all_squares_0_1667_deg", i, ".rds")
   
   ret2 <- cbind(dat[, base_info], ret) 
   
