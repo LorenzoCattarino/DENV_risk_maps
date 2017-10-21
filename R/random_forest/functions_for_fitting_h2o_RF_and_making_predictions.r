3ccb1d0181610d87094c15f98eaf870bc66e4523
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-10-21 15:52:11 +0100

    Added function

--- R/random_forest/functions_for_fitting_h2o_RF_and_making_predictions.r
+++ R/random_forest/functions_for_fitting_h2o_RF_and_making_predictions.r
@@ -65,3 +65,21 @@ wrapper_to_make_preds <- function(
   out  
 
 }
+
+wrapper_to_load_admin_dataset <- function(
+  dat, sel_preds, parallel, 
+  model_in_path, 
+  out_path, no_fits){
+  
+  foi <- wrapper_to_make_preds(
+    no_fits = no_fits, 
+    model_in_path = model_in_path, 
+    dataset = dat, 
+    predictors = sel_preds, 
+    parallel = parallel)  
+  
+  foi[foi < 0] <- 0
+  
+  write_out_rds(foi, out_path, file_name)
+  
+}
