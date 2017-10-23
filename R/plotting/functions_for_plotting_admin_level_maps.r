992192050dbeacfb817d2bd7f34e6319599e485e
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-10-23 13:56:48 +0100

    Added functions for plotting admin level predictions using ggplot

--- R/plotting/functions_for_plotting_admin_level_maps.r
+++ R/plotting/functions_for_plotting_admin_level_maps.r
@@ -1,3 +1,108 @@
+wrapper_to_admin_map <- function(
+  i, vars, statistics, my_colors, 
+  titles_vec, df_long, country_shp, 
+  out_path, do.p9.logic,
+  plot_wdt, plot_hgt){
+  
+  
+  # ----------------------------------------
+
+  statsc <- statistics[i]
+  out_fl_nm <- paste0(statsc, "_", vars,"_adm_1.png")
+  col <- my_colors[[2]]
+  ttl <- titles_vec[i]
+  do.p9 <- do.p9.logic[i]
+  
+  
+  
+  # ----------------------------------------
+  
+  
+  map_predictions_admin_ggplot(
+    df = df_long, 
+    shp = country_shp,
+    var_to_plot = statsc,
+    out_path = out_path, 
+    out_file_name = out_fl_nm, 
+    my_col = col, 
+    ttl = ttl, 
+    do.p9 = do.p9, 
+    plot_wdt = plot_wdt, 
+    plot_hgt = plot_hgt)
+  
+  
+}
+
+map_predictions_admin_ggplot <- function(
+  df, shp, var_to_plot, out_path, 
+  out_file_name, my_col, ttl, 
+  do.p9, plot_wdt, plot_hgt) {
+  
+  #browser()
+  
+  dir.create(out_path, FALSE, TRUE)
+  
+  png(file.path(out_path, out_file_name),
+      width = plot_wdt,
+      height = plot_hgt,
+      units = "in",
+      pointsize = 12,
+      res = 300)
+  
+  if(do.p9){
+    
+    df$layer1 <- cut(df$layer, breaks = c(-Inf, 50, 70, Inf), right = FALSE)
+    
+    p <- ggplot() + 
+      geom_tile(data = df, aes(x = x, y = y, fill = layer1)) +
+      scale_fill_manual(values = my_col,
+                        labels = c("< 50", "50-70", "> 70"),
+                        guide = guide_legend(title = ttl, 
+                                             keywidth = 4, 
+                                             keyheight = 5))
+  } else {
+    
+    p <- ggplot() +
+      geom_polygon(data = df, 
+                   aes_string(x = "long", 
+                              y = "lat", 
+                              group = "group",
+                              fill = var_to_plot),
+                   color = NA) +
+      scale_fill_gradientn(colours = my_col, 
+                           guide = guide_colourbar(title = ttl, 
+                                                   barwidth = dev.size()[1] * 0.15, 
+                                                   barheight = dev.size()[1] * 0.7),
+                           na.value = "grey60")
+    
+  }
+  
+  p2 <- p + geom_path(data = shp,
+                      aes(x = long, 
+                          y = lat, 
+                          group = group),
+                      colour = "gray40",
+                      size = 0.3) +
+    coord_equal() +
+    scale_x_continuous(labels = NULL, limits = c(-180, 180), expand = c(0, 0)) +
+    scale_y_continuous(labels = NULL, limits = c(-60, 90), expand = c(0, 0)) +
+    theme_void() + 
+    theme(axis.text.x = element_blank(),
+          axis.text.y = element_blank(),
+          axis.ticks = element_blank(),
+          plot.margin = unit(c(0, 0, 0, -0.09), "cm"),
+          legend.position = c(dev.size()[1] * 0.005, dev.size()[1] * 0.008),
+          legend.text = element_text(size = 25),
+          legend.title = element_text(face = "bold", size = 30))#,
+  #legend.background = element_rect(fill = alpha("white", 0.2), colour = "gray50"),
+  #panel.background = element_rect(fill = "#A6CEE3", colour = NA)) # lightblue2
+  
+  print(p2)
+  
+  dev.off()
+  
+}
+
 map_predictions_admin <- function(
   adm_shp_fl, nat_bord_shp_fl_ls, 
   adm_lv, y_var, file_name, 
@@ -64,69 +169,6 @@ map_predictions_admin <- function(
   grid.draw(key)
   
   grid.text(key_title, y = 0.40, x = 0.18, gp = gpar(fontsize = leg_title_size))
-  
-  dev.off()
-  
-}
-
-map_predictions_admin_ggplot <- function(
-  df, shp, out_path, 
-  out_file_name, my_col, ttl, 
-  do.p9, plot_wdt, plot_hgt) {
-  
-  #browser()
-  
-  dir.create(out_path, FALSE, TRUE)
-  
-  png(file.path(out_path, out_file_name),
-      width = plot_wdt,
-      height = plot_hgt,
-      units = "in",
-      pointsize = 12,
-      res = 300)
-  
-  if(do.p9){
-    
-    df$layer1 <- cut(df$layer, breaks = c(-Inf, 50, 70, Inf), right = FALSE)
-    
-    p <- ggplot() + 
-      geom_tile(data = df, aes(x = x, y = y, fill = layer1)) +
-      scale_fill_manual(values = my_col,
-                        labels = c("< 50", "50-70", "> 70"),
-                        guide = guide_legend(title = ttl, 
-                                             keywidth = 4, 
-                                             keyheight = 5))
-  } else {
-    
-    p <- ggplot() +
-      geom_tile(data = df, aes(x = x, y = y, fill = layer)) +
-      scale_fill_gradientn(colours = my_col, 
-                           guide = guide_colourbar(title = ttl, 
-                                                   barwidth = dev.size()[1] * 0.15, 
-                                                   barheight = dev.size()[1] * 0.7),
-                           na.value = "grey60")
-    
-  }
-  
-  p2 <- p + geom_path(data = shp,
-                      aes(x = long, y = lat, group = group),
-                      colour = "gray40",
-                      size = 0.3) +
-    coord_equal() +
-    scale_x_continuous(labels = NULL, limits = c(-180, 180), expand = c(0, 0)) +
-    scale_y_continuous(labels = NULL, limits = c(-60, 90), expand = c(0, 0)) +
-    theme_void() + 
-    theme(axis.text.x = element_blank(),
-          axis.text.y = element_blank(),
-          axis.ticks = element_blank(),
-          plot.margin = unit(c(0, 0, 0, -0.09), "cm"),
-          legend.position = c(dev.size()[1] * 0.005, dev.size()[1] * 0.008),
-          legend.text = element_text(size = 25),
-          legend.title = element_text(face = "bold", size = 30))#,
-  #legend.background = element_rect(fill = alpha("white", 0.2), colour = "gray50"),
-  #panel.background = element_rect(fill = "#A6CEE3", colour = NA)) # lightblue2
-  
-  print(p2)
   
   dev.off()
   
