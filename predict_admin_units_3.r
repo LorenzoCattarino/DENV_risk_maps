39817c17b9c1e5c9ed6e544b6688b18634c352ce
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-10-23 14:03:21 +0100

    Major changes after introducing plot function which uses ggplot rather than spplot

--- predict_admin_units_3.r
+++ predict_admin_units_3.r
@@ -1,171 +1,129 @@
-# load packages 
-library(maptools)
-library(colorRamps)
-library(grid)
-library(lattice)
+# Makes a pretty map of the administrative unit predictions
 
-# load functions 
-source(file.path("R", "random_forest", "plot_foi_map_admin_level.R"))
+options(didehpc.cluster = "fi--didemrchnb")
+
+my_resources <- c(
+  file.path("R", "plotting", "functions_for_plotting_admin_level_maps.r"))
+
+my_pkgs <- c("rgdal", "dplyr", "colorRamps", "ggplot2")
+
+context::context_log_start()
+ctx <- context::context_save(path = "context",
+                             packages = my_pkgs,
+                             sources = my_resources)
+
+CLUSTER <- TRUE
 
 
 # ---------------------------------------- define parameters 
 
 
-adm_levels <- c(1, 2)
+model_tp <- "boot_model_20km_2"
 
-j_flds <- "OBJECTID"
+vars <- "FOI"
 
-in_path_preds <- file.path(
-  "output", 
-  "dengue_dataset", 
-  "predictions", 
-  "admin_level_2")
+statistics <- c("mean", "sd", "interv", "lCI", "uCI")
 
-out_path <- file.path(
+all_titles <- c("mean", "SD", "quantile_diff", "2.5_quantile", "97.5_quantile")
+
+do.p9.logic <- c(FALSE, FALSE, FALSE, FALSE, FALSE)
+
+plot_wdt <- 28 #or: 7
+plot_hgt <- 12 #or: 3
+
+out_pth <- file.path(
   "figures", 
-  "dengue_dataset", 
-  "predictions",
-  "admin_level_2")
+  "predictions_world",
+  model_tp)
 
-xl <- ""
-yl <- ""
-lyl <- -60
-uyl <- 90
-plt_wd <- 7
-plt_hg <- 3
-cex_leg_labs <- 0.9
-leg_tl_sz <- 12
-key_wd <- 1.2
-key_hg <- 0.3
+
+# ---------------------------------------- are you using the cluster? 
+
+
+if (CLUSTER) {
+  
+  obj <- didehpc::queue_didehpc(ctx)
+  
+} else {
+  
+  context::context_load(ctx)
+  
+}
+
+
+# ---------------------------------------- create color palette
+
+
+col_ls <- list(
+  c("red3", "orange", "chartreuse4"),
+  matlab.like(10),
+  colorRampPalette(c("green4", "yellow", "red"))(10),
+  rev(heat.colors(10)))
 
 
 # ---------------------------------------- load data
 
 
-# national border shapefile
-country_border_shp_fl <- readShapePoly(file.path("data", 
-                                                 "shapefiles", 
-                                                 "gadm28_levels.shp", 
-                                                 "gadm28_adm0.shp"))
+prediction_dat <- readRDS(
+  file.path(
+    "output", 
+    "predictions_world", 
+    model_tp, 
+    "means", 
+    "FOI_mean_all_adm_1.rds"))
+
+covariate_dat <- read.csv(
+  file.path(
+    "output", 
+    "env_variables", 
+    "All_adm1_env_var.csv"))
+
+country_shp <- readOGR(dsn = file.path("output", "shapefiles"), 
+                       layer = "gadm28_adm0_eras")
+
+adm_shp <- readOGR(dsn = file.path("output", "shapefiles"), 
+                   layer = "gadm28_adm1_eras")
 
 
 # ---------------------------------------- pre processing
 
 
-var_names <- c("mean_pred", "sd_pred")
+prediction_dat$interv <- prediction_dat$uCI - prediction_dat$lCI 
 
-file_names <- c("foi", "sd_foi")
+prediction_dat <- cbind(OBJECTID = covariate_dat$OBJECTID, prediction_dat) 
 
-key_titles <- c("FOI", "sd FOI")
+adm_shp_fort <- fortify(adm_shp, region = "OBJECTID") %>% 
+  mutate(id = as.numeric(id))
 
-map_cols <- list(col_1 = matlab.like(400), 
-                 col_2 = rev(heat.colors(400)))
+df_to_plot <- left_join(adm_shp_fort, prediction_dat, by = c("id" = "OBJECTID"))
 
-legend_breaks <- c(6, 4)
+na_logic <- apply(as.matrix(df_to_plot[, statistics]), 1, anyNA)
 
-# Remove Antarctica
-country_border_shp_fl <- country_border_shp_fl[!country_border_shp_fl@data$NAME_ENGLI == "Antarctica", ]
-
-# Create list object for country borders
-country_border_shp_list <- list("sp.polygons", 
-                                country_border_shp_fl, 
-                                lwd = 0.2, 
-                                col = "gray30", 
-                                first = FALSE)
+df_to_plot[na_logic, statistics] <- rep(0, length(statistics))
 
 
-# ----------------------------------------------------------------------------------- 
-
-#                                 functional part start 
-
-# -----------------------------------------------------------------------------------
+# ---------------------------------------- start
 
 
-# dataset of predictions 
-prediction_datasets <- lapply(adm_levels, function(x){
-  readRDS(file.path(in_path_preds, paste0("predictions_adm_", x, ".rds")))})
-
-# finer subdivision border shapefiles 
-shape_files <- lapply(adm_levels, function(x){
-  readShapePoly(file.path("data", 
-                          "shapefiles", 
-                          "gadm28_levels.shp", 
-                          paste0("gadm28_adm", x, ".shp")))})
+if(CLUSTER){
   
-# loop through admin levels
-for (i in seq_along(adm_levels)){
+  admin_maps <- queuer::qlapply(
+    seq_along(statistics),
+    wrapper_to_admin_map,
+    obj,
+    vars = vars, 
+    statistics = statistics, 
+    my_colors = col_ls, 
+    titles_vec = all_titles, 
+    df_long = df_to_plot, 
+    country_shp = country_shp, 
+    out_path = out_pth, 
+    do.p9.logic = do.p9.logic,
+    plot_wdt = plot_wdt, 
+    plot_hgt = plot_hgt)
   
-  adm_lvl <- adm_levels[i]
-  
-  preds_df <- prediction_datasets[[i]]
-  
-  shp_fls <- shape_files[[i]]
-
-  
-  # ---------------------------------------- creating and saving plot
+} else {
   
   
-  # attach mean predictions to shp file
-  shp_fl_preds <- merge(shp_fls, preds_df, by = j_flds, all.x = TRUE)
   
-  # find NAs
-  na_logic <- apply(as.matrix(shp_fl_preds@data[, var_names]), 1, anyNA)
-  
-  # fix NAs 
-  shp_fl_preds@data[na_logic, var_names] <- c(0, 0)
-  
-  # loop through variables to plot 
-  for (j in seq_along(var_names)){
-    
-    a <- var_names[j]
-    
-    b <- file_names[j]
-    
-    fl_nm <- paste0(b, "_map_", "adm_", adm_lvl, ".png")
-    
-    c <- map_cols[[j]]
-    
-    d <- key_titles[j]
-    
-    e <- legend_breaks[j]
-      
-    # get the values you want to label in the legend 
-    at_labs <- pretty(shp_fl_preds@data[, a], n = e)
-    
-    # get the values at which you want colours to change
-    at_vals <- seq(min(at_labs), max(at_labs), length.out = 100)
-    # length.out controls the number of times the colour in the legend changes
-    # the higher length.out, the more continous is the legend look
-    
-    myColorkey <- list(space = "left",
-                       at = at_vals, ## where the colors change
-                       labels = list(
-                         at = at_labs, ## where to print labels
-                         cex = cex_leg_labs),
-                       width = key_wd,
-                       height = key_hg)
-    
-    # plot variable
-    map_predictions_admin(
-      adm_shp_fl = shp_fl_preds,
-      nat_bord_shp_fl_ls = country_border_shp_list,
-      adm_lv = adm_lvl,
-      y_var = a,
-      file_name = fl_nm,
-      map_colours = c,
-      my_path = out_path, 
-      plot_scales = FALSE,
-      x_lab = xl,
-      y_lab = yl,
-      key_title = d,
-      color_key = myColorkey,
-      low_y_lim = lyl, 
-      upp_y_lim = uyl,
-      plot_width = plt_wd, 
-      plot_height = plt_hg,
-      leg_title_size = leg_tl_sz,
-      color_change_values = at_vals)
-    
-  }
-
 }
