129184366d3546cf53dab9f4dd838321f200da76
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-11-15 13:28:17 +0000

    Changes after introducing loop over variables and map resizing

--- R/plotting/functions_for_plotting_square_level_maps.r
+++ R/plotting/functions_for_plotting_square_level_maps.r
@@ -1,12 +1,15 @@
 wrapper_to_ggplot_map <- function(
-  i, vars, statistics, my_colors, 
-  titles_vec, df_long, country_shp, 
-  shp_fort, out_path, do.p9.logic,
+  x, my_colors, model_tp, 
+  country_shp, shp_fort, out_path, 
   plot_wdt, plot_hgt){
   
   
-  # ----------------------------------------
+  # ---------------------------------------- define parameters / variables
   
+  
+  var <- x$var 
+  scenario_id <- x$scenario_id
+  statsc <- x$statistic  
   
   gr_size <- 20
   
@@ -15,13 +18,75 @@ wrapper_to_ggplot_map <- function(
   lats <- seq(-90, 90, by = res)
   lons <- seq(-180, 180, by = res)
   
-  statsc <- statistics[i]
-  
   j <- 2
   
   col <- my_colors[[j]]
-  ttl <- titles_vec[i]
-  do.p9 <- do.p9.logic[i]
+  
+  if(statsc == "mean"){
+    ttl <- "mean"
+  }
+  if(statsc == "sd"){
+    ttl <- "SD"
+  }
+  if(statsc == "interv"){
+    ttl <- "quantile_diff"
+  }
+  if(statsc == "lCI"){
+    ttl <- "2.5_quantile"
+  }
+  if(statsc == "uCI"){
+    ttl <- "97.5_quantile"
+  }
+  
+  
+  # ---------------------------------------- load data 
+  
+  
+  if(var == "FOI"){
+    
+    base_info <- c("cell", "lat.grid", "long.grid", "population", "ADM_0", "ADM_1", "ADM_2")
+    
+    out_fl_nm <- paste0(statsc, "_", var,"_0_1667_deg.png")
+    
+    mean_pred_fl_nm <- paste0(var, "_mean_all_squares_0_1667_deg.rds")
+    
+    mean_preds <- readRDS(
+      file.path(
+        "output",
+        "predictions_world",
+        model_tp,
+        "means",
+        mean_pred_fl_nm))
+    
+    all_sqr_covariates <- readRDS(
+      file.path(
+        "output", 
+        "env_variables", 
+        "all_squares_env_var_0_1667_deg.rds"))
+    
+    df_long <- cbind(all_sqr_covariates[, base_info], mean_preds)
+    
+  } else {
+    
+    out_fl_nm <- paste0(statsc, "_", var, "_0_1667_deg_", scenario_id, ".png")
+    
+    mean_pred_fl_nm <- paste0(var, "_mean_all_squares_0_1667_deg_", scenario_id, ".rds")
+    
+    df_long <- readRDS(
+      file.path(
+        "output",
+        "predictions_world",
+        model_tp,
+        "means",
+        mean_pred_fl_nm))
+  
+  }
+
+  
+  # ---------------------------------------- calculate quantile difference 
+  
+  
+  #df_long$interv <- df_long$uCI - df_long$lCI 
   
   
   # ---------------------------------------- create matrix of values
@@ -80,7 +145,7 @@ wrapper_to_ggplot_map <- function(
   # ---------------------------------------- plot differently NA values
   
   
-  if(vars == "FOI" & statsc == "mean"){
+  if(var == "R0_r" & statsc == "mean") {
     
     na_cutoff <- 1 
   
@@ -95,22 +160,20 @@ wrapper_to_ggplot_map <- function(
   
   # ---------------------------------------- make map 
   
-  
-  out_fl_nm <- paste0(statsc, "_", vars,"_0_1667_deg.png")
-  
+
   map_data_pixel_ggplot(df = r_df, 
                         shp = shp_fort, 
                         out_path = out_path, 
                         out_file_name = out_fl_nm,
                         my_col = col, 
                         ttl = ttl,
-                        do.p9 = do.p9,
                         plot_wdt = plot_wdt, 
-                        plot_hgt = plot_hgt)
+                        plot_hgt = plot_hgt,
+                        statsc = statsc)
   
 }
 
-map_data_pixel_ggplot <- function(df, shp, out_path, out_file_name, my_col, ttl, do.p9, plot_wdt, plot_hgt) {
+map_data_pixel_ggplot <- function(df, shp, out_path, out_file_name, my_col, ttl, plot_wdt, plot_hgt, statsc) {
   
   #browser()
   
@@ -123,7 +186,7 @@ map_data_pixel_ggplot <- function(df, shp, out_path, out_file_name, my_col, ttl,
       pointsize = 12,
       res = 300)
   
-  if(do.p9){
+  if(statsc == "p9"){
     
     df$layer1 <- cut(df$layer, breaks = c(-Inf, 50, 70, Inf), right = FALSE)
     
@@ -149,7 +212,7 @@ map_data_pixel_ggplot <- function(df, shp, out_path, out_file_name, my_col, ttl,
   p2 <- p + geom_path(data = shp,
                       aes(x = long, y = lat, group = group),
                       colour = "gray40",
-                      size = 0.3) +
+                      size = 0.1) +                                             # or: 0.3
     coord_equal() +
     scale_x_continuous(labels = NULL, limits = c(-180, 180), expand = c(0, 0)) +
     scale_y_continuous(labels = NULL, limits = c(-60, 90), expand = c(0, 0)) +
@@ -158,9 +221,9 @@ map_data_pixel_ggplot <- function(df, shp, out_path, out_file_name, my_col, ttl,
           axis.text.y = element_blank(),
           axis.ticks = element_blank(),
           plot.margin = unit(c(0, 0, 0, -0.09), "cm"),
-          legend.position = c(dev.size()[1] * 0.005, dev.size()[1] * 0.008),
-          legend.text = element_text(size = 25),
-          legend.title = element_text(face = "bold", size = 30))#,
+          legend.position = c(dev.size()[1] * 0.015, dev.size()[1] * 0.02),    # or: c(0.005, 0.008) 
+          legend.text = element_text(size = 15),                               # or: 25
+          legend.title = element_text(face = "bold", size = 22))#,             # or: 30
   #legend.background = element_rect(fill = alpha("white", 0.2), colour = "gray50"),
   #panel.background = element_rect(fill = "#A6CEE3", colour = NA)) # lightblue2
   
