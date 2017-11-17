e218ccb59fb631675bb6eab17bd11864ed49862c
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-11-17 09:17:07 +0000

    Removed function definition from script

--- EM_algorithm_with_bootstrap_2a.r
+++ EM_algorithm_with_bootstrap_2a.r
@@ -3,6 +3,7 @@
 # the first instance in the bootstrap sample will have id = 1,
 # the second instance will have id = 2.
 
+source(file.path("R", "prepare_datasets", "functions_for_creating_bootstrap_samples.r"))
 source(file.path("R", "utility_functions.r"))
 
 boot_samples <- readRDS(
@@ -12,11 +13,7 @@ boot_samples <- readRDS(
 
 if (names(boot_samples[[1]])[1] != "unique_id") {
   
-  test <- lapply(seq_along(boot_samples), function(i) {
-    x <- boot_samples[[i]]
-    x <- cbind(unique_id=seq_len(nrow(x)), x)
-    x
-  })
+  test <- lapply(seq_along(boot_samples), attach_unique_id, boot_samples)
   
   write_out_rds(test, 
                 file.path("output",
