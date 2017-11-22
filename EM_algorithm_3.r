570cabd4708a2b7708df4989424454c3eae07042
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-11-22 16:21:37 +0000

    Works fine

--- EM_algorithm_3.r
+++ EM_algorithm_3.r
@@ -2,15 +2,11 @@
 
 options(didehpc.cluster = "fi--didemrchnb")
 
-CLUSTER <- FALSE
-
 my_resources <- c(
   file.path("R", "utility_functions.r"),
-  file.path("R", "random_forest", "fit_h2o_random_forest_model.r"),
-  file.path("R", "random_forest", "make_h2o_RF_predictions.r"),
-  file.path("R", "random_forest", "spatial_sampK_cv_rng3.r"))
+  file.path("R", "random_forest", "functions_for_fitting_h2o_RF_and_making_predictions.r"))
 
-my_pkgs <- c("h2o")
+my_pkgs <- "h2o"
 
 context::context_log_start()
 ctx <- context::context_save(path = "context",
@@ -21,44 +17,37 @@ ctx <- context::context_save(path = "context",
 # ---------------------------------------- define parameters
 
 
-y_var <- "FOI"
+dependent_variable <- "R0_3"
+
+pseudoAbsence_value <- 0.5
 
 no_trees <- 500
 
 min_node_size <- 20
 
-pseudoAbs_value <- 0
-
 all_wgt <- 1
 
-pAbs_wgt <- 0.25
+pAbs_wgt <- 1
 
-out_path <- file.path("output", "EM_algorithm", "model_objects")
+pAbs_wgt_AUS <- 20
+
+out_path <- file.path("output", "EM_algorithm", paste0("model_objects_", dependent_variable, "_fit"))
 
 out_name <- "all_data.rds"   
 
 
-# ---------------------------------------- Are you using the cluster?
+# ---------------------------------------- start up
 
 
-if (CLUSTER) {
-  
-  #config <- didewin::didewin_config(template = "24Core")
-  obj <- didehpc::queue_didehpc(ctx)
-
-} else {
-  
-  context::context_load(ctx)
-
-}
+context::context_load(ctx)
 
 
-# ---------------------------------------- Load data
+# ---------------------------------------- load data
 
 
 # load FOI dataset
 foi_data <- read.csv(
-  file.path("output", "foi", "All_FOI_estimates_linear_env_var.csv"),
+  file.path("output", "foi", "All_FOI_estimates_linear_env_var_area.csv"),
   stringsAsFactors = FALSE)
 
 # predicting variable rank
@@ -76,51 +65,41 @@ predictor_rank <- read.csv(
 
 my_predictors <- predictor_rank$variable[1:9]
 
+my_predictors <- c(my_predictors, "RFE_const_term", "pop_den")
+
 
 # ---------------------------------------- pre process the original foi dataset
 
 
 # set pseudo absence value
-foi_data[foi_data$type == "pseudoAbsence", y_var] <- pseudoAbs_value
+foi_data[foi_data$type == "pseudoAbsence", dependent_variable] <- pseudoAbsence_value
 
 # assign weights
 foi_data$new_weight <- all_wgt
 foi_data[foi_data$type == "pseudoAbsence", "new_weight"] <- pAbs_wgt
+foi_data[foi_data$type == "pseudoAbsence" & foi_data$ID_0 == 15, "new_weight"] <- pAbs_wgt_AUS
 
 
 # ---------------------------------------- create objects needed for run
 
 
 # get training dataset (full dataset - no bootstrap)
-training_dataset <- foi_data[, c(y_var, my_predictors, "new_weight")]
+training_dataset <- foi_data[, c(dependent_variable, my_predictors, "new_weight")]
 
 
 # ---------------------------------------- # run job
 
 
-if (CLUSTER) {
-  
-  RF_fit <- obj$enqueue(
-    spatial.cv.rf(
-      preds = my_predictors, 
-      y_var = y_var, 
-      train_set = training_dataset, 
-      no_trees = no_trees, 
-      min_node_size = min_node_size, 
-      my_weights = "new_weight",
-      model_name = out_name,
-      model_path = out_path))
-  
-} else {
-  
-  RF_fit <- spatial.cv.rf(
-    preds = my_predictors, 
-    y_var = y_var, 
-    train_set = training_dataset, 
-    no_trees = no_trees, 
-    min_node_size = min_node_size, 
-    my_weights = "new_weight",
-    model_name = out_name,
-    model_path = out_path)
-  
-}
+h2o.init()
+
+RF_obj <- fit_h2o_RF(dependent_variable = dependent_variable, 
+                     predictors = my_predictors, 
+                     training_dataset = training_dataset, 
+                     no_trees = no_trees, 
+                     min_node_size = min_node_size,
+                     my_weights = "new_weight",
+                     model_nm = out_name)
+
+h2o.saveModel(RF_obj, out_path, force = TRUE)
+
+h2o.shutdown(prompt = FALSE)
