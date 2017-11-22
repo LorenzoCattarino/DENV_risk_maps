491a3afbd4400691260b9137424f9ca9062544ed
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2017-11-22 16:22:48 +0000

    Added population density

--- get_env_variables_for_ALL_adm1.R
+++ get_env_variables_for_ALL_adm1.R
@@ -1,15 +1,16 @@
 # load packages
-library(maptools)
+library(rgdal)
+library(dplyr)
 
 # load functions
 source(file.path("R", "prepare_datasets", "get_env_variables.R"))
 source(file.path("R", "utility_functions.r"))
 
 # load data 
-shp_fl_adm1 <- readShapePoly(file.path("data", "shapefiles", "gadm28_levels.shp", "gadm28_adm1.shp"))
+shp_fl_adm1 <- readOGR(file.path("data", "shapefiles", "gadm28_levels.shp"), "gadm28_adm1")
 
 
-# ---------------------------------------- define variable objects and names 
+# ---------------------------------------- define parameters 
 
 
 population_var <- "pop"
@@ -18,14 +19,20 @@ fourier_transform_elem <- c("const_term",	"Re0",	"Im0",	"Re1",	"Im1")
 FTs_dt <- c("DayTemp", "EVI", "MIR", "NightTemp", "RFE")
 LandCover_var <- paste("lct1_2012001", c(seq(0, 16, 1), 254, 255), sep = "_")
 
+base_info <- c("OBJECTID", "ID_0", "ISO", "country", "ID_1", "name1", "latitude", "longitude")
+
+winkel_tripel_crs <- CRS("+proj=wintri")
+
+
+# ---------------------------------------- define variable
+
+
 # Get number of variables to extract
 number_of_variables <- length(c(altitude_var, LandCover_var)) + (length(c(population_var, fourier_transform_elem)) * length(FTs_dt))
 
 all_var_names <- c(altitude_var, 
                    apply(expand.grid(c(population_var, fourier_transform_elem), FTs_dt), 1, function(x) paste(x[2],x[1], sep="_")), 
                    LandCover_var)
-
-base_info <- c("OBJECTID", "ID_0", "ISO", "country", "ID_1", "name1", "latitude", "longitude")
 
 
 # ---------------------------------------- start
@@ -40,6 +47,8 @@ names(all_adm1) <- base_info
 all_adm1$ISO <- as.character(all_adm1$ISO)
 all_adm1$country <- as.character(all_adm1$country)
 all_adm1$name1 <- as.character(all_adm1$name1)
+all_adm1$ID_0 <- as.numeric(as.character(all_adm1$ID_0))
+all_adm1$ID_1 <- as.numeric(as.character(all_adm1$ID_1))
 
 all_adm1_ls <- df_to_list(all_adm1, use_names = TRUE)
 
@@ -86,10 +95,30 @@ NA_adm1_anv_var <- adm1_env_var[row.has.na, base_info[1:5]]
 adm1_env_var <- adm1_env_var[!row.has.na, ]
 
 
+# ---------------------------------------- attach population density
+
+
+shp_pr <- spTransform(shp_fl_adm1, winkel_tripel_crs)
+
+all_areas <- sapply(shp_pr@polygons, function(x) x@area)
+
+# from sq m to sq km
+shp_pr@data$Shape_Area <- all_areas/1000000 
+
+shp_pr@data$ID_0 <- as.numeric(as.character(shp_pr@data$ID_0))
+shp_pr@data$ID_1 <- as.numeric(as.character(shp_pr@data$ID_1))
+
+shp_pr@data <- shp_pr@data[!duplicated(shp_pr@data[, c("ID_0", "ID_1")]), ]
+
+adm1_env_var_2 <- left_join(adm1_env_var, shp_pr@data[, c("ID_0", "ID_1", "Shape_Area")], by = c("ID_0", "ID_1"))
+
+adm1_env_var_2$pop_den <- adm1_env_var_2$population / adm1_env_var_2$Shape_Area  
+
+
 # ---------------------------------------- write out 
 
 
-write.table(adm1_env_var[order(adm1_env_var$country, adm1_env_var$ID_1), ], 
+write.table(adm1_env_var_2[order(adm1_env_var_2$country, adm1_env_var_2$ID_1), ], 
             file.path("output", 
                       "env_variables", 
                       "All_adm1_env_var.csv"), 
