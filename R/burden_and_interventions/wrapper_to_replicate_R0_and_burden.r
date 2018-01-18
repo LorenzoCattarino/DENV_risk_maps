ed3c3e15308cc46cc4b596489cc9b25507806dd2
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-01-18 14:23:45 +0000

    Modified to allow procesing output from best fit model

--- R/burden_and_interventions/wrapper_to_replicate_R0_and_burden.r
+++ R/burden_and_interventions/wrapper_to_replicate_R0_and_burden.r
@@ -6,7 +6,7 @@ wrapper_to_replicate_R0_and_burden <- function(
   FOI_to_Inf_list,
   FOI_to_C_list,
   age_band_lower_bounds, age_band_upper_bounds, age_band_tags,
-  vec_phis, prob_fun, no_fits, reverse){
+  vec_phis, prob_fun, no_fits, var_to_fit, fit_type){
   
   
   #browser()
@@ -25,17 +25,25 @@ wrapper_to_replicate_R0_and_burden <- function(
   # ---------------------------------------- get vector of foi values and number of people in each square
   
   
-  col_ids <- as.character(seq_len(no_fits))
-  
-  FOI_values <- foi_data[i, col_ids]
-  #FOI_values <- foi_data[i, "mean_pred"]
+  if(!is.null(no_fits)){
+    col_ids <- as.character(seq_len(no_fits))
+  }
   
   sqr_pop <- foi_data[i, "population"]
   #cat("square population =", sqr_pop, "\n")
   
   m_j <- age_struct[age_struct$ADM_0 == foi_data[i, "ADM_0"], age_band_tags]
   
+  if(fit_type == "boot") {
     
+    FOI_values <- foi_data[i, col_ids]
+    
+  } else{
+    
+    FOI_values <- foi_data[i, "best"]
+  }
+  
+  
   # ---------------------------------------- calculates R0 values for different replicates of the same pixel  
   
   
@@ -43,33 +51,39 @@ wrapper_to_replicate_R0_and_burden <- function(
   
   n <- length(vars)
   
-  vapply(FOI_values,
-         wrapper_to_R0_and_burden,
-         numeric(n),
-         n_j = m_j,
-         vars = vars,
-         age_band_lower_bounds = age_band_lower_bounds, 
-         age_band_upper_bounds = age_band_upper_bounds,
-         vec_phis = vec_phis, 
-         prob_fun = prob_fun,
-         scaling_factor = scaling_factor,
-         FOI_to_R0 = FOI_to_R0, 
-         FOI_to_Inf = FOI_to_Inf, 
-         FOI_to_C = FOI_to_C,
-         N = sqr_pop,
-         reverse = reverse)
-  
-  # wrapper_to_R0_and_burden(FOI_values,
-  #                          n_j = m_j,
-  #                          age_band_lower_bounds = age_band_lower_bounds, 
-  #                          age_band_upper_bounds = age_band_upper_bounds,
-  #                          vec_phis = vec_phis, 
-  #                          prob_fun = prob_fun,
-  #                          scaling_factor = scaling_factor,
-  #                          var_names = var_names,
-  #                          FOI_to_R0 = FOI_to_R0, 
-  #                          FOI_to_Inf = FOI_to_Inf, 
-  #                          FOI_to_C = FOI_to_C,
-  #                          N = sqr_pop,
-  #                          reverse = reverse)  
+  if(fit_type == "boot") {
+    
+    vapply(FOI_values,
+           wrapper_to_R0_and_burden,
+           numeric(n),
+           n_j = m_j,
+           vars = vars,
+           age_band_lower_bounds = age_band_lower_bounds, 
+           age_band_upper_bounds = age_band_upper_bounds,
+           vec_phis = vec_phis, 
+           prob_fun = prob_fun,
+           scaling_factor = scaling_factor,
+           FOI_to_R0 = FOI_to_R0, 
+           FOI_to_Inf = FOI_to_Inf, 
+           FOI_to_C = FOI_to_C,
+           N = sqr_pop,
+           var_to_fit = var_to_fit)
+    
+  } else {
+    
+    wrapper_to_R0_and_burden(FOI_values,
+                             n_j = m_j,
+                             vars = vars,
+                             age_band_lower_bounds = age_band_lower_bounds,
+                             age_band_upper_bounds = age_band_upper_bounds,
+                             vec_phis = vec_phis,
+                             prob_fun = prob_fun,
+                             scaling_factor = scaling_factor,
+                             FOI_to_R0 = FOI_to_R0,
+                             FOI_to_Inf = FOI_to_Inf,
+                             FOI_to_C = FOI_to_C,
+                             N = sqr_pop,
+                             var_to_fit = var_to_fit)
+  }
+
 }
