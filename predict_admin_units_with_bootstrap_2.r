a0f92d6bb7bcc73eef38ff148138f2273b7f8b19
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-01-26 15:24:01 +0000

    Works fine

--- predict_admin_units_with_bootstrap_2.r
+++ predict_admin_units_with_bootstrap_2.r
@@ -1,12 +1,13 @@
-# Take mean, sd and 95% CI of foi, R0 and burden measures, for each admin unit 1
+# Take mean, sd and 95% CI of foi for each admin unit 1
 # THIS IS FOR THE MAPS!
 
 options(didehpc.cluster = "fi--didemrchnb")
 
+CLUSTER <- FALSE
+
 my_resources <- c(
   file.path("R", "utility_functions.r"),
-  file.path("R", "prepare_datasets", "calculate_mean_across_fits.r"),
-  file.path("R", "burden_and_interventions", "mean_of_predictions_for_diff_scenarios.r"))
+  file.path("R", "burden_and_interventions", "calculate_mean_across_fits.r"))
 
 my_pkgs <- NULL
 
@@ -15,92 +16,81 @@ ctx <- context::context_save(path = "context",
                              packages = my_pkgs,
                              sources = my_resources)
 
-CLUSTER <- TRUE
+
+# define parameters -----------------------------------------------------------
 
 
-# ---------------------------------------- define parameters
-
-
-model_tp <- "boot_model_20km_2"
+model_tp <- "R0_3_boot_model"
 
 vars <- "FOI" 
 
-no_fits <- 200
+scenario_ids <- 1
 
-no_scenarios <- 9
+no_fits <- 200
 
 col_names <- as.character(seq_len(no_fits))
 
-base_info <- c("cell", "lat.grid", "long.grid", "population", "ADM_0", "ADM_1", "ADM_2")
+base_info <- c("OBJECTID", "ID_0", "country", "ID_1", "name1", "population")
 
 in_path <- file.path("output", "predictions_world", model_tp)
 
-out_path <- file.path("output", "predictions_world", model_tp, "means")
+out_path <- file.path("output", "predictions_world", model_tp)
 
-dts_tag <- "all_adm_1"
+dts_tag <- "all_adm1"
 
 
-# ----------------------------------------
+# are you using the cluster? --------------------------------------------------
 
 
 if (CLUSTER) {
   
-  config <- didehpc::didehpc_config(template = "12and16Core")
+  config <- didehpc::didehpc_config(template = "20Core")
   obj <- didehpc::queue_didehpc(ctx, config = config)
   
 } else{
   
   context::context_load(ctx)
-  context::parallel_cluster_start(8, ctx)
   
 }
 
 
-
-# ---------------------------------------- run one job
-
-
-# t <- obj$enqueue(
-#   average_foi_and_burden_predictions(
-#     seq_along(vars)[2],
-#     vars = vars,
-#     in_path = in_path, 
-#     out_path = out_path, 
-#     no_scenarios = no_scenarios, 
-#     col_names = col_names,
-#     base_info = base_info
-#     dts_tag = dts_tag))
+# load data ------------------------------------------------------------------- 
 
 
-# ---------------------------------------- run
+prediction_datasets <- read.csv(
+  file.path("output", 
+            "env_variables", 
+            "All_adm1_env_var.csv"))
+
+
+# run ------------------------------------------------------------------------- 
 
 
 if (CLUSTER) {
   
-  means_all_scenarios <- queuer::qlapply(
-    seq_along(vars),
-    average_foi_and_burden_predictions,
-    obj,
-    vars = vars,
-    in_path = in_path,
-    out_path = out_path,
-    no_scenarios = no_scenarios,
-    col_names = col_names,
-    base_info = base_info,
-    dts_tag = dts_tag)
+  mean_foi <- obj$enqueue(
+    average_foi_and_burden_predictions(
+      seq_along(vars),
+      vars = vars,
+      in_path = in_path,
+      out_path = out_path,
+      scenario_ids = scenario_ids,
+      col_names = col_names,
+      base_info = base_info,
+      dts_tag = dts_tag,
+      covariate_dts = prediction_datasets))
   
 } else {
   
-  means_all_scenarios <- loop(
+  mean_foi <- average_foi_and_burden_predictions(
     seq_along(vars),
-    average_foi_and_burden_predictions,
     vars = vars,
     in_path = in_path,
     out_path = out_path,
-    no_scenarios = no_scenarios,
+    scenario_ids = scenario_ids,
     col_names = col_names,
     base_info = base_info,
     dts_tag = dts_tag,
-    parallel = TRUE)
+    covariate_dts = prediction_datasets)
   
 }
