d8d586c6be2d6846cd1a72f715a911c7f4c250a3
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-01-29 15:04:48 +0000

    Works fine

--- predict_admin_units_with_bootstrap_4.r
+++ predict_admin_units_with_bootstrap_4.r
@@ -2,7 +2,10 @@
 
 options(didehpc.cluster = "fi--didemrchnb")
 
+CLUSTER <- TRUE
+
 my_resources <- c(
+  file.path("R", "utility_functions.r"),
   file.path("R", "plotting", "functions_for_plotting_admin_level_maps.r"))
 
 my_pkgs <- c("rgdal", "dplyr", "colorRamps", "ggplot2")
@@ -12,24 +15,21 @@ ctx <- context::context_save(path = "context",
                              packages = my_pkgs,
                              sources = my_resources)
 
-CLUSTER <- TRUE
+
+# define parameters -----------------------------------------------------------  
 
 
-# ---------------------------------------- define parameters 
+model_tp <- "R0_3_boot_model"
 
+vars <- "R0_r"
 
-model_tp <- "boot_model_20km_2"
+scenario_id <- 3
 
-vars <- "FOI"
+pred_fl_name <- "R0_r_mean_all_adm1_3.rds"
+  
+statistics <- c("median", "interv")
 
-statistics <- c("mean", "sd", "interv", "lCI", "uCI")
-
-all_titles <- c("mean", "SD", "quantile_diff", "2.5_quantile", "97.5_quantile")
-
-do.p9.logic <- c(FALSE, FALSE, FALSE, FALSE, FALSE)
-
-plot_wdt <- 28 #or: 7
-plot_hgt <- 12 #or: 3
+map_size <- "small"
 
 out_pth <- file.path(
   "figures", 
@@ -37,7 +37,7 @@ out_pth <- file.path(
   model_tp)
 
 
-# ---------------------------------------- are you using the cluster? 
+# are you using the cluster? -------------------------------------------------- 
 
 
 if (CLUSTER) {
@@ -51,17 +51,23 @@ if (CLUSTER) {
 }
 
 
-# ---------------------------------------- create color palette
+# create combination of factors ----------------------------------------------- 
 
 
-col_ls <- list(
-  c("red3", "orange", "chartreuse4"),
-  matlab.like(10),
-  colorRampPalette(c("green4", "yellow", "red"))(10),
-  rev(heat.colors(10)))
+fact_comb_FOI <- expand.grid(var = vars[vars == "FOI"], 
+                             scenario_id = 1, 
+                             statistic = statistics, 
+                             stringsAsFactors = FALSE)
+
+fact_comb_no_FOI <- expand.grid(var = vars[vars != "FOI"], 
+                                scenario_id = scenario_id, 
+                                statistic = statistics, 
+                                stringsAsFactors = FALSE)
+
+fact_comb <- rbind(fact_comb_FOI, fact_comb_no_FOI)
 
 
-# ---------------------------------------- load data
+# load data ------------------------------------------------------------------- 
 
 
 prediction_dat <- readRDS(
@@ -69,14 +75,7 @@ prediction_dat <- readRDS(
     "output", 
     "predictions_world", 
     model_tp, 
-    "means", 
-    "FOI_mean_all_adm_1.rds"))
-
-covariate_dat <- read.csv(
-  file.path(
-    "output", 
-    "env_variables", 
-    "All_adm1_env_var.csv"))
+    pred_fl_name))
 
 country_shp <- readOGR(dsn = file.path("output", "shapefiles"), 
                        layer = "gadm28_adm0_eras")
@@ -85,45 +84,61 @@ adm_shp <- readOGR(dsn = file.path("output", "shapefiles"),
                    layer = "gadm28_adm1_eras")
 
 
-# ---------------------------------------- pre processing
+# pre processing -------------------------------------------------------------- 
 
 
-prediction_dat$interv <- prediction_dat$uCI - prediction_dat$lCI 
-
-prediction_dat <- cbind(OBJECTID = covariate_dat$OBJECTID, prediction_dat) 
+col_ls <- list(
+  c("red3", "orange", "chartreuse4"),
+  matlab.like(10),
+  colorRampPalette(c("green4", "yellow", "red"))(10),
+  rev(heat.colors(10)))
 
 adm_shp_fort <- fortify(adm_shp, region = "OBJECTID") %>% 
   mutate(id = as.numeric(id))
 
 df_to_plot <- left_join(adm_shp_fort, prediction_dat, by = c("id" = "OBJECTID"))
 
-na_logic <- apply(as.matrix(df_to_plot[, statistics]), 1, anyNA)
-
-df_to_plot[na_logic, statistics] <- rep(0, length(statistics))
+fact_comb_ls <- df_to_list(fact_comb, use_names = TRUE)
 
 
-# ---------------------------------------- start
+# submit one job -------------------------------------------------------------- 
+
+
+# t <- obj$enqueue(
+#   wrapper_to_admin_map(
+#     fact_comb_ls[1],
+#     my_colors = col_ls, 
+#     df_long = df_to_plot, 
+#     country_shp = country_shp, 
+#     out_path = out_pth, 
+#     map_size= map_size))
+    
+
+# submit all jobs ------------------------------------------------------------- 
 
 
 if(CLUSTER){
   
   admin_maps <- queuer::qlapply(
-    seq_along(statistics),
+    fact_comb_ls,
     wrapper_to_admin_map,
     obj,
-    vars = vars, 
-    statistics = statistics, 
     my_colors = col_ls, 
-    titles_vec = all_titles, 
     df_long = df_to_plot, 
     country_shp = country_shp, 
     out_path = out_pth, 
-    do.p9.logic = do.p9.logic,
-    plot_wdt = plot_wdt, 
-    plot_hgt = plot_hgt)
+    map_size= map_size)
   
 } else {
   
-  
-  
+  admin_maps <- loop(
+    fact_comb_ls[1],
+    wrapper_to_admin_map,
+    my_colors = col_ls, 
+    df_long = df_to_plot, 
+    country_shp = country_shp, 
+    out_path = out_pth, 
+    map_size= map_size,
+    parallel = FALSE)
+
 }
