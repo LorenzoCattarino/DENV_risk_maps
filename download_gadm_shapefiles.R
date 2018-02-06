58db060fe0489a3be3e73d3385fb7df5ad3271c7
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-02-06 15:53:23 +0000

    New script

---
+++ download_gadm_shapefiles.R
@@ -0,0 +1,20 @@
+#### Download shapefiles of all countries with admin 1 boundaries ####
+
+# load functions
+source(file.path("R", "prepare_datasets", "download_gadm_shp.R"))
+
+all_adm1_countries <- read.csv(
+  file.path("output", "datasets", "all_adm_1_countries.csv"), 
+  row.names = FALSE,
+  stringsAsFactors = FALSE)
+
+all_ISO_codes_admin_1 <- as.character(all_adm1_countries$ISO)
+
+
+no_cores <- parallel::detectCores()
+
+cl <- parallel::makePSOCKcluster(no_cores)
+
+download <- parallel::clusterApply(cl, all_ISO_codes_admin_1, dwnl.gadm.shp)
+
+parallel::stopCluster(cl)
