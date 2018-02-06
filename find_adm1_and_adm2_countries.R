24ecb58613cf162866c2fb8b75f612c60f2dcda0
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-02-06 15:53:02 +0000

    Style edits

--- find_adm1_and_adm2_countries.R
+++ find_adm1_and_adm2_countries.R
@@ -1,57 +1,61 @@
-# Find countries in adm1 and adm2 GADM shapefiles 
+#### Find countries in adm1 and adm2 GADM shapefiles ####
  
 # load packages
 library(rgdal)
 
-# load functions
-source(file.path("R", "prepare_datasets", "download_gadm_shp.R"))
-
 # load data 
 adm0_countries <- readOGR(dsn = file.path("data", "shapefiles", "gadm28_levels.shp"), 
-                          file = "gadm28_adm0")
+                          layer = "gadm28_adm0",
+                          stringsAsFactors = FALSE)
 
 adm1_countries <- readOGR(dsn = file.path("data", "shapefiles", "gadm28_levels.shp"), 
-                          file = "gadm28_adm1")
+                          layer = "gadm28_adm1",
+                          stringsAsFactors = FALSE)
 
 adm2_countries <- readOGR(dsn = file.path("data", "shapefiles", "gadm28_levels.shp"), 
-                          file = "gadm28_adm2")
+                         layer = "gadm28_adm2",
+                         stringsAsFactors = FALSE)
 
-all_countries_in_adm0_countries_duplicated <- adm0_countries@data[,c("ISO", "NAME_ENGLI")]
-all_countries_in_adm0_countries <- all_countries_in_adm0_countries_duplicated[!duplicated(all_countries_in_adm0_countries_duplicated),]
-all_ISO_codes_admin_0 <- as.character(all_countries_in_adm0_countries$ISO)
+adm0_iso_name_duplicated <- adm0_countries@data[, c("ISO", "NAME_ENGLI")]
+adm0_iso_name <- adm0_iso_name_duplicated[!duplicated(adm0_iso_name_duplicated), ]
+adm0_iso <- adm0_iso_name$ISO
 
-all_countries_in_adm1_countries_duplicated <- adm1_countries@data[,c("ISO", "NAME_0")] 
-all_countries_in_adm1_countries <- all_countries_in_adm1_countries_duplicated[!duplicated(all_countries_in_adm1_countries_duplicated),]
-all_ISO_codes_admin_1 <- as.character(all_countries_in_adm1_countries$ISO)
+adm1_iso_name_duplicated <- adm1_countries@data[, c("ISO", "NAME_0")] 
+adm1_iso_name <- adm1_iso_name_duplicated[!duplicated(adm1_iso_name_duplicated), ]
+adm1_iso <- adm1_iso_name$ISO
 
-all_countries_in_adm2_countries_duplicated <- adm2_countries@data[,c("ISO", "NAME_0")] 
-all_countries_in_adm2_countries <- all_countries_in_adm2_countries_duplicated[!duplicated(all_countries_in_adm2_countries_duplicated),]
-all_ISO_codes_admin_2 <- as.character(all_countries_in_adm2_countries$ISO)
+adm2_iso_name_duplicated <- adm2_countries@data[, c("ISO", "NAME_0")] 
+adm2_iso_name <- adm2_iso_name_duplicated[!duplicated(adm2_iso_name_duplicated), ]
+adm2_iso <- adm2_iso_name$ISO
 
-admin_1_in_admin_0.logical <- all_ISO_codes_admin_0 %in% all_ISO_codes_admin_1 
-admin_2_in_admin_1.logical <- all_ISO_codes_admin_1 %in% all_ISO_codes_admin_2 
+adm1_in_adm0.logical <- adm0_iso %in% adm1_iso
+adm2_in_adm1.logical <- adm1_iso %in% adm2_iso
 
-missing_countries.admin_1 <- all_countries_in_adm0_countries[!admin_1_in_admin_0.logical,]
-missing_countries.admin_2 <- all_countries_in_adm1_countries[!admin_2_in_admin_1.logical,]
+missing_countries_adm1 <- adm0_iso_name[!adm1_in_adm0.logical, ]
+missing_countries_adm2 <- adm1_iso_name[!adm2_in_adm1.logical, ]
 
 # Change column names 
-colnames(all_countries_in_adm1_countries) <- c("country_code", "country")
-colnames(all_countries_in_adm2_countries) <- c("country_code", "country")
-colnames(missing_countries.admin_1) <- c("country_code", "country")
-colnames(missing_countries.admin_2) <- c("country_code", "country")
+colnames(adm1_iso_name) <- c("country_code", "country")
+colnames(adm2_iso_name) <- c("country_code", "country")
+colnames(missing_countries_adm1) <- c("country_code", "country")
+colnames(missing_countries_adm2) <- c("country_code", "country")
   
 # write out list of countries which have admin 1 GADM boundaries  
-write.table(all_countries_in_adm1_countries[order(all_countries_in_adm1_countries$country),], 
-            file.path("output", "datasets", "all_adm_1_countries.csv"), row.names = FALSE, sep=",")
+write.csv(adm1_iso_name[order(adm1_iso_name$country), ], 
+          file.path("output", "datasets", "all_adm_1_countries.csv"), 
+          row.names = FALSE)
 
 # write out list of countries which have admin 2 GADM boundaries  
-write.table(all_countries_in_adm2_countries[order(all_countries_in_adm2_countries$country),], 
-            file.path("output", "datasets", "all_adm_2_countries.csv"), row.names = FALSE, sep=",")
+write.csv(adm2_iso_name[order(adm2_iso_name$country), ], 
+          file.path("output", "datasets", "all_adm_2_countries.csv"), 
+          row.names = FALSE)
 
 # write out list of countries which DO NOT have admin 1 boundaries 
-write.table(missing_countries.admin_1[order(missing_countries.admin_1$country),], 
-            file.path("output", "datasets", "missing_countries_adm_1.csv"), row.names = FALSE, sep=",")
+write.csv(missing_countries_adm1[order(missing_countries_adm1$country), ], 
+          file.path("output", "datasets", "missing_countries_adm_1.csv"), 
+          row.names = FALSE)
 
 # write out list of countries which DO NOT have admin 2 boundaries 
-write.table(missing_countries.admin_2[order(missing_countries.admin_2$country),], 
-            file.path("output", "datasets", "missing_countries_adm_2.csv"), row.names = FALSE, sep=",")
+write.csv(missing_countries_adm2[order(missing_countries_adm2$country), ], 
+          file.path("output", "datasets", "missing_countries_adm_2.csv"), 
+          row.names = FALSE)
