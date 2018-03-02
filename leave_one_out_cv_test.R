ae50bfbe49706b33dd83e7e37032eb78eff3fd24
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-03-02 12:54:44 +0000

    Fixed plot sizes

--- leave_one_out_cv_test.R
+++ leave_one_out_cv_test.R
@@ -75,7 +75,7 @@ p <- ggplot(new_foi_data, aes(distance)) +
   geom_histogram(binwidth = 40) +
   scale_x_continuous("distance (km)")
 
-ggsave(file.path("figures", "closest_distance_hist.png"), p)
+ggsave(file.path("figures", "closest_distance_hist.png"), p, width = 15, height = 8, units = "cm")
 
 
 # fit -------------------------------------------------------------------------
@@ -139,4 +139,4 @@ p2 <- ggplot(output, aes(k, V1)) +
   scale_x_continuous("distance class") +
   scale_y_continuous("correlation coefficient")
 
-ggsave(file.path("figures", "correlation_vs_distance_class.png"), p2)
+ggsave(file.path("figures", "correlation_vs_distance_class.png"), p2, width = 15, height = 8, units = "cm")
