c8f8ed385d38048cf4393ec45c3406ed585c40da
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-03-05 09:44:04 +0000

    Style edits

--- R/prepare_datasets/functions_for_geolocating_admin_units.r
+++ R/prepare_datasets/functions_for_geolocating_admin_units.r
@@ -1,3 +1,12 @@
+calculate_viewport_area <- function(x) {
+  
+  areaPolygon(rbind(c(x["point_1_x"], x["point_1_y"]),
+                    c(x["point_2_x"], x["point_2_y"]),
+                    c(x["point_3_x"], x["point_3_y"]),
+                    c(x["point_4_x"], x["point_4_y"])))
+
+}
+
 get_geocode_results <- function(geocode_run){
   
   number_of_matches <- length(geocode_run$results)
@@ -23,12 +32,12 @@ get_geocode_results <- function(geocode_run){
   all_results
 }
 
-get_admin_name <- function(x, country_code_fld){
+get_admin_info <- function(x, info_names){
   
   country_name <- x$country
   #cat("country = ", country_name, "\n")
   
-  country_code <- x[[country_code_fld]]  
+  country_code <- x$ISO
   #cat("country code = ", country_code, "\n")
   
   #admin_level <- as.numeric(x["admin"]) 
@@ -48,47 +57,52 @@ get_admin_name <- function(x, country_code_fld){
   
   if(geocode_run$status!="OK") {stop("no match!")}
     
-  if(length(geocode_run$results)>1)
-  {
+  if(length(geocode_run$results)>1) {
+    
     geocode_results <- get_geocode_results (geocode_run)
   
-    viewport_areas <- apply(geocode_results, 1, function(x) {areaPolygon(rbind(c(x["point_1_x"], x["point_1_y"]),
-                                                                               c(x["point_2_x"], x["point_2_y"]),
-                                                                               c(x["point_3_x"], x["point_3_y"]),
-                                                                               c(x["point_4_x"], x["point_4_y"])))})
+    viewport_areas <- apply(geocode_results, 1, calculate_viewport_area)
+    
     result_match <- which(viewport_areas==max(viewport_areas))[1]
+    
     location_xy <- geocode_results[result_match, c("lng","lat")]
+    
     #cat("coordinates = ", unlist(location_xy), "\n")
-  }else{
-    location_xy <- data.frame(lng=geocode_run$results[[1]]$geometry$location$lng,
-                              lat=geocode_run$results[[1]]$geometry$location$lat)
+  
+    } else {
+    
+    location_xy <- data.frame(lng = geocode_run$results[[1]]$geometry$location$lng,
+                              lat = geocode_run$results[[1]]$geometry$location$lat)
     #cat("coordinates = ", unlist(location_xy), "\n")
-  }
+    }
   
   shp_folder <- paste(country_code, "adm_shp", sep="_")
-  shp_name <- sprintf ("%s_adm%s%s", country_code, admin_level, ".shp") 
-  shp <- readShapePoly(file.path("data", 
-                                 "shapefiles", 
-                                 shp_folder, 
-                                 shp_name))
+  
+  shp_name <- sprintf ("%s_adm%s", country_code, admin_level) 
+  
+  shp <- readOGR(file.path("data", "shapefiles", shp_folder), 
+                 shp_name,
+                 stringsAsFactors = FALSE,
+                 integer64 = "allow.loss")
   
   
   # map overlay to find admin unit name using spatial coordinates ------------- 
   
   
-  overlay <- over(SpatialPoints(location_xy),shp)
+  xy_spdf <- SpatialPoints(location_xy, proj4string = shp@proj4string)
   
-  admin_name <- as.character(overlay$NAME_1)
+  overlay <- over(xy_spdf, shp)
+  
+  #admin_name <- as.character(overlay$NAME_1)
   #cat("admin 1 name = ", admin_name, "\n")
   
   country_numeric_code <- overlay$ID_0
   adm_numeric_code <- overlay$ID_1
   
-  output <- c()
-  output[1] <- admin_name
-  output[2] <- as.numeric(location_xy["lng"])
-  output[3] <- as.numeric(location_xy["lat"])
-  output[4] <- country_numeric_code
-  output[5] <- adm_numeric_code
-  output
+  setNames(c(location_xy$lat,
+             location_xy$lng,
+             country_numeric_code,
+             adm_numeric_code),
+           info_names)
+  
 }
