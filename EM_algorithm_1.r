9137a2256eead3c3924e3a4af1223f59b9ad30e3
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-03-09 15:03:09 +0000

    Run with new dataset (+ 10 points)

--- EM_algorithm_1.r
+++ EM_algorithm_1.r
@@ -20,19 +20,23 @@ ctx <- context::context_save(path = "context",
                              sources = my_resources)
 
 
-# ---------------------------------------- define parameters
+# define parameters ----------------------------------------------------------- 
 
 
-in_pt <- file.path("data", "env_variables", "all_sets_gadm_codes")
+resample_grid_size <- 20
+  
+in_pt <- file.path("output", "env_variables", "all_sets_gadm_codes")
 
 group_fields <- c("data_id", "ADM_0", "ADM_1")
 
-gr_size <- 20
-  
-new_res <- (1 / 120) * gr_size
+
+# define variables ------------------------------------------------------------
 
 
-# ---------------------------------------- are you using the cluster? 
+new_res <- (1 / 120) * resample_grid_size
+
+
+# are you using the cluster? -------------------------------------------------- 
 
 
 if (CLUSTER) {
@@ -46,23 +50,22 @@ if (CLUSTER) {
 }
 
 
-# ---------------------------------------- load data
+# load data ------------------------------------------------------------------- 
 
-
-predictor_rank <- read.csv(
-  file.path("output", 
-            "variable_selection", 
-            "metropolis_hastings", 
-            "exp_1", 
-            "variable_rank_final_fits_exp_1.csv"),
-  stringsAsFactors = FALSE)
 
 foi_data <- read.csv(
   file.path("output", "foi", "All_FOI_estimates_linear_env_var_area.csv"),
   stringsAsFactors = FALSE)
 
+predictor_rank <- read.csv(file.path("output", 
+                                     "variable_selection", 
+                                     "metropolis_hastings", 
+                                     "exp_1", 
+                                     "variable_rank_final_fits_exp_1.csv"),
+                           stringsAsFactors = FALSE)
 
-# ---------------------------------------- pre processing
+
+# pre processing -------------------------------------------------------------- 
 
 
 names(foi_data)[names(foi_data) == "ID_0"] <- "ADM_0"
@@ -71,14 +74,10 @@ names(foi_data)[names(foi_data) == "population"] <- "adm_pop"
 
 my_predictors <- predictor_rank$variable[1:9]
 
-my_predictors <- c(my_predictors, "RFE_const_term")
-
-fi <- list.files(in_pt, 
-                 pattern = "^tile",
-                 full.names = TRUE)
+fi <- list.files(in_pt, pattern = "^tile", full.names = TRUE)
 
 
-# ---------------------------------------- submit one test job
+# submit one test job --------------------------------------------------------- 
 
 
 # t <- obj$enqueue(
@@ -90,7 +89,7 @@ fi <- list.files(in_pt,
 #     grid_size = new_res))
 
 
-# ---------------------------------------- submit all jobs
+# submit all jobs ------------------------------------------------------------- 
 
 
 if (CLUSTER) {
@@ -107,7 +106,7 @@ if (CLUSTER) {
 } else {
 
   pxl_job <- lapply(
-    fi[159],
+    fi,
     filter_and_resample,
     foi_dts = foi_data,
     env_var_names = my_predictors,
