37c7c4c8f1fbff4139ba12d69f158a4016390c2c
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-03-16 09:19:32 +0000

    Works fine

--- stepwise_RF_variable_addition_LocalRun.R
+++ stepwise_RF_variable_addition_LocalRun.R
@@ -1,64 +1,150 @@
-rm(list=ls())
 
-# load packages
-library(ranger)
-library(weights)
-library(ggplot2)
-library(reshape2)
-library(parallel)
+options(didehpc.cluster = "fi--didemrchnb")
 
-# load functions 
-source(file.path("R", "random_forest", "spatial_sampK_cv_rng2_stratif_pred.R"))
-source(file.path("R", "random_forest", "RF_variable_importance_test", "RF_variable_importance_test_common_functions.R"))
-source(file.path("R", "random_forest", "RF_variable_importance_test", "multi_steps_wrapper_LocalParallel.R"))
-source(file.path("R", "random_forest", "RF_variable_importance_test", "stepwise_RF_variable_addition", "stepwise_RF_variable_addition_functions.R"))
-source(file.path("R", "random_forest", "convert_df_to_list.R"))
+CLUSTER <- TRUE
 
-# load data
-dengue_dataset <- read.csv(file.path("data", "foi", "All_FOI_estimates_env_var.csv")) 
+my_resources <- c(
+  file.path("R", "random_forest", "functions_for_fitting_h2o_RF_and_making_predictions.r"),
+  file.path("R", "random_forest", "stepwise_variable_addition_removal.R"),
+  file.path("R", "prepare_datasets", "set_pseudo_abs_weights.R"),
+  file.path("R", "utility_functions.R"))
 
-# remove NA and outliers
-dengue_dataset <- dengue_dataset[!is.na(dengue_dataset$FOI), ]
-dengue_dataset <- subset(dengue_dataset, country != "French Polynesia" & country != "Haiti")
+my_pkgs <- "h2o"
 
-# Get all the combinations of model predictors you want to try, land use classes excluded ---------------------------------------
+context::context_log_start()
+ctx <- context::context_save(path = "context",
+                             sources = my_resources,
+                             packages = my_pkgs)
+
+
+# define parameters ----------------------------------------------------------- 
+
+
+parameters <- list(
+  grid_size = 1,
+  no_trees = 500,
+  min_node_size = 20,
+  no_steps_L1 = 20,   # 20
+  no_steps_L2 = 10,   # 10
+  pseudoAbs.value = -0.02,
+  all_wgt = 1,
+  wgt_limits = c(1, 500),
+  no_reps = 10)       # 10
+
+no_fits <- 50
+
+var_to_fit <- "FOI"
 
 altitude_var_names <- "altitude"
+
 fourier_transform_elements <- c("const_term",	"Re0",	"Im0",	"Re1",	"Im1")
+
 FTs_data_names <- c("DayTemp", "EVI", "MIR", "NightTemp", "RFE")
-all_predictors <- c(altitude_var_names, 
-                    apply(expand.grid(fourier_transform_elements, FTs_data_names), 1, function(x) paste(x[2],x[1], sep="_")))
 
-predictor_combination_list <- list(
-  all_predictors = all_predictors
-)
+out_path <- file.path("output", 
+                      "variable_selection", 
+                      "stepwise",
+                      "addition")
 
-# Get all combinations of factors -------------------------------------------------
+                      
+# define variables ------------------------------------------------------------
 
-factor_combinations <- expand.grid(ID.exp = 6, 
-                                   grid.size = c(1,5,10), 
-                                   cell.fraction = 0.7, 
-                                   train.fraction = 1, 
-                                   pseudoAbs.value = -0.02,
-                                   weights = 1,
-                                   adm = 1,
-                                   pred_tag = "all_predictors",
-                                   replicates = 1:10)
 
-factor_combinations <- cbind(ID.run = seq_len(nrow(factor_combinations)), factor_combinations)
+my_dir <- paste0("grid_size_", parameters$grid_size)
 
-# Fitting RF models ----------------------------------------------------------
 
-factor_combinations_list <- df_to_list (x = factor_combinations, use_names = TRUE)
+# are you using the cluster? -------------------------------------------------- 
 
-# run 1 list element 
-test <- lapply(factor_combinations_list[1], 
-               multi_grid_sizes_wrapper.addition, 
-               dataset = dengue_dataset, 
-               all_predictor_combs = predictor_combination_list, 
-               factor_combs_df = factor_combinations)
 
-# Check intermediate outputs
-readRDS(file.path("output", "dengue_dataset", "predictor_importance_test", 
-                  "addition", "exp_6", "run_1", 
-                  "predictor_importance_test_step_output_exp_6_run_1_level_1_step_1.rds"))
+if (CLUSTER) {
+  
+  config <- didehpc::didehpc_config(template = "20Core")
+  obj <- didehpc::queue_didehpc(ctx, config = config)
+  
+} else {
+  
+  context::context_load(ctx)
+  context::parallel_cluster_start(8, ctx)
+  
+}
+
+
+# load data -------------------------------------------------------------------
+
+
+foi_data <- read.csv(
+  file.path("output", "foi", "All_FOI_estimates_linear_env_var_area.csv"),
+  stringsAsFactors = FALSE) 
+
+boot_samples <- readRDS(file.path("output", 
+                                  "EM_algorithm",
+                                  "bootstrap_models",
+                                  my_dir, 
+                                  "bootstrap_samples.rds"))
+
+
+# pre process -----------------------------------------------------------------
+
+
+all_FT_names <- apply(expand.grid(fourier_transform_elements, FTs_data_names), 
+                      1, 
+                      function(x) paste(x[2], x[1], sep="_"))
+
+all_predictors <- c(altitude_var_names, all_FT_names)
+
+foi_data[foi_data$type == "pseudoAbsence", var_to_fit] <- parameters$pseudoAbs.value
+
+foi_data$new_weight <- parameters$all_wgt
+
+pAbs_wgt <- get_area_scaled_wgts(foi_data, parameters$wgt_limits)
+
+foi_data[foi_data$type == "pseudoAbsence", "new_weight"] <- pAbs_wgt
+
+
+# submit one test job ---------------------------------------------------------
+
+
+# t <- obj$enqueue(
+#   stepwise_addition_boot(
+#     seq_len(no_fits)[1],
+#     boot_ls = boot_samples,
+#     y_var = var_to_fit,
+#     parms = parameters,
+#     predictors = all_predictors,
+#     foi_data = foi_data,
+#     out_path = out_path))
+
+
+# submit all jobs -------------------------------------------------------------
+
+
+if (CLUSTER) {
+
+  bsample_step_addition <- queuer::qlapply(
+    seq_len(no_fits),
+    stepwise_addition_boot,
+    obj,
+    boot_ls = boot_samples,
+    y_var = var_to_fit,
+    parms = parameters,
+    predictors = all_predictors,
+    foi_data = foi_data,
+    out_path = out_path)
+
+} else {
+
+  bsample_step_addition <- lapply(
+    seq_len(no_fits)[1],
+    stepwise_addition_boot,
+    boot_ls = boot_samples,
+    y_var = var_to_fit,
+    parms = parameters,
+    predictors = all_predictors,
+    foi_data = foi_data,
+    out_path = out_path)
+
+}
+
+if (!CLUSTER) {
+  context::parallel_cluster_stop()
+}
