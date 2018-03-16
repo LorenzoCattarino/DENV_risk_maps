56ae09d87a7a2282f90aade9893db7e274527442
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-03-16 09:17:22 +0000

    Added function for running a rf model for each predictor set

--- R/random_forest/functions_for_fitting_h2o_RF_and_making_predictions.r
+++ R/random_forest/functions_for_fitting_h2o_RF_and_making_predictions.r
@@ -109,6 +109,56 @@ get_boot_sample_and_fit_RF <- function(i,
   
 }
 
+get_set_fit_predict_and_error <- function(i,
+                                          predictor_sets,
+                                          dataset, 
+                                          y_var, 
+                                          no_trees, 
+                                          min_node_size,
+                                          foi_data) {
+  
+  x <- predictor_sets[[i]]
+  
+  cat("combination of predictors =", x, "\n") 
+  
+  my_preds <- names(dataset)[x]
+  
+  train_set <- dataset[, c(y_var, my_preds, "new_weight")]
+  
+  RF_obj <- fit_h2o_RF(dependent_variable = y_var, 
+                       predictors = my_preds, 
+                       training_dataset = train_set, 
+                       no_trees = no_trees, 
+                       min_node_size = min_node_size,
+                       my_weights = "new_weight")
+  
+  p_i <- make_h2o_predictions(mod_obj = RF_obj, 
+                              dataset = foi_data, 
+                              sel_preds = my_preds)
+  
+  all_points <- foi_data$data_id
+  
+  train_points <- dataset$data_id
+  
+  unique_train_points <- unique(train_points)
+  
+  valid_points <- all_points[!all_points %in% unique_train_points]
+  
+  y.data <- foi_data$FOI 
+    
+  my_weights <- foi_data$new_weight
+  
+  p_i[p_i < 0] <- 0
+  
+  y.data[y.data < 0] <- 0
+  
+  rmse.train <- sqrt(weighted.mean((y.data[valid_points] - p_i[valid_points])^2, my_weights[valid_points]))
+  rmse.valid <- sqrt(weighted.mean((y.data[train_points] - p_i[train_points])^2, my_weights[train_points]))
+  
+  c(rmse.train = rmse.train, rmse.valid = rmse.valid)
+
+}
+
 load_predict_and_save <- function(i, 
                                   RF_obj_path, 
                                   my_preds, 
