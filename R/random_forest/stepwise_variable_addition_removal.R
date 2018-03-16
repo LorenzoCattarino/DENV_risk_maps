694c9fef1e2b79d075e879851178313bc5c06892
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-03-16 09:18:03 +0000

    Works fine

--- R/random_forest/stepwise_variable_addition_removal.R
+++ R/random_forest/stepwise_variable_addition_removal.R
@@ -5,79 +5,84 @@
 stepwise_addition_boot <- function(i, 
                                    boot_ls, 
                                    y_var, 
-                                   psAb_val, 
-                                   all_wgt, 
-                                   wgt_limits, 
+                                   parms, 
                                    predictors, 
-                                   no_steps_L1,
-                                   no_steps_L2,
-                                   no_trees,
-                                   min_node_size,
                                    foi_data,
-                                   start_h2o,
-                                   shut_h2o,
                                    out_path){
   
-  ID_run <- i
+  stepwise_addition <- function(j){
+    
+    ID_run <- j
+      
+    no_steps_L1 <- parms$no_steps_L1
+    no_steps_L2 <- parms$no_steps_L2
+    no_trees <- parms$no_trees
+    min_node_size <- parms$min_node_size
+    
+    ret_level_1 <- multi_steps_wrapper(dataset = adm_dts_boot, 
+                                       predictors = predictors, 
+                                       no_steps = no_steps_L1, 
+                                       level_num = 1,
+                                       addition = TRUE,
+                                       y_var = y_var, 
+                                       no_trees = no_trees, 
+                                       min_node_size = min_node_size, 
+                                       foi_data = foi_data,
+                                       ID_sample = ID_sample,
+                                       ID_run = ID_run,
+                                       out_path = out_path)
+    
+    subset_of_predictors <- ret_level_1$name
+    
+    ret_level_2 <- multi_steps_wrapper(dataset = adm_dts_boot, 
+                                       predictors = subset_of_predictors, 
+                                       no_steps = no_steps_L2, 
+                                       level_num = 2,
+                                       addition = TRUE,
+                                       y_var = y_var, 
+                                       no_trees = no_trees, 
+                                       min_node_size = min_node_size, 
+                                       foi_data = foi_data,
+                                       ID_sample = ID_sample,
+                                       ID_run = ID_run,
+                                       out_path = out_path)
+    
+    # my_out_path <- file.path(out_path, 
+    #                          paste("sample", ID_sample, sep="_"), 
+    #                          paste("run", ID_run, sep="_"))
+    # 
+    # df_name_level_1 <- sprintf("all_steps_output_%s%s", 
+    #                            paste0("level", 1, sep="_"), 
+    #                            ".csv")  
+    # 
+    # write_out_csv(ret_level_1, my_out_path, df_name_level_1)
+    # 
+    # df_name_level_2 <- sprintf("all_steps_output_%s%s", 
+    #                            paste0("level", 2, sep="_"), 
+    #                            ".csv")
+    # 
+    # write_out_csv(ret_level_2, my_out_path, df_name_level_2)
+    
+    list(ret_level_1, ret_level_2)
+    
+  }
   
-  adm_dts_boot <- boot_ls[[ID_run]]
+  psAb_val <- parms$pseudoAbs.value
+  no_reps <- parms$no_reps
+    
+  ID_sample <- i
+  
+  adm_dts_boot <- boot_ls[[ID_sample]]
   
   adm_dts_boot[adm_dts_boot$type == "pseudoAbsence", y_var] <- psAb_val
   
-  adm_dts_boot$new_weight <- all_wgt
-  pAbs_wgt <- get_area_scaled_wgts(adm_dts_boot, wgt_limits)
-  adm_dts_boot[adm_dts_boot$type == "pseudoAbsence", "new_weight"] <- pAbs_wgt
+  h2o.init(max_mem_size = "20G")
   
-  ret_level_1 <- multi_steps_wrapper(dataset = adm_dts_boot, 
-                                     predictors = predictors, 
-                                     no_steps = no_steps_L1, 
-                                     level_num = 1,
-                                     addition = TRUE,
-                                     y_var = y_var, 
-                                     no_trees = no_trees, 
-                                     min_node_size = min_node_size, 
-                                     foi_data = foi_data,
-                                     start_h2o = start_h2o,
-                                     shut_h2o = shut_h2o, 
-                                     ID_run = ID_run,
-                                     out_path = out_path)
+  out <- lapply(seq_len(no_reps), stepwise_addition)
   
-  df_name_level_1 <- sprintf("all_steps_output_%s%s", 
-                             paste0("level", 1, sep="_"), 
-                             ".csv")  
+  h2o.shutdown(prompt = FALSE)
   
-  my_out_path <- file.path(out_pth, paste("run", ID_run, sep="_"))
-  
-  
-  write_out_csv(ret_level_1, my_out_path, df_name_level_1)
-  
-  subset_of_predictors <- ret_level_1$name
-  
-  ret_level_2 <- multi_steps_wrapper(dataset = adm_dts_boot, 
-                                     predictors = subset_of_predictors, 
-                                     no_steps = no_steps_L2, 
-                                     level_num = 2,
-                                     addition = TRUE,
-                                     y_var = y_var, 
-                                     no_trees = no_trees, 
-                                     min_node_size = min_node_size, 
-                                     foi_data = foi_data,
-                                     start_h2o = start_h2o,
-                                     shut_h2o = shut_h2o, 
-                                     ID_run = ID_run,
-                                     out_path = out_path)
-  
-  df_name_level_2 <- sprintf("all_steps_output_%s%s", 
-                             paste0("level", 2, sep="_"), 
-                             ".csv")
-  
-  write_out_csv(ret_level_1, my_out_path, df_name_level_2)
-  
-  fact_combs_df_name <- "factor_combinations.csv"
-  
-  write_out_csv(factor_combs_df, out_path, fact_combs_df_name)
-  
-  list(ret_level_1, ret_level_2)
+  out
   
 }
 
@@ -90,10 +95,11 @@ multi_steps_wrapper <- function(dataset,
                                 no_trees, 
                                 min_node_size, 
                                 foi_data,
-                                start_h2o,
-                                shut_h2o, 
-                                ID_run, 
-                                out_pth){
+                                ID_sample, 
+                                ID_run,
+                                out_path){
+  
+  #browser()
   
   # Transform character to numeric
   vector_of_predictors <- which(names(dataset) %in% predictors)
@@ -106,7 +112,7 @@ multi_steps_wrapper <- function(dataset,
   
   }
   
-  cat("level of stepwise addition = ", level_num, "\n")
+  cat("level of stepwise addition =", level_num, "\n")
   
   # create empty vectors to store selected predictors and corr coeff value 
   changed_predictor <- NULL
@@ -120,7 +126,7 @@ multi_steps_wrapper <- function(dataset,
   
   for (i in seq_len(no_steps)) {
     
-    cat("step of change = ", i, "\n")
+    cat("step of change =", i, "\n")
     
     # create combination of predictors by removing one predictor at the time from the vector of selected predictors
     
@@ -146,25 +152,23 @@ multi_steps_wrapper <- function(dataset,
       combinations_of_predictors <- lapply(as.list(as.numeric(variables_to_change)), 
                                            get_combs_fun, 
                                            vector_of_predictors)
-    
+      
     }
     
-    predictor_importance_test_output_local <- parallel::clusterApply(NULL, 
-                                                                     combinations_of_predictors, 
-                                                                     predictor_combs_wrapper, 
-                                                                     dataset = dataset, 
-                                                                     y_var = y_var, 
-                                                                     no_trees = no_trees, 
-                                                                     min_node_size = min_node_size, 
-                                                                     foi_data = foi_data,
-                                                                     start_h2o - start_h2o,
-                                                                     shut_h2o - shut_h2o)
+    ret <- lapply(seq_along(combinations_of_predictors), 
+                  get_set_fit_predict_and_error, 
+                  predictor_sets = combinations_of_predictors,
+                  dataset = dataset, 
+                  y_var = y_var, 
+                  no_trees = no_trees, 
+                  min_node_size = min_node_size, 
+                  foi_data = foi_data)
     
     # extract results 
-    diagnostics <- as.data.frame(do.call("rbind", lapply(predictor_importance_test_output_local, "[[", 1)))
+    diagnostics <- do.call("rbind", ret)
 
     # create matrix of all combinations of predictors used
-    combinations_of_predictors_mat <- do.call(rbind, combinations_of_predictors)
+    combinations_of_predictors_mat <- do.call("rbind", combinations_of_predictors)
     combinations_of_predictors_mat <- cbind(combinations_of_predictors_mat, 
                                             as.numeric(variables_to_change), 
                                             rep(0, nrow(combinations_of_predictors_mat)))
@@ -174,7 +178,7 @@ multi_steps_wrapper <- function(dataset,
                                                   "rmse_valid")
     
     # combine together run outputs and matrix of predictor combs 
-    combinations_of_predictors_mat[,"rmse_valid"] <- diagnostics$rmse.valid
+    combinations_of_predictors_mat[,"rmse_valid"] <- diagnostics[, "rmse.valid"]
     final_output_df <- as.data.frame(combinations_of_predictors_mat, stringsAsFactors = FALSE)
     
     # sort df by rmse value
@@ -182,11 +186,11 @@ multi_steps_wrapper <- function(dataset,
     
     # get column index in dataset of predictor which contributed to the smallest increase in rmse 
     most_important_predictor_col_index <- final_output_df_sorted[1,"changed_predictor"]
-    cat("index of changed predictor = ", most_important_predictor_col_index, "\n")
+    cat("index of changed predictor =", most_important_predictor_col_index, "\n")
     
     # get name of predictor which contributed to the smallest rmse
     most_important_changed_predictor_name <- names(dataset)[most_important_predictor_col_index]
-    cat("most important changed predictor name = ", most_important_changed_predictor_name, "\n")
+    cat("most important changed predictor name =", most_important_changed_predictor_name, "\n")
     
     if (addition) {
       NULL
@@ -203,16 +207,19 @@ multi_steps_wrapper <- function(dataset,
     
     
     # get name and save df
-    df_name_ext <- sprintf("per_step_output_%s_%s_%s%s", 
+    df_name_ext <- sprintf("per_step_output_%s_%s%s", 
                            paste("level", level_num, sep="_"), 
                            paste("step", i, sep="_"), 
                            ".rds")
     
-    my_out_path <- file.path(out_pth, paste("run", ID_run, sep="_"))
+    my_out_path <- file.path(out_path, 
+                             paste("sample", ID_sample, sep="_"), 
+                             paste("run", ID_run, sep="_"))
     
     write_out_rds(final_output_df_sorted, my_out_path, df_name_ext)
-    
   
+    h2o.removeAll()
+    
   }
   
   data.frame(Step = seq_len(no_steps), 
@@ -220,28 +227,4 @@ multi_steps_wrapper <- function(dataset,
              name = names(dataset)[changed_predictor], 
              rmse_valid = changed_predictor_rmse)
 
-}
-
-predictor_combs_wrapper <- function(x, 
-                                    dataset, 
-                                    y_var, 
-                                    no_trees, 
-                                    min_node_size, 
-                                    psAb_val, 
-                                    all_wgt, 
-                                    wgt_limits,
-                                    foi_data,
-                                    start_h2o,
-                                    shut_h2o){
-  
-  cat("combination of predictors = ", x, "\n")
-  
-  get_bsam_fit_predict_and_error(boot_data = dataset, 
-                                 y_var = y_var, 
-                                 my_preds = x, 
-                                 no_trees = no_trees, 
-                                 min_node_size = min_node_size, 
-                                 foi_data = foi_data,
-                                 start_h2o = start_h2o,
-                                 shut_h2o = shut_h2o)
 }
