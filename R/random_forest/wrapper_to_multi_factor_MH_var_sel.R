b7d384a7d6243d365a4e0c9bb80d1a7bd9d83f08
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-03-21 08:49:23 +0000

    Major changes to accomodate new cv framework and h2o

--- R/random_forest/wrapper_to_multi_factor_MH_var_sel.R
+++ R/random_forest/wrapper_to_multi_factor_MH_var_sel.R
@@ -1,76 +1,75 @@
-wrapper_to_multi_factor_MH_var_sel <- function(
-  x, model_dataset, predictors, dependent_variable, 
-  no_trees, min_node_size, grid_size, pseudoAbs_value, 
-  Niter, var_scale){
+MH_variable_selection_boot <- function(i, 
+                                       boot_ls,
+                                       foi_data, 
+                                       predictors, 
+                                       dependent_variable, 
+                                       parms,
+                                       out_fig_pth,
+                                       out_tab_pth){
   
   
-  # ---------------------------------------- get parameters
+  # ---------------------------------------- define parameters
   
   
-  exp_id <- x$exp_ID
-  cat("exp ID =", exp_id, "\n")
+  #browser()
   
-  run_id <- x$run_ID
-  cat("run ID =", run_id, "\n")
+  no_trees <- parms$no_trees 
+  min_node_size <- parms$min_node_size 
+  grid_size <- parms$grid_size
+  psAb_val <- parms$pseudoAbs_value
+  Niter <- parms$it
+  scaling_factor <- parms$scaling_factor
+  var_scale <- parms$var_scale
   
-  scaling_factor <- x$scaling_factor
-  cat("scaling factor =", scaling_factor, "\n")
-  
-
-  # ---------------------------------------- define parameters 
-
-  
-  diagnostic_nms <- c(
-    "iter", 
-    "cur.OF_before", 
-    "next.OF", 
-    "rnd_number", 
-    "MH_prob", 
-    "cur.OF_after", 
-    "accept", 
-    "cur.n.sel")
+  diagnostic_nms <- c("iter", 
+                      "cur.OF_before", 
+                      "next.OF", 
+                      "rnd_number", 
+                      "MH_prob", 
+                      "cur.OF_after", 
+                      "accept", 
+                      "cur.n.sel")
   
   
   # ---------------------------------------- create empty objects 
   
   
+  ID_sample <- i
+  
+  adm_dts_boot <- boot_ls[[ID_sample]]
+  
+  adm_dts_boot[adm_dts_boot$type == "pseudoAbsence", dependent_variable] <- psAb_val
+  
+  h2o.init(max_mem_size = "20G")
+  
   n.vars <- length(predictors)
   
-  tracker <- setNames(matrix(0, nrow = Niter, ncol = length(diagnostic_nms) + n.vars), 
-                      nm = c(diagnostic_nms, predictors))
+  tracker <- matrix(0, nrow = Niter, ncol = length(diagnostic_nms) + n.vars)
+  colnames(tracker) <- 1:dim(tracker)[2]
+  colnames(tracker) <- c(diagnostic_nms, predictors)
   
-
-  # ---------------------------------------- pre processing
-  
-  
-  no_data <- nrow(model_dataset)
-  
-  # find pseudo absences
-  pseudo_abs_logical <- model_dataset$type == "pseudoAbsence"
-  
-  # set pseudo absence value 
-  model_dataset[model_dataset$type == "pseudoAbsence", dependent_variable] <- pseudoAbs_value
-
-  # get observations 
-  y.data <- model_dataset[!pseudo_abs_logical, dependent_variable]
-  
-  # get predictor values 
-  x.data <- model_dataset[!pseudo_abs_logical, predictors, drop = FALSE]
-  
-  # overlay squared grid on data points 
-  gridded_dataset <- grid_up(model_dataset, grid_size)
-  
-  # get the cells occupied with at least one data point
-  occupied_cells <- unique(gridded_dataset$cell)
-  
-  # do bootstrapping and get the full training dataset
-  training_dataset <- do_boostrap(gridded_dataset, occupied_cells)
-
-  # get the position (1/0) of the points in the validating dataset
-  valid_point_pos <- get_validating_point_positions(no_data, training_dataset)
-  
-  # remove pseudo absences
-  valid_point_pos <- valid_point_pos[!pseudo_abs_logical]   
+  # no_data <- nrow(model_dataset)
+  # 
+  # # get observations 
+  # y.data <- model_dataset[!pseudo_abs_logical, dependent_variable]
+  # 
+  # # get predictor values 
+  # x.data <- model_dataset[!pseudo_abs_logical, predictors, drop = FALSE]
+  # 
+  # # overlay squared grid on data points 
+  # gridded_dataset <- grid_up(model_dataset, grid_size)
+  # 
+  # # get the cells occupied with at least one data point
+  # occupied_cells <- unique(gridded_dataset$cell)
+  # 
+  # # do bootstrapping and get the full training dataset
+  # training_dataset <- do_boostrap(gridded_dataset, occupied_cells)
+  # 
+  # # get the position (1/0) of the points in the validating dataset
+  # valid_point_pos <- get_validating_point_positions(no_data, training_dataset)
+  # 
+  # # remove pseudo absences
+  # valid_point_pos <- valid_point_pos[!pseudo_abs_logical]   
   
   
   # -------------------------------------- first iteration
@@ -84,18 +83,15 @@ wrapper_to_multi_factor_MH_var_sel <- function(
   selected_predictors <- predictors[cur.var.list == 1]
   
   #run core routine
-  run_core <- spatial.cv.rf(
-    preds = selected_predictors, 
-    y_var = dependent_variable, 
-    train_set = training_dataset, 
-    no_trees = no_trees, 
-    min_node_size = min_node_size,
-    x_data = x.data, 
-    y_data = y.data, 
-    valid_points = valid_point_pos)
-  
+  run_core <- fit_predict_and_error(dataset = adm_dts_boot, 
+                                    y_var = dependent_variable, 
+                                    my_preds = predictors,
+                                    no_trees = no_trees, 
+                                    min_node_size = min_node_size,
+                                    foi_data = foi_data)
+
   # extract sum of squared errors
-  SS <- run_core[[3]]
+  SS <- run_core["rmse.valid"]
   
   cur.OF <- scaling_factor * SS
   
@@ -115,18 +111,15 @@ wrapper_to_multi_factor_MH_var_sel <- function(
     selected_predictors <- predictors[next.var.list == 1]
     
     # run core routine 
-    run_core <- spatial.cv.rf(
-      preds = selected_predictors, 
-      y_var = dependent_variable, 
-      train_set = training_dataset, 
-      no_trees = no_trees, 
-      min_node_size = min_node_size,
-      x_data = x.data, 
-      y_data = y.data, 
-      valid_points = valid_point_pos)
+    run_core <- fit_predict_and_error(dataset = adm_dts_boot, 
+                                      y_var = dependent_variable, 
+                                      my_preds = selected_predictors,
+                                      no_trees = no_trees, 
+                                      min_node_size = min_node_size,
+                                      foi_data = foi_data)
     
     # extract sum of squared errors
-    SS <- run_core[[3]]
+    SS <- run_core["rmse.valid"]
     
     next.OF <- scaling_factor * SS
     
@@ -155,12 +148,45 @@ wrapper_to_multi_factor_MH_var_sel <- function(
     
   }
   
-  # write out the chain 
-  write_out_rds (tracker, run_id, exp_id)
+  h2o.shutdown(prompt = FALSE)
+  
+  out_tab_name <- paste0("sample_", ID_sample, ".rds")
     
-  # save plot of chain and frequency distribution 
-  plot_MH_var_sel_outputs(tracker, run_id, exp_id)
+  out_fig_name <- paste0("sample_", ID_sample, ".pdf")
+    
+  write_out_rds(tracker, out_tab_pth, out_tab_name)
+    
+  my_path <- file.path(out_fig_pth, paste0("sample_", ID_sample))
+  
+  plot_MH_var_sel_outputs(tracker, my_path, out_fig_name)
   
   tracker
+  
+}
+
+plot_MH_var_sel_outputs <- function(data_to_plot, out_path, file_tag){
+  
+  if(!is.data.frame(data_to_plot)){
+    
+    data_to_plot <- as.data.frame(data_to_plot)
+    
+  }
+  
+  dir.create(out_path, FALSE, TRUE)
+  
+  # plot and save
+  p1 <- ggplot(
+    data_to_plot, aes(iter, cur.OF_after)) +
+    geom_line() +
+    scale_x_continuous("Iterations")
+  
+  p2 <- ggplot(
+    data_to_plot, aes(cur.OF_after)) +
+    geom_histogram() + 
+    scale_y_continuous("Frequency")
+  
+  p3 <- grid.arrange(p1, p2)
+  
+  ggsave(file.path(out_path, file_tag), plot = p3)
   
 }
