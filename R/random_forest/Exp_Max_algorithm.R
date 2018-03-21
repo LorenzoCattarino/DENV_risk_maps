0678e936bcccf5d75c5aadb93fc55c390ca98e17
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-03-21 09:03:05 +0000

    Added EM functions together

--- R/random_forest/Exp_Max_algorithm.R
+++ R/random_forest/Exp_Max_algorithm.R
@@ -28,7 +28,7 @@ exp_max_algorithm <- function(niter,
   colnames(out_mat) <- diagnostics
   
   h2o.init(ignore_config = TRUE)
-    
+  
   for (i in seq_len(niter)){
     
     #browser()
@@ -52,25 +52,25 @@ exp_max_algorithm <- function(niter,
     
     
     dd$wgt_prime <- dd$wgt_prime * dd$new_weight
-     
-
+    
+    
     # 3. calculate new pseudo data value -------------------------------------- 
     
     
     psAbs <- dd$type == "pseudoAbsence"
-  
+    
     u_i <- rep(0, nrow(dd))
-      
+    
     if(var_to_fit == "FOI"){
       
       u_i[!psAbs] <- (((dd$o_j[!psAbs] - l_f) * (dd$p_i[!psAbs] - l_f)) / (dd$a_sum[!psAbs] - l_f)) + l_f 
       u_i[psAbs] <- ifelse(dd$p_i[psAbs] > 0, l_f, dd$p_i[psAbs])
-    
+      
     } else {
       
       u_i[!psAbs] <- (dd$o_j[!psAbs] * dd$p_i[!psAbs]) / dd$a_sum[!psAbs]
       u_i[psAbs] <- ifelse(dd$p_i[psAbs] > 1, l_f, dd$p_i[psAbs])
-    
+      
     }
     
     dd$u_i <- u_i
@@ -91,7 +91,7 @@ exp_max_algorithm <- function(niter,
                          min_node_size = 20, 
                          my_weights = "wgt_prime", 
                          model_nm = RF_obj_name)
-      
+    
     RF_ms_i <- h2o.mse(RF_obj)
     
     
@@ -103,7 +103,7 @@ exp_max_algorithm <- function(niter,
     n_NA_pred <- sum(is.na(p_i))
     
     dd$p_i <- p_i
-
+    
     
     # map of square predictions -----------------------------------------------  
     
@@ -117,17 +117,17 @@ exp_max_algorithm <- function(niter,
     
     
     dd_2 <- dd
-      
+    
     
     # plot of observed vs predicted square values -----------------------------  
-
+    
     
     if(var_to_fit == "FOI"){
       
       dd_2$u_i[psAbs] <- 0 
-    
-    }
       
+    }
+    
     dd_2$p_i[psAbs] <- ifelse(dd_2$p_i[psAbs] < 0, 0, dd_2$p_i[psAbs])
     
     sqr_sp_nm <- paste0("pred_vs_obs_sqr_iter_", i, ".png")
@@ -159,11 +159,11 @@ exp_max_algorithm <- function(niter,
     } else {
       
       cc <- orig_dataset
-    
+      
     }
     
     psAbs_adm <- cc$type == "pseudoAbsence" 
-      
+    
     if(var_to_fit == "FOI"){
       
       cc$o_j[psAbs_adm] <- 0
@@ -206,11 +206,11 @@ exp_max_algorithm <- function(niter,
                          y = "adm_pred", 
                          file_name = adm_sp_nm, 
                          file_path = sct_plt_path)  
-
-        
+    
+    
     # 8. calculate admin unit level sum of square ----------------------------- 
     
-
+    
     ss_j <- sum(aa$new_weight * (aa$mean_p_i - aa$o_j)^2)
     
     
@@ -232,4 +232,118 @@ exp_max_algorithm <- function(niter,
   h2o.shutdown(prompt = FALSE)
   
   out
+}
+
+exp_max_algorithm_boot <- function(i, 
+                                   boot_samples, 
+                                   pxl_dataset_orig, 
+                                   psAbs, 
+                                   my_preds, 
+                                   no_trees, 
+                                   min_node_size, 
+                                   grp_flds, 
+                                   niter, 
+                                   RF_obj_path, 
+                                   RF_obj_name,
+                                   diagn_tab_path, 
+                                   diagn_tab_name,
+                                   map_path, 
+                                   map_name, 
+                                   sct_plt_path,
+                                   adm_dataset, 
+                                   pxl_dts_pt,
+                                   var_to_fit, 
+                                   train_dts_path,
+                                   train_dts_name){
+  
+  
+  #browser()
+  
+  
+  # ---------------------------------------- define variables
+  
+  
+  pxl_dts_nm <- paste0("env_vars_and_foi_20km_", i, ".rds")
+  
+  
+  # ---------------------------------------- load bootstrapped data sets 
+  
+  
+  pxl_dts_boot <- readRDS(file.path(pxl_dts_pt, pxl_dts_nm))
+  
+  foi_data_boot <- boot_samples[[i]]
+  
+  
+  # ---------------------------------------- get output name 
+  
+  
+  a <- RF_obj_name[i]
+  b <- diagn_tab_name[i]
+  cc <- map_path[i]  
+  ee <- map_name[i] 
+  ff <- sct_plt_path[i]
+  gg <- train_dts_name[i]
+  
+  # ---------------------------------------- pre process the bootstrapped foi data set
+  
+  
+  if(var_to_fit == "FOI"){
+    
+    names(foi_data_boot)[names(foi_data_boot) == "FOI"] <- "o_j"
+    
+  } else {
+    
+    names(foi_data_boot)[names(foi_data_boot) == var_to_fit] <- "o_j"
+    
+  }
+  
+  foi_data_boot[foi_data_boot$type == "pseudoAbsence", "o_j"] <- psAbs
+  
+  names(foi_data_boot)[names(foi_data_boot) == "ADM_0"] <- grp_flds[1]
+  names(foi_data_boot)[names(foi_data_boot) == "ADM_1"] <- grp_flds[2]
+  
+  
+  # ---------------------------------------- pre process the square data set
+  
+  
+  names(pxl_dts_boot)[names(pxl_dts_boot) == "ADM_0"] <- grp_flds[1]
+  names(pxl_dts_boot)[names(pxl_dts_boot) == "ADM_1"] <- grp_flds[2]
+  
+  pxl_dts_grp <- pxl_dts_boot %>% group_by_(.dots = grp_flds) 
+  
+  aa <- pxl_dts_grp %>% summarise(pop_sqr_sum = sum(population))
+  
+  pxl_dts_boot <- left_join(pxl_dts_boot, aa)
+  
+  pxl_dts_boot$pop_weight <- pxl_dts_boot$population / pxl_dts_boot$pop_sqr_sum
+  
+  
+  # ---------------------------------------- attach original data and weigths to square dataset
+  
+  
+  pxl_dts_boot <- inner_join(pxl_dts_boot, foi_data_boot[, c(grp_flds, "o_j", "new_weight")])
+  
+  
+  # ---------------------------------------- run the EM 
+  
+  
+  exp_max_algorithm(niter = niter, 
+                    orig_dataset = foi_data_boot, 
+                    pxl_dataset = pxl_dts_boot,
+                    pxl_dataset_full = pxl_dataset_orig,
+                    l_f = psAbs,
+                    my_predictors = my_preds, 
+                    grp_flds = grp_flds, 
+                    RF_obj_path = RF_obj_path,
+                    RF_obj_name = a,
+                    diagn_tab_path = diagn_tab_path, 
+                    diagn_tab_name = b,
+                    map_path = cc, 
+                    map_name = ee,
+                    sct_plt_path = ff,
+                    var_to_fit = var_to_fit,
+                    train_dts_path = train_dts_path,
+                    train_dts_name = gg,
+                    adm_dataset = adm_dataset)
+  
 }
