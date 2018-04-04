fd0c7a1678696a1d2f526f321826c00d8400ca19
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-04-04 15:06:43 +0100

    calculate minimum distance between squares and data

---
+++ calculate_square_data_min_distance.R
@@ -0,0 +1,74 @@
+
+library(geosphere) # for distm()
+library(ggplot2)
+
+
+# load data ------------------------------------------------------------------- 
+
+
+foi_data1 <- read.csv(file.path("output", 
+                               "foi", 
+                               "All_FOI_estimates_linear_env_var_area.csv"),
+                     stringsAsFactors = FALSE) 
+
+all_sqr_covariates <- readRDS(file.path("output", 
+                                        "env_variables", 
+                                        "all_squares_env_var_0_1667_deg.rds"))
+
+
+# pre processing --------------------------------------------------------------
+
+
+names(all_sqr_covariates)[names(all_sqr_covariates) == "lat.grid"] <- "latitude"
+names(all_sqr_covariates)[names(all_sqr_covariates) == "long.grid"] <- "longitude"
+
+foi_data <- as.matrix(foi_data1[, c("longitude","latitude")])
+
+all_sqr_covariates <- as.matrix(all_sqr_covariates)
+
+foi_points <- foi_data[, c("longitude","latitude")]
+squares <- all_sqr_covariates[, c("longitude","latitude")]
+
+
+# calculate matrix of distances -----------------------------------------------
+
+
+d <- distm(squares, foi_points, fun = distGeo)
+
+
+# attach to dataset -----------------------------------------------------------
+
+
+min.d <- apply(d, 1, function(x) order(x, decreasing = FALSE)[1])
+
+all_sqr_covariates <- cbind(all_sqr_covariates,
+                            neighbor = foi_data1[min.d, "data_id"],
+                            distance = apply(d, 1, function(x) sort(x, decreasing = FALSE)[1]))
+
+all_sqr_covariates[, "distance"] <- all_sqr_covariates[, "distance"] / 1000
+
+
+# histogram -------------------------------------------------------------------
+
+
+pretty_x_vals <- pretty(all_sqr_covariates[, "distance"], 15)
+
+all_sqr_covariates_df <- as.data.frame(all_sqr_covariates)
+
+p <- ggplot(all_sqr_covariates_df, aes(distance)) +
+  geom_histogram(binwidth = 40) +
+  scale_x_continuous("distance (km)",
+                     breaks = pretty_x_vals,
+                     labels = pretty_x_vals,
+                     limits = c(min(pretty_x_vals), max(pretty_x_vals)))
+
+ggsave(file.path("figures", "min_distance_sqr_to_data.png"), 
+       p, 
+       width = 15, 
+       height = 8, 
+       units = "cm")
+
+saveRDS(all_sqr_covariates_df, 
+        file.path("output", 
+                  "env_variables", 
+                  "all_squares_env_var_0_1667_deg_dis.rds"))
