1d9f0146ac709e446bec99739d6df8cbd6c259fe
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-04-12 09:38:41 +0100

    Updated script

--- predict_squares_1.r
+++ predict_squares_1.r
@@ -1,10 +1,10 @@
-# Makes foi predictions for all the squares in the world.
+# Makes predictions for all the squares in the world.
 
 options(didehpc.cluster = "fi--didemrchnb")
 
 my_resources <- c(
   file.path("R", "utility_functions.r"),
-  file.path("R", "random_forest", "functions_for_fitting_h2o_RF_and_making_predictions.r"),
+  file.path("R", "random_forest", "fit_h2o_RF_and_make_predictions.r"),
   file.path("R", "plotting", "functions_for_plotting_square_level_maps.r"))
   
 my_pkgs <- "h2o"
@@ -15,60 +15,63 @@ ctx <- context::context_save(path = "context",
                              sources = my_resources)
 
 
-# ---------------------------------------- define parameters
+# define parameters ----------------------------------------------------------- 
 
 
-var_to_fit <- "R0_3"
+var_to_fit <- "FOI"
 
-fit_type <- "best"
+number_of_predictors <- 9
   
-model_tp <- paste0(var_to_fit, "_", fit_type, "_model")
-
 RF_mod_name <- "RF_obj.rds"
 
 base_info <- c("cell", "lat.grid", "long.grid", "population", "ADM_0", "ADM_1", "ADM_2")
 
-out_pt <- file.path("output", "predictions_world", model_tp)
+
+# define variables ------------------------------------------------------------
+
+
+model_tp <- paste0(var_to_fit, "_best_model")
+
+out_pt <- file.path("output", "predictions_world", "best_fit_models", model_tp)
   
-out_fl_nm <- paste0("FOI", "_", fit_type, "_20km.rds")
+out_fl_nm <- "response.rds"
 
 
-# ---------------------------------------- are you using the cluster? 
+# are you using the cluster? -------------------------------------------------- 
 
 
 context::context_load(ctx)
 
 
-# ---------------------------------------- load data
+# load data ------------------------------------------------------------------- 
 
 
-all_sqr_covariates <- readRDS(file.path("output", "env_variables", "all_squares_env_var_0_1667_deg.rds"))
+all_sqr_covariates <- readRDS(file.path("output", 
+                                        "env_variables", 
+                                        "all_squares_env_var_0_1667_deg.rds"))
 
-RF_obj_path <- file.path(
-  "output",
-  "EM_algorithm",
-  model_tp,
-  "optimized_model_objects")
+RF_obj_path <- file.path("output",
+                         "EM_algorithm",
+                         "best_fit_models",
+                         model_tp,
+                         "optimized_model_objects")
 
 # predicting variable rank
-predictor_rank <- read.csv(
-  file.path("output", 
-            "variable_selection", 
-            "metropolis_hastings", 
-            "exp_1", 
-            "variable_rank_final_fits_exp_1.csv"),
-  stringsAsFactors = FALSE)
+predictor_rank <- read.csv(file.path("output", 
+                                     "variable_selection", 
+                                     "metropolis_hastings", 
+                                     "exp_1", 
+                                     "variable_rank_final_fits_exp_1.csv"),
+                           stringsAsFactors = FALSE)
 
 
-# ---------------------------------------- get best predictor
+# get best predictor ---------------------------------------------------------- 
 
 
-my_predictors <- predictor_rank$variable[1:9]
-
-#my_predictors <- c(my_predictors, "RFE_const_term")
+my_predictors <- predictor_rank$name[1:number_of_predictors]
 
 
-# ---------------------------------------- submit one job 
+# submit one job -------------------------------------------------------------- 
 
 
 h2o.init()
