4ff1f1e4c75c0057c5bd62fc574f2e4ad61d9723
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-05-23 11:35:10 +0100

    Added script description and cv calculation

--- predict_squares_with_bootstrap_1b.R
+++ predict_squares_with_bootstrap_1b.R
@@ -1,3 +1,7 @@
+# Calculates the sd of each pixel prediction by interpolating the sd values, 
+# calculated using bootstrap samples created with different grid sizes (or distances),
+# based on the distance of the pixel to the closest data point
+
 
 source(file.path("R", "burden_and_interventions", "calculate_mean_across_fits.r"))
 
@@ -63,3 +67,46 @@ for (i in seq_len(N)){
 }
 
 saveRDS(all_sqr_covariates[, base_info], file.path(out_path, out_name))
+
+
+# =============================================================================
+#
+#                     calculate coefficient of variation, CV
+#
+# =============================================================================
+
+
+# # define parameters -----------------------------------------------------------
+# 
+# 
+# out_name <- "response_CV.rds"
+# 
+# out_path <- file.path("output",
+#                       "predictions_world",
+#                       "bootstrap_models")
+# 
+# 
+# # load data ------------------------------------------------------------------- 
+# 
+# 
+# best_prediction <- readRDS(file.path("output", 
+#                                      "predictions_world", 
+#                                      "best_fit_models",
+#                                      "FOI_best_model",
+#                                      "response.rds"))
+# 
+# interpolated_sd <- readRDS(file.path("output", 
+#                                      "predictions_world", 
+#                                      "bootstrap_models",
+#                                      "response_mean.rds"))
+# 
+# 
+# # start -----------------------------------------------------------------------
+# 
+# 
+# interpolated_sd$CV <- interpolated_sd$sd / best_prediction$best
+# 
+# interpolated_sd$CV[is.na(interpolated_sd$CV)] <- 0
+# interpolated_sd$CV[!is.finite(interpolated_sd$CV)] <- 0
+# 
+# saveRDS(interpolated_sd, file.path(out_path, out_name))
