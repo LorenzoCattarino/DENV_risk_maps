0d0fc445ee98ba4ecab37e4a06b1b77bd55e9bab
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-05-31 14:52:21 +0100

    Moved function for nice map first

--- R/plotting/functions_for_plotting_square_level_maps.r
+++ R/plotting/functions_for_plotting_square_level_maps.r
@@ -1,3 +1,156 @@
+make_nice_map <- function(i, 
+                          map_projs,
+                          countries,
+                          bbox,
+                          df_long,
+                          statsc,
+                          na_cutoff,
+                          ttl,
+                          out_path,
+                          out_file_names){
+  
+  
+  # get values ------------------------------------------------------------------
+  
+  
+  map_proj <- map_projs[i]
+  out_file_name <- out_file_names[i]
+  
+  
+  # convert to ggplot-friendly object -------------------------------------------  
+  
+  
+  if (!is.na(map_proj)){
+    countries <- spTransform(countries, CRS(map_proj))
+  }
+  
+  countries_df <- fortify(countries)
+  
+  if (!is.na(map_proj)){
+    bbox <- spTransform(bbox, CRS(map_proj))
+  }
+  
+  bbox_df<- fortify(bbox)
+  
+  
+  # create matrix of values ----------------------------------------------------- 
+  
+  
+  gr_size <- 20
+  
+  res <- (1 / 120) * gr_size
+  
+  lats <- seq(-90, 90, by = res)
+  lons <- seq(-180, 180, by = res)
+  
+  df_long$lat.int <- floor(df_long$lat.grid * 6 + 0.5)
+  df_long$long.int <- floor(df_long$long.grid * 6 + 0.5)
+  
+  lats.int <- lats * 6
+  lons.int <- lons * 6
+  
+  mat <- matrix(0, nrow = length(lons), ncol = length(lats))
+  
+  i.lat <- findInterval(df_long$lat.int, lats.int)
+  i.lon <- findInterval(df_long$long.int, lons.int)
+  
+  mat[cbind(i.lon, i.lat)] <- df_long[, statsc]
+  
+  
+  # convert matrix to ggplot-compatible object ---------------------------------- 
+  
+  
+  mat_ls <- list(x = lons,
+                 y = lats,
+                 z = mat)
+  
+  r_mat <- raster(mat_ls)
+  
+  if (!is.na(map_proj)){
+    r_mat <- projectRaster(r_mat, crs = map_proj)
+  }
+  browser()
+  # my_ext <- matrix(r_mat@extent[], nrow = 2, byrow = TRUE) 
+  # 
+  # countries@bbox <- my_ext
+  # 
+  # r_mat_msk <- mask(r_mat, countries)
+  #
+  # r_spdf <- as(r_mat_msk, "SpatialPixelsDataFrame")
+  
+  r_spdf <- as(r_mat, "SpatialPixelsDataFrame")
+  
+  r_df <- as.data.frame(r_spdf)
+  
+  r_df$layer[r_df$layer < na_cutoff] <- NA
+  
+  r_df <- subset(r_df, !is.na(layer))
+  
+  
+  # make map --------------------------------------------------------------------
+  
+  
+  plot_wdt <- 20
+  plot_hgt <- 10 
+  barwdt <- 1.5
+  barhgt <- 6.5
+  pol_brd_sz <- 0.1
+  leg_pos_x <- 0.10
+  leg_pos_y <- 0.25
+  leg_txt_sz <- 10 
+  leg_ttl_sz <- 12
+  
+  my_col <- matlab.like(10)
+  
+  dir.create(out_path, FALSE, TRUE)
+  
+  leg_val <- pretty(r_df$layer, 5)
+  
+  png(file.path(out_path, out_file_name),
+      width = plot_wdt,
+      height = plot_hgt,
+      units = "cm",
+      pointsize = 12,
+      res = 300)
+  
+  p <- ggplot() +
+    geom_polygon(data = bbox_df, aes(long, lat, group = group), fill = "aliceblue") +
+    geom_tile(data = r_df, aes(x = x, y = y, fill = layer)) +
+    scale_fill_gradientn(breaks = leg_val,
+                         labels = leg_val,
+                         limits = c(min(leg_val), max(r_df$layer)),
+                         colours = my_col, 
+                         guide = guide_colourbar(title = ttl, 
+                                                 barwidth = barwdt, 
+                                                 barheight = barhgt),
+                         na.value = "grey70") + 
+    # geom_path(data = countries_df,
+    #           aes(x = long, y = lat, group = group),
+    #           colour = "gray40",
+    #           size = pol_brd_sz) +
+    geom_path(data = bbox_df, 
+              aes(long, lat, group = group), 
+              colour = "black", 
+              size = 0.3) +
+    coord_equal() +
+    scale_x_continuous(labels = NULL, expand = c(0, 0)) +
+    scale_y_continuous(labels = NULL, expand = c(0, 0)) +
+    theme_void() + 
+    theme(axis.text.x = element_blank(),
+          axis.text.y = element_blank(),
+          axis.ticks = element_blank(),
+          plot.margin = unit(c(0.1, 0.1, 0.1, 0.1), "cm"),
+          legend.position = c(leg_pos_x, leg_pos_y),    
+          legend.text = element_text(size = leg_txt_sz),                       
+          legend.title = element_text(face = "bold", size = leg_ttl_sz),
+          legend.box.background = element_rect(fill = "white", colour = "black"))
+  
+  print(p)
+  
+  dev.off()
+  
+}
+
 wrapper_to_square_map <- function(x, 
                                   my_colors, 
                                   model_tp, 
@@ -263,155 +416,6 @@ map_predictions_pixel_ggplot <- function(df,
           legend.title = element_text(face = "bold", size = leg_ttl_sz))
   
   print(p2)
-  
-  dev.off()
-  
-}
-
-make_very_nice_map <- function(i, 
-                               map_projs,
-                               countries,
-                               bbox,
-                               df_long,
-                               statsc,
-                               na_cutoff,
-                               ttl,
-                               out_path,
-                               out_file_names){
-  
-  
-  # get values ------------------------------------------------------------------
-  
-  
-  map_proj <- map_projs[i]
-  out_file_name <- out_file_names[i]
-  
-  
-  # convert to ggplot-friendly object -------------------------------------------  
-  
-  
-  if (!is.na(map_proj)){
-    countries <- spTransform(countries, CRS(map_proj))
-  }
-  
-  countries_df <- fortify(countries)
-  
-  if (!is.na(map_proj)){
-    bbox <- spTransform(bbox, CRS(map_proj))
-  }
-  
-  bbox_df<- fortify(bbox)
-  
-  
-  # create matrix of values ----------------------------------------------------- 
-  
-  
-  gr_size <- 20
-  
-  res <- (1 / 120) * gr_size
-  
-  lats <- seq(-90, 90, by = res)
-  lons <- seq(-180, 180, by = res)
-  
-  df_long$lat.int <- floor(df_long$lat.grid * 6 + 0.5)
-  df_long$long.int <- floor(df_long$long.grid * 6 + 0.5)
-  
-  lats.int <- lats * 6
-  lons.int <- lons * 6
-  
-  mat <- matrix(0, nrow = length(lons), ncol = length(lats))
-  
-  i.lat <- findInterval(df_long$lat.int, lats.int)
-  i.lon <- findInterval(df_long$long.int, lons.int)
-  
-  mat[cbind(i.lon, i.lat)] <- df_long[, statsc]
-  
-  
-  # convert matrix to ggplot-compatible object ---------------------------------- 
-  
-  
-  mat_ls <- list(x = lons,
-                 y = lats,
-                 z = mat)
-  
-  r_mat <- raster(mat_ls)
-  
-  if (!is.na(map_proj)){
-    r_mat <- projectRaster(r_mat, crs = map_proj)
-  }
-  
-  my_ext <- matrix(r_mat@extent[], nrow = 2, byrow = TRUE) 
-  
-  countries@bbox <- my_ext
-  
-  r_mat_msk <- mask(r_mat, countries)
-  
-  r_spdf <- as(r_mat_msk, "SpatialPixelsDataFrame")
-  
-  r_df <- as.data.frame(r_spdf)
-  
-  r_df$layer[r_df$layer < na_cutoff] <- NA
-  
-  
-  # make map --------------------------------------------------------------------
-  
-  
-  plot_wdt <- 8
-  plot_hgt <- 4  
-  barwdt <- 1.5
-  barhgt <- 6.5
-  pol_brd_sz <- 0.1
-  leg_pos_x <- 0.10
-  leg_pos_y <- 0.25
-  leg_txt_sz <- 10 
-  leg_ttl_sz <- 12
-  
-  my_col <- matlab.like(10)
-  
-  dir.create(out_path, FALSE, TRUE)
-  
-  leg_val <- pretty(r_df$layer, 5)
-  
-  png(file.path(out_path, out_file_name),
-      width = plot_wdt,
-      height = plot_hgt,
-      units = "in",
-      pointsize = 12,
-      res = 300)
-  
-  p <- ggplot() +
-    geom_polygon(data = bbox_df, aes(long, lat, group = group), fill = "aliceblue") +
-    geom_tile(data = r_df, aes(x = x, y = y, fill = layer)) +
-    scale_fill_gradientn(breaks = leg_val,
-                         labels = leg_val,
-                         limits = c(min(leg_val), max(r_df$layer)),
-                         colours = my_col, 
-                         guide = guide_colourbar(title = ttl, 
-                                                 barwidth = barwdt, 
-                                                 barheight = barhgt),
-                         na.value = "grey70") + 
-    geom_path(data = countries_df,
-              aes(x = long, y = lat, group = group),
-              colour = "gray40",
-              size = pol_brd_sz) +
-    geom_path(data = bbox_df, 
-              aes(long, lat, group = group), 
-              colour = "black", 
-              size = 0.3) +
-    coord_equal() +
-    scale_x_continuous(labels = NULL, expand = c(0, 0)) +
-    scale_y_continuous(labels = NULL, expand = c(0, 0)) +
-    theme_void() + 
-    theme(axis.text.x = element_blank(),
-          axis.text.y = element_blank(),
-          axis.ticks = element_blank(),
-          plot.margin = unit(c(0.1, 0.1, 0.1, 0.1), "in"),
-          legend.position = c(leg_pos_x, leg_pos_y),    
-          legend.text = element_text(size = leg_txt_sz),                       
-          legend.title = element_text(face = "bold", size = leg_ttl_sz),
-          legend.box.background = element_rect(fill = "white", colour = "black"))
-  
-  print(p)
   
   dev.off()
   
