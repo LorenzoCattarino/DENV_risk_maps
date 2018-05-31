47039000fd8e24b2e3a4485c6df9d9dff531aad9
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-05-31 09:38:07 +0100

    Added function

--- R/prepare_datasets/calculate_mean_across_fits.R
+++ R/prepare_datasets/calculate_mean_across_fits.R
@@ -79,3 +79,15 @@ calculate_mean_of_burden_predictions_for_different_scenarios <- function(
 get_grid_size_sd <- function(i, pred_ls){
   pred_ls[[i]]$sd
 }
+
+wrapper_to_mean_across_fits <- function(i, var_names, out_list){
+  
+  fl <- out_list[[i]] 
+  
+  ret <- mean_across_fits(fl)
+  
+  col_nms <- paste0(var_names[i], "_", colnames(ret))
+  
+  setNames(ret, col_nms)
+  
+}
