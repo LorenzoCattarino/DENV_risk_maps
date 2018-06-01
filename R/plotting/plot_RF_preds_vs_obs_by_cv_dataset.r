577a2b6368789fe8e941759e1357aacf382bf5b5
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-06-01 11:21:59 +0100

    switched from in to cm

--- R/plotting/plot_RF_preds_vs_obs_by_cv_dataset.r
+++ R/plotting/plot_RF_preds_vs_obs_by_cv_dataset.r
@@ -23,20 +23,20 @@ RF_preds_vs_obs_plot_stratif <- function(df, x, y, facet_var, file_name, file_pa
     facet_grid(as.formula(paste0("dataset ~", facet_var)),
                labeller = labeller(dataset = facet_plot_names_y,
                                    scale = facet_plot_names_x)) + 
-    geom_point(aes_string(x = x, y = y), size = 1) +
+    geom_point(aes_string(x = x, y = y), size = 0.8) +
     geom_abline(slope = 1, intercept = 0, linetype = 2) +
     scale_x_continuous("Observations",  
-                       #limits = c(min_x_value, max_x_value), 
+                       limits = c(min_x_value, max_x_value), 
                        breaks = x_values,
                        labels = x_values) +
     scale_y_continuous("Predictions", 
                        #limits = c(min_y_value, max_y_value), 
                        breaks = y_values,
                        labels = y_values) +
-    theme(axis.title.x = element_text(size = 15, margin = margin(t = 20)),
-          axis.title.y = element_text(size = 15, margin = margin(r = 20)),
-          axis.text.x = element_text(size = 11),
-          axis.text.y = element_text(size = 11),
+    theme(axis.title.x = element_text(size = 12, margin = margin(t = 20)),
+          axis.title.y = element_text(size = 12, margin = margin(r = 20)),
+          axis.text.x = element_text(size = 10),
+          axis.text.y = element_text(size = 10),
           plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm"),
           strip.text.x = element_text(size = 12),
           strip.text.y = element_text(size = 12))
@@ -49,7 +49,7 @@ RF_preds_vs_obs_plot_stratif <- function(df, x, y, facet_var, file_name, file_pa
                   label = paste0("italic(r) == ", V1)),
               parse = TRUE,
               inherit.aes = FALSE,
-              size = 5) +
+              size = 4) +
     facet_grid(as.formula(paste0("dataset ~", facet_var)),
                labeller = labeller(dataset = facet_plot_names_y,
                                    scale = facet_plot_names_x))
@@ -57,9 +57,9 @@ RF_preds_vs_obs_plot_stratif <- function(df, x, y, facet_var, file_name, file_pa
   dir.create(file_path, FALSE, TRUE)
   
   png(filename = file.path(file_path, file_name), 
-      width = 10, 
-      height = 7, 
-      units = "in", 
+      width = 16, 
+      height = 12, 
+      units = "cm", 
       pointsize = 12,
       res = 200)
   
