b4cc4f51d71c16b1c2515b3f98cc36260e8d3e08
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-06-05 15:29:03 +0100

    works fine

--- make_nice_map.R
+++ make_nice_map.R
@@ -14,6 +14,45 @@ ctx <- context::context_save(path = "context",
                              packages = my_pkgs)
 
 
+# define parameters ----------------------------------------------------------- 
+
+
+parameters <- list(
+  dependent_variable = "R0_3",
+  resample_grid_size = 20,
+  na_cutoff = 0)   
+
+# statsc <- "best"
+statsc <- "sd"
+
+# ttl <- expression("R"[0])
+ttl <- "SD"
+
+# mean_pred_fl_nm <- "response.rds"
+mean_pred_fl_nm <- "response_mean.rds"
+
+map_projs <- c(NA, "+proj=robin", "+proj=moll")
+
+out_file_names <- c("nice_map.png", "nice_map_robin.png", "nice_map_moll.png")
+
+
+# define variables ------------------------------------------------------------  
+
+
+# model_type <- paste0(parameters$dependent_variable, "_best_model")
+model_type <- paste0(parameters$dependent_variable, "_boot_model")
+
+# out_path <- file.path("figures", 
+#                       "predictions_world", 
+#                       "best_fit_models", 
+#                       model_type)
+out_path <- file.path("figures", 
+                      "predictions_world", 
+                      "bootstrap_models", 
+                      "grid_size_interpolated",
+                      model_type)
+
+
 # are you using the cluster? -------------------------------------------------- 
 
 
@@ -29,43 +68,18 @@ if (CLUSTER) {
 }
 
 
-# define parameters ----------------------------------------------------------- 
-
-
-parameters <- list(
-  dependent_variable = "R0_3",
-  resample_grid_size = 20)   
-
-mean_pred_fl_nm <- "response.rds"
-
-map_projs <- c(NA, "+proj=robin", "+proj=moll")
-
-out_file_names <- c("nice_map.png", "nice_map_robin.png", "nice_map_moll.png")
-
-statsc <- "best"
-
-ttl <- expression("R"[0])
-
-na_cutoff <- 1
-
-
-# define variables ------------------------------------------------------------  
-
-
-model_type <- paste0(parameters$dependent_variable, "_best_model")
-
-out_path <- file.path("figures", 
-                      "predictions_world", 
-                      "best_fit_models", 
-                      model_type)
-
-
 # load data ------------------------------------------------------------------- 
 
 
+# df_long <- readRDS(file.path("output",
+#                              "predictions_world",
+#                              "best_fit_models",
+#                              model_type,
+#                              mean_pred_fl_nm))
 df_long <- readRDS(file.path("output",
                              "predictions_world",
-                             "best_fit_models",
+                             "bootstrap_models",
+                             "grid_size_interpolated",
                              model_type,
                              mean_pred_fl_nm))
 
@@ -85,14 +99,17 @@ countries <- countries[!countries@data$NAME_ENGLI == "Caspian Sea", ]
 # submit one job --------------------------------------------------------------
 
 
+names(df_long)[names(df_long) == "latitude"] <- "lat.grid"
+names(df_long)[names(df_long) == "longitude"] <- "long.grid"
+
 v_nice_map <- obj$enqueue(
   make_nice_map(seq_along(map_projs)[3],
+                parameters,
                 map_projs,
                 countries,
                 bbox,
                 df_long,
                 statsc,
-                na_cutoff,
                 ttl,
                 out_path,
                 out_file_names))
@@ -107,12 +124,12 @@ v_nice_map <- obj$enqueue(
 #     seq_along(map_projs),
 #     make_nice_map,
 #     obj,
+#     parameters
 #     map_projs,
 #     countries,
 #     bbox,
 #     df_long,
 #     statsc,
-#     na_cutoff,
 #     ttl,
 #     out_path,
 #     out_file_names)
@@ -121,12 +138,12 @@ v_nice_map <- obj$enqueue(
 # 
 #   v_nice_map <- make_nice_map(
 #     seq_along(map_projs)[3],
+#     parameters,
 #     map_projs,
 #     countries,
 #     bbox,
 #     df_long,
 #     statsc,
-#     na_cutoff,
 #     ttl,
 #     out_path,
 #     out_file_names)
