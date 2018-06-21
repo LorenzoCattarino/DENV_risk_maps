81acb2557efbee29440a2eaafff5a9b71cbe70e7
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-06-21 15:03:20 +0100

    Plot Sam Bhatt's predictors

--- plot_env_var_individual_tiles.R
+++ plot_env_var_individual_tiles.R
@@ -2,7 +2,7 @@ library(data.table)
 library(fields)
 library(RColorBrewer)
 
-source(file.path("R", "prepare_datasets", "plot_data_one_tile.R"))
+source(file.path("R", "plotting", "plot_data_one_tile.R"))
 source(file.path("R", "prepare_datasets", "calculate_amplitude.R"))
 
 # load data 
@@ -13,23 +13,29 @@ tiles <- read.csv(file.path("data",
                   sep = ",", 
                   stringsAsFactors = FALSE)
 
-run_id <- 45
+run_id <- 72
 
 one_tile_ID <- tiles[run_id, "tile.id"]
 
-one_tile <- fread(file.path("data",
+one_tile <- fread(file.path("output",
                             "env_variables",
-                            "all_sets", 
+                            "tile_set_2", 
                             paste0("tile_", one_tile_ID, ".txt")), 
+                  header = TRUE,
+                  sep = ",",
+                  na.strings = c("NA", "-1.#IND", "Peipsi", "Moskva", "IJsselmeer", "Zeeuwse meren"),
+                  fill = TRUE,
                   data.table = FALSE)
 
 FTs_data <- c("DayTemp", "NightTemp", "EVI", "MIR", "RFE")
 
-FT_term_to_plot <- c("const_term", "amplitude")
+FT_term_to_plot <- c("const_term", "amplitude", "Re0", "Im0", "Re1", "Im1")
 
-vars_to_plot <- apply(expand.grid(FT_term_to_plot, FTs_data), 1, function(x) paste(x[2],x[1], sep="_"))
+all_combs <- expand.grid(FTs_data, FT_term_to_plot)
 
-vars_to_plot <- c(vars_to_plot, "population")
+vars_to_plot <- apply(all_combs, 1, function(x) paste(x[1], x[2], sep = "_"))
+
+vars_to_plot <- c(vars_to_plot, "population", "travel_time", "TSI", "aedes_gen")
 
 my_pal <- rev(colorRampPalette(brewer.pal(11, "RdBu"))(100))
 
