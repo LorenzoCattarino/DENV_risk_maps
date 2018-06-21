4aa28b46ae0cef05788cbcb9c71b05db87abb100
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-06-21 15:04:26 +0100

    train the RF model with and without salje data and predict with and without log(1+pop_den)

--- validate_bangladesh_data_5.R
+++ validate_bangladesh_data_5.R
@@ -23,6 +23,8 @@ no_predictors <- 9
 
 map_out_pt <- file.path("figures", "data", "salje")
 
+plot_ttl <- "yes salje points in training & yes log_pop"
+
 
 # load data -------------------------------------------------------------------
 
@@ -51,11 +53,13 @@ predictor_rank <- read.csv(file.path("output",
 
 foi_dataset <- foi_dataset[setdiff(names(foi_dataset), c("R0_1", "R0_2", "R0_3"))]
   
+# select the dataset
+# all_foi <- foi_dataset
 all_foi <- rbind(foi_dataset, salje_data)
 
-all_foi$data_id <- seq_len(nrow(all_foi))
+all_foi$log_pop_den <- log(1 + all_foi$pop_den)
 
-my_predictors <- predictor_rank$name[1:no_predictors]
+all_foi$data_id <- seq_len(nrow(all_foi))
 
 all_foi[all_foi$type == "pseudoAbsence", var_fit] <- pseudoAbs_value
 
@@ -63,11 +67,17 @@ all_foi$new_weight <- all_wgt
 pAbs_wgt <- get_area_scaled_wgts(all_foi, wgt_limits)
 all_foi[all_foi$type == "pseudoAbsence", "new_weight"] <- pAbs_wgt
 
-training_dataset <- all_foi[, c(var_fit, my_predictors, "new_weight")]
+salje_data$log_pop_den <- log(1 + salje_data$pop_den)
+
+# choose the predictors
+my_predictors <- predictor_rank$name[1:no_predictors]
+my_predictors <- c(my_predictors, "log_pop_den")
 
 
 # fit -------------------------------------------------------------------------
 
+
+training_dataset <- all_foi[, c(var_fit, my_predictors, "new_weight")]
 
 h2o.init()
 
@@ -79,32 +89,29 @@ RF_obj <- fit_h2o_RF(dependent_variable = var_fit,
                      my_weights = "new_weight",
                      model_nm = "test")
 
-all_foi$foi_adm <- make_h2o_predictions(RF_obj, all_foi, my_predictors)
+salje_data$foi_adm <- make_h2o_predictions(RF_obj, salje_data, my_predictors)
 
 h2o.shutdown(prompt = FALSE)
 
-all_foi$foi_adm[all_foi$foi_adm < 0] <- 0
+salje_data$foi_adm[salje_data$foi_adm < 0] <- 0
 
 
 # plot ------------------------------------------------------------------------
 
 
-henriks_points <- all_foi[all_foi$ID_0 == 20, ]
-henriks_points <- henriks_points[-1,] # the first BGD point is from Natsuko 
-  
-corr_coeff <- round(cor(henriks_points$FOI, henriks_points$foi_adm), 3)
+corr_coeff <- round(cor(salje_data$FOI, salje_data$foi_adm), 3)
 
 p <- ggplot() +
-  #geom_point(aes(x = FOI, y = p_i), data = all_foi, size = 1) +
-  geom_point(aes(x = FOI, y = foi_adm, colour = "red"), data = henriks_points, size = 1) +
+  geom_point(aes(x = FOI, y = foi_adm, colour = "red"), data = salje_data, size = 1) +
   scale_colour_identity(name = "", guide = "legend", labels = "salje") +
   geom_abline(slope = 1, intercept = 0, linetype = 2) +
-  geom_text(aes(x = FOI, y = foi_adm, label = ID_1), data = henriks_points, nudge_y = 0.0004) +
+  geom_text(aes(x = FOI, y = foi_adm, label = ID_1), data = salje_data, nudge_y = 0.0009) +
   geom_text(aes(x = 0.006, y = 0.004, label = paste0("r = ", corr_coeff))) +
-  scale_x_continuous("observed admin 1 FOI", limits = c(0, 0.01)) +
-  scale_y_continuous("predicted admin 1 FOI", limits = c(0, 0.01))
+  scale_x_continuous("observed admin 1 FOI", limits = c(0,0.01)) +
+  scale_y_continuous("predicted admin 1 FOI", limits = c(0,0.02)) +
+  ggtitle(plot_ttl)
 
-ggsave(file.path(map_out_pt, "correlation_adm_pred_vs_observations.png"), 
+ggsave(file.path(map_out_pt, "correlation_adm_pred_vs_observations_4.png"), 
        p, 
        width = 15, 
        height = 8, 
