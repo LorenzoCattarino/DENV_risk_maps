11e55632cd93f5155007d53d3a36bb5d9d82d253
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-06-21 14:30:52 +0100

    Added year.f and year.i arguments

--- R/prepare_datasets/calculate_amplitude.R
+++ R/prepare_datasets/calculate_amplitude.R
@@ -1,3 +1,4 @@
-calc_amplitude <- function(x, var_1, var_2, a, b) {
-  2 * sqrt((x[, var_1])^2 + (x[, var_2])^2) / (a * b)
+calc_amplitude <- function(x, var_1, var_2, year.f, year.i, ppyear) {
+  ntimes <- (year.f - year.i + 1) * ppyear
+  2 * sqrt((x[, var_1])^2 + (x[, var_2])^2) / ntimes
 }
