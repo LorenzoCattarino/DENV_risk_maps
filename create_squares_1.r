c51d3ee3a020e48bce4685f807162cd76891bd1d
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-06-22 10:59:51 +0100

    created new 20 km square tiles with Sam Bhatt and Kris Murray's predictors

--- create_squares_1.r
+++ create_squares_1.r
@@ -6,44 +6,54 @@ CLUSTER <- TRUE
 
 my_resources <- c(
   file.path("R", "prepare_datasets", "resample.R"),
-  file.path("R", "prepare_datasets", "clean_and_resample.r"),
+  file.path("R", "prepare_datasets", "clean_and_resample.R"),
   file.path("R", "prepare_datasets", "grid_up.R"),
-  file.path("R", "prepare_datasets", "remove_NA_rows.r"),
+  file.path("R", "prepare_datasets", "remove_NA_rows.R"),
   file.path("R", "prepare_datasets", "average_up.R"))
 
 my_pkgs <- c("data.table", "dplyr")
 
-lcf <- provisionr::package_sources(local = file.path("R_binaries", "data.table_1.10.5.zip"))
 context::context_log_start()
 ctx <- context::context_save(path = "context",
                              packages = my_pkgs,
-                             sources = my_resources,
-                             package_sources = lcf)
+                             sources = my_resources)
 
 
-# ---------------------------------------- define parameters
+# define parameters ----------------------------------------------------------- 
 
 
-gr_size <- 5 # kms
-
-new_res <- (1 / 120) * gr_size # decimal degrees
+parameters <- list(
+  grid_size = 1 / 120,       # decimal degrees
+  resample_grid_size = 20)   # kms  
 
 group_fields <- c("cell", "lat.grid", "long.grid")
 
-in_pt <- file.path("data", "env_variables", "all_sets_gadm_codes")
+in_pt <- file.path("output", "env_variables", "tile_set_2", "gadm")
 
-out_pt <- file.path(
-  "output",
-  "env_variables",
-  paste0("tile_set_", gr_size, "_km"))
+altitude_var <- "altitude"
+fourier_transform_elem <- c("const_term",	"Re0",	"Im0",	"Re1",	"Im1")
+FTs_dt <- c("DayTemp", "EVI", "MIR", "NightTemp", "RFE")
 
 
-# ---------------------------------------- are you using the cluster? 
+# define variables ------------------------------------------------------------
+
+
+re_gr_size <- parameters$resample_grid_size 
+
+new_res <- parameters$grid_size * re_gr_size
+
+out_pt <- file.path("output",
+                    "env_variables",
+                    paste0("tile_set_2_", re_gr_size, "km"))
+
+
+# are you using the cluster? --------------------------------------------------  
 
 
 if (CLUSTER) {
   
-  obj <- didehpc::queue_didehpc(ctx)
+  config <- didehpc::didehpc_config(template = "20Core")
+  obj <- didehpc::queue_didehpc(ctx, config = config)
   
 } else {
   
@@ -52,30 +62,23 @@ if (CLUSTER) {
 }
 
 
-# ---------------------------------------- load data 
+# pre processing -------------------------------------------------------------- 
 
 
-all_predictors <- read.table(
-  file.path("output", 
-            "datasets", 
-            "all_predictors.txt"), 
-  header = TRUE, 
-  stringsAsFactors = FALSE)
+all_combs_df <- expand.grid(FTs_dt, fourier_transform_elem)
 
+all_combs_names <- apply(all_combs_df, 1, function(x) paste(x[1], x[2], sep = "_"))
 
-# ---------------------------------------- pre processing
-
-
-wanted_preds_ids <- grep("^lct", all_predictors$variable, invert = TRUE) 
-
-var_names <- all_predictors$variable[wanted_preds_ids]
+var_names <- c(altitude_var, all_combs_names)
+                   
+var_names <- c(var_names, "travel_time", "TSI", "aedes_gen")
 
 fi <- list.files(in_pt, 
                  pattern = "^tile",
                  full.names = TRUE)
 
 
-# ---------------------------------------- submit one job
+# submit one job -------------------------------------------------------------- 
 
 
 # t <- obj$enqueue(
@@ -86,7 +89,7 @@ fi <- list.files(in_pt,
 #   out_path = out_pt))
 
 
-# ---------------------------------------- submit all jobs
+# submit all jobs ------------------------------------------------------------- 
 
 
 if (CLUSTER) {
@@ -103,7 +106,7 @@ if (CLUSTER) {
 }else{
 
   resample_tiles <- lapply(
-    fi[185],
+    fi[45],
     resample,
     grp_flds = group_fields,
     grid_size = new_res,
