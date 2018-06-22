ce8e11d1cf68c361c3d7dc32fa44ffa89488647e
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-06-22 11:38:28 +0100

    changed function names

--- R/prepare_datasets/resample.R
+++ R/prepare_datasets/resample.R
@@ -1,4 +1,4 @@
-resample <- function(x, grp_flds, grid_size, env_var_names, out_path){
+load_clean_and_average <- function(x, grp_flds, grid_size, env_var_names, out_path){
   
   # browser()
   
@@ -11,7 +11,7 @@ resample <- function(x, grp_flds, grid_size, env_var_names, out_path){
   
   if(is.character(tile$ID_0)) stop("ID_0 is a character")
   
-  cc <- clean_and_resample(tile, env_var_names, grid_size, grp_flds)
+  cc <- clean_and_average(tile, env_var_names, grid_size, grp_flds)
   
   names(cc)[names(cc) == "lat.grid"] <- "latitude"
   
