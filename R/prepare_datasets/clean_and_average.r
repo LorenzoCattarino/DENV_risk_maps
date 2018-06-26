5963d8fa0214aacde68667cfe8b5e0598d95d75c
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-06-26 09:20:15 +0100

    set the NA aedes generation values to zero

--- R/prepare_datasets/clean_and_average.r
+++ R/prepare_datasets/clean_and_average.r
@@ -4,9 +4,15 @@ clean_and_average <- function(dat,
                               grp_flds,
                               resample) {
   
-  #browser()
+  # browser()
   
-  dat[, env_vars][dat[, env_vars] == 0] <- NA
+  dat[is.na(dat$aedes_gen), "aedes_gen"] <- 0 
+    
+  extra_layers <- c("travel_time", "TSI", "aedes_gen")
+  
+  env_vars_to_filter <- env_vars[!env_vars %in% extra_layers]
+  
+  dat[, env_vars_to_filter][dat[, env_vars_to_filter] == 0] <- NA
   
   xx <- remove_NA_rows(dat, env_vars)
   
