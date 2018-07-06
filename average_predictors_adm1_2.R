50eeaaf6bda28040f533b0033a2f57322d050278
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-07-06 09:47:39 +0100

    moved part when you calculate the admin unit area  to a different script

--- average_predictors_adm1_2.R
+++ average_predictors_adm1_2.R
@@ -7,7 +7,7 @@ my_resources <- c(
   file.path("R", "prepare_datasets", "average_up.R"),
   file.path("R", "utility_functions.R"))
 
-my_pkgs <- c("data.table", "dplyr", "rgdal")
+my_pkgs <- "data.table"
 
 context::context_log_start()
 ctx <- context::context_save(path = "context",
@@ -32,14 +32,6 @@ FTs_dt <- c("DayTemp", "EVI", "MIR", "NightTemp", "RFE")
 out_fl_nm <- "all_adm1_env_var.csv"
 
 out_pt <- file.path("output", "env_variables")
-
-map_proj <- "+proj=moll"
-
-
-# load data -------------------------------------------------------------------
-
-
-shp_fl_adm1 <- readOGR(file.path("data", "shapefiles", "gadm28_levels.shp"), "gadm28_adm1")
 
 
 # pre processing -------------------------------------------------------------- 
@@ -75,27 +67,8 @@ all_adm1_covariates_dupl <- do.call("rbind", all_tiles)
 ret <- average_up(all_adm1_covariates_dupl, group_fields, my_predictors)
 
 
-# calculate population density ------------------------------------------------
-
-
-shp_pr <- spTransform(shp_fl_adm1, map_proj)
-
-all_areas <- sapply(shp_pr@polygons, function(x) x@area)
-
-# from sq m to sq km
-shp_pr@data$Shape_Area <- all_areas / 1000000 
-
-shp_pr@data$ID_0 <- as.numeric(as.character(shp_pr@data$ID_0))
-shp_pr@data$ID_1 <- as.numeric(as.character(shp_pr@data$ID_1))
-
-shp_pr@data <- shp_pr@data[!duplicated(shp_pr@data[, c("ID_0", "ID_1")]), ]
-
-ret_2 <- left_join(ret, shp_pr@data[, c("ID_0", "ID_1", "Shape_Area")], by = c("ID_0", "ID_1"))
-
-ret_2$log_pop_den <- log(1 + (ret_2$population / ret_2$Shape_Area))  
-
-
 # save ------------------------------------------------------------------------
 
 
-write_out_csv(ret_2, out_pt, out_fl_nm)
+write_out_csv(ret, out_pt, out_fl_nm)
+
