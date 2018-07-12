c89d015f8ed7881e6ae029153f1ab30d76315991
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-07-12 10:27:55 +0100

    add function to plot EM diagnostics

---
+++ R/plotting/plot_EM_diagnostics.R
@@ -0,0 +1,46 @@
+plot_EM_diagnostics <- function(my_data, 
+                                out_path,
+                                out_name){
+  
+  diagnostic_vars <- c("RF_ms_i", "ss_i", "ss_j", "r_av_sqr", "r_adm")
+  
+  strip_labs <- c("mean square error", 
+                  "pixel level sum of square", 
+                  "admin unit level sum of square",
+                  "pixel level correlation",
+                  "admin unit level correlation") 
+  
+  names(strip_labs) <- diagnostic_vars
+  
+  data_to_plot <- as.data.frame(my_data)
+  
+  data_to_plot$iter <- seq_len(nrow(data_to_plot))
+  
+  data_to_plot_long <- reshape2::melt(data_to_plot, 
+                                      id.vars = "iter", 
+                                      measure.vars = diagnostic_vars)
+  
+  dir.create(out_path, FALSE, TRUE)
+  
+  p <- ggplot(data_to_plot_long, aes(iter, value)) +
+    geom_line() +
+    scale_x_continuous("Iterations", breaks = seq_len(10), labels = seq_len(10)) +
+    scale_y_continuous(NULL) +
+    facet_wrap(~ variable, ncol = 2, scales = "free_y", labeller = labeller(variable = strip_labs)) +
+    theme(axis.title.x = element_text(size = 12),
+          axis.title.y = element_text(size = 12),
+          axis.text.x = element_text(size = 10),
+          axis.text.y = element_text(size = 10))
+  
+  png(file.path(out_path, out_name), 
+      width = 13, 
+      height = 13, 
+      units = "cm",
+      pointsize = 12,
+      res = 200)
+  
+  print(p)
+  
+  dev.off()
+  
+}
