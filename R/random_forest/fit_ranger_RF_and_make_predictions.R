8b8ae5e92992451e0d864f2f152d24895f77942e
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-07-17 11:28:15 +0100

    added two functions which use ranger()

--- R/random_forest/fit_ranger_RF_and_make_predictions.R
+++ R/random_forest/fit_ranger_RF_and_make_predictions.R
@@ -29,6 +29,24 @@ make_ranger_predictions <- function(mod_obj, dataset, sel_preds){
   
 }
 
+wrapper_to_make_ranger_preds <- function(i, 
+                                         RF_mod_name, 
+                                         model_in_path, 
+                                         dataset,
+                                         predictors) {
+  
+  #browser()
+  
+  cat("bootstrap sample =", i, "\n")
+  
+  RF_obj_nm <- paste0(RF_mod_name, "_", i, ".rds")
+  
+  RF_obj <- readRDS(file.path(model_in_path, RF_obj_nm))
+  
+  make_ranger_predictions(RF_obj, dataset, predictors)
+  
+}
+
 fit_predict_and_error <- function(dataset, 
                                   y_var, 
                                   my_preds,
@@ -201,4 +219,129 @@ load_predict_and_save <- function(i,
   
   write_out_rds(pxl_dts_boot, out_file_path, out_file_name)
   
+}
+
+attach_pred_different_scale_to_data <- function(i, 
+                                                model_path, 
+                                                foi_data,
+                                                adm_dts, 
+                                                predictors, 
+                                                all_sqr_preds,
+                                                sqr_dts, 
+                                                tile_ids,
+                                                bt_samples,
+                                                out_path,
+                                                grp_fields,
+                                                parms){
+  
+  
+  #browser()
+  
+  
+  # -------------------------------------- define variables
+  
+  
+  var_to_fit <- parms$dependent_variable
+  foi_offset <- parms$foi_offset
+  
+  RF_obj_nm <- paste0("RF_obj_sample_", i, ".rds")
+  
+  out_name <- paste0("all_scale_predictions_", i, ".rds")
+  
+  
+  # -------------------------------------- load data
+  
+  
+  RF_obj <- readRDS(file.path(model_path, RF_obj_nm))
+  
+  
+  # -------------------------------------- process admin predictions
+  
+  
+  adm_dts_2 <- remove_NA_rows(adm_dts, predictors)
+  
+  adm_pred <- make_ranger_predictions(RF_obj, adm_dts_2, predictors)
+  
+  if(var_to_fit == "FOI"){
+    
+    adm_pred <- adm_pred - foi_offset 
+    all_sqr_preds <- all_sqr_preds - foi_offset
+    
+  }
+  
+  adm_dts_2$admin <- adm_pred
+  
+  fltr_adm <- inner_join(adm_dts_2, foi_data[, grp_fields])
+  
+  
+  # -------------------------------------- process square predictions
+  
+  
+  sqr_preds <- all_sqr_preds[, i]
+  
+  sqr_dts <- cbind(sqr_dts[, c(grp_fields, "population")],
+                   square = sqr_preds)
+  
+  average_sqr <- average_up(
+    pxl_df = sqr_dts,
+    grp_flds = grp_fields,
+    var_names = "square")
+  
+  
+  # -------------------------------------- process 1 km predictions
+  
+  
+  # #[c(140, 141, 170, 171)]
+  # 
+  # tile_prds <- loop(
+  #   seq_along(tile_ids),
+  #   load_predict_filter,
+  #   ids_vec = tile_ids,
+  #   predictors = predictors,
+  #   RF_obj = RF_obj,
+  #   foi_dts = foi_data,
+  #   grp_flds = grp_fields,
+  #   parallel = FALSE)
+  # 
+  # tile_prds_rb <- do.call("rbind", tile_prds)
+  # 
+  # average_pxl <- average_up(
+  #   pxl_df = tile_prds_rb,
+  #   grp_flds = grp_fields,
+  #   var_names = "pred")
+  # 
+  # names(average_pxl)[names(average_pxl) == "pred"] <- "mean_pxl_pred"
+  
+  
+  # -------------------------------------- join admin, square and pixel level predictions
+  
+  
+  df_lst <- list(foi_data[, c(grp_fields, "type", "o_j")],
+                 fltr_adm[, c(grp_fields, "admin")],
+                 average_sqr[, c(grp_fields, "square")])#,
+  #average_pxl[, c(grp_fields, "mean_pxl_pred")]) 
+  
+  join_all <- Reduce(function(...) left_join(...), df_lst)
+  
+  
+  # --------------------------------------
+  
+  
+  bt_dts <- bt_samples[[i]]
+  
+  ids <- unique(bt_dts$data_id)
+  
+  train_ids <- rep(0, nrow(foi_data))
+  
+  train_ids[ids] <- 1
+  
+  join_all$train <- train_ids
+  
+  
+  # -------------------------------------- save 
+  
+  
+  write_out_rds(join_all, out_path, out_name)
+  
+
 }
