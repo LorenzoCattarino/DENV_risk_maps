8207385650c201f1770bca77984962a10503b563
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-08-01 11:27:23 +0100

    constrained the value of some predictors (edited off now); assinged a background weight (new_weight) of 1 only to the 20 km squares containing the original serology points.

--- EM_algorithm_05.R
+++ EM_algorithm_05.R
@@ -2,7 +2,7 @@
 
 options(didehpc.cluster = "fi--didemrchnb")
 
-CLUSTER <- TRUE
+CLUSTER <- FALSE
 
 my_resources <- c(
   file.path("R", "random_forest", "fit_ranger_RF_and_make_predictions.R"),
@@ -11,9 +11,10 @@ my_resources <- c(
   file.path("R", "plotting", "quick_raster_map.R"),
   file.path("R", "plotting", "generic_scatter_plot.R"),
   file.path("R", "prepare_datasets", "calculate_wgt_corr.R"),
-  file.path("R", "utility_functions.R"))  
+  file.path("R", "utility_functions.R"),
+  file.path("R", "plotting", "functions_for_plotting_square_level_maps.R"))  
 
-my_pkgs <- c("ranger", "dplyr", "fields", "ggplot2", "weights", "colorRamps")
+my_pkgs <- c("ranger", "dplyr", "fields", "ggplot2", "weights", "colorRamps", "raster")
 
 context::context_log_start()
 ctx <- context::context_save(path = "context",
@@ -25,6 +26,7 @@ ctx <- context::context_save(path = "context",
 
 
 parameters <- list(
+  resample_grid_size = 20,
   dependent_variable = "FOI",
   shape_1 = 0,
   shape_2 = 5,
@@ -50,11 +52,11 @@ tra_dts_nm <- "train_dts.rds"
 
 out_fl_nm <- "square_predictions_all_data.rds"
 
-foi_dts_nm <- "All_FOI_estimates_and_predictors.csv"
+foi_dts_nm <- "All_FOI_estimates_and_predictors_2.csv"
 
 pxl_dts_name <- "covariates_and_foi_20km.rds"
 
-model_type_tag <- "_best_model_3"
+model_type_tag <- "_best_model_5"
 
 extra_predictors <- NULL
 
@@ -73,6 +75,8 @@ pseudoAbsence_value <- parameters$pseudoAbs_value
 all_wgt <- parameters$all_wgt
 
 model_type <- paste0(var_to_fit, model_type_tag)
+
+res <- (1 / 120) * parameters$resample_grid_size
 
 RF_out_pth <- file.path("output", 
                         "EM_algorithm", 
@@ -112,7 +116,7 @@ out_pt <- file.path("output", "EM_algorithm", "best_fit_models", model_type)
 
 if (CLUSTER) {
   
-  config <- didehpc::didehpc_config(template = "24Core")
+  config <- didehpc::didehpc_config(template = "20Core")
   obj <- didehpc::queue_didehpc(ctx, config = config)
 
 } else {
@@ -149,6 +153,14 @@ adm_covariates <- read.csv(file.path("output",
 # pre processing --------------------------------------------------------------
 
 
+# pxl_data$NightTemp_const_term <- ifelse(pxl_data$NightTemp_const_term > 6000, 6000, pxl_data$NightTemp_const_term)
+# pxl_data$log_pop_den <- ifelse(pxl_data$log_pop_den > 0.6, 0.6, pxl_data$log_pop_den)
+# pxl_data[pxl_data$square == 229595, "population"] <- 10000
+# grid_size <- (1 / 120) * 20
+# sqr_area_km <- (grid_size * 111.32)^2
+# pxl_data[pxl_data$square == 229595, "pop_den"] <- pxl_data[pxl_data$square == 229595, "population"] / sqr_area_km
+# pxl_data[pxl_data$square == 229595, "log_pop_den"] <- log(1 + pxl_data[pxl_data$square == 229595, "pop_den"])
+
 names(foi_data)[names(foi_data) == var_to_fit] <- "o_j"
 
 foi_data[foi_data$type == "pseudoAbsence", "o_j"] <- pseudoAbsence_value
@@ -165,18 +177,79 @@ if(var_to_fit == "FOI"){
 
 adm_covariates <- adm_covariates[!duplicated(adm_covariates[, c("ID_0", "ID_1")]), ]
 
-pxl_dts_grp <- pxl_data %>% group_by_(.dots = grp_flds) 
+pxl_data <- inner_join(pxl_data, foi_data[, c(grp_flds, "type", "o_j", "new_weight")])
+
+
+# fix serology new_weights ----------------------------------------------------
+
+
+pxl_data[pxl_data$type == "serology", "new_weight"] <- 0
+
+sero_points <- foi_data[foi_data$type == "serology", ]
+
+pxl_data$lat.int <- round(pxl_data$latitude / res)
+pxl_data$long.int <- round(pxl_data$longitude / res)
+
+sero_points$lat.int <- round(sero_points$latitude / res)
+sero_points$long.int <- round(sero_points$longitude / res)
+
+sero_points$cell <- 0
+sero_points$no_square <- 0
+
+for (i in seq_len(nrow(sero_points))){
+
+  sero_long <- sero_points[i, "long.int"]
+  sero_lat <- sero_points[i, "lat.int"]
+
+  matches <- pxl_data$type == "serology" & pxl_data$lat.int == sero_lat & pxl_data$long.int == sero_long
+
+  if(sum(matches) != 0){
+
+    message(i)
+
+    cell_id <- which(matches == TRUE)[1]
+    sero_points[i, "cell"] <- cell_id
+    pxl_data[cell_id, "new_weight"] <- 1
+
+  } else {
+
+    sero_points[i, "no_square"] <- 1
+
+  }
+
+}
+
+missing_square <- sero_points[sero_points$no_square == 1, ]
+write.csv(missing_square, file.path("output", "EM_algorithm", "missing_squares.csv"), row.names = FALSE)
+
+sero_pxl_no_dup <- pxl_data$type == "serology" & pxl_data$new_weight == 1
+
+pxl_data_2 <- pxl_data[!sero_pxl_no_dup, ]
+
+sero_pxl_dup <- pxl_data[sero_points$cell, ]
+
+sero_pxl_dup$data_id <- sero_points$data_id
+
+pxl_data_3 <- rbind(pxl_data_2, sero_pxl_dup)
+
+
+# get pop weights -------------------------------------------------------------
+
+
+pxl_dts_grp <- pxl_data_3 %>% group_by_(.dots = grp_flds) 
 
 aa <- pxl_dts_grp %>% summarise(pop_sqr_sum = sum(population))
 # ncells <- pxl_dts_grp %>% summarise(n_sqr = n())
-  
-pxl_data <- left_join(pxl_data, aa)
+
+pxl_data_3 <- left_join(pxl_data_3, aa)
 # pxl_data <- left_join(pxl_data, ncells)
 
-pxl_data$pop_weight <- pxl_data$population / pxl_data$pop_sqr_sum
+pxl_data_3$pop_weight <- pxl_data_3$population / pxl_data_3$pop_sqr_sum
 # pxl_data$pop_weight <- 1 / pxl_data$n_sqr
 
-pxl_data <- inner_join(pxl_data, foi_data[, c(grp_flds, "o_j", "new_weight")])
+
+# -----------------------------------------------------------------------------
+
 
 my_predictors <- predictor_rank$name[1:number_of_predictors]
 my_predictors <- c(my_predictors, extra_predictors)
@@ -191,7 +264,7 @@ if (CLUSTER) {
     exp_max_algorithm(
       parms = parameters,
       orig_dataset = foi_data,
-      pxl_dataset = pxl_data,
+      pxl_dataset = pxl_data_3,
       pxl_dataset_full = pxl_data,
       my_predictors = my_predictors, 
       grp_flds = grp_flds,
@@ -212,7 +285,7 @@ if (CLUSTER) {
   EM_alg_run <- exp_max_algorithm(
     parms = parameters,
     orig_dataset = foi_data,
-    pxl_dataset = pxl_data, 
+    pxl_dataset = pxl_data_3, 
     pxl_dataset_full = pxl_data,
     my_predictors = my_predictors, 
     grp_flds = grp_flds,
