6b22d9991505fda9c12a805de6cc02333cde4337
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-08-01 11:23:57 +0100

    create the disaggregated dataset by filtering the global 20 km square data set (~400k squares)

--- EM_algorithm_02.R
+++ EM_algorithm_02.R
@@ -1,74 +1,29 @@
-# Loads filtered and resampled tiles, and rbind them together
-# Also removed NA squares, assigned ID and renamed cell field
-
-options(didehpc.cluster = "fi--didemrchnb")
-
-CLUSTER <- TRUE
-
-my_resources <- c(
-  file.path("R", "prepare_datasets", "filter_and_resample.R"),
-  file.path("R", "prepare_datasets", "clean_and_average.R"),
-  file.path("R", "prepare_datasets", "remove_NA_rows.R"),
-  file.path("R", "prepare_datasets", "grid_up.R"),
-  file.path("R", "prepare_datasets", "average_up.R"),
-  file.path("R", "utility_functions.R"))
-
-my_pkgs <- c("data.table", "dplyr")
-
-context::context_log_start()
-ctx <- context::context_save(path = "context",
-                             packages = my_pkgs,
-                             sources = my_resources)
+# Filters the global 20 km square dataset based on the original foi dataset 
 
 
-# define parameters -----------------------------------------------------------  
+library(dplyr)
 
 
-parameters <- list(
-  resample_grid_size = 20,
-  no_predictors = 26)   
-
-out_pt <- file.path("output", "EM_algorithm", "best_fit_models", "env_variables")
-
-out_fl_nm <- "env_vars_20km.rds"
+# define parameters -----------------------------------------------------------
 
 
-# rebuild the queue ----------------------------------------------------------- 
+join_fields <- c("data_id", "ID_0", "ID_1")
 
 
-if (CLUSTER) {
-  
-  obj <- didehpc::queue_didehpc(ctx)
-
-} else {
-  
-  context::context_load(ctx)
-
-}
-
-all_tasks <- obj$task_bundle_info()
-
-task_b_name <- all_tasks[nrow(all_tasks), "name"]
-
-pxl_job_t <- obj$task_bundle_get(task_b_name)
-
-pxl_job <- pxl_job_t$results()
+# load data -------------------------------------------------------------------
 
 
-# run ------------------------------------------------------------------------- 
+foi_data <- read.csv(file.path("output", "foi", "All_FOI_estimates_and_predictors.csv"),
+                     stringsAsFactors = FALSE) 
+
+all_sqr_covariates <- readRDS(file.path("output", 
+                                        "env_variables", 
+                                        "all_squares_env_var_0_1667_deg.rds"))
 
 
-all_pixel_df <- do.call("rbind", pxl_job)
+# join ------------------------------------------------------------------------
 
-# check duplicate cell values - it is OK to have duplicate! 
-# sum(duplicated(all_pixel_df[,1:4]))
 
-# assign cell ID
-all_pixel_df$cell <- seq_len(nrow(all_pixel_df))
+pxl_data <- inner_join(all_sqr_covariates, foi_data[, join_fields])
 
-all_pixel_df$log_pop_den <- log(1 + all_pixel_df$pop_den) 
-
-# rename 
-names(all_pixel_df)[names(all_pixel_df) == "cell"] <- "square"
-
-write_out_rds(all_pixel_df, out_pt, out_fl_nm)
+write_out_rds(pxl_data, out_pt, "env_vars_20km.rds")
