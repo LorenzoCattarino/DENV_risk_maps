34155e7990c1f47228abc9bf302fcdd78217a17e
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-08-02 09:52:35 +0100

    removed edited off lines

--- R/prepare_datasets/load_clean_and_average.R
+++ R/prepare_datasets/load_clean_and_average.R
@@ -13,9 +13,6 @@ load_clean_and_average <- function(x, grp_flds, grid_size, env_var_names, out_pa
   
   cc <- clean_and_average(tile, env_var_names, grid_size, grp_flds, resample)
   
-  # names(cc)[names(cc) == "lat.grid"] <- "latitude"
-  # names(cc)[names(cc) == "long.grid"] <- "longitude"
-  
   file_name <- basename(x) 
   
   dir.create(out_path, FALSE, TRUE)
