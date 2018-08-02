62d1fea71a67a78c6f564859ce58bb1ed013c71b
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-08-02 13:17:44 +0100

    broke up into 1) geocoding and 2) map overlaying routines

--- assemble_foi_dataset_1.r
+++ assemble_foi_dataset_1.r
@@ -1,15 +1,10 @@
-# 1) Geocode and map overlay to find the admin unit of each data point 
-#    in each source dataset
-# 2) Combine different source datasets (serology, case surveillance) 
-#    into a single data frame
+# Geocode location of each data point to find long and lat
 
 # install dev version of ggmap for geocode()
 if(!require("ggmap", character.only = TRUE)) {
   devtools::install_github("dkahle/ggmap")
 }
 
-# load packages
-library(rgdal)
 library(geosphere) # for areaPolygon() 
 
 
@@ -24,9 +19,8 @@ library(geosphere) # for areaPolygon()
 # ===================================================================
 
 
-# load functions 
-source(file.path("R", "prepare_datasets", "functions_for_geolocating_admin_units.r"))
-source(file.path("R", "utility_functions.r"))
+source(file.path("R", "prepare_datasets", "geolocating_data_points.R"))
+source(file.path("R", "utility_functions.R"))
 
 
 # define parameters -----------------------------------------------------------  
@@ -34,19 +28,23 @@ source(file.path("R", "utility_functions.r"))
 
 my_api_key <- "AIzaSyBuHLASHGLaorGdZidB5sNa-9C2fxXYj1c"
 
-datasets <- c("NonSerotypeSpecificDatasets",
-              "SerotypeSpecificDatasets",
-              "All_caseReport_datasets",
-              "additional serology",
-              "additional_India_sero_data_Shah",
+datasets <- c("additional serology",
               "additional_India_sero_data_Garg",
-              "additional_sero_data_feb2018")  
+              "additional_India_sero_data_Shah",
+              "additional_sero_data_feb2018",
+              "All_caseReport_datasets",
+              "NonSerotypeSpecificDatasets",
+              "SerotypeSpecificDatasets")
+
+in_pt <- file.path("data", "foi")
+
+foi_out_pt <- file.path("output", "foi")
+
+foi_out_nm <- "FOI_estimates_lon_lat.csv"
 
 fields <- c("type", 
-            "ID_0", 
             "ISO",
             "country", 
-            "ID_1", 
             "FOI", 
             "variance", 
             "latitude",
@@ -54,30 +52,21 @@ fields <- c("type",
             "reference", 
             "date")
 
-foi_out_pt <- file.path("output", "foi")
-foi_out_nm <- "All_FOI_estimates_linear.txt"
-
-info_names <- c("latitude", "longitude", "ID_0", "ID_1")
-
 
 # register your api key ------------------------------------------------------- 
 
 
-register_google(key = my_api_key, account_type = "premium", day_limit = 100000)
+register_google(key = my_api_key, account_type = "premium", second_limit = 10000, day_limit = 10000000)
 
 
 # pre processing -------------------------------------------------------------- 
 
 
-output_dts <- vector("list", length = length(datasets))
+dts_paths <- list.files(in_pt, 
+                        pattern = "csv$",
+                        full.names = TRUE)
 
-dts_names <- sapply(datasets, function(x) paste(x, "csv", sep = "."), USE.NAMES = FALSE)
-
-dts_paths <- sapply(dts_names, function(x) file.path("data", "foi", x), USE.NAMES = FALSE)
-
-
-# load data ------------------------------------------------------------------- 
-
+output_dts <- vector("list", length = length(dts_paths))
 
 all_dts <- lapply(dts_paths, read.csv, header = TRUE, sep = ",", stringsAsFactors = FALSE)
 
@@ -85,13 +74,13 @@ all_dts <- lapply(dts_paths, read.csv, header = TRUE, sep = ",", stringsAsFactor
 # loop ------------------------------------------------------------------------ 
 
 
-for (i in 1:length(datasets)){
+for (i in seq_along(all_dts)){
 
   one_dts <- all_dts[[i]]  
 
   dts_name <- datasets[i]
     
-  if(dts_name == "All_caseReport_datasets"){
+  if(dts_name == "All_caseReport_datasets") {
     
     one_dts$type <- "caseReport"
     
@@ -103,116 +92,34 @@ for (i in 1:length(datasets)){
   
   one_dts_ls <- df_to_list(one_dts, use_names = TRUE)  
   
-  n <- length(info_names)
+  n <- 2 # lon and lat
   
-  admin_info <- vapply(one_dts_ls, get_admin_info, numeric(n), info_names)  
+  x_and_y <- vapply(one_dts_ls, get_xy, numeric(n))  
   
-  output_dts[[i]] <- cbind(one_dts, t(admin_info))
+  x_and_y <- t(x_and_y)
   
-}
+  colnames(x_and_y) <- c("longitude", "latitude")
+  
+  output_dts[[i]] <- cbind(one_dts, x_and_y)
+  
+}  
+  
 
-# subset
-output_dts_2 <- lapply(output_dts, function(x) x[, fields])
+# subset ----------------------------------------------------------------------
 
-# bind all together 
+
+output_dts_2 <- lapply(output_dts, subset_list_df, fields)
+
 All_FOI_estimates <- do.call("rbind", output_dts_2)
 
-# remove missing data 
 All_FOI_estimates <- subset(All_FOI_estimates, !is.na(FOI))
   
-# remove outliers 
 All_FOI_estimates <- subset(All_FOI_estimates, ISO != "PYF" & ISO != "HTI")
-
-#dengue_point_countries <- All_FOI_estimates[!duplicated(All_FOI_estimates[, c("country", "ID_0")]), c("country", "ID_0")]
 
 
 # save ------------------------------------------------------------------------  
 
 
-write.table(All_FOI_estimates, 
-            file.path(foi_out_pt, foi_out_nm), 
-            row.names = FALSE, 
-            sep = ",")
-
-
-# # ---------------------------------------- Calculate FOI^2  
-# 
-# 
-# All_FOI_estimates$FOI <- All_FOI_estimates$FOI^2
-# 
-# write.table(All_FOI_estimates, 
-#             file.path("data", "foi", "All_FOI_estimates_squared.csv"), row.names = FALSE, sep = ",")
-# 
-# 
-# #--------------------------------------------------------------------------------------------------------------
-# # Calculate mean and se of FOI on the log scale, 
-# # by assuming that FOI on the linear scale is lognormally distributed
-# 
-# get.mean.logscale <- function(mean_lognormal, var_lognormal)
-# {
-#   log(mean_lognormal^2 / sqrt(var_lognormal + mean_lognormal^2))  
-# }  
-#   
-# get.var.logscale <- function(mean_lognormal, var_lognormal)
-# {
-#   log(1 + (var_lognormal / mean_lognormal^2))  
-# }  
-# 
-# NonSerotypeSpecific_datasets_adm_names$FOI_logscale <- get.mean.logscale(NonSerotypeSpecific_datasets_adm_names$FOI, 
-#                                                                          NonSerotypeSpecific_datasets_adm_names$variance)
-# 
-# NonSerotypeSpecific_datasets_adm_names$var_logscale <- get.var.logscale(NonSerotypeSpecific_datasets_adm_names$FOI, 
-#                                                                         NonSerotypeSpecific_datasets_adm_names$variance)
-# 
-# SerotypeSpecific_datasets_adm_names$FOI_logscale <- get.mean.logscale(SerotypeSpecific_datasets_adm_names$FOI,
-#                                                                       SerotypeSpecific_datasets_adm_names$variance)
-# 
-# SerotypeSpecific_datasets_adm_names$var_logscale <- get.var.logscale(SerotypeSpecific_datasets_adm_names$FOI,
-#                                                                      SerotypeSpecific_datasets_adm_names$variance)
-# 
-# additional_serology_datasets_adm_names$FOI_logscale <- get.mean.logscale(additional_serology_datasets_adm_names$FOI,
-#                                                                          additional_serology_datasets_adm_names$variance)
-# 
-# additional_serology_datasets_adm_names$var_logscale <- get.var.logscale(additional_serology_datasets_adm_names$FOI,
-#                                                                         additional_serology_datasets_adm_names$variance)
-# 
-# caseReport_datasets_adm_names$FOI_logscale <- log(caseReport_datasets_adm_names$FOI)
-# 
-# All_FOI_estimates_log_scale <- rbind(NonSerotypeSpecific_datasets_adm_names[, c("type","country","country_code","adm1","latitude","longitude","FOI_logscale","var_logscale")],
-#                                      SerotypeSpecific_datasets_adm_names[, c("type","country","country_code","adm1","latitude","longitude","FOI_logscale","var_logscale")],
-#                                      additional_serology_datasets_adm_names[, c("type","country","country_code","adm1","latitude","longitude","FOI_logscale","var_logscale")],
-#                                      caseReport_datasets_adm_names[, c("type","country","country_code","adm1","latitude","longitude","FOI_logscale","var_logscale")])
-# 
-# names(All_FOI_estimates_log_scale)[7:8] <- c("FOI", "variance")
-# 
-# write.table(All_FOI_estimates_log_scale, 
-#             file.path("data", "foi", "All_FOI_estimates_log_scale.csv"), row.names=FALSE, sep=",")
-# 
-# # Check density plot of variance on linear and log scale 
-# plot(density(x = All_FOI_estimates$variance, na.rm = TRUE))
-# plot(density(x = All_FOI_estimates_log_scale$variance, na.rm = TRUE))
-# 
-# tiff("histogram_of_variance_linear_scale.tiff")
-# print(hist(All_FOI_estimates$variance, breaks=1000, main = "linear scale"))
-# dev.off()
-# 
-# tiff("histogram_of_variance_log_scale.tiff")
-# print(hist(All_FOI_estimates_log_scale$variance, breaks=1000, main = "log scale"))
-# dev.off()
-# 
-# 
-# #--------------------------------------------------------------------------------------------------------------
-# # Calculate logit(FOI)  
-# 
-# All_FOI_estimates$FOI <- logit(All_FOI_estimates$FOI)
-# 
-# write.table(All_FOI_estimates, 
-#             file.path("data", "foi", "All_FOI_estimates_logit_scale.csv"), row.names=FALSE, sep=",")
-# 
-# #--------------------------------------------------------------------------------------------------------------
-# # Calculate 1/FOI  
-# 
-# All_FOI_estimates$FOI <- 1 / All_FOI_estimates$FOI
-# 
-# write.table(All_FOI_estimates, 
-#             file.path("data", "foi", "All_FOI_estimates_inverse.csv"), row.names = FALSE, sep = ",")
+write.csv(All_FOI_estimates, 
+          file.path(foi_out_pt, foi_out_nm), 
+          row.names = FALSE)
