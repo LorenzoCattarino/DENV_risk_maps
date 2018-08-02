ac29f8c97f3e1662e275be1e998db6a5056df2a2
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-08-02 09:57:21 +0100

    break up large function into several smaller ones

--- R/prepare_datasets/functions_for_geolocating_admin_units.r
+++ R/prepare_datasets/functions_for_geolocating_admin_units.r
@@ -1,23 +1,20 @@
-get_admin_info <- function(x, info_names){
+get_xy <- function(x){
+  
+  # geocode to find spatial coordinates using name of location
   
   country_name <- x$country
-  #cat("country = ", country_name, "\n")
+  # cat("country =", country_name, "\n")
   
   country_code <- x$ISO
-  #cat("country code = ", country_code, "\n")
+  # cat("country code =", country_code, "\n")
   
-  #admin_level <- as.numeric(x["admin"]) 
   admin_level <- 1 
   
   location <- x$test_location
-  #cat("location = ", location, "\n")
+  # cat("location =", location, "\n")
   
   location_str <- ifelse(country_name != location, paste(location, country_name, sep = ", "), location)
   #cat("location string = ", location_str, "\n")
-  
-  
-  # geocode to find spatial coordinates using name of location ---------------
-  
   
   geocode_run <- geocode(enc2utf8(location_str), output = "all") # take care of funny characters 
   
@@ -33,43 +30,69 @@ get_admin_info <- function(x, info_names){
     
     location_xy <- geocode_results[result_match, c("lng","lat")]
     
-    #cat("coordinates = ", unlist(location_xy), "\n")
+    location_xy <- as.matrix(location_xy)
     
   } else {
     
-    location_xy <- data.frame(lng = geocode_run$results[[1]]$geometry$location$lng,
-                              lat = geocode_run$results[[1]]$geometry$location$lat)
-    #cat("coordinates = ", unlist(location_xy), "\n")
+    location_xy <- cbind(geocode_run$results[[1]]$geometry$location$lng,
+                         geocode_run$results[[1]]$geometry$location$lat)
+    
   }
   
-  shp_folder <- paste(country_code, "adm_shp", sep="_")
+  location_xy
   
+}
+
+get_admin_info <- function(x, info_names, shp_ls){
+  
+  # map overlay to find admin unit name using spatial coordinates
+  
+  country_code <- x$ISO
+  # cat("country code =", country_code, "\n")
+  
+  shp <- shp_ls[[country_code]]
+
+  overlay <- map_overlay(location_xy, shp)
+  
+  setNames(c(overlay[["ID_0"]],
+             overlay[["ID_1"]]),
+           info_names)
+  
+}
+
+get_unique_iso <- function(x){
+  unique(x$ISO)
+}
+
+load_shp <- function(x){
+  
+  admin_level <- 1 
+  
+  country_code <- x 
+  shp_folder <- paste(country_code, "adm_shp", sep = "_")
   shp_name <- sprintf ("%s_adm%s", country_code, admin_level) 
   
-  shp <- readOGR(file.path("data", "shapefiles", shp_folder), 
-                 shp_name,
-                 stringsAsFactors = FALSE,
-                 integer64 = "allow.loss")
+  readOGR(file.path("data", "shapefiles", shp_folder), 
+          shp_name,
+          stringsAsFactors = FALSE,
+          integer64 = "allow.loss")
+
+}
+
+map_overlay <- function(dat, shp){
   
-  
-  # map overlay to find admin unit name using spatial coordinates ------------- 
-  
-  
+  location_xy <- dat[, c("longitude", "latitude")]
   xy_spdf <- SpatialPoints(location_xy, proj4string = shp@proj4string)
   
-  overlay <- over(xy_spdf, shp)
+  overlay <- sp::over(xy_spdf, shp)
   
-  #admin_name <- as.character(overlay$NAME_1)
-  #cat("admin 1 name = ", admin_name, "\n")
+  out_names <- c("ID_0", "ID_1", "name_0", "name_1")
   
-  country_numeric_code <- overlay$ID_0
-  adm_numeric_code <- overlay$ID_1
-  
-  setNames(c(location_xy$lat,
-             location_xy$lng,
-             country_numeric_code,
-             adm_numeric_code),
-           info_names)
+  setNames(list(overlay$ID_0,
+                overlay$ID_1,
+                overlay$NAME_0,
+                overlay$NAME_1), 
+           out_names)
   
 }
 
@@ -79,7 +102,7 @@ calculate_viewport_area <- function(x) {
                     c(x["point_2_x"], x["point_2_y"]),
                     c(x["point_3_x"], x["point_3_y"]),
                     c(x["point_4_x"], x["point_4_y"])))
-
+  
 }
 
 get_geocode_results <- function(geocode_run){
@@ -105,4 +128,11 @@ get_geocode_results <- function(geocode_run){
   all_results$point_4_y <- sapply(geocode_run$results, function(x){x$geometry$viewport$northeast$lat})
   
   all_results
+
+}
+
+subset_list_df <- function(x, fields){
+
+  x[, fields]
+
 }
