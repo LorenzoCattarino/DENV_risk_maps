6bd1ad0830304064b6d5969108fb111d69ba705e
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-08-06 11:48:00 +0100

    source foi dataset with tweaked lat and lon

--- EM_algorithm_01.R
+++ EM_algorithm_01.R
@@ -3,17 +3,21 @@
 
 library(dplyr)
 
+source(file.path("R", "utility_functions.R"))
+
 
 # define parameters -----------------------------------------------------------
 
 
 join_fields <- c("data_id", "ID_0", "ID_1")
 
+out_pt <- file.path("output", "EM_algorithm", "best_fit_models", "env_variables")
+
 
 # load data -------------------------------------------------------------------
 
 
-foi_data <- read.csv(file.path("output", "foi", "All_FOI_estimates_and_predictors.csv"),
+foi_data <- read.csv(file.path("output", "foi", "All_FOI_estimates_and_predictors_2.csv"),
                      stringsAsFactors = FALSE) 
 
 all_sqr_covariates <- readRDS(file.path("output", 
