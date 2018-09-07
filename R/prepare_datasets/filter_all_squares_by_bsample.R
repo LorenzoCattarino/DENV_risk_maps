1db49dc567cbf8ea4ebeeff792a58c11879d02f1
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-09-07 10:13:46 +0100

    remove edited off code (old filtering and resampling of bootstrap samples) which was wrong)

--- R/prepare_datasets/filter_all_squares_by_bsample.R
+++ R/prepare_datasets/filter_all_squares_by_bsample.R
@@ -11,27 +11,6 @@ filter_all_squares_by_bsample <- function(i,
   
   pxl_dataset <- inner_join(all_squares, foi_data[, jn_flds])
   
-  # names(foi_data)[names(foi_data) == "population"] <- "adm_pop"
-  #
-  # pxl_job <- loop(
-  #   tile_ls,
-  #   filter_and_resample,
-  #   foi_dts = foi_data, 
-  #   env_var_names = predictors, 
-  #   grp_flds = grp_flds, 
-  #   jn_flds = jn_flds,
-  #   grid_size = new_res,
-  #   resample = resample,
-  #   parallel = parallel_2)
-  # 
-  # pxl_dataset <- do.call("rbind", pxl_job)
-  # 
-  # pxl_dataset$cell <- seq_len(nrow(pxl_dataset))
-  # 
-  # pxl_dataset$log_pop_den <- log(1 + pxl_dataset$pop_den) 
-  # 
-  # names(pxl_dataset)[names(pxl_dataset) == "cell"] <- "square"
-  
   a <- out_file_name[i]
   
   write_out_rds(pxl_dataset, out_file_path, a)
