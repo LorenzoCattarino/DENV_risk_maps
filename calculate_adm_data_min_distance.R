6c3a1e2766bf771ca58e50040e50a0a7b1213c76
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-09-12 15:04:46 +0100

    remove unused functions and data; change name of input and output foi dataset

--- calculate_adm_data_min_distance.R
+++ calculate_adm_data_min_distance.R
@@ -1,23 +1,14 @@
 
 library(geosphere) # for distm()
 
-source(file.path("R", "random_forest", "fit_h2o_RF_and_make_predictions.r"))
-
 
 # load data ------------------------------------------------------------------- 
 
 
 foi_data <- read.csv(file.path("output", 
                                "foi", 
-                               "All_FOI_estimates_linear_env_var_area.csv"),
+                               "All_FOI_estimates_and_predictors.csv"),
                      stringsAsFactors = FALSE) 
-
-predictor_rank <- read.csv(file.path("output", 
-                                     "variable_selection", 
-                                     "metropolis_hastings", 
-                                     "exp_1", 
-                                     "variable_rank_final_fits_exp_1.csv"),
-                           stringsAsFactors = FALSE)
 
 
 # process ---------------------------------------------------------------------
@@ -36,5 +27,5 @@ new_foi_data <- cbind(foi_data, neighbor = foi_data[min.d,"data_id"],
 new_foi_data$distance <- new_foi_data$distance / 1000
 
 write.csv(new_foi_data, 
-          file.path("output", "foi", "All_FOI_estimates_linear_env_var_area_dis.csv"), 
+          file.path("output", "foi", "All_FOI_estimates_and_predictors_dis.csv"), 
           row.names = FALSE)
