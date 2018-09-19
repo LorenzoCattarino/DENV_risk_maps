542f74df3c397a3ca3ccb33d00e15015baae4518
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-09-19 15:41:58 +0100

    add all parameters; remove my_dir and another subdirectory

--- EM_algorithm_with_bootstrap_02.R
+++ EM_algorithm_with_bootstrap_02.R
@@ -21,26 +21,35 @@ ctx <- context::context_save(path = "context",
 
 
 parameters <- list(
+  id = 1,
+  shape_1 = 0,
+  shape_2 = 5,
+  shape_3 = 1e6,
+  all_wgt = 1,
   dependent_variable = "FOI",
   pseudoAbs_value = -0.02,
+  grid_size = 1 / 120,
+  no_predictors = 9,
+  resample_grid_size = 20,
   foi_offset = 0.03,
-  grid_size = 5,
   no_trees = 500,
   min_node_size = 20,
-  no_samples = 200,  
-  no_predictors = 23) 
+  no_samples = 200,
+  EM_iter = 10) 
 
 
 # define variables ------------------------------------------------------------
 
+
+model_type <- paste0("model_", parameters$id)
 
 my_dir <- paste0("grid_size_", parameters$grid_size)
 
 out_pt <- file.path("output", 
                     "EM_algorithm",
                     "bootstrap_models",
-                    my_dir, 
-                    paste0("model_objects_", parameters$dependent_variable, "_fit"), 
+                    model_type, 
+                    "model_objects", 
                     "boot_samples")
 
 
@@ -49,7 +58,7 @@ out_pt <- file.path("output",
 
 if (CLUSTER) {
   
-  config <- didehpc::didehpc_config(template = "20Core")
+  config <- didehpc::didehpc_config(template = "16Core")
   obj <- didehpc::queue_didehpc(ctx, config = config)
   
 } else {
