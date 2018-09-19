ddb7fa9964ff096aa1e069c1e31265b1a8e2dbb3
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-09-19 15:40:56 +0100

    add all parameters; remove my_dir

--- EM_algorithm_with_bootstrap_01.R
+++ EM_algorithm_with_bootstrap_01.R
@@ -21,14 +21,29 @@ ctx <- context::context_save(path = "context",
 
 
 parameters <- list(
-  grid_size = 5,
-  no_samples = 200)   
+  id = 1,
+  shape_1 = 0,
+  shape_2 = 5,
+  shape_3 = 1e6,
+  all_wgt = 1,
+  dependent_variable = "FOI",
+  pseudoAbs_value = -0.02,
+  grid_size = 1 / 120,
+  no_predictors = 9,
+  resample_grid_size = 20,
+  foi_offset = 0.03,
+  no_trees = 500,
+  min_node_size = 20,
+  no_samples = 200,
+  EM_iter = 10) 
 
 join_fields <- c("unique_id", "data_id", "ID_0", "ID_1")
 
 
 # define variables ------------------------------------------------------------
 
+
+model_type <- paste0("model_", parameters$id)
 
 no_samples <- parameters$no_samples
 
@@ -37,7 +52,7 @@ my_dir <- paste0("grid_size_", parameters$grid_size)
 out_pt <- file.path("output", 
                     "EM_algorithm",
                     "bootstrap_models",
-                    my_dir, 
+                    model_type, 
                     "env_variables", 
                     "boot_samples")
 
