6745377dd3c62a0636be5dd5a6bab933500aa0ee
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-09-20 09:40:29 +0100

    major changes - now it works fine

--- EM_algorithm_with_bootstrap_full_1.R
+++ EM_algorithm_with_bootstrap_full_1.R
@@ -5,15 +5,16 @@ CLUSTER <- TRUE
 
 my_resources <- c(
   file.path("R", "random_forest", "fit_ranger_RF_and_make_predictions.R"),
+  file.path("R", "random_forest", "EM_full_routine.R"),
   file.path("R", "random_forest", "exp_max_algorithm.R"),
-  file.path("R", "plotting", "quick_raster_map.R"),
+  file.path("R", "plotting", "functions_for_plotting_raster_maps.R"),
   file.path("R", "plotting", "generic_scatter_plot.R"),
-  file.path("R", "prepare_datasets", "calculate_wgt_corr.R"),
   file.path("R", "prepare_datasets", "remove_NA_rows.R"),
   file.path("R", "prepare_datasets", "average_up.R"),
+  file.path("R", "prepare_datasets", "calculate_wgt_corr.R"),
   file.path("R", "utility_functions.R"))
 
-my_pkgs <- c("ranger", "dplyr", "fields", "ggplot2", "weights", "colorRamps")
+my_pkgs <- c("ranger", "dplyr", "fields", "ggplot2", "weights")
 
 context::context_log_start()
 ctx <- context::context_save(path = "context",
@@ -25,21 +26,27 @@ ctx <- context::context_save(path = "context",
 
 
 parameters <- list(
-  dependent_variable = "FOI",
-  pseudoAbs_value = -0.02,
+  shape_1 = 0,
+  shape_2 = 5,
+  shape_3 = 1e6,
+  all_wgt = 1,
+  pseudoAbs_value = c(-0.02, 0.5),
+  resample_grid_size = 20,
   foi_offset = 0.03,
-  grid_size = 5,
   no_trees = 500,
   min_node_size = 20,
   no_samples = 200,
-  EM_iter = 10,
-  no_predictors = 26)   
+  EM_iter = 10) 
 
 grp_flds_1 <- c("ID_0", "ID_1", "unique_id")
 
 grp_flds_2 <- c("ID_0", "ID_1", "data_id")
 
-no_exp <- 20
+predictor_numbers <- c(9, 23, 26)
+
+response_vars <- c("FOI", "R0_1", "R0_2", "R0_3")
+  
+grid_sizes <- c(1/120, 0.5, 1, 2, 5, 10)
 
 
 # define variables ------------------------------------------------------------  
@@ -75,7 +82,6 @@ if (CLUSTER) {
 } else {
   
   context::context_load(ctx)
-  context::parallel_cluster_start(8, ctx)
   
 }
 
@@ -88,61 +94,63 @@ foi_data <- read.csv(file.path("output",
                                "All_FOI_estimates_and_predictors.csv"),
                      stringsAsFactors = FALSE) 
 
-bt_samples <- readRDS(file.path("output", 
-                                "EM_algorithm", 
-                                "bootstrap_models", 
-                                my_dir, 
-                                "bootstrap_samples.rds"))
-
 predictor_rank <- read.csv(file.path("output", 
                                      "variable_selection",
                                      "stepwise",
                                      "predictor_rank.csv"), 
                            stringsAsFactors = FALSE)
 
-covariates_squares <- readRDS(file.path("output", 
-                                        "EM_algorithm",
-                                        "best_fit_models",
+all_sqr_covariates <- readRDS(file.path("output", 
                                         "env_variables", 
-                                        "env_vars_20km.rds"))
+                                        "all_squares_env_var_0_1667_deg.rds"))
 
-covariates_adm <- read.csv(file.path("output",
-                                     "env_variables",
-                                     "All_adm1_env_var.csv"),
-                           header = TRUE,
-                           stringsAsFactors = FALSE)
+data_sqr_covariates <- readRDS(file.path("output", 
+                                         "EM_algorithm",
+                                         "best_fit_models",
+                                         "env_variables", 
+                                         "env_vars_20km_2.rds"))
+
+admin_covariates <- read.csv(file.path("output",
+                                       "env_variables",
+                                       "All_adm1_env_var.csv"),
+                             header = TRUE,
+                             stringsAsFactors = FALSE)
 
 
 # pre process -----------------------------------------------------------------
 
 
-names(foi_data)[names(foi_data) == parameters$dependent_variable] <- "o_j"
-
-foi_data[foi_data$type == "pseudoAbsence", "o_j"] <- parameters$pseudoAbs_value
-
 all_predictors <- predictor_rank$name
 
-test <- expand.grid(exp_id = seq_len(no_exp), rep_id = seq_len(no_samples))
-
-test <- test[order(test$exp_id, decreasing = FALSE), ]
-
-test_ls <- df_to_list(test, TRUE)
+test1 <- expand.grid(var = response_vars, gs = grid_sizes[c(1, 5)], no_pred = predictor_numbers, stringsAsFactors = FALSE)
   
+test2 <- expand.grid(var = response_vars[4], gs = grid_sizes[-c(1, 5)], no_pred = predictor_numbers[2], stringsAsFactors = FALSE)
+ 
+test <- rbind(test1, test2)
+
+test_all <- cbind(exp_id = seq_len(nrow(test)), test)
+
+test_all_2 <- expand.grid(exp_id = test_all$exp_id, rep_id = seq_len(no_samples))
+
+test_all_2 <- test_all_2[order(test_all_2$exp_id, decreasing = FALSE), ]
+
+test_all_3 <- left_join(test_all_2, test_all)
+
+test_ls <- df_to_list(test_all_3, TRUE)
+
   
 # run one job -----------------------------------------------------------------
 
 
 # t <- obj$enqueue(EM_full_routine(test_ls[[1]],
 #                                  parms = parameters,
-#                                  out_path = out_path,
-#                                  boot_ls = bt_samples,
-#                                  in_path = in_path,
+#                                  data_squares = data_sqr_covariates,
+#                                  all_squares = all_sqr_covariates,
 #                                  predictors = all_predictors,
 #                                  grp_flds_1 = grp_flds_1,
 #                                  grp_flds_2 = grp_flds_2,
-#                                  adm_dataset = covariates_adm,
-#                                  foi_data = foi_data,
-#                                  sqr_data = covariates_squares))
+#                                  adm_dataset = admin_covariates,
+#                                  foi_data = foi_data))
                  
 
 # run -------------------------------------------------------------------------
@@ -151,38 +159,34 @@ test_ls <- df_to_list(test, TRUE)
 if (CLUSTER) {
 
   multi_full_EM_experiments <- queuer::qlapply(
-    test_ls[401:4000],
+    test_ls[seq_len(200)],
     EM_full_routine,
     obj,
     parms = parameters,
-    out_path = out_path,
-    boot_ls = bt_samples,
-    in_path = in_path,
+    data_squares = data_sqr_covariates,
+    all_squares = all_sqr_covariates,
     predictors = all_predictors,
     grp_flds_1 = grp_flds_1,
     grp_flds_2 = grp_flds_2,
-    adm_dataset = covariates_adm,
-    foi_data = foi_data,
-    sqr_data = covariates_squares)
-  
+    adm_dataset = admin_covariates,
+    foi_data = foi_data)
+
 } else {
 
   multi_full_EM_experiments <- lapply(
     test_ls[1],
     EM_full_routine,
     parms = parameters,
-    out_path = out_path,
-    boot_ls = bt_samples,
-    in_path = in_path,
+    data_squares = data_sqr_covariates,
+    all_squares = all_sqr_covariates,
     predictors = all_predictors,
     grp_flds_1 = grp_flds_1,
     grp_flds_2 = grp_flds_2,
-    adm_dataset = covariates_adm,
-    foi_data = foi_data,
-    sqr_data = covariates_squares)
+    adm_dataset = admin_covariates,
+    foi_data = foi_data)
 
 }
 
 if(!CLUSTER){
-  context::parallel_cluster_stop()  
+  context::parallel_cluster_stop()
 }
