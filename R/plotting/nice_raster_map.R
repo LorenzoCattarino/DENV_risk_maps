9598b9efda5dec6af4b00c7e64da81f9b981fded
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-09-26 11:24:09 +0100

    move preprocessing outside of make_nice_map()

--- R/plotting/nice_raster_map.R
+++ R/plotting/nice_raster_map.R
@@ -1,71 +1,36 @@
 make_nice_map <- function(parms,
-                          map_proj,
-                          countries,
-                          bbox,
+                          countries_df,
+                          bbox_df,
                           pred,
                           sd,
                           out_path,
                           out_file_name){
   
-  
-  gr_size <- parms$resample_grid_size
-  
-  my_col <- matlab.like(10)
-  
-  plot_wdt <- 20
-  plot_hgt <- 20 
-  barwdt <- 1.5
-  barhgt <- 6.5
-  pol_brd_sz <- 0.1
-  leg_pos_x <- 0.10
-  leg_pos_y <- 0.25
-  leg_txt_sz <- 10 
-  leg_ttl_sz <- 12
+  plot_wdt <- parms$plot_wdt
+  plot_hgt <- parms$plot_hgt
+  barwdt <- parms$barwdt
+  barhgt <- parms$barhgt
+  pol_brd_sz <- parms$pol_brd_sz
+  leg_pos_x <- parms$leg_pos_x
+  leg_pos_y <- parms$leg_pos_y
+  leg_txt_sz <- parms$leg_txt_sz 
+  leg_ttl_sz <- parms$leg_ttl_sz
+
+  my_col <- matlab.like(100)
   
   
-  # common pre processing -------------------------------------------------------
+  # plot predictions ----------------------------------------------------------
   
   
-  res <- (1 / 120) * gr_size
-  
-  lats <- seq(-90, 90, by = res)
-  lons <- seq(-180, 180, by = res)
-  
-  countries <- countries[!countries@data$NAME_ENGLI == "Caspian Sea", ]
-  countries <- spTransform(countries, CRS(map_proj))
-  countries_df <- fortify(countries)
-  bbox <- spTransform(bbox, CRS(map_proj))
-  bbox_df<- fortify(bbox)
-  
-  
-  # plot predictions ------------------------------------------------------------
-  
-  
-  pred_mat <- prediction_df_to_matrix(lats, lons, pred, "best")
-  
-  pred_mat_ls <- list(x = lons,
-                      y = lats,
-                      z = pred_mat)
-  
-  pred_r_mat <- raster(pred_mat_ls)
-  
-  pred_r_mat <- projectRaster(pred_r_mat, crs = map_proj)
-  
-  pred_r_spdf <- as(pred_r_mat, "SpatialPixelsDataFrame")
-  
-  pred_r_df <- as.data.frame(pred_r_spdf)
-  
-  pred_r_df <- subset(pred_r_df, layer >= 1)
-  
-  pred_leg_val <- pretty(pred_r_df$layer, 5)
+  pred_leg_val <- pretty(pred$layer, 5)
   
   pred_map <- ggplot() +
     geom_polygon(data = bbox_df, aes(long, lat, group = group), fill = "aliceblue") +
     geom_polygon(data = countries_df, aes(long, lat, group = group), fill = "grey70") +
-    geom_tile(data = pred_r_df, aes(x = x, y = y, fill = layer)) +
+    geom_tile(data = pred, aes(x = x, y = y, fill = layer)) +
     scale_fill_gradientn(breaks = pred_leg_val,
                          labels = pred_leg_val,
-                         limits = c(min(pred_leg_val), max(pred_r_df$layer)),
+                         limits = c(min(pred_leg_val), max(pred$layer)),
                          colours = my_col, 
                          guide = guide_colourbar(title = expression("R"[0]), 
                                                  barwidth = barwdt, 
@@ -92,41 +57,18 @@ make_nice_map <- function(parms,
           legend.box.background = element_rect(fill = "white", colour = "black"))
   
   
-  # plot sd ---------------------------------------------------------------------
+  # plot sd -------------------------------------------------------------------
   
   
-  names(sd)[names(sd) == "latitude"] <- "lat.grid"
-  names(sd)[names(sd) == "longitude"] <- "long.grid"
-  
-  sd_mat <- prediction_df_to_matrix(lats, lons, sd, "sd")
-  
-  sd_mat_ls <- list(x = lons,
-                    y = lats,
-                    z = sd_mat)
-  
-  sd_r_mat <- raster(sd_mat_ls)
-  
-  sd_r_mat <- projectRaster(sd_r_mat, crs = map_proj)
-  
-  my_ext <- matrix(sd_r_mat@extent[], nrow = 2, byrow = TRUE)
-  
-  countries@bbox <- my_ext
-  
-  sd_r_mat_msk <- mask(sd_r_mat, countries)
-  
-  sd_r_spdf <- as(sd_r_mat_msk, "SpatialPixelsDataFrame")
-  
-  sd_r_df <- as.data.frame(sd_r_spdf)
-  
-  sd_leg_val <- pretty(sd_r_df$layer, 5)
+  sd_leg_val <- pretty(sd$layer, 5)
   
   sd_map <- ggplot() +
     geom_polygon(data = bbox_df, aes(long, lat, group = group), fill = "aliceblue") +
-    geom_polygon(data = countries_df, aes(long, lat, group = group), fill = "grey70") +
-    geom_tile(data = sd_r_df, aes(x = x, y = y, fill = layer)) +
+    geom_polygon(data = countries_df, aes(long, lat, group = group), fill = my_col[1]) +
+    geom_tile(data = sd, aes(x = x, y = y, fill = layer)) +
     scale_fill_gradientn(breaks = sd_leg_val,
                          labels = sd_leg_val,
-                         limits = c(min(sd_leg_val), max(sd_r_df$layer)),
+                         limits = c(min(sd_leg_val), max(sd$layer)),
                          colours = my_col, 
                          guide = guide_colourbar(title = "SD", 
                                                  barwidth = barwdt, 
