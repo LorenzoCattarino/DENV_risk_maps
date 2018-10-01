bd39cdbe45c8bc8e4a26fabed0a29daff2c1bfa4
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-10-01 10:27:36 +0100

    calculate vaccine impact on 200 bootstrap samples

--- calculate_vaccine_impact.r
+++ calculate_vaccine_impact.r
@@ -1,149 +1,138 @@
-library(dplyr)
 
-source(file.path("R", "prepare_datasets", "average_up.r"))
-source(file.path("R", "utility_functions.r"))
+options(didehpc.cluster = "fi--didemrchnb")
 
+CLUSTER <- FALSE
 
-# ---------------------------------------- define parameters 
-
-
-model_type <- "boot_model_20km_2"
-
-scenario_id <- 3
-
-base_info <- c("cell", "lat.grid", "long.grid", "population", "ADM_0", "ADM_1", "ADM_2")
-
-P_a <- 0.7
-
-out_pth <- file.path("output", 
-                     "predictions_world", 
-                     model_type, 
-                     "means")
-
-out_fl_nm <- paste0("C_inc_after_vacc_mean_all_squares_0_1667_deg_", scenario_id, ".rds")
-
-
-# ---------------------------------------- load data 
-
-
-covariates_vals <- readRDS(
-  file.path("output",
-            "env_variables",
-            "all_squares_env_var_0_1667_deg.rds"))
-
-foi <- readRDS(
-  file.path("output", 
-            "predictions_world", 
-            model_type, 
-            "means", 
-            "FOI_mean_all_squares_0_1667_deg.rds"))
-
-R0_vals <- readRDS(
-  file.path("output", 
-            "predictions_world", 
-            model_type, 
-            "means", 
-            paste0("R0_r_mean_all_squares_0_1667_deg_", scenario_id, ".rds")))
+my_resources <- c(
+  file.path("R", "burden_and_interventions", "wrappers_to_vaccine_impact_calculation.R"),
+  file.path("R", "prepare_datasets", "average_up.r"),
+  file.path("R", "utility_functions.r"))
   
-look_up_table <- read.csv(
-  file.path("data",
-            "vaccine",
-            "R0_to_prop_cases_averted_lookup.csv"),
-  header = TRUE)
+my_pkgs <- c("dplyr")
+
+context::context_log_start()
+ctx <- context::context_save(path = "context",
+                             packages = my_pkgs,
+                             sources = my_resources)
+
+context::context_load(ctx)
+context::parallel_cluster_start(7, ctx)
 
 
-cases <- readRDS(
-  file.path("output", 
-            "predictions_world", 
-            model_type, 
-            "means", 
-            paste0("C_num_mean_all_squares_0_1667_deg_", scenario_id, ".rds")))
+# define parameters -----------------------------------------------------------  
+
+
+parameters <- list(
+  id = 16,
+  no_samples = 200,
+  wolbachia_scenario_id = 3,
+  no_R0_assumptions = 3,
+  screening_ages = c(9, 16),
+  burden_measure = c("infections", "cases", "hosp")) 
+
+parallel_2 <- TRUE
+
+phi_set_id_tag <- "phi_set_id"
+
+base_info <- c("cell", "latitude", "longitude", "population", "ID_0", "ID_1", "ID_2")
+
+
+# load experimental design ----------------------------------------------------
+
+
+bootstrap_experiments <- read.csv(file.path("output", 
+                                            "EM_algorithm", 
+                                            "bootstrap_models", 
+                                            "boostrap_fit_experiments_uni.csv"),
+                                  stringsAsFactors = FALSE)
+
+
+# define variables ------------------------------------------------------------
+
+
+burden_measures <- parameters$burden_measure
+
+screening_ages <- parameters$screening_ages
+
+w_scenario_id <- parameters$wolbachia_scenario_id
+
+model_type <- paste0("model_", parameters$id)
+
+predictions_file_name <- paste0("response_r_", w_scenario_id, ".rds")
+
+out_path <- file.path("output", 
+                      "predictions_world", 
+                      "bootstrap_models",
+                      model_type)
+
+fit_var <- bootstrap_experiments[bootstrap_experiments$exp_id == parameters$id, "var"]
+
+assumption <- as.numeric(unlist(strsplit(fit_var, "_"))[2])
+
+
+# load data -------------------------------------------------------------------  
+
+
+R0_pred <- readRDS(file.path("output", 
+                             "predictions_world", 
+                             "bootstrap_models",
+                             model_type, 
+                             predictions_file_name))
+
   
+
+# create table of scenarios --------------------------------------------------- 
+
+
+phi_set_id <- seq_len(parameters$no_R0_assumptions)
+
+fct_c <- setNames(expand.grid(phi_set_id, burden_measures, screening_ages, stringsAsFactors = FALSE),
+                  nm = c(phi_set_id_tag, "burden_measure", "screening_age"))
+
+fct_c <- cbind(id = seq_len(nrow(fct_c)), fct_c)
+
+fct_c_2 <- subset(fct_c, phi_set_id == assumption)  
+
+write_out_csv(fct_c_2, out_path, "scenario_table_vaccine.csv")
+
+fctr_combs <- df_to_list(fct_c_2, use_names = TRUE)
+
+
+# pre processing -------------------------------------------------------------- 
+
+
+R0_pred_2 <- as.matrix(R0_pred)
+
+
+# submit ----------------------------------------------------------------------
+
+
+if (CLUSTER) {
   
-# ---------------------------------------- change names to avoid confusion when joining tables 
-
-
-names(R0_vals)[names(R0_vals)=="mean"] <- "R0"
-R0_vals <- R0_vals[setdiff(names(R0_vals), c("sd", "lCI", "uCI"))]
-
-names(cases)[names(cases)=="mean"] <- "C_num"
-cases <- cases[setdiff(names(cases), c("sd", "lCI", "uCI"))]
-
-
-# ---------------------------------------- start
-
-
-R0_vals <- cbind(R0_vals, cases[, "C_num", drop = FALSE])
-
-foi <- cbind(covariates_vals[, base_info], foi)
+  vaccine_impact <- queuer::qlapply(fctr_combs,
+                                    wrapper_to_multi_factor_vaccine_impact,
+                                    obj,
+                                    preds = R0_pred_2, 
+                                    parallel_2 = parallel_2, 
+                                    parms = parameters, 
+                                    base_info = base_info, 
+                                    out_path = out_path)
   
-adm_foi <- average_up(
-  foi, 
-  c("ADM_0", "ADM_1"), 
-  "mean")
-
-adm_foi$a <- (-log(1 - P_a)) / (4 * adm_foi$mean)
-
-adm_foi$a <- round(adm_foi$a)
-
-adm_foi$a[adm_foi$a < 9] <- 9
-
-adm_foi$a[adm_foi$a >= 9 & adm_foi$a <= 18] <- adm_foi$a[adm_foi$a >= 9 & adm_foi$a <= 18]
-
-adm_foi$a[adm_foi$a > 18] <- -1
-
-R0_vals <- left_join(R0_vals, adm_foi[, c("ADM_0", "ADM_1", "a")], by = c("ADM_0", "ADM_1"))
-
-R0_vals$pCav <- 0
-
-logical_subset <- R0_vals$a != -1
-
-R0_vals_2 <- R0_vals[logical_subset, ]
-
-
-# ---------------------------------------- fix the extreme R0 values in lookup_table
-
-
-new_first_row <- cbind(R0 = 0, look_up_table[1, 2:18])
-new_last_row <- cbind(R0 = 10, look_up_table[nrow(look_up_table), 2:18])
-
-look_up_table_2 <- rbind(new_first_row, look_up_table, new_last_row)
-
-
-# ---------------------------------------- convert to matrices
-
-
-R0_vals_2 <- as.matrix(R0_vals_2)
-
-look_up_table_2 <- as.matrix(look_up_table_2)
-
-
-# ---------------------------------------- loop
-
-
-for (i in seq_len(nrow(R0_vals_2))){
+} else {
   
-  a_i <- R0_vals_2[i, "a"]
-  #cat("a =", a_i, "\n")
-  
-  R0_i <- R0_vals_2[i, "R0"]
-    
-  prop_cases_avert <- approx(look_up_table_2[, "R0"], look_up_table_2[, a_i], xout = R0_i)$y
-
-  R0_vals_2[i, "pCav"] <- prop_cases_avert
+  vaccine_impact <- loop(fctr_combs, 
+                         wrapper_to_multi_factor_vaccine_impact,
+                         preds = R0_pred_2, 
+                         parallel_2 = parallel_2, 
+                         parms = parameters, 
+                         base_info = base_info, 
+                         out_path = out_path,
+                         parallel = FALSE)
   
 }
 
-
-# ---------------------------------------- 
-
-
-R0_vals_3 <- rbind(R0_vals_2, R0_vals[!logical_subset, ])
+if (!CLUSTER){
   
-C_num_net_vaccine <- (1 - R0_vals_3[, "pCav"]) * R0_vals_3[, "C_num"]
-
-C_inc_net_vaccine <- (C_num_net_vaccine / R0_vals_3[, "population"]) * 1000
-
-R0_vals_3 <- cbind(R0_vals_3, C_num_vacc = C_num_net_vaccine, mean = C_inc_net_vaccine)
-
-write_out_rds(as.data.frame(R0_vals_3), out_pth, out_fl_nm)
+  context::parallel_cluster_stop()
+  
+}
