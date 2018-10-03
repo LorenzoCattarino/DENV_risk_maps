b0626b71527f64fcdf375ca8e69e3f4e47f3a7e2
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-10-03 16:23:01 +0100

    modify the reshaping part after vectorization of approx(); add the FOI_to_HC look up table as argument

--- R/burden_and_interventions/wrapper_to_multi_factor_R0_and_burden.r
+++ R/burden_and_interventions/wrapper_to_multi_factor_R0_and_burden.r
@@ -8,19 +8,20 @@ wrapper_to_multi_factor_R0_and_burden <- function(x,
                                                   FOI_values, 
                                                   FOI_to_Inf_list, 
                                                   FOI_to_C_list, 
+                                                  FOI_to_HC_list,
                                                   prob_fun, 
                                                   parms, 
                                                   base_info, 
                                                   out_path){
   
   
-  #browser()
+  # browser()
   
   no_fits <- parms$no_samples
   var_to_fit <- parms$dependent_variable
   fit_type <- parms$fit_type
   
-  var_names <- c("response_r", "transformed_r", "I_num", "C_num")
+  var_names <- c("response_r", "transformed_r", "I_num", "C_num", "H_num")
   
   
   # ---------------------------------------- Extract factor values
@@ -43,8 +44,6 @@ wrapper_to_multi_factor_R0_and_burden <- function(x,
  
 
   vec_phis <- c(phi_1, phi_2, phi_3, phi_4)
-  
-  model_tp <- paste0(var_to_fit, "_", fit_type, "_model")
   
   
   # ---------------------------------------- create FOI -> R0 look up tables
@@ -90,6 +89,7 @@ wrapper_to_multi_factor_R0_and_burden <- function(x,
     FOI_to_R0_list = FOI_to_R0_list,
     FOI_to_Inf_list = FOI_to_Inf_list,
     FOI_to_C_list = FOI_to_C_list,
+    FOI_to_HC_list = FOI_to_HC_list,
     age_band_lower_bounds = age_band_lower_bounds,
     age_band_upper_bounds = age_band_upper_bounds,
     age_band_tags = age_band_tags,
@@ -103,38 +103,40 @@ wrapper_to_multi_factor_R0_and_burden <- function(x,
   
 
   # ---------------------------------------- reshape and save
-
-
+  
+  
   if(fit_type == "boot"){
-    
+
     for (b in seq_along(var_names)){
-      
+
+      ret1 <- lapply(burden_estimates, "[", var_names[b], TRUE)
+
+      ret2 <- do.call("rbind", ret1)
+
+      ret3 <- cbind(foi_data[, base_info], ret2)
+
+      fl_nm <- paste0(var_names[b], "_", run_ID, ".rds")
+
+      write_out_rds(ret3, out_path, fl_nm)
+
+    }
+
+  } else {
+
+    for (b in seq_along(var_names)){
+
       ret1 <- lapply(burden_estimates, "[", var_names[b], TRUE)
       
       ret2 <- do.call("rbind", ret1)
-      
-      ret3 <- cbind(foi_data[, base_info], ret2)
-      
-      fl_nm <- paste0(var_names[b], "_", run_ID, ".rds")
-      
+        
+      ret3 <- cbind(as.data.frame(foi_data[, base_info]), best = ret2)
+
+      fl_nm <- paste0(var_names[b], "_wolbachia_", run_ID, ".rds")
+
       write_out_rds(ret3, out_path, fl_nm)
-      
+
     }
-    
-  } else {
-    
-    for (b in seq_along(var_names)){
-      
-      ret2 <- do.call("rbind", burden_estimates)
-      
-      ret3 <- cbind(foi_data[, base_info], best = ret2[,var_names[b]])
-      
-      fl_nm <- paste0(var_names[b], "_best_", run_ID, ".rds")
-      
-      write_out_rds(as.data.frame(ret3), out_path, fl_nm)
-      
-    }
-    
+
   }
   
 }
