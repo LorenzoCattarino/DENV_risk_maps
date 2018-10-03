dcf7ff4ac373b0253249ac245cd307f1bd19370d
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-10-03 16:21:02 +0100

    vectorize the approx() command (rather than having an additional loop - great move); edit off code which is not needed anymore

--- R/burden_and_interventions/wrapper_to_replicate_R0_and_burden.r
+++ R/burden_and_interventions/wrapper_to_replicate_R0_and_burden.r
@@ -5,6 +5,7 @@ wrapper_to_replicate_R0_and_burden <- function(i,
                                                FOI_to_R0_list,
                                                FOI_to_Inf_list,
                                                FOI_to_C_list,
+                                               FOI_to_HC_list,
                                                age_band_lower_bounds, 
                                                age_band_upper_bounds, 
                                                age_band_tags,
@@ -27,6 +28,7 @@ wrapper_to_replicate_R0_and_burden <- function(i,
   FOI_to_R0 <- FOI_to_R0_list[[idx]]
   FOI_to_Inf <- FOI_to_Inf_list[[idx]]
   FOI_to_C <- FOI_to_C_list[[idx]]
+  FOI_to_HC <- FOI_to_HC_list[[idx]] 
   
   
   # ---------------------------------------- get vector of foi values and number of people in each square
@@ -36,54 +38,82 @@ wrapper_to_replicate_R0_and_burden <- function(i,
     col_ids <- as.character(seq_len(no_fits))
   }
   
-  m_j <- age_struct[age_struct$ADM_0 == foi_data[i, "ADM_0"], age_band_tags]
+  m_j <- age_struct[age_struct$ADM_0 == foi_data[i, "ID_0"], age_band_tags]
   
   if(fit_type == "boot") {
     
-    FOI_values <- foi_data[i, col_ids]
+    preds <- foi_data[i, col_ids]
     
   } else{
     
-    FOI_values <- foi_data[i, "best"]
+    preds <- foi_data[i, "best"]
   }
+  
+  N <- foi_data[i, "population"]
   
   
   # ---------------------------------------- calculates R0 values for different replicates of the same pixel  
   
+
+  red_preds <- preds * scaling_factor
+  red_trans <- approx(FOI_to_R0[, "y"], FOI_to_R0[, "x"], xout = red_preds)$y
+  Infections_pc <- approx(FOI_to_Inf[, "x"], FOI_to_Inf[, "y"], xout = red_trans)$y 
+  Cases_pc <- approx(FOI_to_C[, "x"], FOI_to_C[, "y"], xout = red_trans)$y
+  Hosp_cases_pc <- approx(FOI_to_HC[, "x"], FOI_to_HC[, "y"], xout = red_trans)$y
+  Infections <- Infections_pc * N
+  Cases <- Cases_pc * N
+  HCases <- Hosp_cases_pc * N 
+  #Infections_inc <- Infections_pc * 1000
+  #Cases_inc <- Cases_pc * 1000 
+  out <- rbind(red_preds, red_trans, Infections, Cases, HCases)
+  rownames(out) <- vars
+  out
   
-  n <- length(vars)
   
-  if(fit_type == "boot") {
-    
-    vapply(FOI_values,
-           wrapper_to_R0_and_burden,
-           numeric(n),
-           n_j = m_j,
-           vars = vars,
-           age_band_lower_bounds = age_band_lower_bounds, 
-           age_band_upper_bounds = age_band_upper_bounds,
-           vec_phis = vec_phis, 
-           prob_fun = prob_fun,
-           scaling_factor = scaling_factor,
-           FOI_to_R0 = FOI_to_R0, 
-           FOI_to_Inf = FOI_to_Inf, 
-           FOI_to_C = FOI_to_C,
-           var_to_fit = var_to_fit)
-    
-  } else {
-    
-    wrapper_to_R0_and_burden(FOI_values,
-                             n_j = m_j,
-                             vars = vars,
-                             age_band_lower_bounds = age_band_lower_bounds,
-                             age_band_upper_bounds = age_band_upper_bounds,
-                             vec_phis = vec_phis,
-                             prob_fun = prob_fun,
-                             scaling_factor = scaling_factor,
-                             FOI_to_R0 = FOI_to_R0,
-                             FOI_to_Inf = FOI_to_Inf,
-                             FOI_to_C = FOI_to_C,
-                             var_to_fit = var_to_fit)
-  }
+  # # n <- length(vars)
+  # 
+  # if(fit_type == "boot") {
+  #   
+  #   red_preds <- preds * scaling_factor
+  #   red_trans <- approx(FOI_to_R0[, "y"], FOI_to_R0[, "x"], xout = red_preds)$y
+  #   Infections_pc <- approx(FOI_to_Inf[, "x"], FOI_to_Inf[, "y"], xout = red_trans)$y 
+  #   Cases_pc <- approx(FOI_to_C[, "x"], FOI_to_C[, "y"], xout = red_trans)$y
+  #   Infections <- Infections_pc * N
+  #   Cases <- Cases_pc * N
+  #   Infections_inc <- Infections_pc * 1000
+  #   Cases_inc <- Cases_pc * 1000 
+  #   out <- rbind(red_preds, red_trans, Infections, Cases, Infections_inc, Cases_inc)
+  #   rownames(out) <- vars
+  #   out
+  #   # vapply(FOI_values,
+  #   #        wrapper_to_R0_and_burden,
+  #   #        numeric(n),
+  #   #        n_j = m_j,
+  #   #        vars = vars,
+  #   #        age_band_lower_bounds = age_band_lower_bounds, 
+  #   #        age_band_upper_bounds = age_band_upper_bounds,
+  #   #        vec_phis = vec_phis, 
+  #   #        prob_fun = prob_fun,
+  #   #        scaling_factor = scaling_factor,
+  #   #        FOI_to_R0 = FOI_to_R0, 
+  #   #        FOI_to_Inf = FOI_to_Inf, 
+  #   #        FOI_to_C = FOI_to_C,
+  #   #        var_to_fit = var_to_fit)
+  #   
+  # } else {
+  #   
+  #   wrapper_to_R0_and_burden(FOI_values,
+  #                            n_j = m_j,
+  #                            vars = vars,
+  #                            age_band_lower_bounds = age_band_lower_bounds,
+  #                            age_band_upper_bounds = age_band_upper_bounds,
+  #                            vec_phis = vec_phis,
+  #                            prob_fun = prob_fun,
+  #                            scaling_factor = scaling_factor,
+  #                            FOI_to_R0 = FOI_to_R0,
+  #                            FOI_to_Inf = FOI_to_Inf,
+  #                            FOI_to_C = FOI_to_C,
+  #                            var_to_fit = var_to_fit)
+  # }
 
 }
