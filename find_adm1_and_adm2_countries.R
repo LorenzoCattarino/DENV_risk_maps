7611ac167db9112a9febfe279365d3830a706c27
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-10-31 10:46:11 +0000

    output name of country and admin unit 1 as well

--- find_adm1_and_adm2_countries.R
+++ find_adm1_and_adm2_countries.R
@@ -20,7 +20,7 @@ adm0_iso_name_duplicated <- adm0_countries@data[, c("ID_0", "ISO", "NAME_ENGLI")
 adm0_iso_name <- adm0_iso_name_duplicated[!duplicated(adm0_iso_name_duplicated), ]
 adm0_iso <- adm0_iso_name$ISO
 
-adm1_iso_name_duplicated <- adm1_countries@data[, c("ISO", "NAME_0")] 
+adm1_iso_name_duplicated <- adm1_countries@data[, c("ISO", "ID_0", "NAME_0", "ID_1", "NAME_1")] 
 adm1_iso_name <- adm1_iso_name_duplicated[!duplicated(adm1_iso_name_duplicated), ]
 adm1_iso <- adm1_iso_name$ISO
 
@@ -35,19 +35,19 @@ missing_countries_adm1 <- adm0_iso_name[!adm1_in_adm0.logical, ]
 missing_countries_adm2 <- adm1_iso_name[!adm2_in_adm1.logical, ]
 
 # Change column names 
-colnames(adm0_iso_name)[colnames(adm0_iso_name) == "NAME_ENGLI"] <- "country"
-colnames(adm1_iso_name) <- c("country_code", "country")
-colnames(adm2_iso_name) <- c("country_code", "country")
-colnames(missing_countries_adm1) <- c("country_code", "country")
-colnames(missing_countries_adm2) <- c("country_code", "country")
+# colnames(adm0_iso_name)[colnames(adm0_iso_name) == "NAME_ENGLI"] <- "country"
+# colnames(adm1_iso_name) <- c("country_code", "country")
+# colnames(adm2_iso_name) <- c("country_code", "country")
+# colnames(missing_countries_adm1) <- c("country_code", "country")
+# colnames(missing_countries_adm2) <- c("country_code", "country")
   
 # write out list of all countries in the world  
 write.csv(adm0_iso_name[order(adm0_iso_name$country), ], 
           file.path("output", "datasets", "all_adm_0_countries.csv"), 
           row.names = FALSE)
 
-# write out list of countries which have admin 1 GADM boundaries  
-write.csv(adm1_iso_name[order(adm1_iso_name$country), ], 
+# write out all admin 1 GADM boundaries  
+write.csv(adm1_iso_name[order(adm1_iso_name$NAME_0), ], 
           file.path("output", "datasets", "all_adm_1_countries.csv"), 
           row.names = FALSE)
 
