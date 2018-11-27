22135a21005e46b79153ca84978a97c4c26ae7b6
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2018-11-27 09:59:26 +0000

    fix list.file() sorting issue; remove plotting part

--- interventions_squares_with_bootstrap_03.R
+++ interventions_squares_with_bootstrap_03.R
@@ -1,14 +1,12 @@
 # For wolbachia impact
 
-# 1) take the mean, sd and 95%CI across bootstrap samples, of
+# take the mean, sd and 95%CI across bootstrap samples, of
 # total number of infections and cases, summed over all squares, AND:
 # total number of infections and cases, summed by country 
 
-# 2) create barplots of total numbers
 
 library(dplyr)
 library(data.table)
-library(ggplot2)
 library(countrycode)
 
 source(file.path("R", "prepare_datasets", "calculate_mean_across_fits.R"))
@@ -19,23 +17,22 @@ source(file.path("R", "utility_functions.R"))
 
 
 parameters <- list(
-  id = c(14, 15, 16),
+  id = c(22, 23, 24, 21),
   no_samples = 200,
   burden_measures = c("infections", "cases", "hosp"),
-  desired_n_int = c(8, 8, 5),
-  baseline_scenario_ids = c(1, 2, 3))   
+  baseline_scenario_ids = c(1, 2, 3, 4))   
 
 intervention_name <- "wolbachia"
 
 treatment_name <- "scaling_factor"
+
+phi_factor_levels <- c("2S", "4S", "4S(sym = 2x asym)", "FOI")
 
 
 # define variables ------------------------------------------------------------
 
 
 baseline_scenario_ids <- parameters$baseline_scenario_ids 
-  
-desired_n_int <- parameters$desired_n_int
 
 burden_measures <- parameters$burden_measures
 
@@ -55,10 +52,6 @@ out_table_path <- file.path("output",
                             "bootstrap_models", 
                             model_type,
                             intervention_name)
-
-out_fig_path <- file.path("figures", 
-                          "predictions_world", 
-                          "bootstrap_models")
 
 fct_comb_fl_nm <- paste0("scenario_table_", intervention_name, ".csv")
 
@@ -86,14 +79,14 @@ age_struct$region <- as.factor(countrycode(sourcevar = age_struct[, "country"],
                                            destination = "region"))
 
 
-# aggreaggating --------------------------------------------------------------- 
+# aggreggating ---------------------------------------------------------------- 
 
 
 for (j in seq_along(vars)){                                 # loop over burden measures 
   
   out_ls <- vector("list", length(model_type))
   out_ls_2 <- vector("list", length(model_type))
-    
+  
   my_var_name <- burden_measures[j]
   cat("burden measure =", my_var_name, "\n")
   
@@ -102,7 +95,7 @@ for (j in seq_along(vars)){                                 # loop over burden m
   root_name <- paste0(my_var, "_num_", intervention_name)
   
   for (k in seq_along(model_type)){                         # loop over R0 assumptions
-
+    
     cat("R0 assumption =", k, "\n")
     
     # index <- k + (length(in_path)*(j-1))
@@ -116,11 +109,17 @@ for (j in seq_along(vars)){                                 # loop over burden m
                      pattern = paste0("^", root_name),
                      full.names = TRUE)
     
-    dat <- lapply(fi, readRDS)
+    num.sort <- as.numeric(gsub(".*_|\\.rds$", "\\1", fi, perl = TRUE))
+    
+    fi_sort <- fi[order(num.sort)]
+    
+    message(fi_sort)
+    
+    dat <- lapply(fi_sort, readRDS)
     
     small_out_ls <- vector("list", length(dat))
     small_out_ls_2 <- vector("list", length(dat))
-      
+    
     baseline_id <- baseline_scenario_ids[k]
     
     baseline <- readRDS(file.path(my_in_path, paste0(root_name, "_", baseline_id, ".rds"))) 
@@ -172,12 +171,12 @@ for (j in seq_along(vars)){                                 # loop over burden m
       ret7 <- average_boot_samples_dim1(ret6)
       
       small_out_ls_2[[i]] <- ret7
-    
+      
     }
     
     out_ls[[k]] <- cbind(my_fct_comb, do.call("rbind", small_out_ls))
     out_ls_2[[k]] <- cbind(my_fct_comb, do.call("rbind", small_out_ls_2))
-  
+    
   }
   
   summary_table <- do.call("rbind", out_ls)  
@@ -190,12 +189,21 @@ for (j in seq_along(vars)){                                 # loop over burden m
                                          levels = treatment_levels,
                                          labels = treatment_levels)
   
-  phi_factor_levels <- c("2S", "4S", "4S(sym = 2x asym)")
-  
   summary_table$phi_set_id <- factor(summary_table$phi_set_id, 
-                                     levels = c(1, 2, 3), 
+                                     levels = seq_len(length(phi_factor_levels)), 
                                      labels = phi_factor_levels)
-
+  
+  summary_tab_fl_nm <- paste0("total_", 
+                              my_var_name, 
+                              "_", 
+                              intervention_name, 
+                              ".csv")
+  
+  write_out_csv(summary_table, file.path("output", 
+                                         "predictions_world", 
+                                         "bootstrap_models"), 
+                summary_tab_fl_nm)
+  
   summary_table_2 <- do.call("rbind", out_ls_2)  
   
   names(summary_table_2)[names(summary_table_2) == treatment_name] <- "treatment"
@@ -206,18 +214,20 @@ for (j in seq_along(vars)){                                 # loop over burden m
                                            levels = treatment_levels,
                                            labels = treatment_levels)
   
-  phi_factor_levels <- c("2S", "4S", "4S(sym = 2x asym)")
-  
   summary_table_2$phi_set_id <- factor(summary_table_2$phi_set_id, 
-                                     levels = c(1, 2, 3), 
-                                     labels = phi_factor_levels)
+                                       levels = seq_len(length(phi_factor_levels)), 
+                                       labels = phi_factor_levels)
   
-  summary_tab_fl_nm <- paste0("prop_change_", my_var_name, "_", intervention_name, ".csv")
-    
+  summary_tab_fl_nm_2 <- paste0("prop_change_", 
+                                my_var_name, 
+                                "_", 
+                                intervention_name, 
+                                ".csv")
+  
   write_out_csv(summary_table_2, file.path("output", 
                                            "predictions_world", 
                                            "bootstrap_models"), 
-                summary_tab_fl_nm)
+                summary_tab_fl_nm_2)
   
   
   # save table of baseline burden ---------------------------------------------
@@ -235,48 +245,5 @@ for (j in seq_along(vars)){                                 # loop over burden m
                   basel_burden_fl_nm)
     
   }
-  
-  
-  # plotting ------------------------------------------------------------------
-
-
-  y_values <- pretty(summary_table$mean, desired_n_int[j])
-  max_y_value <- max(summary_table$uCI)
-
-  p <- ggplot(summary_table, aes(treatment, mean, fill = treatment, ymin = lCI, ymax = uCI)) +
-    geom_bar(stat = "identity", position = "dodge", width = 1) +
-    geom_errorbar(width = .25, position = position_dodge(.9)) +
-    facet_grid(. ~ phi_set_id) +
-    scale_fill_manual(values = c("lightskyblue1", "lightskyblue3", "lightskyblue4"),
-                      labels = c("0%", "50%", "70%"),
-                      guide = guide_legend(title = expression('R'['0']*' reduction'),
-                                           keywidth = 2,
-                                           keyheight = 2)) +
-    xlab(NULL) +
-    scale_y_continuous("Mean (95% CI)",
-                       breaks = y_values,
-                       labels = format(y_values/1000000),
-                       limits = c(min(y_values), max_y_value)) +
-    theme(axis.title.x = element_blank(),
-          axis.text.x = element_blank(),
-          axis.ticks.x = element_blank(),
-          axis.text.y = element_text(size = 12),
-          plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm"),
-          strip.text.x = element_text(size = 8))
-
-  dir.create(out_fig_path, FALSE, TRUE)
-
-  barplot_fl_nm <- paste0("total_", my_var_name, "_", intervention_name, ".png")
-
-  png(file.path(out_fig_path, barplot_fl_nm),
-      width = 17,
-      height = 7,
-      units = "cm",
-      pointsize = 12,
-      res = 300)
-
-  print(p)
-
-  dev.off()
   
 }
