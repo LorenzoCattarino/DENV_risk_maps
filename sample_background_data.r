ba8f6a2a743628f6328c6a16e3a6dee20deafbf3
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2019-02-18 09:02:48 +0000

    use rgdal instead of maptools pkg; add a pseudo absence in Chile; fix the max latitude for sampling sligthly below +60; style edits

--- sample_background_data.r
+++ sample_background_data.r
@@ -1,7 +1,7 @@
 # Locates pseudo absence points
 
 # load packages
-library(maptools) 
+library(rgdal)
 library(raster)
 library(rgeos)
 library(ggplot2)
@@ -10,15 +10,14 @@ library(dplyr)
 source(file.path("R", "prepare_datasets", "functions_for_locating_pseudo_absences.r"))
 
 
-# ---------------------------------------- define paramaters 
+# define paramaters ----------------------------------------------------------- 
 
 
-pseudo_absence_proportion <- 2.5
+pseudo_absence_proportion <- 2.6
 
-shp_fl_pth <- file.path(
-  "output",
-  "shapefiles", 
-  "gadm28_adm1_dengue_cropped.shp")
+cr_shp_fl_nm <- "gadm28_adm1_dengue_cropped"
+
+cr_shp_fl_pth <- file.path("output", "shapefiles")
 
 out_pt <- file.path("output", "datasets")
 
@@ -35,101 +34,8 @@ custom_pap_countries <- list("Algeria",
                              "Libya", 
                              "Mongolia", 
                              "United Kingdom", 
-                             "Western Sahara")
-
-
-# ---------------------------------------- load data
-
-
-All_FOI_estimates <- read.table(
-  file.path("output", 
-            "foi", 
-            "All_FOI_estimates_linear.txt"), 
-  header = TRUE, 
-  sep = ",")
-
-adm_shp_file <- readShapePoly(
-  file.path("output",
-            "shapefiles", 
-            "gadm28_adm1_dengue"))
-
-  
-# ---------------------------------------- define variables 
-
-
-no_data_points <- nrow(All_FOI_estimates) 
-  
-no_pseudo_absence_points <- floor(no_data_points * pseudo_absence_proportion)  
-
-
-# ---------------------------------------- subset to -60 +60 latitude AND dengue
-
-
-fl_ex <- file.exists(shp_fl_pth)
-
-if(fl_ex){
-  
-  adm_shp_file_cropped <- readShapePoly(shp_fl_pth)
-
-} else {
-  
-  adm_shp_file_cropped <- crop(
-  adm_shp_file, 
-  extent(bbox(adm_shp_file)[1,1], 
-         bbox(adm_shp_file)[1,2], 
-         bbox(adm_shp_file)[2,1], 60))
-  
-  writeSpatialShape(adm_shp_file_cropped, shp_fl_pth)
-  
-}
-
-sub_world <- subset(adm_shp_file_cropped, adm_shp_file_cropped@data$dengue == 0)
-
-
-# ---------------------------------------- sample at random pseudo absences
-
-
-pseudo_absence_points <- spsample(sub_world, n = no_pseudo_absence_points, type = "random")
-  
-  
-# ----------------------------------------
-
-
-# Get admin 1 names for pseudo absence points  
-overlay_1 <- over(pseudo_absence_points, adm_shp_file)
-
-# Create df
-psAbs_df1 <- setNames(data.frame("pseudoAbsence",
-                                 overlay_1[, var_1],
-                                 pseudo_absence_points@coords[, var_2]),
-                      nm = col_nms)
-
-
-# ---------------------------------------- remove adm 1 duplicates
-
-
-dup <- duplicated(psAbs_df1[, c("ID_0", "ID_1")])
-psAbs_df1 <- psAbs_df1[!dup, ]
-
-
-# ---------------------------------------- save
-
-
-write.csv(psAbs_df1, 
-          file.path(out_pt, "pseudo_absence_points_1.csv"), 
-          row.names = FALSE)
-
-
-# ===================================================================
-#
-#                        STOP - customize the pseudo absence data set
-#
-# ===================================================================
-
-
-ID_1_maps <- lapply(custom_pap_countries, 
-                    map_ID_1,
-                    adm_shp_file)
+                             "Western Sahara",
+                             "Chile")
 
 custom_adm1_codes <- list(
   alg = c(22, 41, 3, 44, 6),
@@ -139,21 +45,117 @@ custom_adm1_codes <- list(
   lib = c(6, 16, 14, 21, 18, 22, 5),
   mon = c(1, 9, 18, 8),
   uk = c(3, 2),
-  wsa = c(1, 4))
+  wsa = c(1, 4),
+  chl = 48)
+
+max_lat <- 57 
+
+# NOTE: 
+# some buffer around +60 latitude to avoid issues with 
+# admin units in countries straddling the +60 
+# (for which some covariates are missing)
 
 
-# ===================================================================
+# load data ------------------------------------------------------------------- 
+
+
+All_FOI_estimates <- read.csv(file.path("output", 
+                                        "foi", 
+                                        "FOI_estimates_lon_lat_gadm_R0.csv"), 
+                              header = TRUE,
+                              stringsAsFactors = FALSE)
+
+adm_shp <- readOGR(dsn = file.path("output", "shapefiles"), 
+                   layer = paste0("gadm28_adm1_dengue"),
+                   stringsAsFactors = FALSE,
+                   integer64 = "allow.loss")
+
+ID_1_maps <- lapply(custom_pap_countries, 
+                    map_ID_1,
+                    adm_shp)
+
+
+# define variables ------------------------------------------------------------  
+
+
+no_data_points <- nrow(All_FOI_estimates) 
+  
+no_pseudo_absence_points <- floor(no_data_points * pseudo_absence_proportion)  
+
+
+# subset to -60 +60 latitude AND dengue --------------------------------------- 
+
+
+fl_ex <- file.exists(file.path(cr_shp_fl_pth, paste0(cr_shp_fl_nm, ".shp")))
+
+if(fl_ex){
+  
+  adm_shp_cropped <- readOGR(dsn = cr_shp_fl_pth,
+                             layer = cr_shp_fl_nm,
+                             stringsAsFactors = FALSE,
+                             integer64 = "allow.loss")
+
+} else {
+  
+  adm_shp_cropped <- crop(adm_shp, extent(bbox(adm_shp)[1,1], 
+                                          bbox(adm_shp)[1,2], 
+                                          bbox(adm_shp)[2,1], 
+                                          max_lat)) 
+  writeOGR(adm_shp_cropped, 
+           dsn = cr_shp_fl_pth, 
+           layer = cr_shp_fl_nm, 
+           driver = "ESRI Shapefile")
+  
+}
+
+sub_world <- subset(adm_shp_cropped, adm_shp_cropped@data$dengue == 0)
+
+
+# sample at random pseudo absences -------------------------------------------- 
+
+
+pseudo_absence_points <- spsample(sub_world, n = no_pseudo_absence_points, type = "random")
+  
+  
+# -----------------------------------------------------------------------------
+
+
+# Get admin 1 names for pseudo absence points  
+overlay_1 <- over(pseudo_absence_points, adm_shp)
+
+# Create df
+psAbs_df1 <- setNames(data.frame("pseudoAbsence",
+                                 overlay_1[, var_1],
+                                 pseudo_absence_points@coords[, var_2]),
+                      nm = col_nms)
+
+
+# remove adm 1 duplicates ----------------------------------------------------- 
+
+
+dup <- duplicated(psAbs_df1[, c("ID_0", "ID_1")])
+psAbs_df1 <- psAbs_df1[!dup, ]
+
+
+# save ------------------------------------------------------------------------ 
+
+
+write.csv(psAbs_df1, 
+          file.path(out_pt, "pseudo_absence_points_1.csv"), 
+          row.names = FALSE)
+
+
+# =============================================================================
 #
-#                             
+#                        STOP - customize the pseudo absence data set
 #
-# ===================================================================
+# =============================================================================
 
 
-# ---------------------------------------- create df of adm 1 NOT included in the first sampling round
+# create df of adm 1 NOT included in the first sampling round
 
-
-my_c_logic <- adm_shp_file@data$NAME_0 %in% custom_pap_countries
-adm0_codes_vec <- unique(adm_shp_file@data[my_c_logic, "ID_0"])
+my_c_logic <- adm_shp@data$NAME_0 %in% custom_pap_countries
+adm0_codes_vec <- unique(adm_shp@data[my_c_logic, "ID_0"])
 
 custom_adm0_codes <- lapply(
   seq_along(adm0_codes_vec), 
@@ -172,8 +174,8 @@ names(custom_adm0_adm1) <- c("ID_0", "ID_1")
 regions_to_sample <- anti_join(custom_adm0_adm1, psAbs_df1)
 
 
-# ---------------------------------------- create a spatial polygon object with the polygons not sampled yet
-
+# ----------------------------------------------------------------------------- 
+# create a spatial polygon object with the polygons not sampled yet
 
 # get polygon ids 
 sub_world_data_subset <- inner_join(sub_world@data, regions_to_sample)
@@ -183,14 +185,14 @@ ids <- sub_world_data_subset$OBJECTID
 sub_world_copy <- sub_world[sub_world$OBJECTID %in% ids, ]
 
 
-# ---------------------------------------- sample one point in each adm 1
+# sample one point in each adm 1 ---------------------------------------------- 
 
 
 more_pap_list <- sapply(slot(sub_world_copy, "polygons"), function(i) spsample(i, n = 1, type = "random"))
 
 pseudo_absence_points_2 <- do.call("rbind", more_pap_list)
 
-overlay_2 <- over(pseudo_absence_points_2, adm_shp_file)
+overlay_2 <- over(pseudo_absence_points_2, adm_shp)
 
 psAbs_df2 <- setNames(data.frame("pseudoAbsence",
                                  overlay_2[, var_1],
@@ -198,13 +200,20 @@ psAbs_df2 <- setNames(data.frame("pseudoAbsence",
                       nm = col_nms)
 
 
-# ---------------------------------------- rbind all pseudoabsences
+# =============================================================================
+#
+#                             
+#
+# =============================================================================
+
+
+# rbind all pseudoabsences ---------------------------------------------------- 
 
 
 all_psAbs <- rbind(psAbs_df1, psAbs_df2)
 
 
-# ---------------------------------------- save
+# save ------------------------------------------------------------------------ 
 
 
 write.csv(all_psAbs, 
