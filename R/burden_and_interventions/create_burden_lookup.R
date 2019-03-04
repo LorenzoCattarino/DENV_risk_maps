593642d31b744c2376d73d063bb70a9da9748561
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2019-03-04 10:09:27 +0000

    leave vector of mapped FOI values in the 1D lookup tables

--- R/burden_and_interventions/create_burden_lookup.R
+++ R/burden_and_interventions/create_burden_lookup.R
@@ -22,15 +22,17 @@ create_lookup_tables <- function(i,
     
       message("1D lookup")
       
-      lookup_list <- loop(seq_len(nrow(age_struct)), 
-                          wrapper_to_lookup,
-                          age_struct = age_struct, 
-                          tags = age_band_tags, 
-                          FOI_values = FOI_values, 
-                          my_fun = my_fun,
-                          age_band_lower_bounds = age_band_L_bounds, 
-                          age_band_upper_bounds = age_band_U_bounds,
-                          parallel = parallel_2)
+      Infection_values <- loop(seq_len(nrow(age_struct)), 
+                               wrapper_to_lookup,
+                               age_struct = age_struct, 
+                               tags = age_band_tags, 
+                               FOI_values = FOI_values, 
+                               my_fun = my_fun,
+                               age_band_lower_bounds = age_band_L_bounds, 
+                               age_band_upper_bounds = age_band_U_bounds,
+                               parallel = parallel_2)
+      
+      lookup_list <- lapply(Infection_values, cbind_FOI_to_lookup, FOI_values)
       
       saveRDS(lookup_list, file.path(out_path, out_nm))
       
@@ -71,7 +73,7 @@ create_lookup_tables <- function(i,
       
       cat("weights vector =", my_weights, "\n")
       
-      lookup_list_fixed_prop_sym <- loop(seq_len(nrow(age_struct)), 
+      case_values <- loop(seq_len(nrow(age_struct)), 
                                          wrapper_to_lookup,
                                          age_struct = age_struct, 
                                          tags = age_band_tags, 
@@ -82,7 +84,9 @@ create_lookup_tables <- function(i,
                                          weights_vec = my_weights,
                                          parallel = parallel_2)
       
-      saveRDS(lookup_list_fixed_prop_sym, file.path(out_path, out_nm_2))
+      lookup_list <- lapply(case_values, cbind_FOI_to_lookup, FOI_values)
+      
+      saveRDS(lookup_list, file.path(out_path, out_nm_2))
       
     }
   
@@ -122,7 +126,7 @@ create_lookup_tables <- function(i,
       
       cat("weights vector =", my_weights, "\n")
       
-      lookup_list_fixed_prop_sym <- loop(seq_len(nrow(age_struct)), 
+      HCase_values <- loop(seq_len(nrow(age_struct)), 
                                          wrapper_to_lookup,
                                          age_struct = age_struct, 
                                          tags = age_band_tags, 
@@ -134,7 +138,9 @@ create_lookup_tables <- function(i,
                                          weights_vec = my_weights,
                                          parallel = parallel_2)
       
-      saveRDS(lookup_list_fixed_prop_sym, file.path(out_path, out_nm_2))
+      lookup_list <- lapply(HCase_values, cbind_FOI_to_lookup, FOI_values)
+      
+      saveRDS(lookup_list, file.path(out_path, out_nm_2))
       
     }
     
@@ -152,23 +158,27 @@ create_lookup_tables <- function(i,
       
       cat("weights vector =", my_weights, "\n")
       
-      lookup_list <- loop(seq_len(nrow(age_struct)), 
-                          wrapper_to_lookup,
-                          age_struct = age_struct, 
-                          tags = age_band_tags, 
-                          FOI_values = FOI_values, 
-                          my_fun = my_fun,
-                          age_band_lower_bounds = age_band_L_bounds, 
-                          age_band_upper_bounds = age_band_U_bounds,
-                          weights_vec = my_weights,
-                          parallel = parallel_2)
+      R0_values <- loop(seq_len(nrow(age_struct)), 
+                        wrapper_to_lookup,
+                        age_struct = age_struct, 
+                        tags = age_band_tags, 
+                        FOI_values = FOI_values, 
+                        my_fun = my_fun,
+                        age_band_lower_bounds = age_band_L_bounds, 
+                        age_band_upper_bounds = age_band_U_bounds,
+                        weights_vec = my_weights,
+                        parallel = parallel_2)
+      
+      lookup_list <- lapply(R0_values, cbind_FOI_to_lookup, FOI_values)
+      
+      lookup_list <- lapply(lookup_list, fix_R0_lookup_limits)
       
       saveRDS(lookup_list, file.path(out_path, out_nm))
       
     }
     
   }
-
+  
   if (i == "R0_2") {
     
     my_fun <- target_fun$calculate_R0
@@ -181,16 +191,20 @@ create_lookup_tables <- function(i,
       
       cat("weights vector =", my_weights, "\n")
       
-      lookup_list <- loop(seq_len(nrow(age_struct)), 
-                          wrapper_to_lookup,
-                          age_struct = age_struct, 
-                          tags = age_band_tags, 
-                          FOI_values = FOI_values, 
-                          my_fun = my_fun,
-                          age_band_lower_bounds = age_band_L_bounds, 
-                          age_band_upper_bounds = age_band_U_bounds,
-                          weights_vec = my_weights,
-                          parallel = parallel_2)
+      R0_values <- loop(seq_len(nrow(age_struct)), 
+                        wrapper_to_lookup,
+                        age_struct = age_struct, 
+                        tags = age_band_tags, 
+                        FOI_values = FOI_values, 
+                        my_fun = my_fun,
+                        age_band_lower_bounds = age_band_L_bounds, 
+                        age_band_upper_bounds = age_band_U_bounds,
+                        weights_vec = my_weights,
+                        parallel = parallel_2)
+      
+      lookup_list <- lapply(R0_values, cbind_FOI_to_lookup, FOI_values)
+      
+      lookup_list <- lapply(lookup_list, fix_R0_lookup_limits)
       
       saveRDS(lookup_list, file.path(out_path, out_nm))
       
@@ -204,45 +218,29 @@ create_lookup_tables <- function(i,
     param_post <- additional_dts$inf_weights
     my_weights <- parms$vec_phis_R0_3
     out_nm <- sprintf("FOI_to_%s_lookup_tables.rds", i)
-    out_nm_2 <- sprintf("FOI_to_%s_lookup_tables_fixed_params.rds", i)
     
     if (!file.exists(file.path(out_path, out_nm))) {
-      
-      message("2D lookup")
-      
-      lookup_list <- loop(seq_len(nrow(age_struct)), 
-                          wrapper_to_lookup_2D,
-                          age_struct = age_struct, 
-                          param_post = param_post,
-                          tags = age_band_tags, 
-                          FOI_values = FOI_values, 
-                          my_fun = my_fun,
-                          age_band_lower_bounds = age_band_L_bounds,
-                          age_band_upper_bounds = age_band_U_bounds,
-                          parallel = parallel_2)
-      
-      saveRDS(lookup_list, file.path(out_path, out_nm))
-      
-    }
-    
-    if (!file.exists(file.path(out_path, out_nm_2))) {
       
       message("1D lookup")
       
       cat("weights vector =", my_weights, "\n")
       
-      lookup_list_fixed_prop_sym <- loop(seq_len(nrow(age_struct)), 
-                                         wrapper_to_lookup,
-                                         age_struct = age_struct, 
-                                         tags = age_band_tags, 
-                                         FOI_values = FOI_values, 
-                                         my_fun = my_fun,
-                                         age_band_lower_bounds = age_band_L_bounds,
-                                         age_band_upper_bounds = age_band_U_bounds,
-                                         weights_vec = my_weights,
-                                         parallel = parallel_2)
+      R0_values <- loop(seq_len(nrow(age_struct)), 
+                        wrapper_to_lookup,
+                        age_struct = age_struct, 
+                        tags = age_band_tags, 
+                        FOI_values = FOI_values, 
+                        my_fun = my_fun,
+                        age_band_lower_bounds = age_band_L_bounds,
+                        age_band_upper_bounds = age_band_U_bounds,
+                        weights_vec = my_weights,
+                        parallel = parallel_2)
       
-      saveRDS(lookup_list_fixed_prop_sym, file.path(out_path, out_nm_2))
+      lookup_list <- lapply(R0_values, cbind_FOI_to_lookup, FOI_values)
+      
+      lookup_list <- lapply(lookup_list, fix_R0_lookup_limits)
+      
+      saveRDS(lookup_list, file.path(out_path, out_nm))
       
     }
     
