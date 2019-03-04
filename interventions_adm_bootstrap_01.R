17317d43af6a24ca147999589766bf54bb3724c3
Lorenzo Cattarino <l.cattarino@imperial.ac.uk>
2019-03-04 10:33:13 +0000

    pass all lookup tables as arguments; do not create lookup tables in this scripts; create list of parameters by calling the appropriate function

--- interventions_adm_bootstrap_01.R
+++ interventions_adm_bootstrap_01.R
@@ -18,7 +18,8 @@ my_resources <- c(
   file.path("R", "burden_and_interventions", "wrapper_to_R0_and_burden.R"),
   file.path("R", "burden_and_interventions", "functions_for_calculating_burden.R"),
   file.path("R", "prepare_datasets", "functions_for_calculating_R0.R"),
-  file.path("R", "utility_functions.R"))
+  file.path("R", "utility_functions.R"),
+  file.path("R", "create_parameter_list.R"))
 
 my_pkgs <- c("data.table", "dplyr")
 
@@ -27,49 +28,45 @@ ctx <- context::context_save(path = "context",
                              packages = my_pkgs,
                              sources = my_resources)
 
-context::context_load(ctx)
-context::parallel_cluster_start(7, ctx)
+
+# define extra parameters -----------------------------------------------------
 
 
-# define parameters ----------------------------------------------------------- 
+extra_prms <- list(id = 24,
+                   fit_type = "boot",
+                   parallel_2 = TRUE,
+                   phi_set_id_tag = "phi_set_id",
+                   base_info = c("population", 
+                                 "ID_0", 
+                                 "ID_1"))
 
 
-parameters <- list(
-  id = 24,
-  no_samples = 200,
-  gamma_1 = 0.45,
-  rho = 0.85,
-  gamma_3 = 0.15,
-  Q_1 = 0.04,
-  Q_3 = 0.04,
-  Q_4 = 0.04,
-  Q_2 = 0.1,
-  fit_type = "boot") 
+# are you using the cluster? --------------------------------------------------
 
-parallel_2 <- TRUE
 
-sf_vals <- c(1, 0.7, 0.3)
-phi_set_id_tag <- "phi_set_id"
-
-R0_assumptions <- list(
-  v1 = c(1, 1, 0, 0),
-  v2 = c(1, 1, 1, 1),  
-  v3 = calculate_infectiousness_wgts_for_sym_asym_assumption(parameters))
-
-FOI_values <- seq(0, 0.2, by = 0.0002)
-
-prob_fun <- list("calculate_primary_infection_prob",
-                 "calculate_secondary_infection_prob",
-                 "calculate_tertiary_infection_prob",
-                 "calculate_quaternary_infection_prob")
-
-base_info <- c("population", "ID_0", "ID_1")
+if (CLUSTER) {
+  
+  config <- didehpc::didehpc_config(template = "16Core")
+  obj <- didehpc::queue_didehpc(ctx, config = config)
+  
+} else {
+  
+  context::context_load(ctx)
+  context::parallel_cluster_start(7, ctx)
+  
+}
 
 
 # define variables ------------------------------------------------------------
 
 
+parameters <- create_parameter_list(extra_params = extra_prms)
+
 model_type <- paste0("model_", parameters$id)
+
+lookup_path <- file.path("output", 
+                         "predictions_world", 
+                         "bootstrap_models")
 
 out_path <- file.path("output", 
                       "predictions_world", 
@@ -77,11 +74,31 @@ out_path <- file.path("output",
                       model_type,
                       "adm_1")
 
-no_R0_assumptions <- length(R0_assumptions)
+no_R0_assumptions <- 3
+
+sf_vals <- parameters$sf_vals
+
+phi_set_id_tag <- parameters$phi_set_id_tag
 
 
 # load data ------------------------------------------------------------------- 
 
+
+FOI_to_Inf_list <- readRDS(file.path(lookup_path, "FOI_to_I_lookup_tables.rds"))
+
+FOI_to_C_list <- readRDS(file.path(lookup_path, "FOI_to_C_lookup_tables.rds"))
+
+FOI_to_HC_list <- readRDS(file.path(lookup_path, "FOI_to_HC_lookup_tables.rds"))
+
+FOI_to_R0_1_list <- readRDS(file.path(lookup_path, "FOI_to_R0_1_lookup_tables.rds"))
+
+FOI_to_R0_2_list <- readRDS(file.path(lookup_path, "FOI_to_R0_2_lookup_tables.rds"))
+
+FOI_to_R0_3_list <- readRDS(file.path(lookup_path, "FOI_to_R0_3_lookup_tables.rds"))
+
+FOI_to_C_list_fixed <- readRDS(file.path(lookup_path, "FOI_to_C_lookup_tables_fixed_params.rds"))
+
+FOI_to_HC_list_fixed <- readRDS(file.path(lookup_path, "FOI_to_HC_lookup_tables_fixed_params.rds"))
 
 bootstrap_experiments <- read.csv(file.path("output", 
                                             "EM_algorithm", 
@@ -119,14 +136,15 @@ sqr_preds <- inner_join(age_struct[, c("age_id", "ID_0")],
                         sqr_preds, 
                         by = "ID_0")
 
+sqr_preds <- as.matrix(sqr_preds)
+
+# sqr_preds <- sqr_preds[285510:285520,]
+
 
 # create table of scenarios --------------------------------------------------- 
 
 
 phi_set_id <- seq_len(no_R0_assumptions)
-
-phi_combs <- setNames(data.frame(phi_set_id, do.call("rbind", R0_assumptions)),
-                      nm = c(phi_set_id_tag, paste0("phi", seq_len(4))))
 
 fct_c <- setNames(expand.grid(phi_set_id, sf_vals),
                   nm = c(phi_set_id_tag, "scaling_factor"))
@@ -137,101 +155,9 @@ assumption <- as.numeric(unlist(strsplit(fit_var, "_"))[2])
 
 fct_c <- subset(fct_c, phi_set_id == assumption)  
 
-fct_c_2 <- left_join(fct_c, phi_combs, by = phi_set_id_tag)
+write_out_csv(fct_c, out_path, "scenario_table_wolbachia.csv")
 
-write_out_csv(fct_c_2, out_path, "scenario_table_wolbachia.csv")
-
-fctr_combs <- df_to_list(fct_c_2, use_names = TRUE)
-
-
-# create FOI -> Inf and FOI -> C lookup tables -------------------------------- 
-
-
-if(!file.exists(file.path(out_path, "FOI_to_I_lookup_tables.rds"))){
-  
-  Infection_values <- loop(seq_len(nrow(age_struct)), 
-                           wrapper_to_lookup,
-                           age_struct = age_struct, 
-                           tags = age_band_tgs, 
-                           FOI_values = FOI_values, 
-                           my_fun = calculate_infections,
-                           prob_fun = prob_fun,
-                           age_band_lower_bounds = age_band_L_bounds,
-                           age_band_upper_bounds = age_band_U_bounds,
-                           parallel = TRUE)
-  
-  FOI_to_Inf_list <- lapply(Infection_values, cbind_FOI_to_lookup, FOI_values)
-  
-  saveRDS(FOI_to_Inf_list, file.path(out_path, "FOI_to_I_lookup_tables.rds"))
-  
-} else {
-  
-  FOI_to_Inf_list <- readRDS(file.path(out_path, "FOI_to_I_lookup_tables.rds"))
-  
-}
-
-if(!file.exists(file.path(out_path, "FOI_to_C_lookup_tables.rds"))){
-  
-  Case_values <- loop(seq_len(nrow(age_struct)), 
-                      wrapper_to_lookup,
-                      age_struct = age_struct, 
-                      tags = age_band_tgs, 
-                      FOI_values = FOI_values, 
-                      my_fun = calculate_cases,
-                      prob_fun = prob_fun,
-                      age_band_lower_bounds = age_band_L_bounds,
-                      age_band_upper_bounds = age_band_U_bounds,
-                      parms = parameters,
-                      parallel = TRUE)
-  
-  FOI_to_C_list <- lapply(Case_values, cbind_FOI_to_lookup, FOI_values)
-  
-  saveRDS(FOI_to_C_list, file.path(out_path, "FOI_to_C_lookup_tables.rds"))
-  
-} else {
-  
-  FOI_to_C_list <- readRDS(file.path(out_path, "FOI_to_C_lookup_tables.rds"))
-  
-}
-
-if(!file.exists(file.path(out_path, "FOI_to_HC_lookup_tables.rds"))){
-  
-  HCase_values <- loop(seq_len(nrow(age_struct)), 
-                       wrapper_to_lookup,
-                       age_struct = age_struct, 
-                       tags = age_band_tgs, 
-                       FOI_values = FOI_values, 
-                       my_fun = calculate_hosp_cases,
-                       prob_fun = prob_fun,
-                       age_band_lower_bounds = age_band_L_bounds,
-                       age_band_upper_bounds = age_band_U_bounds,
-                       parms = parameters,
-                       parallel = TRUE)
-  
-  FOI_to_HC_list <- lapply(HCase_values, cbind_FOI_to_lookup, FOI_values)
-  
-  saveRDS(FOI_to_HC_list, file.path(out_path, "FOI_to_HC_lookup_tables.rds"))
-  
-} else {
-  
-  FOI_to_HC_list <- readRDS(file.path(out_path, "FOI_to_HC_lookup_tables.rds"))
-  
-}
-
-
-# convert to matrix -----------------------------------------------------------
-
-
-sqr_preds <- as.matrix(sqr_preds)
-
-# sqr_preds <- sqr_preds[285510:285520,]
-
-if (CLUSTER) {
-  
-  config <- didehpc::didehpc_config(template = "20Core")
-  obj <- didehpc::queue_didehpc(ctx, config = config)
-  
-}
+fctr_combs <- df_to_list(fct_c, use_names = TRUE)
 
 
 # submit one job --------------------------------------------------------------
@@ -244,15 +170,17 @@ if (CLUSTER) {
 #                                         age_band_tags = age_band_tgs,
 #                                         age_band_lower_bounds = age_band_L_bounds,
 #                                         age_band_upper_bounds = age_band_U_bounds,
-#                                         parallel_2 = parallel_2,
-#                                         FOI_values = FOI_values,
 #                                         FOI_to_Inf_list = FOI_to_Inf_list,
 #                                         FOI_to_C_list = FOI_to_C_list,
+#                                         FOI_to_C_list_fixed = FOI_to_C_list_fixed,
 #                                         FOI_to_HC_list = FOI_to_HC_list,
-#                                         prob_fun = prob_fun,
+#                                         FOI_to_HC_list_fixed = FOI_to_HC_list_fixed,
+#                                         FOI_to_R0_1_list = FOI_to_R0_1_list,
+#                                         FOI_to_R0_2_list = FOI_to_R0_2_list,
+#                                         FOI_to_R0_3_list = FOI_to_R0_3_list,
 #                                         parms = parameters,
-#                                         base_info = base_info,
-#                                         out_path = out_path))
+#                                         out_path = out_path,
+#                                         var_to_fit = fit_var))
 
 
 # submit a bundle -------------------------------------------------------------
@@ -268,14 +196,15 @@ if (CLUSTER) {
                             age_band_tags = age_band_tgs,
                             age_band_lower_bounds = age_band_L_bounds,
                             age_band_upper_bounds = age_band_U_bounds,
-                            parallel_2 = parallel_2,
-                            FOI_values = FOI_values,
                             FOI_to_Inf_list = FOI_to_Inf_list,
                             FOI_to_C_list = FOI_to_C_list,
+                            FOI_to_C_list_fixed = FOI_to_C_list_fixed,
                             FOI_to_HC_list = FOI_to_HC_list,
-                            prob_fun = prob_fun,
+                            FOI_to_HC_list_fixed = FOI_to_HC_list_fixed,
+                            FOI_to_R0_1_list = FOI_to_R0_1_list,
+                            FOI_to_R0_2_list = FOI_to_R0_2_list,
+                            FOI_to_R0_3_list = FOI_to_R0_3_list,
                             parms = parameters,
-                            base_info = base_info,
                             out_path = out_path,
                             var_to_fit = fit_var)
   
@@ -288,14 +217,15 @@ if (CLUSTER) {
                  age_band_tags = age_band_tgs,
                  age_band_lower_bounds = age_band_L_bounds,
                  age_band_upper_bounds = age_band_U_bounds,
-                 parallel_2 = parallel_2,
-                 FOI_values = FOI_values,
                  FOI_to_Inf_list = FOI_to_Inf_list,
                  FOI_to_C_list = FOI_to_C_list,
+                 FOI_to_C_list_fixed = FOI_to_C_list_fixed,
                  FOI_to_HC_list = FOI_to_HC_list,
-                 prob_fun = prob_fun,
+                 FOI_to_HC_list_fixed = FOI_to_HC_list_fixed,
+                 FOI_to_R0_1_list = FOI_to_R0_1_list,
+                 FOI_to_R0_2_list = FOI_to_R0_2_list,
+                 FOI_to_R0_3_list = FOI_to_R0_3_list,
                  parms = parameters,
-                 base_info = base_info,
                  out_path = out_path,
                  var_to_fit = fit_var,
                  parallel = FALSE)
