5600269abbf7efb41dff49147dbeaaa4dfced848
Lorenzo Cattarino <lcattari@dide.local>
2019-05-14 17:55:49 +0100

    change some parameters for rerunning; edit off plotting code; specify new parameters in fit_ranger_RF() call

--- EM_algorithm_02.R
+++ EM_algorithm_02.R
@@ -19,17 +19,17 @@ ctx <- context::context_save(path = "context",
 
 
 parameters <- list(
-  dependent_variable = "R0_3",
+  dependent_variable = "FOI",
+  pseudoAbs_value = -0.02,
+  no_predictors = 26,
   shape_1 = 0,
   shape_2 = 5,
   shape_3 = 1.6e6,
-  pseudoAbs_value = 0.5,
   foi_offset = 0.03,
   no_trees = 500,
   min_node_size = 20,
-  all_wgt = 1,
-  wgt_limits = c(1, 500),
-  no_predictors = 26) 
+  ranger_threds = NULL,
+  all_wgt = 1) 
 
 out_name <- "all_data.rds"  
 
@@ -45,10 +45,10 @@ var_to_fit <- parameters$dependent_variable
 
 foi_offset <- parameters$foi_offset
   
-out_path <- file.path("output", 
-                      "EM_algorithm", 
-                      "best_fit_models", 
-                      paste0("model_objects_", var_to_fit, "_fit"))
+out_pth <- file.path("output", 
+                     "EM_algorithm", 
+                     "best_fit_models", 
+                     paste0("model_objects_", var_to_fit, "_fit"))
 
 
 # start up -------------------------------------------------------------------- 
@@ -94,44 +94,13 @@ if(var_to_fit == "FOI"){
 training_dataset <- foi_data[, c(var_to_fit, my_predictors, "new_weight")]
 
 
-####
-# plot
-
-# library(ggplot2)
-# 
-# sub_foi <- foi_data[foi_data$type == "pseudoAbsence",]
-# 
-# png(file.path("figures", "wgt_area_relationship.png"),
-#     width = 10,
-#     height = 6,
-#     units = "in",
-#     pointsize = 12,
-#     res = 200)
-# 
-# p <- ggplot(sub_foi, aes(Shape_Area, new_weight)) +
-#   geom_point() +
-#   geom_text(aes(label = ISO), nudge_y = -0.5, size = 1.8) +
-#   scale_y_continuous(name = "Observation weight",
-#                      limits = c(0, max(wgt_limits)),
-#                      expand = c(0.1,0.1),
-#                      breaks = pretty(sub_foi$new_weight, 4),
-#                      labels = pretty(sub_foi$new_weight, 4)) +
-#   scale_x_continuous(name = expression("Pseudo absence admin unit area " ~ (10^{3} ~ "km"^{2})),
-#                      breaks = pretty(sub_foi$Shape_Area, 4),
-#                      labels = format(pretty(sub_foi$Shape_Area, 4) / 1000, scientific = FALSE))
-# 
-# print(p)
-# dev.off()
-
-
 # run job --------------------------------------------------------------------- 
 
 
-RF_obj <- fit_ranger_RF(dependent_variable = parameters$dependent_variable, 
+RF_obj <- fit_ranger_RF(parms = parameters, 
+                        dependent_variable = parameters$dependent_variable,
                         predictors = my_predictors, 
                         training_dataset = training_dataset, 
-                        no_trees = parameters$no_trees, 
-                        min_node_size = parameters$min_node_size,
                         my_weights = "new_weight")
 
-write_out_rds(RF_obj, out_path, out_name)
+write_out_rds(RF_obj, out_pth, out_name)
