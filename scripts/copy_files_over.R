a64269b8b23bd8d83ad065f2e0e0eb16a6c781e8
Lorenzo Cattarino <lcattari@imperial.ac.uk>
2019-05-16 16:32:30 -0700

    copy over mean country age dataset; do not overwrite when copy additional shapefiles files

--- scripts/copy_files_over.R
+++ scripts/copy_files_over.R
@@ -12,6 +12,9 @@ paths_from <- list(
   file.path("output", 
             "datasets", 
             "country_age_structure.csv"),
+  file.path("output",
+            "datasets",
+            "country_age_structure_mean.csv"),
   file.path("output", 
             "env_variables", 
             "All_adm1_env_var.csv"),
@@ -60,13 +63,13 @@ for (i in seq_along(paths_from)){
     evr_but_last_three_digits <- sub(".([^.]*)$", "", path_i)
     
     path_from_i_1 <- file.path(root, paste0(evr_but_last_three_digits, ".dbf"))  
-    file.copy(path_from_i_1, path_to_i, overwrite = TRUE)
+    file.copy(path_from_i_1, path_to_i, overwrite = FALSE)
     
     path_from_i_2 <- file.path(root, paste0(evr_but_last_three_digits, ".prj"))  
-    file.copy(path_from_i_2, path_to_i, overwrite = TRUE)
+    file.copy(path_from_i_2, path_to_i, overwrite = FALSE)
     
     path_from_i_3 <- file.path(root, paste0(evr_but_last_three_digits, ".shx"))  
-    file.copy(path_from_i_3, path_to_i, overwrite = TRUE)
+    file.copy(path_from_i_3, path_to_i, overwrite = FALSE)
     
   }
   
