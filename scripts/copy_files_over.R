3c3ed5aa6d8be8d44fa044732d672d5cc6e8d6f7
Lorenzo Cattarino <lcattari@imperial.ac.uk>
2019-05-16 16:22:12 -0700

    copy all the four different files of a shapefile

--- scripts/copy_files_over.R
+++ scripts/copy_files_over.R
@@ -51,6 +51,23 @@ for (i in seq_along(paths_from)){
   
   if(!dir.exists(path_to_i)) dir.create(path_to_i, FALSE, TRUE)
   
-  file.copy(path_from_i, path_to_i, overwrite = TRUE)
+  file.copy(path_from_i, path_to_i, overwrite = FALSE)
 
+  last_three_digits <- sub("^([^.]*).", "", path_i)
+  
+  if(last_three_digits == "shp"){
+    
+    evr_but_last_three_digits <- sub(".([^.]*)$", "", path_i)
+    
+    path_from_i_1 <- file.path(root, paste0(evr_but_last_three_digits, ".dbf"))  
+    file.copy(path_from_i_1, path_to_i, overwrite = TRUE)
+    
+    path_from_i_2 <- file.path(root, paste0(evr_but_last_three_digits, ".prj"))  
+    file.copy(path_from_i_2, path_to_i, overwrite = TRUE)
+    
+    path_from_i_3 <- file.path(root, paste0(evr_but_last_three_digits, ".shx"))  
+    file.copy(path_from_i_3, path_to_i, overwrite = TRUE)
+    
+  }
+  
 }
