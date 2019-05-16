39edd14cda643f99543cba28b1fbb56615df562e
Lorenzo Cattarino <lcattari@imperial.ac.uk>
2019-05-16 15:52:56 -0700

    run - it worked!

--- scripts/copy_files_over.R
+++ scripts/copy_files_over.R
@@ -1,6 +1,6 @@
 # copy over files from shared drive
 
-root <- file.path("Y:", "DENV_risk_mapping")
+root <- file.path("Y:", "DENV_risk_maps")
 
 paths_from <- list(
   file.path("data", 
@@ -18,6 +18,19 @@ paths_from <- list(
   file.path("output", 
             "datasets", 
             "pseudo_absence_points_2.csv"),
+  file.path("data", 
+            "env_variables", 
+            "plus60minus60_tiles.csv"),
+  file.path("output", 
+            "datasets", 
+            "NA_pixel_tiles_20km.txt"),
+  file.path("output", 
+            "variable_selection",
+            "stepwise_v3",
+            "predictor_rank.csv"),
+  file.path("output", 
+            "env_variables", 
+            "all_squares_env_var_0_1667_deg.rds"),
   file.path("output", 
             "shapefiles",
             "gadm28_adm1_eras.shp"),
@@ -29,12 +42,15 @@ for (i in seq_along(paths_from)){
   
   path_i <- paths_from[[i]]
   
+  message(path_i)
+  
   path_from_i <- file.path(root, path_i) 
   
-  path_to_i <- sub("^(.*?/.*?)/.*", "\\1", path_i)
+  # match everything before the last occurrence of /
+  path_to_i <- sub("/([^/]*)$", "", path_i)
   
   if(!dir.exists(path_to_i)) dir.create(path_to_i, FALSE, TRUE)
   
-  file.copy(path_from_i, path_to_i)
+  file.copy(path_from_i, path_to_i, overwrite = TRUE)
 
 }
