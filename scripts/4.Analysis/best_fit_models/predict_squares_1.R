08378b129d905cd4611fe99dbc3ff7963422b376
Lorenzo Cattarino <lcattari@imperial.ac.uk>
2019-05-17 15:30:28 -0700

    rerun on 17 of may

--- scripts/4.Analysis/best_fit_models/predict_squares_1.R
+++ scripts/4.Analysis/best_fit_models/predict_squares_1.R
@@ -1,33 +1,23 @@
 # Makes predictions for all the squares in the world.
 
-options(didehpc.cluster = "fi--didemrchnb")
+library(ranger)
 
-my_resources <- c(
-  file.path("R", "random_forest", "fit_ranger_RF_and_make_predictions.R"),
-  file.path("R", "plotting", "functions_for_plotting_raster_maps.R"),
-  file.path("R", "utility_functions.R"))
-  
-my_pkgs <- "ranger"
-
-context::context_log_start()
-ctx <- context::context_save(path = "context",
-                             packages = my_pkgs,
-                             sources = my_resources)
+source(file.path("R", "random_forest", "fit_ranger_RF_and_make_predictions.R"))
+source(file.path("R", "plotting", "functions_for_plotting_raster_maps.R"))
+source(file.path("R", "utility_functions.R"))
+source(file.path("R", "create_parameter_list.R"))
 
 
 # define parameters ----------------------------------------------------------- 
 
 
-parameters <- list(
-  dependent_variable = "R0_3",
-  foi_offset = 0.03,
-  no_predictors = 26)   
+extra_prms <- list(id = 13,
+                   dependent_variable = "Z",
+                   no_predictors = 26)   
 
 RF_mod_name <- "RF_obj.rds"
 
 base_info <- c("cell", "latitude", "longitude", "population", "ID_0", "ID_1", "ID_2")
-
-model_id <- 12
 
 extra_predictors <- NULL
 
@@ -35,19 +25,17 @@ extra_predictors <- NULL
 # define variables ------------------------------------------------------------
 
 
-foi_offset <- parameters$foi_offset
+parameters <- create_parameter_list(extra_params = extra_prms)
 
-model_type <- paste0("model_", model_id)
+model_type <- paste0("model_", parameters$id)
+
+var_to_fit <- parameters$dependent_variable
+
+foi_offset <- parameters$foi_offset
 
 out_pt <- file.path("output", "predictions_world", "best_fit_models", model_type)
   
 out_fl_nm <- "response.rds"
-
-
-# are you using the cluster? -------------------------------------------------- 
-
-
-context::context_load(ctx)
 
 
 # load data ------------------------------------------------------------------- 
@@ -65,7 +53,7 @@ RF_obj_path <- file.path("output",
 
 predictor_rank <- read.csv(file.path("output", 
                                      "variable_selection",
-                                     "stepwise",
+                                     "stepwise_v3",
                                      "predictor_rank.csv"), 
                            stringsAsFactors = FALSE)
 
@@ -84,10 +72,30 @@ RF_obj <- readRDS(file.path(RF_obj_path, RF_mod_name))
 
 p_i <- make_ranger_predictions(RF_obj, all_sqr_covariates, my_predictors)
 
-p_i <- p_i - foi_offset
+if(var_to_fit == "FOI"){
+  
+  p_i <- p_i - foi_offset
 
-p_i[p_i < 0] <- 0
+}
 
-world_sqr_preds <- cbind(all_sqr_covariates[, base_info], best = p_i)
+#### added on 17052019 - but it needs to go before (e.g. to assemble_foi_data_set_x)
+mean_age_data <- read.csv(file.path("output",
+                                    "datasets",
+                                    "country_age_structure_mean.csv"),
+                          stringsAsFactors = FALSE)
+all_sqr_covariates <- cbind(all_sqr_covariates, p_i = p_i)
+library(dplyr)
+all_sqr_covariates <- inner_join(all_sqr_covariates, mean_age_data[, c("ID_0", "mean_age", "sd_age")])
+####
+
+if(var_to_fit == "Z"){
+  
+  all_sqr_covariates$p_i <- (all_sqr_covariates$p_i - foi_offset) * all_sqr_covariates$mean_age
+  
+}
+
+all_sqr_covariates$p_i[all_sqr_covariates$p_i < 0] <- 0
+
+world_sqr_preds <- cbind(all_sqr_covariates[, c(base_info, "mean_age", "sd_age")], best = all_sqr_covariates$p_i)
 
 write_out_rds(world_sqr_preds, out_pt, out_fl_nm)  
