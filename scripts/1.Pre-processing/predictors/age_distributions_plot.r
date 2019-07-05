7e450cb98accc6691e6b26bbbb411b014748bd04
Lorenzo Cattarino <lcattari@dide.local>
2019-07-05 10:56:53 +0100

    remove calculation of grouped data statistics

--- scripts/1.Pre-processing/predictors/age_distributions_plot.r
+++ scripts/1.Pre-processing/predictors/age_distributions_plot.r
@@ -1,77 +1,45 @@
 
-library("ggplot2")
-        
+library(dplyr)
+library(ggplot2)
+
 source(file.path("R", "prepare_datasets", "grouped_data_stats.R"))
-source(file.path("R", "utility_functions.R"))
 
 
 # define parameters -----------------------------------------------------------
 
 
-dts_out_pt <- file.path("output", "datasets")
-  
-dts_out_nm <- "country_age_structure_mean.csv"
-
 out_path <- file.path("figures", "age_distribution_plots")
-  
+
 out_file_name <- "country_age_distributions"
 
 
 # load data -------------------------------------------------------------------
 
 
-age_distr <- read.csv(file.path("output", 
+age_struct_data <- read.csv(file.path("output", 
                                 "datasets", 
                                 "country_age_structure.csv"))
 
 
-# pre processing --------------------------------------------------------------
+# preprocessing ---------------------------------------------------------------
 
 
-age_distr <- age_distr[setdiff(names(age_distr), c("band_80_99", "band_85_99"))]
+old_col_names <- grep("band", names(age_struct_data), value = TRUE)
 
-age_band_tgs <- grep("band", names(age_distr), value = TRUE)
+new_col_names <- edit_age_band_tags(age_struct_data)
 
-age_bounds_num <- sub("^[^_]+_", "", age_band_tgs)
+age_struct_data <- age_struct_data %>% 
+  rename_at(vars(old_col_names), ~ new_col_names)
 
-age_bounds_num_2 <- sub("_", "-",age_bounds_num)
-
-names(age_distr)[names(age_distr) %in% age_band_tgs] <- age_bounds_num_2
-
-xx <- strsplit(age_bounds_num_2, "-")
-zz <- lapply(xx, as.numeric)
-yy <- vapply(zz, mean, numeric(1))
-
-mean_ages <- apply(age_distr[, age_bounds_num_2], 1, mean_grouped_data, yy)
-
-n_c <- nrow(age_distr)
-
-sd <- vapply(
-  seq_len(n_c), 
-  sd_grouped_data,
-  numeric(1),
-  age_dat = age_distr[, age_bounds_num_2],
-  mean_vals = mean_ages) 
-  
-age_distr$mean_age <- mean_ages
-
-age_distr$sd_age <- sd
-
-age_distr <- age_distr[order(age_distr$mean_age), ]
-
-
-# save ------------------------------------------------------------------------
-
-
-write_out_csv(age_distr, dts_out_pt, dts_out_nm)
+age_struct_data <- age_struct_data[order(age_struct_data$mean_age), ]
 
 
 # plot ------------------------------------------------------------------------
 
 
-age_distr$mean_label <- paste0("Mean = ", round(age_distr$mean_age, 2))
+age_struct_data$mean_label <- paste0("Mean = ", round(age_struct_data$mean_age, 2))
 
-age_distr$sd_label <- paste0("SD = ", round(age_distr$sd_age, 2))
+age_struct_data$sd_label <- paste0("SD = ", round(age_struct_data$sd_age, 2))
 
 dir.create(out_path, FALSE, TRUE)
 
@@ -84,9 +52,9 @@ for (j in 1:13){
   cat("i =", i, "\n")
   cat("ii =", ii, "\n")
   
-  dat_to_plot <- reshape2::melt(age_distr[i:ii,], 
+  dat_to_plot <- reshape2::melt(age_struct_data[i:ii,], 
                                 id.vars = "country", 
-                                measure.vars = age_bounds_num_2)
+                                measure.vars = new_col_names)
   
   dat_to_plot$country <- droplevels(dat_to_plot$country)
   
@@ -104,10 +72,10 @@ for (j in 1:13){
   
   p <- ggplot(dat_to_plot, aes(x = variable, y = value)) +
     geom_bar(stat = "identity") +
-    geom_text(data = age_distr[i:ii,], 
+    geom_text(data = age_struct_data[i:ii,], 
               mapping = aes(x = 17, y = 0.15, label = mean_label), 
               size = 10) +
-    geom_text(data = age_distr[i:ii,], 
+    geom_text(data = age_struct_data[i:ii,], 
               mapping = aes(x = 17, y = 0.1, label = sd_label), 
               size = 10) +
     facet_wrap(~country, ncol = 4, nrow = 4) +
