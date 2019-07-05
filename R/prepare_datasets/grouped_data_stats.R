e475e9941a1ccac06d144d926ed6a198e3c59539
Lorenzo Cattarino <lcattari@dide.local>
2019-07-05 10:55:55 +0100

    add function for editing age band tags

--- R/prepare_datasets/grouped_data_stats.R
+++ R/prepare_datasets/grouped_data_stats.R
@@ -5,3 +5,13 @@ sd_grouped_data <- function(i, age_dat, mean_vals) {
   (sqrt(sum((yy^2) * age_dat[i,]) - mean_vals[i]^2))
 
 }
+
+edit_age_band_tags <- function(age_distr_df) {
+  
+  age_band_tgs <- grep("band", names(age_distr_df), value = TRUE)
+  
+  age_bounds_num <- sub("^[^_]+_", "", age_band_tgs)
+  
+  sub("_", "-", age_bounds_num)
+  
+}
