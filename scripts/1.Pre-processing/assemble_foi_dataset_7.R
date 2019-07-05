3c66db4135c201e752d6a161366c06ba93216756
Lorenzo Cattarino <lcattari@dide.local>
2019-07-05 09:56:04 +0100

    do not join the country age structure

--- scripts/1.Pre-processing/assemble_foi_dataset_7.R
+++ scripts/1.Pre-processing/assemble_foi_dataset_7.R
@@ -1,4 +1,4 @@
-# Attach mean country age to each data point and calculalte new response variable
+# Calculalte new response variable
 
 library(dplyr)
 library(ggplot2)
@@ -14,18 +14,11 @@ foi_data <- read.csv(file.path("output",
                                "All_FOI_estimates_and_predictors.csv"),
                      stringsAsFactors = FALSE) 
 
-mean_age_data <- read.csv(file.path("output",
-                                    "datasets",
-                                    "country_age_structure_mean.csv"),
-                          stringsAsFactors = FALSE)
-
 
 # process ---------------------------------------------------------------------
 
 
-foi_data_2 <- left_join(foi_data, mean_age_data[, c("ID_0", "mean_age", "sd_age")])
-
-my_plot <- ggplot(subset(foi_data_2, type != "pseudoAbsence")) +
+my_plot <- ggplot(subset(foi_data, type != "pseudoAbsence")) +
   geom_point(aes(FOI, mean_age, colour = type), size = 1)
 
 save_plot(my_plot, 
@@ -34,8 +27,8 @@ save_plot(my_plot,
           12,
           8)
 
-foi_data_2$Z <- 35 * (foi_data_2$FOI / foi_data_2$mean_age)
+foi_data$Z <- 35 * (foi_data$FOI / foi_data$mean_age)
 
-write_out_csv(foi_data_2, 
+write_out_csv(foi_data, 
               file.path("output", "foi"), 
               "All_FOI_estimates_and_predictors.csv")
