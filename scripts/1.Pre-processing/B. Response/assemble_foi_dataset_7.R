12972e54b873c8b302e399a49965a24e1ba67d8e
Lorenzo Cattarino <lcattari@dide.local>
2019-07-05 11:44:44 +0100

    create new response variable

--- scripts/1.Pre-processing/B. Response/assemble_foi_dataset_7.R
+++ scripts/1.Pre-processing/B. Response/assemble_foi_dataset_7.R
@@ -1,4 +1,4 @@
-# Calculalte new response variable
+# Calculalte new response variables
 
 library(dplyr)
 library(ggplot2)
@@ -27,7 +27,18 @@ save_plot(my_plot,
           12,
           8)
 
-foi_data$Z <- 35 * (foi_data$FOI / foi_data$mean_age)
+# foi_data$Z <- 35 * (foi_data$FOI / foi_data$mean_age)
+
+my_plot <- ggplot(subset(foi_data, type != "pseudoAbsence")) +
+  geom_point(aes(FOI, birth_rate, colour = type), size = 1)
+
+save_plot(my_plot, 
+          file.path("figures", "data"),
+          "FOI_vs_country_birth_rate",
+          12,
+          8)
+
+foi_data$Z <- foi_data$FOI / (35 * foi_data$birth_rate)
 
 write_out_csv(foi_data, 
               file.path("output", "foi"), 
