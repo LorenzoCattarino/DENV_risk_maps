8dcdf2f2875bb8e6d920b365ef0002135b9eaa7b
Lorenzo Cattarino <lcattari@dide.local>
2019-07-08 12:44:53 +0100

    remove from utility functions

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -44,24 +44,6 @@ df_to_list <- function (x, use_names) {
   ret
 }
 
-lm_eqn <- function(df, y, x){
-  
-  # fit linear model - no intercept 
-  frmla <- as.formula(paste0(y, " ~ ", x, " - 1"))
-  m <- lm(frmla, df)
-  
-  #browser()
-  
-  # calculates R2
-  r2 <- 1 - crossprod(residuals(m)) / crossprod(df[,y] - mean(df[,y]))
-  #r2 <- summary(m)$r.squared # R 
-  
-  eq <- substitute(italic(y) == b %.% italic(x)*","~~italic(R)^2~"="~r2, 
-                   list(b = format(coef(m), digits = 4), 
-                        r2 = format(r2, digits = 4)))
-  as.character(as.expression(eq))                 
-}
-
 save_plot <- function(plot_obj, out_pth, out_fl_nm, wdt, hgt){
   
   dir.create(out_pth, FALSE, TRUE)
