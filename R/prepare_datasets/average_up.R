5f38845513a3f0a97896399261c14bff232a6350
Lorenzo Cattarino <lcattari@dide.local>
2019-07-18 11:00:20 +0100

    define function to use the pixel level predictions, instead of the mean of the predictions, for the serology data points

--- R/prepare_datasets/average_up.R
+++ R/prepare_datasets/average_up.R
@@ -16,12 +16,12 @@ average_up <- function(pxl_df, grp_flds, var_names){
 
 }
 
-average_data_points <- function(i){
-  
-  i %>%
-    group_by(ADM_0, ADM_1) %>%
-    summarise(o_j = mean(o_j), admin = mean(admin), square = mean(square))
-}
+# average_data_points <- function(i){
+#   
+#   i %>%
+#     group_by(ADM_0, ADM_1) %>%
+#     summarise(o_j = mean(o_j), admin = mean(admin), square = mean(square))
+# }
 
 multi_col_average_up <- function(i, x, grp_flds){
   
@@ -37,5 +37,18 @@ remove_pop_col <- function(i){
 how_many_below_1 <- function(x){
   
   sum(x < 1)  
+  
+}
+
+fitted_sero_cell_to_adm <- function(join_all, sqr_dataset_2, fld_1, fld_2) {
+  
+  fitted_sero_cells <- sqr_dataset_2[sqr_dataset_2$type == "serology" & sqr_dataset_2$new_weight == 1, fld_1]
+  
+  id_field <- fld_1[1]
+  
+  join_all %>%
+    left_join(fitted_sero_cells, by = id_field) %>%
+    mutate(mean_p_i = ifelse(type == "serology", p_i, mean_p_i)) %>%
+    select(fld_2)
   
 }
