595407c83529c996424184a5e50fbbc979129484
Lorenzo Cattarino <lcattari@dide.local>
2019-07-21 14:27:20 +0100

    changes following adding preprocessing at the beginning and style edits

--- scripts/4.Analysis/bootstrap_models/EM_algorithm_with_bootstrap_06.R
+++ scripts/4.Analysis/bootstrap_models/EM_algorithm_with_bootstrap_06.R
@@ -10,10 +10,13 @@ options(didehpc.cluster = "fi--didemrchnb")
 CLUSTER <- TRUE
 
 my_resources <- c(
-  file.path("R", "prepare_datasets", "average_up.R"),
-  file.path("R", "prepare_datasets", "remove_NA_rows.R"),
+  file.path("R", "utility_functions.R"),
+  file.path("R", "create_parameter_list.R"),
   file.path("R", "random_forest", "fit_ranger_RF_and_make_predictions.R"),
-  file.path("R", "utility_functions.R"))
+  file.path("R", "random_forest", "join_predictions.R"),
+  file.path("R", "random_forest", "wrapper_functions_for_boot_analysis.R"),
+  file.path("R", "prepare_datasets", "average_up.R"),
+  file.path("R", "prepare_datasets", "set_pseudo_abs_weights.R"))
 
 my_pkgs <- c("ranger", "dplyr")
 
@@ -26,34 +29,37 @@ ctx <- context::context_save(path = "context",
 # define parameters -----------------------------------------------------------  
 
 
-parameters <- list(
-  id = 1,
-  shape_1 = 0,
-  shape_2 = 5,
-  shape_3 = 1e6,
-  all_wgt = 1,
-  dependent_variable = "FOI",
-  pseudoAbs_value = -0.02,
-  grid_size = 1 / 120,
-  no_predictors = 9,
-  resample_grid_size = 20,
-  foi_offset = 0.03,
-  no_trees = 500,
-  min_node_size = 20,
-  no_samples = 200,
-  EM_iter = 10) 
+extra_prms <- list(id = 29,
+                   dependent_variable = "FOI",
+                   no_predictors = 26,
+                   id_fld = "data_id",
+                   grp_flds = c("data_id", "ID_0", "ID_1"))
 
-grp_flds <- c("ID_0", "ID_1", "data_id")
+
+# are you using the cluster? --------------------------------------------------  
+
+
+if (CLUSTER) {
+  
+  obj <- didehpc::queue_didehpc(ctx)
+  
+} else {
+  
+  context::context_load(ctx)
+  
+}
 
 
 # define variables ------------------------------------------------------------ 
 
 
+parameters <- create_parameter_list(extra_params = extra_prms)
+
 model_type <- paste0("model_", parameters$id)
 
 var_to_fit <- parameters$dependent_variable
 
-my_dir <- paste0("grid_size_", parameters$grid_size)
+no_samples <- parameters$no_samples
 
 RF_obj_path <- file.path("output",
                          "EM_algorithm",
@@ -67,50 +73,38 @@ out_pt <- file.path("output",
                     model_type,
                     "data_admin_predictions")
 
-data_sqr_predictions_in_path <- file.path("output",
+data_sqr_predictions_pth <- file.path("output",
                                           "EM_algorithm",
                                           "bootstrap_models",
                                           model_type,
                                           "data_square_predictions")
 
+covariates_dir <- parameters$covariates_dir
 
-# are you using the cluster? --------------------------------------------------  
-
-
-if (CLUSTER) {
-  
-  config <- didehpc::didehpc_config(template = "16Core")
-  obj <- didehpc::queue_didehpc(ctx, config = config)
-  
-} else {
-  
-  context::context_load(ctx)
-  context::parallel_cluster_start(8, ctx)
-  
-}
-
-# obj$enqueue(install.packages(file.path("R_sources", "h2o_3.18.0.8.tar.gz"), repos=NULL, type="source"))$wait(Inf)
+foi_data_pth <- file.path("output", 
+                          "EM_algorithm",
+                          "bootstrap_models",
+                          model_type, 
+                          "adm_foi_data",
+                          "boot_samples") 
 
 
 # load data -------------------------------------------------------------------  
 
 
-foi_dataset <- read.csv(file.path("output", 
-                                  "foi", 
-                                  "All_FOI_estimates_and_predictors.csv"),
-                        stringsAsFactors = FALSE) 
-
-boot_samples <- readRDS(file.path("output",
-                                  "EM_algorithm",
-                                  "bootstrap_models",
-                                  my_dir, 
-                                  "bootstrap_samples.rds"))
+original_foi <- readRDS(file.path("output", 
+                                  "EM_algorithm", 
+                                  "bootstrap_models", 
+                                  model_type,
+                                  "adm_foi_data",
+                                  "adm_foi_data.rds")) 
   
-sqr_dataset <- readRDS(file.path("output", 
-                                 "EM_algorithm", 
-                                 "best_fit_models",
-                                 "env_variables", 
-                                 "env_vars_20km_2.rds"))
+data_sqr_covariates <- readRDS(file.path("output", 
+                                         "EM_algorithm",
+                                         "best_fit_models",
+                                         "model_15",
+                                         "env_variables", 
+                                         "env_vars_20km.rds"))
 
 adm_dataset <- read.csv(file.path("output",
                                   "env_variables",
@@ -120,69 +114,31 @@ adm_dataset <- read.csv(file.path("output",
 
 predictor_rank <- read.csv(file.path("output", 
                                      "variable_selection",
-                                     "stepwise",
+                                     covariates_dir,
                                      "predictor_rank.csv"), 
                            stringsAsFactors = FALSE)
 
-# tiles
-tile_summary <- read.csv(file.path("data", 
-                                   "env_variables", 
-                                   "plus60minus60_tiles.csv"), 
-                         header = TRUE, 
-                         stringsAsFactors = FALSE)
 
-# NA pixel tiles 
-NA_pixel_tiles <- read.table(file.path("output", 
-                                       "datasets", 
-                                       "NA_pixel_tiles_20km.txt"), 
-                             sep = ",",
-                             header = TRUE)
+# pre processing --------------------------------------------------------------
 
-
-# process the original data ---------------------------------------------------
-
-
-names(foi_dataset)[names(foi_dataset) == var_to_fit] <- "o_j"
-
-foi_dataset[foi_dataset$type == "pseudoAbsence", "o_j"] <- parameters$pseudoAbs_value
-
-
-# pre process admin predictions ----------------------------------------------- 
-
-
-adm_dataset <- adm_dataset[!duplicated(adm_dataset[, c("ID_0", "ID_1")]), ]
-
-
-# create some objects ---------------------------------------------------------  
-
-
-tile_ids <- tile_summary$tile.id
-
-NA_pixel_tile_ids <- NA_pixel_tiles$tile_id
-
-tile_ids_2 <- tile_ids[!tile_ids %in% NA_pixel_tile_ids]  
 
 my_predictors <- predictor_rank$name[1:parameters$no_predictors]
-
-no_samples <- parameters$no_samples
 
 
 # submit one job --------------------------------------------------------------
 
 
 # t <- obj$enqueue(
-#   attach_pred_different_scale_to_data(
+#   get_bsample_and_join_predictions(
 #     seq_len(no_samples)[1],
 #     model_path = RF_obj_path,
-#     foi_data = foi_dataset,
+#     original_foi = original_foi,
+#     boot_foi_data_path = foi_data_pth,
 #     adm_dts = adm_dataset,
 #     predictors = my_predictors,
-#     data_sqr_predictions_in_path = data_sqr_predictions_in_path,
-#     sqr_dts = sqr_dataset,
-#     tile_ids = tile_ids_2,
-#     bt_samples = boot_samples,
+#     data_sqr_predictions_path = data_sqr_predictions_pth,
+#     sqr_dts = data_sqr_covariates,
 #     out_path = out_pt,
-#     grp_fields = grp_flds,
 #     parms = parameters))
 
 
@@ -193,35 +149,31 @@ if (CLUSTER) {
 
   bsamples_preds <- queuer::qlapply(
     seq_len(no_samples),
-    attach_pred_different_scale_to_data,
+    get_bsample_and_join_predictions,
     obj,
     model_path = RF_obj_path,
-    foi_data = foi_dataset,
+    original_foi = original_foi,
+    boot_foi_data_path = foi_data_pth,
     adm_dts = adm_dataset,
     predictors = my_predictors,
-    data_sqr_predictions_in_path = data_sqr_predictions_in_path,
-    sqr_dts = sqr_dataset,
-    tile_ids = tile_ids_2,
-    bt_samples = boot_samples,
+    data_sqr_predictions_path = data_sqr_predictions_pth,
+    sqr_dts = data_sqr_covariates,
     out_path = out_pt,
-    grp_fields = grp_flds,
     parms = parameters)
 
 } else {
 
   bsamples_preds <- lapply(
     seq_len(no_samples)[1],
-    attach_pred_different_scale_to_data,
+    get_bsample_and_join_predictions,
     model_path = RF_obj_path,
-    foi_data = foi_dataset,
+    original_foi = original_foi,
+    boot_foi_data_path = foi_data_pth,
     adm_dts = adm_dataset,
     predictors = my_predictors,
-    data_sqr_predictions_in_path = data_sqr_predictions_in_path,
-    sqr_dts = sqr_dataset,
-    tile_ids = tile_ids_2,
-    bt_samples = boot_samples,
+    data_sqr_predictions_path = data_sqr_predictions_pth,
+    sqr_dts = data_sqr_covariates,
     out_path = out_pt,
-    grp_fields = grp_flds,
     parms = parameters)
 
 }
