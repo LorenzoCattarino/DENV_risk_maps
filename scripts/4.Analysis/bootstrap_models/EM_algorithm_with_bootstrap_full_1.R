11c11b3cfc5c1f821c52f6a6985156ff91ca51da
Lorenzo Cattarino <lcattari@dide.local>
2019-07-23 20:42:44 +0100

    rewrite after major changes

--- scripts/4.Analysis/bootstrap_models/EM_algorithm_with_bootstrap_full_1.R
+++ scripts/4.Analysis/bootstrap_models/EM_algorithm_with_bootstrap_full_1.R
@@ -1,20 +1,23 @@
 
 options(didehpc.cluster = "fi--didemrchnb")
 
-CLUSTER <- TRUE
+CLUSTER <- FALSE
 
 my_resources <- c(
+  file.path("R", "utility_functions.R"),
+  file.path("R", "create_parameter_list.R"),
+  file.path("R", "random_forest", "preprocess.R"),
   file.path("R", "random_forest", "fit_ranger_RF_and_make_predictions.R"),
-  file.path("R", "random_forest", "EM_full_routine.R"),
   file.path("R", "random_forest", "exp_max_algorithm.R"),
-  file.path("R", "plotting", "functions_for_plotting_raster_maps.R"),
-  file.path("R", "plotting", "generic_scatter_plot.R"),
-  file.path("R", "prepare_datasets", "remove_NA_rows.R"),
+  file.path("R", "random_forest", "join_predictions.R"),
+  file.path("R", "random_forest", "full_routine_bootstrap.R"),
+  file.path("R", "random_forest", "wrapper_functions_for_boot_analysis.R"),
+  file.path("R", "prepare_datasets", "set_pseudo_abs_weights.R"),
   file.path("R", "prepare_datasets", "average_up.R"),
   file.path("R", "prepare_datasets", "calculate_wgt_corr.R"),
-  file.path("R", "utility_functions.R"))
+  file.path("R", "plotting", "plot_EM_diagnostics.R"))
 
-my_pkgs <- c("ranger", "dplyr", "fields", "ggplot2", "weights", "colorRamps")
+my_pkgs <- c("ranger", "dplyr", "weights", "ggplot2")
 
 context::context_log_start()
 ctx <- context::context_save(path = "context",
@@ -25,34 +28,15 @@ ctx <- context::context_save(path = "context",
 # define parameters ----------------------------------------------------------- 
 
 
-parameters <- list(
-  shape_1 = 0,
-  shape_2 = 5,
-  shape_3 = 1e6,
-  all_wgt = 1,
-  pseudoAbs_value = c(-0.02, 0.5),
-  resample_grid_size = 20,
-  foi_offset = 0.03,
-  no_trees = 500,
-  min_node_size = 20,
-  no_samples = 200,
-  EM_iter = 10) 
+extra_prms <- list(grp_flds = c("unique_id", "ID_0", "ID_1"),
+                   id_fld = "unique_id",
+                   ranger_threads = 1) 
 
-grp_flds_1 <- c("ID_0", "ID_1", "unique_id")
+predictor_numbers <- c(9, 26)
 
-grp_flds_2 <- c("ID_0", "ID_1", "data_id")
-
-predictor_numbers <- c(9, 23, 26)
-
-response_vars <- c("FOI", "R0_1", "R0_2", "R0_3")
+response_vars <- "FOI"
   
 grid_sizes <- c(1/120, 0.5, 1, 2, 5, 10)
-
-
-# define variables ------------------------------------------------------------  
-
-
-no_samples <- parameters$no_samples
 
 
 # start up the cluster --------------------------------------------------------
@@ -69,6 +53,16 @@ if (CLUSTER) {
 }
 
 
+# define variables ------------------------------------------------------------  
+
+
+parameters <- create_parameter_list(extra_params = extra_prms)
+
+covariates_dir <- parameters$covariates_dir
+
+no_samples <- parameters$no_samples
+
+
 # load data ------------------------------------------------------------------- 
 
 
@@ -79,7 +73,7 @@ foi_data <- read.csv(file.path("output",
 
 predictor_rank <- read.csv(file.path("output", 
                                      "variable_selection",
-                                     "stepwise",
+                                     covariates_dir,
                                      "predictor_rank.csv"), 
                            stringsAsFactors = FALSE)
 
@@ -87,17 +81,18 @@ all_sqr_covariates <- readRDS(file.path("output",
                                         "env_variables", 
                                         "all_squares_env_var_0_1667_deg.rds"))
 
-data_sqr_covariates <- readRDS(file.path("output", 
-                                         "EM_algorithm",
-                                         "best_fit_models",
-                                         "env_variables", 
-                                         "env_vars_20km_2.rds"))
-
 admin_covariates <- read.csv(file.path("output",
                                        "env_variables",
                                        "All_adm1_env_var.csv"),
                              header = TRUE,
                              stringsAsFactors = FALSE)
+
+data_sqr_covariates <- readRDS(file.path("output", 
+                                         "EM_algorithm",
+                                         "best_fit_models",
+                                         "model_1",
+                                         "env_variables", 
+                                         "env_vars_20km.rds"))
 
 
 # pre process -----------------------------------------------------------------
@@ -105,10 +100,16 @@ admin_covariates <- read.csv(file.path("output",
 
 all_predictors <- predictor_rank$name
 
-test1 <- expand.grid(var = response_vars, gs = grid_sizes[c(1, 5)], no_pred = predictor_numbers, stringsAsFactors = FALSE)
-  
-test2 <- expand.grid(var = response_vars[4], gs = grid_sizes[-c(1, 5)], no_pred = predictor_numbers[2], stringsAsFactors = FALSE)
- 
+test1 <- expand.grid(var = response_vars, 
+                     gs = grid_sizes[c(1, 5)], 
+                     no_pred = predictor_numbers, 
+                     stringsAsFactors = FALSE)
+
+test2 <- expand.grid(var = response_vars, 
+                     gs = grid_sizes[-c(1, 5)], 
+                     no_pred = predictor_numbers[2], 
+                     stringsAsFactors = FALSE)
+
 test <- rbind(test1, test2)
 
 test_all <- cbind(exp_id = seq_len(nrow(test)), test)
@@ -125,49 +126,43 @@ write_out_csv(test_all_3,
 
 test_ls <- df_to_list(test_all_3, TRUE)
 
-  
+
 # run one job -----------------------------------------------------------------
 
 
-# t <- obj$enqueue(EM_full_routine(test_ls[[1]],
-#                                  parms = parameters,
-#                                  data_squares = data_sqr_covariates,
-#                                  all_squares = all_sqr_covariates,
-#                                  predictors = all_predictors,
-#                                  grp_flds_1 = grp_flds_1,
-#                                  grp_flds_2 = grp_flds_2,
-#                                  adm_dataset = admin_covariates,
-#                                  foi_data = foi_data))
-                 
+# t <- obj$enqueue(full_routine_bootstrap(test_ls[[1]],
+#                                         parms = parameters,
+#                                         original_foi_data = foi_data,
+#                                         adm_covariates = admin_covariates,
+#                                         data_squares = data_sqr_covariates,
+#                                         all_squares = all_sqr_covariates,
+#                                         all_predictors = all_predictors))
+
 
 # run -------------------------------------------------------------------------
 
 
 if (CLUSTER) {
-
+  
   multi_full_EM_experiments_2 <- queuer::qlapply(
-    test_ls[2001:5600],
-    EM_full_routine,
+    test_ls[1:200],
+    full_routine_bootstrap,
     obj,
     parms = parameters,
+    original_foi_data = foi_data,
+    adm_covariates = admin_covariates,
     data_squares = data_sqr_covariates,
     all_squares = all_sqr_covariates,
-    predictors = all_predictors,
-    grp_flds_1 = grp_flds_1,
-    grp_flds_2 = grp_flds_2,
-    adm_dataset = admin_covariates,
-    foi_data = foi_data)
-
+    all_predictors = all_predictors)
+  
 } else {
   
-  EM_full_routine(test_ls[[1]],
-                  parms = parameters,
-                  data_squares = data_sqr_covariates,
-                  all_squares = all_sqr_covariates,
-                  predictors = all_predictors,
-                  grp_flds_1 = grp_flds_1,
-                  grp_flds_2 = grp_flds_2,
-                  adm_dataset = admin_covariates,
-                  foi_data = foi_data)
+  full_routine_bootstrap(test_ls[[1]],
+                         parms = parameters,
+                         original_foi_data = foi_data,
+                         adm_covariates = admin_covariates,
+                         data_squares = data_sqr_covariates,
+                         all_squares = all_sqr_covariates,
+                         all_predictors = all_predictors)
   
 }
