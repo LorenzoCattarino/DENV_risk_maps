d07d086ee60a5065d87ec328d04df8e05f90af38
Lorenzo Cattarino <lcattari@dide.local>
2019-07-26 09:39:55 +0100

    remove cluster support

--- RUBBISH/scripts/predict_squares_with_bootstrap_1.R
+++ RUBBISH/scripts/predict_squares_with_bootstrap_1.R
@@ -1,55 +1,39 @@
 # Load back square predictions for the world, for each model fit 
 # Save a n squares x n fits matrix. 
 
-options(didehpc.cluster = "fi--didemrchnb")
-
-my_resources <- c(
-  file.path("R", "random_forest", "fit_ranger_RF_and_make_predictions.R"),
-  file.path("R", "utility_functions.R"))
-
-my_pkgs <- "ranger"
-
-context::context_log_start()
-ctx <- context::context_save(path = "context",
-                             packages = my_pkgs,
-                             sources = my_resources)
-
-context::context_load(ctx)
+source(file.path("R", "utility_functions.R"))
+source(file.path("R", "create_parameter_list.R"))
+source(file.path("R", "random_forest", "fit_ranger_RF_and_make_predictions.R"))
 
 
 # define parameters ----------------------------------------------------------- 
 
 
-parameters <- list(
-  id = 1,
-  shape_1 = 0,
-  shape_2 = 5,
-  shape_3 = 1e6,
-  all_wgt = 1,
-  dependent_variable = "FOI",
-  pseudoAbs_value = -0.02,
-  grid_size = 1 / 120,
-  no_predictors = 9,
-  resample_grid_size = 20,
-  foi_offset = 0.03,
-  no_trees = 500,
-  min_node_size = 20,
-  no_samples = 200,
-  EM_iter = 10) 
+extra_prms <- list(id = 8,
+                   dependent_variable = "FOI",
+                   base_info = c("cell", 
+                                 "latitude", 
+                                 "longitude", 
+                                 "population", 
+                                 "ID_0", 
+                                 "ID_1", 
+                                 "ID_2")) 
 
 out_fl_nm <- "response.rds"
-
-base_info <- c("cell", "latitude", "longitude", "population", "ID_0", "ID_1", "ID_2")
 
 
 # define variables ------------------------------------------------------------
 
+
+parameters <- create_parameter_list(extra_params = extra_prms)
 
 model_type <- paste0("model_", parameters$id)
 
 var_to_fit <- parameters$dependent_variable
 
 foi_offset <- parameters$foi_offset
+
+base_info <- parameters$base_info
 
 global_predictions_in_path <- file.path("output", 
                                         "predictions_world",
