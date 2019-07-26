e42abb90509a421803ed0020101f341d780211ac
Lorenzo Cattarino <lcattari@dide.local>
2019-07-26 15:23:20 +0100

    style edits; remove edited off code

--- scripts/4.Analysis/bootstrap_models/predict_squares_with_bootstrap_5.R
+++ scripts/4.Analysis/bootstrap_models/predict_squares_with_bootstrap_5.R
@@ -3,21 +3,25 @@
 # based on the distance of the pixel to the closest data point
 
 
-source(file.path("R", "prepare_datasets", "calculate_mean_across_fits.R"))
 source(file.path("R", "utility_functions.R"))
+source(file.path("R", "prepare_datasets", "calculate_mean_across_fits.R"))
 
 
 # define parameters -----------------------------------------------------------
 
 
-parameters <- list(
-  id = c(20, 24, 25, 16, 26),
-  dependent_variable = "R0_3",
-  grid_size = c(0.5, 1, 2, 5, 10))   
+parameters <- list(id = c(5, 6, 7, 4, 8),
+                   grid_size = c(0.5, 1, 2, 5, 10),
+                   base_info = c("cell", 
+                                 "latitude", 
+                                 "longitude", 
+                                 "population", 
+                                 "ID_0", 
+                                 "ID_1", 
+                                 "ID_2", 
+                                 "sd"))   
 
 out_name <- "response_mean.rds"
-
-base_info <- c("cell", "latitude", "longitude", "population", "ID_0", "ID_1", "ID_2", "sd")
 
 
 # define variables ------------------------------------------------------------
@@ -26,6 +30,8 @@ base_info <- c("cell", "latitude", "longitude", "population", "ID_0", "ID_1", "I
 model_type <- paste0("model_", parameters$id)
 
 grid_sizes <- parameters$grid_size
+  
+base_info <- parameters$base_info
   
 in_path <- file.path("output",
                      "predictions_world",
@@ -44,7 +50,7 @@ out_path <- file.path("output",
 
 all_sqr_covariates <- readRDS(file.path("output", 
                                         "env_variables", 
-                                        "all_squares_env_var_0_1667_deg_dis.rds"))
+                                        "all_squares_env_var_0_1667_deg.rds"))
 
 mean_predictions_all_gr_szs <- lapply(in_path, readRDS)
 
@@ -54,7 +60,10 @@ mean_predictions_all_gr_szs <- lapply(in_path, readRDS)
 
 n <- nrow(mean_predictions_all_gr_szs[[1]])
 
-all_sd <- vapply(seq_along(grid_sizes), get_grid_size_sd, numeric(n), pred_ls = mean_predictions_all_gr_szs)
+all_sd <- vapply(seq_along(grid_sizes), 
+                 get_grid_size_sd, 
+                 numeric(n), 
+                 pred_ls = mean_predictions_all_gr_szs)
 
 new_col_names <- paste0("sd_", grid_sizes)
 
@@ -77,46 +86,3 @@ for (i in seq_len(N)){
 }
 
 write_out_rds(all_sqr_covariates[, base_info], out_path, out_name)
-
-
-# =============================================================================
-#
-#                     calculate coefficient of variation, CV
-#
-# =============================================================================
-
-
-# # define parameters -----------------------------------------------------------
-# 
-# 
-# out_name <- "response_CV.rds"
-# 
-# out_path <- file.path("output",
-#                       "predictions_world",
-#                       "bootstrap_models")
-# 
-# 
-# # load data ------------------------------------------------------------------- 
-# 
-# 
-# best_prediction <- readRDS(file.path("output", 
-#                                      "predictions_world", 
-#                                      "best_fit_models",
-#                                      "FOI_best_model",
-#                                      "response.rds"))
-# 
-# interpolated_sd <- readRDS(file.path("output", 
-#                                      "predictions_world", 
-#                                      "bootstrap_models",
-#                                      "response_mean.rds"))
-# 
-# 
-# # start -----------------------------------------------------------------------
-# 
-# 
-# interpolated_sd$CV <- interpolated_sd$sd / best_prediction$best
-# 
-# interpolated_sd$CV[is.na(interpolated_sd$CV)] <- 0
-# interpolated_sd$CV[!is.finite(interpolated_sd$CV)] <- 0
-# 
-# saveRDS(interpolated_sd, file.path(out_path, out_name))
