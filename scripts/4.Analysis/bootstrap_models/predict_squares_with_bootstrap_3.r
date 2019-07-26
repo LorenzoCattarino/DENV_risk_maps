69a2bb75a57e9681939033cb1b5f99b271c3fe1e
Lorenzo Cattarino <lcattari@dide.local>
2019-07-26 13:53:42 +0100

    remove cluster support

--- scripts/4.Analysis/bootstrap_models/predict_squares_with_bootstrap_3.r
+++ scripts/4.Analysis/bootstrap_models/predict_squares_with_bootstrap_3.r
@@ -1,41 +1,17 @@
 # Take mean, median, sd and 95% CI of foi, R0 and burden measures, for each 20 km square
 # THIS IS FOR THE MAP!
 
-options(didehpc.cluster = "fi--didemrchnb")
-
-CLUSTER <- TRUE
-
-my_resources <- c(
-  file.path("R", "utility_functions.R"),
-  file.path("R", "create_parameter_list.R"),
-  file.path("R", "prepare_datasets", "calculate_mean_across_fits.R"))
-
-context::context_log_start()
-ctx <- context::context_save(path = "context",
-                             sources = my_resources)
+source(file.path("R", "utility_functions.R"))
+source(file.path("R", "create_parameter_list.R"))
+source(file.path("R", "prepare_datasets", "calculate_mean_across_fits.R"))
 
 
 # define parameters ----------------------------------------------------------- 
 
 
-extra_prms <- list(id = 4) 
+extra_prms <- list(id = 8) 
 
 vars_to_average <- "response"
-
-
-# are you using the cluster? -------------------------------------------------- 
-
-
-if (CLUSTER) {
-  
-  # config <- didehpc::didehpc_config(template = "24Core")
-  obj <- didehpc::queue_didehpc(ctx)
-  
-} else{
-  
-  context::context_load(ctx)
-  
-}
 
 
 # define variables ------------------------------------------------------------
@@ -53,45 +29,17 @@ in_path <- file.path("output",
                      model_type)
 
 
-# run one job -----------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 
-t <- obj$enqueue(
-  wrapper_to_average_bsamples(
-    seq_along(vars_to_average)[1],
-    vars = vars_to_average,
-    in_path = in_path,
-    out_path = in_path,
-    col_names = col_names))
-  
-  
-# run -------------------------------------------------------------------------
+dat <- readRDS(file.path(in_path, paste0(vars_to_average, ".rds")))
 
+ret <- average_boot_samples_dim2(dat[, col_names])
 
-# if (CLUSTER) {
-# 
-#   means_all_scenarios <- queuer::qlapply(
-#     seq_along(vars_to_average),
-#     wrapper_to_average_bsamples,
-#     obj,
-#     vars = vars_to_average,
-#     in_path = in_path,
-#     out_path = in_path,
-#     col_names = col_names)
-# 
-# } else {
-# 
-#   means_all_scenarios <- loop(
-#     seq_along(vars_to_average),
-#     wrapper_to_average_bsamples,
-#     vars = vars_to_average,
-#     in_path = in_path,
-#     out_path = in_path,
-#     col_names = col_names,
-#     parallel = FALSE)
-# 
-# }
-# 
-# if(!CLUSTER){
-#   context::parallel_cluster_stop()
-# }
+base_info <- dat[, setdiff(names(dat), col_names)]
+
+ret2 <- cbind(base_info, ret)
+
+out_name <- paste0(vars_to_average, "_mean.rds")
+
+write_out_rds(ret2, in_path, out_name)
