8c1527d26161068ce145db73197e2fa2fe17a424
Lorenzo Cattarino <lcattari@dide.local>
2019-08-02 10:23:49 +0100

    update script

--- scripts/4.Analysis/bootstrap_models/predict_squ_with_bootstrap_2.r
+++ scripts/4.Analysis/bootstrap_models/predict_squ_with_bootstrap_2.r
@@ -2,37 +2,35 @@
 
 
 source(file.path("R", "utility_functions.r"))
+source(file.path("R", "create_parameter_list.R"))
 
 
 # define parameters -----------------------------------------------------------
 
 
-parameters <- list(
-  dependent_variable = "FOI",
-  grid_size = 1,
-  no_samples = 200)   
-
-age <- 9
-
-out_fl_nm <- "p9.rds"
+extra_prms  <- list(id = 4,
+                    age = 16)
 
 prediction_fl_nm <- "response.rds"
-  
-  
+
+
 # define variables ------------------------------------------------------------
 
 
-model_type <- paste0(parameters$dependent_variable, "_boot_model")
+parameters <- create_parameter_list(extra_params = extra_prms)
 
-my_dir <- paste0("grid_size_", parameters$grid_size)
+model_type <- paste0("model_", parameters$id)
 
 out_pt <- file.path("output", 
                     "predictions_world",
                     "bootstrap_models",
-                    my_dir,
                     model_type)
 
 col_ids <- as.character(seq_len(parameters$no_samples))
+
+age <- parameters$age
+
+out_fl_nm <- sprintf("p%s%s", age, ".rds")
 
 
 # load data ------------------------------------------------------------------- 
@@ -41,18 +39,17 @@ col_ids <- as.character(seq_len(parameters$no_samples))
 sqr_preds <- readRDS(file.path("output", 
                                "predictions_world",
                                "bootstrap_models",
-                               my_dir,
                                model_type,
                                prediction_fl_nm))
 
 
-# calculate p9 ----------------------------------------------------------------
+# calculate seroprevalence at age X -------------------------------------------
 
 
-p9 <- 100 * (1 - exp(-4 * age * sqr_preds[, col_ids]))
+serop_var <- 100 * (1 - exp(-4 * age * sqr_preds[, col_ids])) # percentage
 
 base_info <- sqr_preds[, setdiff(names(sqr_preds), col_ids)]
   
-final_dts <- cbind(base_info, p9)
+final_dts <- cbind(base_info, serop_var)
 
 write_out_rds(final_dts, out_pt, out_fl_nm)  
