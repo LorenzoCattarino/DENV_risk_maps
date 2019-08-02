9800b27a9342981cdc1939a008c95e22cc4a7cf5
Lorenzo Cattarino <lcattari@dide.local>
2019-08-02 14:16:06 +0100

    rename scripts

--- RUBBISH/scripts/interventions_adm_bootstrap_04.R
+++ RUBBISH/scripts/interventions_adm_bootstrap_04.R
@@ -1,235 +1,103 @@
-# When optimally picking screening age:
-
-# 1) take the mean, sd and 95%CI across bootstrap samples, of
-# total number of infections and cases, summed over all squares, AND:
-# total number of infections and cases, summed by country 
-
-# 2) create barplots of total numbers
+# Plot proportional reduction in infections, cases and hospitalized cases 
+# for vaccination when optimally picking screening age
 
 library(dplyr)
-library(countrycode)
+library(ggplot2)
 
 source(file.path("R", "utility_functions.R"))
-source(file.path("R", "create_parameter_list.R"))
-source(file.path("R", "prepare_datasets", "calculate_mean_across_fits.R"))
-
-
-# define parameters ----------------------------------------------------------- 
-
-
-extra_prms <- list(id = 4,
-                   R0_scenario = c(1, 2),
-                   baseline_scenario_ids = 4,
-                   intervention_name = "vaccine",
-                   phi_factor_levels = c("2S", "4S"))
-
-
-# define variables ------------------------------------------------------------
-
-
-parameters <- create_parameter_list(extra_params = extra_prms)
-
-R0_scenario <- parameters$R0_scenario
-
-baseline_scenario_ids <- parameters$baseline_scenario_ids 
-
-model_type <- paste0("model_", parameters$id)
-
-intervention_name <- parameters$intervention_name
-
-phi_factor_levels <- parameters$phi_factor_levels
-
-in_path <- file.path("output", 
-                     "predictions_world", 
-                     "bootstrap_models", 
-                     model_type,
-                     "adm_1")
-
-var_to_sum <- as.character(seq_len(parameters$no_samples))
-
-out_table_path <- file.path("output", 
-                            "predictions_world", 
-                            "bootstrap_models", 
-                            model_type,
-                            "adm_1",
-                            intervention_name)
 
 out_fig_path <- file.path("figures", 
                           "predictions_world", 
                           "bootstrap_models",
                           "adm_1")
 
-fct_comb_fl_nm <- paste0("scenario_table_", intervention_name, ".csv")
+intervention_name <- "vaccine"
 
-out_ls <- vector("list", length(model_type))
-out_ls_2 <- vector("list", length(model_type))
+burden_measures <- c("infections", "cases", "hosp") 
 
-
-# load data ------------------------------------------------------------------- 
+y_axis_titles <- c("Reduction in infections", "Reduction in cases", "Reduction in hopsitalized cases")
 
 
-fct_comb <- read.csv(file.path(in_path, fct_comb_fl_nm), 
-                     stringsAsFactors = FALSE)
+# plotting ------------------------------------------------------------------
 
-age_struct <- read.csv(file.path("output", 
-                                 "datasets",
-                                 "country_age_structure.csv"), 
-                       header = TRUE) 
-
-
-# pre processing -------------------------------------------------------------- 
-
-
-age_struct$continent <- as.factor(countrycode(sourcevar = age_struct[, "country"], 
-                                              origin = "country.name", 
-                                              destination = "continent"))
-
-age_struct$region <- as.factor(countrycode(sourcevar = age_struct[, "country"], 
-                                           origin = "country.name", 
-                                           destination = "region"))
-
-
-# aggreaggating --------------------------------------------------------------- 
-
-
-for (k in seq_along(R0_scenario)){                                  # loop over R0 assumptions
-  
-  cat("R0 assumption =", k, "\n")
-  
-  my_in_path <- in_path
-  my_out_path <- out_table_path
-  
-  my_fct_comb <- fct_comb
-  
-  my_fct_comb$phi_set_id <- k
-  
-  small_out_ls <- vector("list", length(nrow(my_fct_comb)))
-  small_out_ls_2 <- vector("list", length(nrow(my_fct_comb)))
-  
-  for (i in seq_len(nrow(my_fct_comb))){                           # loop over scenario ids
-    
-    scenario_id <- my_fct_comb[i, "id"]
-    cat("scenario table id =", scenario_id, "\n")
-    
-    burden_measure <- my_fct_comb[i, "burden_measure"]
-    
-    out_file_tag <- toupper(substr(burden_measure, 1, 1))
-    
-    root_name <- sprintf("%s_num_%s_%s_%s%s", out_file_tag, k, intervention_name, scenario_id, ".rds")
-    
-    baseline_id <- baseline_scenario_ids
-    baseline_fl_nm <- paste0(out_file_tag, "_num_wolbachia_", baseline_id, "_fixed.rds")
-    baseline <- readRDS(file.path(my_in_path, baseline_fl_nm)) 
-    
-    dat <- readRDS(file.path(my_in_path, root_name))
-    
-    one_dat <- as.data.frame(dat)
-    # one_dat <- left_join(one_dat, age_struct[, c("continent", "region", "country", "ID_0")])
-    # 
-    # by_country <- one_dat %>% group_by(ID_0)
-    # country_sums <- by_country %>% summarise_at(var_to_sum, "sum")
-    # ret <- average_boot_samples_dim2(country_sums[, var_to_sum])
-    # ret2 <- cbind(ID_0 = country_sums$ID_0, ret)
-    # ret3 <- merge(age_struct[, c("country", "ID_0")], ret2, by = "ID_0", all.x = FALSE)
-    # write_out_csv(ret3, 
-    #               my_out_path, 
-    #               paste0(burden_measure, "_by_country_", scenario_id, ".csv"))
-    # 
-    # by_continent <- one_dat %>% group_by(continent)
-    # continent_sums <- by_continent %>% summarise_at(var_to_sum, "sum")       
-    # ret <- average_boot_samples_dim2(continent_sums[, var_to_sum])
-    # ret2 <- cbind(continent = continent_sums$continent, ret)  
-    # write_out_csv(ret2, 
-    #               my_out_path, 
-    #               paste0(burden_measure, "_by_continent_", scenario_id, ".csv"))
-    # 
-    # by_region <- one_dat %>% group_by(region)
-    # region_sums <- by_region %>% summarise_at(var_to_sum, "sum")
-    # ret <- average_boot_samples_dim2(region_sums[, var_to_sum])
-    # ret2 <- cbind(region = region_sums$region, ret)  
-    # write_out_csv(ret2, 
-    #               my_out_path, 
-    #               paste0(burden_measure, "_by_region_", scenario_id, ".csv"))
-    # 
-    # ret4 <- colSums(one_dat[, var_to_sum])
-    # ret5 <- average_boot_samples_dim1(ret4)
-    # 
-    # small_out_ls[[i]] <- ret5
-    
-    bl <- baseline[, var_to_sum]
-    od <- one_dat[, var_to_sum]
-    
-    bl_colsum <- colSums(bl)
-    od_colsum <- colSums(od)
-    ret6 <- (bl_colsum - od_colsum) / bl_colsum
-    ret7 <- average_boot_samples_dim1(ret6)
-    
-    small_out_ls_2[[i]] <- ret7
-    
-  }
-  
-  # out_ls[[k]] <- cbind(my_fct_comb, do.call("rbind", small_out_ls))
-  out_ls_2[[k]] <- cbind(my_fct_comb, do.call("rbind", small_out_ls_2))
-  
-}
-
-# summary_table <- do.call("rbind", out_ls)  
-# 
-# summary_table$phi_set_id <- factor(summary_table$phi_set_id, 
-#                                    levels = seq_len(length(phi_factor_levels)), 
-#                                    labels = phi_factor_levels)
-# 
-# summary_tab_fl_nm <- paste0("total_", intervention_name, ".csv")
-# 
-# write_out_csv(summary_table, file.path("output", 
-#                                        "predictions_world", 
-#                                        "bootstrap_models",
-#                                        "adm_1"), 
-#               summary_tab_fl_nm)
-
-summary_table_2 <- do.call("rbind", out_ls_2)  
-
-summary_table_2$phi_set_id <- factor(summary_table_2$phi_set_id, 
-                                     levels = seq_len(length(phi_factor_levels)), 
-                                     labels = phi_factor_levels)
-
-summary_tab_fl_nm_2 <- paste0("prop_change_", intervention_name, ".csv")
-
-write_out_csv(summary_table_2, file.path("output", 
-                                         "predictions_world", 
-                                         "bootstrap_models",
-                                         "adm_1"), 
-              summary_tab_fl_nm_2,
-              row.names = FALSE)
-
-
-# subdivide by vaccine estimate -----------------------------------------------
-
-
-vacc_estimates <- unique(summary_table_2$estimate)
-
-burden_measures <- unique(summary_table_2$burden_measure)
 
 for (j in seq_along(burden_measures)) {
   
-  for (i in seq_along(vacc_estimates)) {
-    
-    bur_meas <- burden_measures[j]
-    
-    vacc_est <- vacc_estimates[i]
-    
-    summary_table_2_sub <- subset(summary_table_2, 
-                                  burden_measure == bur_meas & estimate == vacc_est)
-    
-    summary_tab_fl_nm <- sprintf("prop_change_%s_%s_%s%s", 
-                                 bur_meas, vacc_est, intervention_name, ".csv")
-    
-    write_out_csv(summary_table_2_sub, file.path("output", 
-                                                 "predictions_world", 
-                                                 "bootstrap_models",
-                                                 "adm_1"), 
-                  summary_tab_fl_nm,
-                  row.names = FALSE)
-  }
+  my_var_name <- burden_measures[j]
+  
+  y_axis_title <- y_axis_titles[j]
+  
+  summary_table_orig_mean <- read.csv(file.path("output", 
+                                                "predictions_world", 
+                                                "bootstrap_models",
+                                                "adm_1",
+                                                paste0("prop_change_", my_var_name, "_mean_", intervention_name, ".csv")),
+                                      header = TRUE)
+  
+  summary_table_orig_L95 <- read.csv(file.path("output", 
+                                               "predictions_world", 
+                                               "bootstrap_models",
+                                               "adm_1",
+                                               paste0("prop_change_", my_var_name, "_L95_", intervention_name, ".csv")),
+                                     header = TRUE)
+  
+  summary_table_orig_U95 <- read.csv(file.path("output", 
+                                               "predictions_world", 
+                                               "bootstrap_models",
+                                               "adm_1",
+                                               paste0("prop_change_", my_var_name, "_U95_", intervention_name, ".csv")),
+                                     header = TRUE)
+  
+  summary_table_orig <- summary_table_orig_mean
+  
+  summary_table_orig$lCI <- summary_table_orig_L95$mean - (1.92 * summary_table_orig$sd)
+  
+  summary_table_orig$uCI <- summary_table_orig_U95$mean + (1.92 * summary_table_orig$sd)
+  
+  summary_table <- summary_table_orig
+  
+  out_fl_nm <- sprintf("prop_change_%s_%s%s", my_var_name, intervention_name, ".csv")
+  
+  write_out_csv(summary_table, file.path("output", 
+                                         "predictions_world", 
+                                         "bootstrap_models",
+                                         "adm_1"),
+                out_fl_nm,
+                row.names = FALSE)
+  
+  y_values <- seq(0, 0.5, 0.1)
+
+  p <- ggplot(summary_table, aes(x = burden_measure, y = mean, ymin = lCI, ymax = uCI)) +
+    geom_bar(stat = "identity", position = "dodge", width = 0.5, fill = "lightskyblue3") +
+    geom_errorbar(width = .05, position = position_dodge(.9)) +
+    facet_grid(. ~ phi_set_id) +
+    xlab(NULL) +
+    scale_y_continuous(y_axis_title,
+                       breaks = y_values,
+                       labels = paste0(y_values * 100, "%"),
+                       limits = c(min(y_values), max(y_values)),
+                       expand = expand_scale(mult = c(0, .05))) +
+    theme_bw() +
+    theme(axis.title.x = element_blank(),
+          axis.text.x = element_blank(),
+          axis.ticks.x = element_blank(),
+          axis.text.y = element_text(size = 12),
+          plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm"),
+          strip.text.x = element_text(size = 10))
+  
+  dir.create(out_fig_path, FALSE, TRUE)
+  
+  barplot_fl_nm <- paste0("proportional_reduction_in_", my_var_name, "_", intervention_name, ".png")
+  
+  png(file.path(out_fig_path, barplot_fl_nm),
+      width = 12,
+      height = 9,
+      units = "cm",
+      pointsize = 12,
+      res = 300)
+  
+  print(p)
+  
+  dev.off()
   
 }
