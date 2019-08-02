6a102d56829b2f9561ab03d267143653cedb9e4a
Lorenzo Cattarino <lcattari@dide.local>
2019-08-02 15:25:46 +0100

    rename scripts

---
+++ scripts/4.Analysis/bootstrap_models/predict_squ_with_bootstrap_2.r
@@ -0,0 +1,45 @@
+# Take mean, median, sd and 95% CI of foi, R0 and burden measures, for each 20 km square
+# THIS IS FOR THE MAP!
+
+source(file.path("R", "utility_functions.R"))
+source(file.path("R", "create_parameter_list.R"))
+source(file.path("R", "prepare_datasets", "calculate_mean_across_fits.R"))
+
+
+# define parameters ----------------------------------------------------------- 
+
+
+extra_prms <- list(id = 8) 
+
+vars_to_average <- "response"
+
+
+# define variables ------------------------------------------------------------
+
+
+parameters <- create_parameter_list(extra_params = extra_prms)
+
+model_type <- paste0("model_", parameters$id)
+
+col_names <- as.character(seq_len(parameters$no_samples))
+
+in_path <- file.path("output", 
+                     "predictions_world",
+                     "bootstrap_models",
+                     model_type)
+
+
+# -----------------------------------------------------------------------------
+
+
+dat <- readRDS(file.path(in_path, paste0(vars_to_average, ".rds")))
+
+ret <- average_boot_samples_dim2(dat[, col_names])
+
+base_info <- dat[, setdiff(names(dat), col_names)]
+
+ret2 <- cbind(base_info, ret)
+
+out_name <- paste0(vars_to_average, "_mean.rds")
+
+write_out_rds(ret2, in_path, out_name)
--- scripts/4.Analysis/bootstrap_models/predict_squ_with_bootstrap_3.r
+++ scripts/4.Analysis/bootstrap_models/predict_squ_with_bootstrap_3.r
@@ -1,45 +1,84 @@
-# Take mean, median, sd and 95% CI of foi, R0 and burden measures, for each 20 km square
-# THIS IS FOR THE MAP!
+# Makes a map of the square predictions and save the raster
 
-source(file.path("R", "utility_functions.R"))
-source(file.path("R", "create_parameter_list.R"))
-source(file.path("R", "prepare_datasets", "calculate_mean_across_fits.R"))
+source(file.path("R", "plotting", "functions_for_plotting_raster_maps.r"))
 
-
-# define parameters ----------------------------------------------------------- 
+library(fields)
+library(colorRamps)
 
 
-extra_prms <- list(id = 8) 
+# define parameters -----------------------------------------------------------  
+
+
+parameters <- list(id = 4,
+                   dependent_variable = "FOI",
+                   z_range = list(FOI = c(0, 0.06),
+                                  R0_1 = c(0, 8),
+                                  R0_2 = c(0, 4),
+                                  R0_3 = c(0, 5))) 
 
 vars_to_average <- "response"
+
+statistic <- "mean"
 
 
 # define variables ------------------------------------------------------------
 
 
-parameters <- create_parameter_list(extra_params = extra_prms)
+var_to_fit <- parameters$dependent_variable
+
+z_range <- parameters$z_range[[var_to_fit]]
 
 model_type <- paste0("model_", parameters$id)
-
-col_names <- as.character(seq_len(parameters$no_samples))
 
 in_path <- file.path("output", 
                      "predictions_world",
                      "bootstrap_models",
                      model_type)
+  
+out_path <- file.path("figures", 
+                      "predictions_world",
+                      "bootstrap_models",
+                      model_type)
 
 
-# -----------------------------------------------------------------------------
+# pre processing -------------------------------------------------------------- 
 
 
-dat <- readRDS(file.path(in_path, paste0(vars_to_average, ".rds")))
+my_col <- matlab.like(100)
 
-ret <- average_boot_samples_dim2(dat[, col_names])
+mean_pred_fl_nm <- paste0(vars_to_average, "_mean", ".rds")
 
-base_info <- dat[, setdiff(names(dat), col_names)]
+df_long <- readRDS(file.path(in_path, mean_pred_fl_nm))
 
-ret2 <- cbind(base_info, ret)
+out_fl_nm <- paste0(vars_to_average, "_", statistic, ".png")
 
-out_name <- paste0(vars_to_average, "_mean.rds")
 
-write_out_rds(ret2, in_path, out_name)
+# plot ------------------------------------------------------------------------ 
+
+
+quick_raster_map(pred_df = df_long, 
+                 variable = vars_to_average, 
+                 statistic = statistic, 
+                 my_col = my_col, 
+                 out_pt = out_path, 
+                 out_name = out_fl_nm,
+                 z_range = z_range)
+
+
+# save the raster -------------------------------------------------------------
+
+
+my_ras <- raster::rasterFromXYZ(df_long[, c("longitude","latitude", statistic)])
+
+# check 
+# raster::plot(my_ras, zlim=c(0,0.06), col = my_col)
+
+# save 
+raster::writeRaster(my_ras, filename = file.path(in_path, "foi_map.tif"), format = "GTiff", overwrite = TRUE)
+
+# test
+# my_ras <- raster::raster(file.path("output", 
+#                                    "predictions_world",
+#                                    "bootstrap_models",
+#                                    "model_21", 
+#                                    "foi_map.tif"))
