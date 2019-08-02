9b18dfa1b3c6fad37d95da6654d8b93c074e72a0
Lorenzo Cattarino <lcattari@dide.local>
2019-08-02 14:12:57 +0100

    average the sqr FoI predictions

--- scripts/4.Analysis/bootstrap_models/predict_adm_with_bootstrap_01.R
+++ scripts/4.Analysis/bootstrap_models/predict_adm_with_bootstrap_01.R
@@ -21,8 +21,6 @@ context::context_load(ctx)
 
 
 extra_prms <- list(id = 4,
-                   R0_scenario = 2,
-                   baseline_scenario_ids = 4,
                    dependent_variable = "FOI",
                    base_info = c("cell", 
                                  "latitude", 
@@ -33,7 +31,7 @@ extra_prms <- list(id = 4,
                                  "ID_2"),
                    grp_fields = c("ID_0", "ID_1")) 
 
-input_fl_name <- "transformed"
+input_fl_name <- "response.rds"
 
 
 # define variables ------------------------------------------------------------
@@ -45,10 +43,6 @@ var_to_fit <- parameters$dependent_variable
 
 model_type <- paste0("model_", parameters$id)
 
-R0_scenario <- parameters$R0_scenario
-
-baseline_scenario_ids <- parameters$baseline_scenario_ids
-  
 grp_fields <- parameters$grp_fields
 
 out_pt <- file.path("output", 
@@ -61,18 +55,11 @@ out_pt <- file.path("output",
 # load data -------------------------------------------------------------------
 
 
-fl_nm <- sprintf("%s_%s_%s_%s%s", 
-                 input_fl_name, 
-                 R0_scenario, 
-                 "wolbachia", 
-                 baseline_scenario_ids, 
-                 ".rds")
-  
 sqr_preds <- readRDS(file.path("output", 
                                "predictions_world",
                                "bootstrap_models",
                                model_type,
-                               fl_nm))
+                               "response.rds"))
 
 
 # average up the sqr predictions ----------------------------------------------
@@ -90,4 +77,4 @@ all_adm_pred <- do.call("cbind", test2)
 
 all_adm_pred <- cbind(test[[1]][, c(grp_fields, "population")], all_adm_pred)
 
-write_out_rds(all_adm_pred, out_pt, fl_nm)  
+write_out_rds(all_adm_pred, out_pt, input_fl_name)  
