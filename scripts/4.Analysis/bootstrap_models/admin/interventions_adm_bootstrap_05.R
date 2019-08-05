7b8deb697c73048797871c04a1d59aa2ebd11d52
Lorenzo Cattarino <lcattari@dide.local>
2019-08-05 13:10:01 +0100

    style edits

--- scripts/4.Analysis/bootstrap_models/admin/interventions_adm_bootstrap_05.R
+++ scripts/4.Analysis/bootstrap_models/admin/interventions_adm_bootstrap_05.R
@@ -1,5 +1,7 @@
-# Takes mean, median, sd and 95% CI of max vaccine impact age, for each admin unit
-# THIS IS FOR THE MAP!
+# Takes mean, median, sd and 95% CI of 
+
+# 1) optimal vaccine impact age, for each admin unit, across bootstrap samples
+# 2) seroprevalence at optimal vaccine impact age
 
 source(file.path("R", "utility_functions.R"))
 source(file.path("R", "create_parameter_list.R"))
@@ -10,8 +12,9 @@ source(file.path("R", "prepare_datasets", "calculate_mean_across_fits.R"))
 
 
 extra_prms <- list(id = 4,
-                   vaccine_id = 4,
-                   R0_scenario = 2) 
+                   vaccine_id = 12,
+                   R0_scenario = 2,
+                   age = FALSE) 
 
 
 # define variables ------------------------------------------------------------
@@ -21,17 +24,19 @@ parameters <- create_parameter_list(extra_params = extra_prms)
 
 model_type <- paste0("model_", parameters$id)
 
-R0_scenario <- parameters$R0_scenario
-
-vaccine_id <- parameters$vaccine_id
-
-col_names <- as.character(seq_len(parameters$no_samples))
-
 in_path <- file.path("output", 
                      "predictions_world",
                      "bootstrap_models",
                      model_type,
                      "adm_1")
+
+col_ids <- as.character(seq_len(parameters$no_samples))
+
+R0_scenario <- parameters$R0_scenario
+
+vaccine_id <- parameters$vaccine_id
+
+age <- parameters$age
 
 
 # load data # -----------------------------------------------------------------
@@ -48,17 +53,21 @@ fct_c <- read.csv(file.path("output",
 # -----------------------------------------------------------------------------
 
 
-burden_measure_s <- fct_c[fct_c$id == vaccine_id, "burden_measure"]
-
-burden_measure <- toupper(substr(burden_measure_s, 1, 1))
+burden_measure <- toupper(substr(fct_c[fct_c$id == vaccine_id, "burden_measure"], 1, 1))
 
 vars_to_average <- sprintf("%s_num_%s_max_age_vaccine_%s", burden_measure, R0_scenario, vaccine_id)
 
+if(!age){
+  
+  vars_to_average <- sprintf("%s_%s", "p", vars_to_average)
+  
+}
+
 dat <- readRDS(file.path(in_path, paste0(vars_to_average, ".rds")))
 
-ret <- average_boot_samples_dim2(dat[, col_names])
+ret <- average_boot_samples_dim2(dat[, col_ids])
 
-base_info <- dat[, setdiff(names(dat), col_names)]
+base_info <- dat[, setdiff(names(dat), col_ids)]
 
 ret2 <- cbind(base_info, ret)
 
