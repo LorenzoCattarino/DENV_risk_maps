f1e528962fd26d5d8884cfd008209f4e4ba8d52a
Lorenzo Cattarino <lcattari@imperial.ac.uk>
2019-08-05 09:39:50 +0100

    fix bug; style edits

--- scripts/4.Analysis/bootstrap_models/interventions_adm_bootstrap_06.R
+++ scripts/4.Analysis/bootstrap_models/interventions_adm_bootstrap_06.R
@@ -3,16 +3,16 @@
 source(file.path("R", "create_parameter_list.R"))
 source(file.path("R", "plotting", "quick_polygon_map.R"))
 
-library(rgdal)
-library(lattice)
+library(sf)
+library(ggplot2)
 
 
 # define parameters -----------------------------------------------------------  
 
 
 extra_prms <- list(id = 4,
-                   vaccine_id = 4,
-                   R0_scenario = 1,
+                   vaccine_id = 12,
+                   R0_scenario = 2,
                    statistic = "mean")
 
 
@@ -52,7 +52,7 @@ fct_c <- read.csv(file.path("output",
                             "adm_1",
                             "scenario_table_vaccine.csv"))
 
-adm_shp <- readOGR(dsn = file.path("output", "shapefiles"), 
+adm_shp <- st_read(dsn = file.path("output", "shapefiles"), 
                    layer = paste0("gadm28_adm1_eras"),
                    stringsAsFactors = FALSE)
 
@@ -62,9 +62,9 @@ adm_shp <- readOGR(dsn = file.path("output", "shapefiles"),
 
 my_col <- colorRamps::matlab.like(100)
 
-adm_shp_2 <- adm_shp[!duplicated(data.frame(adm_shp)[,c("ID_0", "ID_1")]),]
+burden_measure_s <- fct_c[fct_c$id == vaccine_id, "burden_measure"]
 
-burden_measure <- toupper(substr(fct_c[vaccine_id, "burden_measure"], 1, 1))
+burden_measure <- toupper(substr(burden_measure_s, 1, 1))
 
 vars_to_average <- sprintf("%s_num_%s_max_age_vaccine_%s", burden_measure, R0_scenario, vaccine_id)
 
@@ -72,18 +72,18 @@ mean_pred_fl_nm <- paste0(vars_to_average, "_mean", ".rds")
 
 df_long <- readRDS(file.path(in_path, mean_pred_fl_nm))
 
+df_long[, statistic] <- df_long[, statistic] + 1
+
 out_fl_nm <- paste0(vars_to_average, "_", statistic, ".png")
 
-adm_shp_pred <- merge(adm_shp_2, 
+adm_shp_pred <- merge(adm_shp, 
                       df_long[, c("ID_0", "ID_1", statistic)], 
                       by = c("ID_0", "ID_1"), 
                       all.x = TRUE)
-
-# fill NA 
-# adm_shp_pred@data[is.na(adm_shp_pred@data[, statistic]), statistic] <- 0
 
 
 # plot ------------------------------------------------------------------------
 
 
-quick_polygon_map(adm_shp_pred, my_col, statistic, out_pth, out_fl_nm)
+quick_polygon_map(adm_shp_pred, my_col, statistic, out_pth, out_fl_nm,
+                  z_vals = seq(2, 18, 2))
