a36d01a288b66b6bc82c01501caf4ac8a633a07a
Lorenzo Cattarino <lcattari@imperial.ac.uk>
2019-08-05 09:40:58 +0100

    style edits

--- scripts/4.Analysis/bootstrap_models/predict_adm_with_bootstrap_05.R
+++ scripts/4.Analysis/bootstrap_models/predict_adm_with_bootstrap_05.R
@@ -3,8 +3,8 @@
 source(file.path("R", "create_parameter_list.R"))
 source(file.path("R", "plotting", "quick_polygon_map.R"))
 
-library(rgdal)
-library(lattice)
+library(sf)
+library(ggplot2)
 
 
 # define parameters -----------------------------------------------------------  
@@ -20,11 +20,15 @@ extra_prms <- list(id = 4,
 
 parameters <- create_parameter_list(extra_params = extra_prms)
 
-model_type <- paste0("model_", parameters$id)
-
 age <- parameters$age
 
+# vars_to_average <- paste0("p", age)
+# vars_to_average <- "response_endemic"
+vars_to_average <- "transformed_2_wolbachia_4"
+
 statistic <- parameters$statistic
+
+model_type <- paste0("model_", parameters$id)
 
 in_path <- file.path("output", 
                      "predictions_world",
@@ -42,22 +46,13 @@ out_pth <- file.path("figures",
 # load data ------------------------------------------------------------------- 
 
 
-# country_shp <- readOGR(dsn = file.path("output", "shapefiles"), 
-#                        layer = "gadm28_adm0_eras",
-#                        stringsAsFactors = FALSE)
-
-adm_shp <- readOGR(dsn = file.path("output", "shapefiles"), 
+adm_shp <- st_read(dsn = file.path("output", "shapefiles"), 
                    layer = paste0("gadm28_adm1_eras"),
                    stringsAsFactors = FALSE)
 
 
 # pre processing -------------------------------------------------------------- 
 
-
-adm_shp_2 <- adm_shp[!duplicated(data.frame(adm_shp)[,c("ID_0", "ID_1")]),]
-
-vars_to_average <- paste0("p", age)
-# vars_to_average <- "response_endemic"
 
 my_col <- colorRamps::matlab.like(100)
 
@@ -67,13 +62,10 @@ df_long <- readRDS(file.path(in_path, mean_pred_fl_nm))
 
 out_fl_nm <- paste0(vars_to_average, "_", statistic, ".png")
 
-adm_shp_pred <- merge(adm_shp_2, 
+adm_shp_pred <- merge(adm_shp, 
                       df_long[, c("ID_0", "ID_1", statistic)], 
                       by = c("ID_0", "ID_1"), 
                       all.x = TRUE)
-
-# fill NA 
-# adm_shp_pred@data[is.na(adm_shp_pred@data[, statistic]), statistic] <- 0
 
 
 # plot ------------------------------------------------------------------------
