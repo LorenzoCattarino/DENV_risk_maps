15a4d6e31d058e0a833e3cfa22292e92afd7a207
Lorenzo Cattarino <lcattari@dide.local>
2019-08-05 13:10:17 +0100

    style edits

--- scripts/4.Analysis/bootstrap_models/admin/interventions_adm_bootstrap_06.R
+++ scripts/4.Analysis/bootstrap_models/admin/interventions_adm_bootstrap_06.R
@@ -1,4 +1,7 @@
-# Makes a map of the mean max vaccine impact age
+# Makes a map of mean
+
+# 1) optimal vaccine impact age
+# 2) seroprevalence at optimal vaccine impact age
 
 source(file.path("R", "create_parameter_list.R"))
 source(file.path("R", "plotting", "quick_polygon_map.R"))
@@ -13,6 +16,7 @@ library(ggplot2)
 extra_prms <- list(id = 4,
                    vaccine_id = 12,
                    R0_scenario = 2,
+                   age = FALSE,
                    statistic = "mean")
 
 
@@ -22,12 +26,6 @@ extra_prms <- list(id = 4,
 parameters <- create_parameter_list(extra_params = extra_prms)
 
 model_type <- paste0("model_", parameters$id)
-
-R0_scenario <- parameters$R0_scenario
-
-vaccine_id <- parameters$vaccine_id
-
-statistic <- parameters$statistic
 
 in_path <- file.path("output", 
                      "predictions_world",
@@ -41,6 +39,14 @@ out_pth <- file.path("figures",
                      model_type,
                      "adm_1")
 
+R0_scenario <- parameters$R0_scenario
+
+vaccine_id <- parameters$vaccine_id
+
+age <- parameters$age
+
+statistic <- parameters$statistic
+
 
 # load data ------------------------------------------------------------------- 
 
@@ -52,6 +58,10 @@ fct_c <- read.csv(file.path("output",
                             "adm_1",
                             "scenario_table_vaccine.csv"))
 
+national_borders <- st_read(dsn = file.path("output", "shapefiles"),
+                            layer = "gadm28_adm0_eras",
+                            stringsAsFactors = FALSE)
+
 adm_shp <- st_read(dsn = file.path("output", "shapefiles"), 
                    layer = paste0("gadm28_adm1_eras"),
                    stringsAsFactors = FALSE)
@@ -62,17 +72,24 @@ adm_shp <- st_read(dsn = file.path("output", "shapefiles"),
 
 my_col <- colorRamps::matlab.like(100)
 
-burden_measure_s <- fct_c[fct_c$id == vaccine_id, "burden_measure"]
+z_values <- seq(2, 18, 2)
 
-burden_measure <- toupper(substr(burden_measure_s, 1, 1))
+burden_measure <- toupper(substr(fct_c[fct_c$id == vaccine_id, "burden_measure"], 1, 1))
 
 vars_to_average <- sprintf("%s_num_%s_max_age_vaccine_%s", burden_measure, R0_scenario, vaccine_id)
+
+if(!age){
+  
+  # plot seroprevalence 
+  vars_to_average <- sprintf("%s_%s", "p", vars_to_average)
+  
+  z_values <- seq(0, 100, 20)
+  
+}
 
 mean_pred_fl_nm <- paste0(vars_to_average, "_mean", ".rds")
 
 df_long <- readRDS(file.path(in_path, mean_pred_fl_nm))
-
-df_long[, statistic] <- df_long[, statistic] + 1
 
 out_fl_nm <- paste0(vars_to_average, "_", statistic, ".png")
 
@@ -86,4 +103,5 @@ adm_shp_pred <- merge(adm_shp,
 
 
 quick_polygon_map(adm_shp_pred, my_col, statistic, out_pth, out_fl_nm,
-                  z_vals = seq(2, 18, 2))
+                  z_vals = z_values,
+                  country_borders = national_borders)
