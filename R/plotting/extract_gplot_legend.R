5a4ff1652912ec801bbf1bce83a62f2630a6f885
Lorenzo Cattarino <lcattari@dide.local>
2019-08-07 13:28:13 +0100

    style edits

--- R/plotting/extract_gplot_legend.R
+++ R/plotting/extract_gplot_legend.R
@@ -1,5 +1,11 @@
 g_legend <- function(a.gplot){
+
   tmp <- ggplot_gtable(ggplot_build(a.gplot))
+  
   leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  
   legend <- tmp$grobs[[leg]]
-  return(legend)}
+  
+  return(legend)
+
+}
