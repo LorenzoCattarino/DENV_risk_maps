1457e78bf1069fe1c868d790fd720ff163493e54
Lorenzo Cattarino <lcattari@dide.local>
2019-08-08 16:36:48 +0100

    do a loop

--- scripts/4.Analysis/bootstrap_models/admin/predict_adm_with_bootstrap_04.R
+++ scripts/4.Analysis/bootstrap_models/admin/predict_adm_with_bootstrap_04.R
@@ -9,8 +9,7 @@ source(file.path("R", "prepare_datasets", "calculate_mean_across_fits.R"))
 # define parameters ----------------------------------------------------------- 
 
 
-extra_prms <- list(id = 4,
-                   age = 16) 
+extra_prms <- list(id = 4) 
 
 
 # define variables ------------------------------------------------------------
@@ -18,9 +17,17 @@ extra_prms <- list(id = 4,
 
 parameters <- create_parameter_list(extra_params = extra_prms)
 
-model_type <- paste0("model_", parameters$id)
+vars_to_average <- c("response_endemic", 
+                     "p9", 
+                     "p16",
+                     "p9_FP_specifA",
+                     "p16_FP_specifA",
+                     "p9_FP_specifB",
+                     "p16_FP_specifB")#,
+                     #"transformed_1_wolbachia_4",
+                     #"transformed_2_wolbachia_4")
 
-age <- parameters$age
+model_type <- paste0("model_", parameters$id)
 
 col_names <- as.character(seq_len(parameters$no_samples))
 
@@ -34,22 +41,26 @@ in_path <- file.path("output",
 # -----------------------------------------------------------------------------
 
 
-# vars_to_average <- "response_endemic"
-# vars_to_average <- paste0("p", age)
-# vars_to_average <- "transformed_2_wolbachia_4"
-vars_to_average <- sprintf("p%s_FP_specifB", age)
-
-dat <- readRDS(file.path(in_path, paste0(vars_to_average, ".rds")))
-
-dat <- as.data.frame(dat)
-
-ret <- average_boot_samples_dim2(dat[, col_names])
-
-base_info <- dat[, setdiff(names(dat), col_names)]
-
-ret2 <- cbind(base_info, ret)
-
-out_name <- paste0(vars_to_average, "_mean.rds")
-
-write_out_rds(ret2, in_path, out_name)
-
+for (i in seq_along(vars_to_average)){
+  
+  # z_values <- seq(0, 10, 2)
+  
+  var_to_average <- vars_to_average[i]
+  
+  message(var_to_average)  
+  
+  dat <- readRDS(file.path(in_path, paste0(var_to_average, ".rds")))
+  
+  dat <- as.data.frame(dat)
+  
+  ret <- average_boot_samples_dim2(dat[, col_names])
+  
+  base_info <- dat[, setdiff(names(dat), col_names)]
+  
+  ret2 <- cbind(base_info, ret)
+  
+  out_name <- paste0(var_to_average, "_mean.rds")
+  
+  write_out_rds(ret2, in_path, out_name)
+  
+}
