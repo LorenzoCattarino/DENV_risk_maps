eb6ce35a5788ed53fbf069bb06d108bd8f63bbb3
Lorenzo Cattarino <lcattari@dide.local>
2019-08-09 10:13:21 +0100

    modify after adding treatment for adm unit analysis

--- scripts/4.Analysis/bootstrap_models/admin/interventions_adm_bootstrap_03.R
+++ scripts/4.Analysis/bootstrap_models/admin/interventions_adm_bootstrap_03.R
@@ -1,10 +1,12 @@
-# When optimally picking screening age:
+# For vaccine impact
 
 # take the mean, sd and 95%CI across bootstrap samples, of
 # total number of infections and cases, summed over all squares, AND:
 # total number of infections and cases, summed by country 
 
+
 library(dplyr)
+library(countrycode)
 
 source(file.path("R", "utility_functions.R"))
 source(file.path("R", "create_parameter_list.R"))
@@ -18,6 +20,7 @@ extra_prms <- list(id = 4,
                    R0_scenario = c(1, 2),
                    baseline_scenario_ids = 4,
                    intervention_name = "vaccine",
+                   treatment_name = "screening_age",
                    phi_factor_levels = c("2S", "4S"))
 
 
@@ -29,6 +32,8 @@ parameters <- create_parameter_list(extra_params = extra_prms)
 R0_scenario <- parameters$R0_scenario
 
 baseline_scenario_ids <- parameters$baseline_scenario_ids 
+
+treatment_name <- parameters$treatment_name
 
 model_type <- paste0("model_", parameters$id)
 
@@ -74,6 +79,29 @@ age_struct <- read.csv(file.path("output",
                        header = TRUE) 
 
 
+# pre processing -------------------------------------------------------------- 
+
+
+age_struct$continent <- as.factor(countrycode(sourcevar = age_struct[, "country"], 
+                                              origin = "country.name", 
+                                              destination = "continent"))
+
+age_struct$region <- as.factor(countrycode(sourcevar = age_struct[, "country"], 
+                                           origin = "country.name", 
+                                           destination = "region"))
+
+# remove text in brackets 
+nice_strings <- gsub("\\s*\\([^\\)]+\\)", "", age_struct$country)
+
+# remove text after comma
+nice_strings_2 <- gsub("(.*),.*", "\\1", nice_strings)
+
+# remove "*"
+nice_strings_3 <- gsub("\\*", "", nice_strings_2)
+
+age_struct$country <- nice_strings_3
+
+
 # aggreaggating --------------------------------------------------------------- 
 
 
@@ -110,6 +138,33 @@ for (k in seq_along(R0_scenario)){                                  # loop over 
     
     one_dat <- as.data.frame(dat)
 
+    one_dat <- inner_join(one_dat, age_struct[, c("continent", "region", "country", "ID_0")])
+    
+    by_country <- one_dat %>% group_by(ID_0)
+    country_sums <- by_country %>% summarise_at(var_to_sum, "sum")
+    ret <- average_boot_samples_dim2(country_sums[, var_to_sum])
+    ret <- round(ret, -2)
+    ret2 <- cbind(ID_0 = country_sums$ID_0, ret)
+    ret3 <- merge(age_struct[, c("country", "ID_0")], ret2, by = "ID_0", all.x = FALSE)
+    write_out_csv(ret3, 
+                  my_out_path, 
+                  paste0(burden_measure, "_by_country_", scenario_id, ".csv"))
+    
+    by_continent <- one_dat %>% group_by(continent)
+    continent_sums <- by_continent %>% summarise_at(var_to_sum, "sum")       
+    ret <- average_boot_samples_dim2(continent_sums[, var_to_sum])
+    ret <- round(ret, -2)
+    ret2 <- cbind(continent = continent_sums$continent, ret)  
+    write_out_csv(ret2, 
+                  my_out_path, 
+                  paste0(burden_measure, "_by_continent_", scenario_id, ".csv"))
+    
+    ret4 <- colSums(one_dat[, var_to_sum])
+    ret5 <- average_boot_samples_dim1(ret4)
+    ret5 <- round(ret5, -2)
+    
+    small_out_ls[[i]] <- ret5
+    
     bl <- baseline[, var_to_sum]
     od <- one_dat[, var_to_sum]
     
@@ -122,11 +177,43 @@ for (k in seq_along(R0_scenario)){                                  # loop over 
     
   }
   
+  out_ls[[k]] <- cbind(my_fct_comb, do.call("rbind", small_out_ls))
   out_ls_2[[k]] <- cbind(my_fct_comb, do.call("rbind", small_out_ls_2))
   
 }
 
+summary_table <- do.call("rbind", out_ls)  
+
+names(summary_table)[names(summary_table) == treatment_name] <- "treatment"
+
+treatment_levels <- unique(summary_table$treatment)
+
+summary_table[, "treatment"] <- factor(summary_table[, "treatment"],
+                                       levels = treatment_levels,
+                                       labels = treatment_levels)
+
+summary_table$phi_set_id <- factor(summary_table$phi_set_id, 
+                                   levels = seq_len(length(phi_factor_levels)), 
+                                   labels = phi_factor_levels)
+
+summary_tab_fl_nm <- paste0("total_", intervention_name, ".csv")
+
+write_out_csv(summary_table, file.path("output", 
+                                       "predictions_world", 
+                                       "bootstrap_models",
+                                       "adm_1"), 
+              summary_tab_fl_nm,
+              row.names = FALSE)
+
 summary_table_2 <- do.call("rbind", out_ls_2)  
+
+names(summary_table_2)[names(summary_table_2) == treatment_name] <- "treatment"
+
+treatment_levels <- unique(summary_table_2$treatment)
+
+summary_table_2[, "treatment"] <- factor(summary_table_2[, "treatment"],
+                                         levels = treatment_levels,
+                                         labels = treatment_levels)
 
 summary_table_2$phi_set_id <- factor(summary_table_2$phi_set_id, 
                                      levels = seq_len(length(phi_factor_levels)), 
