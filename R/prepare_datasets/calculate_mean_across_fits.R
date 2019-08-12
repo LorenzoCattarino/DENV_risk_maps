11c094c021ed4ed0b3837f0beef90eed85bef8a9
Lorenzo Cattarino <lcattari@dide.local>
2019-08-12 08:43:42 +0100

    allow to set na.rm argument

--- R/prepare_datasets/calculate_mean_across_fits.R
+++ R/prepare_datasets/calculate_mean_across_fits.R
@@ -1,13 +1,13 @@
-average_boot_samples_dim2 <- function(dat){
+average_boot_samples_dim2 <- function(dat, ...){
   out_names <- c("mean", "sd", "lCI", "uCI", "interv", "median")
-  mean_val <- rowMeans(dat)
-  st_dev <- apply(dat, 1, FUN = sd)
-  percentiles <- apply(dat, 1, FUN = quantile, probs = c(0.025, 0.975))
+  mean_val <- rowMeans(dat, ...)
+  st_dev <- apply(dat, 1, FUN = sd, ...)
+  percentiles <- apply(dat, 1, FUN = quantile, probs = c(0.025, 0.975), ...)
   percentiles <- t(percentiles)
   l_b <- percentiles[, 1]
   u_b <- percentiles[, 2]
   interv <- u_b - l_b
-  median <- apply(dat, 1, median) 
+  median <- apply(dat, 1, median, ...) 
   setNames(data.frame(mean_val, st_dev, l_b, u_b, interv, median), out_names)
 }
 
@@ -25,4 +25,8 @@ average_boot_samples_dim1 <- function(dat){
 
 get_grid_size_sd <- function(i, pred_ls){
   pred_ls[[i]]$sd
+}
+
+test_NA <- function (x) {
+  anyNA(x[as.character(1:200)])
 }
