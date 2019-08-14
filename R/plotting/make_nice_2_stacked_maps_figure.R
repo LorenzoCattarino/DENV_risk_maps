413e7405eb9a25b594559586407429b0130c879c
Lorenzo Cattarino <lcattari@dide.local>
2019-08-14 10:08:49 +0100

    change background grey colour

--- R/plotting/make_nice_2_stacked_maps_figure.R
+++ R/plotting/make_nice_2_stacked_maps_figure.R
@@ -19,7 +19,7 @@ make_nice_2_stacked_maps_figure <- function(parms,
   # pred_leg_val <- pretty(pred$layer, 5)
   pred_leg_val <- c(0, 0.02, 0.04, 0.06)
   
-  pred_map <- make_nice_map(bbox_df, countries_df, pred, pred_leg_val, parms, my_col, "grey70", "FOI")
+  pred_map <- make_nice_map(bbox_df, countries_df, pred, pred_leg_val, parms, my_col, "grey80", "FOI")
   
   
   # plot sd -------------------------------------------------------------------
@@ -30,7 +30,7 @@ make_nice_2_stacked_maps_figure <- function(parms,
   # sd_leg_val <- pretty(sd$layer, 5)
   sd_leg_val <- seq(0, 0.02, 0.01)
   
-  sd_map <- make_nice_map(bbox_df, countries_df, sd, sd_leg_val, parms, my_col, "grey70", "SD")
+  sd_map <- make_nice_map(bbox_df, countries_df, sd, sd_leg_val, parms, my_col, "grey80", "SD")
   
   
   # save ------------------------------------------------------------------------
