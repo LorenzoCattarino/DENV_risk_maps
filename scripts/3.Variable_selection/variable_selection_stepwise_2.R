eadd28a19433214f378015675cdf16a5e366ee03
Lorenzo Cattarino <lcattari@dide.local>
2019-08-21 21:16:58 +0100

    change post processing of addition routine

--- scripts/3.Variable_selection/variable_selection_stepwise_2.R
+++ scripts/3.Variable_selection/variable_selection_stepwise_2.R
@@ -1,49 +1,19 @@
 
-options(didehpc.cluster = "fi--didemrchnb")
+source(file.path("R", "utility_functions.R"))
+source(file.path("R", "create_parameter_list.R"))
+source(file.path("R", "random_forest", "variable_selection_stepwise.R"))
 
-CLUSTER <- TRUE
-
-my_resources <- c(
-  file.path("R", "random_forest", "fit_ranger_RF_and_make_predictions.R"),
-  file.path("R", "random_forest", "variable_selection_stepwise.R"),
-  file.path("R", "prepare_datasets", "set_pseudo_abs_weights.R"),
-  file.path("R", "utility_functions.R"),
-  file.path("R", "create_parameter_list.R"))
-
-my_pkgs <- c("ranger", "ggplot2")
-
-context::context_log_start()
-ctx <- context::context_save(path = "context",
-                             sources = my_resources,
-                             packages = my_pkgs)
+library(ggplot2)
 
 
 # define parameters ----------------------------------------------------------- 
 
 
 extra_prms <- list(var_to_fit = "FOI",
-                   no_reps = 10, 
+                   no_reps = 1, 
                    addition = TRUE,
                    parallel_2 = TRUE,
-                   top_ones_within_reps = 10,
-                   top_ones_across_reps = 20,
-                   stepwise_exp_id = 5)
-
-
-# rebuild the queue object? --------------------------------------------------- 
-
-
-if (CLUSTER) {
-  
-  config <- didehpc::didehpc_config(template = "24Core")
-  obj <- didehpc::queue_didehpc(ctx, config = config)
-  
-} else {
-  
-  context::context_load(ctx)
-  context::parallel_cluster_start(8, ctx)
-  
-}
+                   stepwise_exp_id = 6)
 
 
 # define variables ------------------------------------------------------------
@@ -66,6 +36,8 @@ plot_out_path <- file.path("figures",
                            "variable_selection", 
                            stepwise_exp_dir)
 
+no_samples <- seq_len(parameters$no_samples)
+
 
 # load data -------------------------------------------------------------------
 
@@ -77,38 +49,55 @@ boot_samples <- readRDS(file.path("output",
                                   "bootstrap_samples.rds"))
 
 
-# get results ----------------------------------------------------------------- 
+# ----------------------------------------------------------------------------- 
 
 
-bundles <- obj$task_bundle_info()
+all_dir_paths <- file.path(table_out_path, 
+                           paste0("sample_", no_samples), 
+                           "addition", 
+                           "output_from_addition.rds")
 
-my_task_id <- bundles[nrow(bundles), "name"] 
+bsample_step_addition <- lapply(all_dir_paths, readRDS)
 
-bsample_step_addition_t <- obj$task_bundle_get(my_task_id)
-
-bsample_step_addition <- bsample_step_addition_t$results()
-
-# plot data only for the first 10 samples 
-lapply(seq_along(bsample_step_addition)[1:10], 
-       plot_RMSE_addition, 
-       bsample_step_addition, 
+# plot data only for the first 10 samples
+lapply(seq_along(bsample_step_addition)[1:10],
+       plot_RMSE_addition,
+       bsample_step_addition,
        plot_out_path)
 
-all_preds <- lapply(bsample_step_addition, 
-                    get_changed_predictors, 
-                    nrow(bsample_step_addition[[1]][[1]]))
+write_out_rds(bsample_step_addition, 
+              table_out_path, 
+              "list_of_outputs_from_addition.rds")  
 
-all_preds_top <- lapply(all_preds, get_top_from_replicates, top_ones_within_reps)
-  
-# calculate selection frequency of all 26 predictors across replicates
-ret1 <- lapply(all_preds_top, calculate_sel_freq, top_ones_across_reps)
-
-no_samples <- parameters$no_samples
-
-# save best predictors from stepwise addition - for each bootstrap sample
-lapply(seq(no_samples),
-       save_addition_best_preds,
-       results = ret1, 
-       names = names(boot_samples[[1]]), 
-       table_out_path)
+# bundles <- obj$task_bundle_info()
+# 
+# my_task_id <- bundles[nrow(bundles), "name"] 
+# 
+# bsample_step_addition_t <- obj$task_bundle_get(my_task_id)
+# 
+# bsample_step_addition <- bsample_step_addition_t$results()
+# 
+# # plot data only for the first 10 samples 
+# lapply(seq_along(bsample_step_addition)[1:10], 
+#        plot_RMSE_addition, 
+#        bsample_step_addition, 
+#        plot_out_path)
+# 
+# all_preds <- lapply(bsample_step_addition, 
+#                     get_changed_predictors, 
+#                     nrow(bsample_step_addition[[1]][[1]]))
+# 
+# all_preds_top <- lapply(all_preds, get_top_from_replicates, top_ones_within_reps)
+#   
+# # calculate selection frequency of all 26 predictors across replicates
+# ret1 <- lapply(all_preds_top, calculate_sel_freq, top_ones_across_reps)
+# 
+# no_samples <- parameters$no_samples
+# 
+# # save best predictors from stepwise addition - for each bootstrap sample
+# lapply(seq(no_samples),
+#        save_addition_best_preds,
+#        results = ret1, 
+#        names = names(boot_samples[[1]]), 
+#        table_out_path)
   
