148bcc461a31cc0af674b16f28babb0f78dafd29
Lorenzo Cattarino <lcattari@dide.local>
2019-08-21 21:14:37 +0100

    add function to calculate R squared

--- R/prepare_datasets/calculate_wgt_corr.R
+++ R/prepare_datasets/calculate_wgt_corr.R
@@ -1,3 +1,23 @@
 calculate_wgt_cor <- function(d.sub, x, y){
+  
   round(wtd.cor(d.sub[,x], d.sub[,y], weight = d.sub[,"new_weight"])[,"correlation"], 3)
+
+}
+
+calculate_R_squared <- function(df, y_i, f_i) {
+  
+  y_i <- df[,y_i]
+  
+  f_i <- df[,f_i]
+    
+  y_hat <- mean(y_i)
+  
+  e_i <- y_i - f_i
+  
+  SS_t <- sum((y_i - y_hat)^2)
+  
+  SS_res <- sum(e_i^2)
+  
+  round(1 - (SS_res / SS_t), 3)
+  
 }
