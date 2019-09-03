0d2de64338ed87e222908ab4113ac28fd55d9205
Lorenzo Cattarino <lcattari@dide.local>
2019-09-03 12:32:37 +0100

    keep all the functions for this analysis together

--- R/random_forest/sensitivity_analysis_boot_cv.R
+++ R/random_forest/sensitivity_analysis_boot_cv.R
@@ -137,3 +137,89 @@ wrapper_to_multi_factor_RF_fit <- function(
   out
 
 }
+
+wrapper_to_core_fun <- function(
+  i, model_dataset, grid_size, 
+  predictors, dependent_variable, 
+  no_trees, min_node_size, 
+  x_data, y_data){
+  
+  no_data <- nrow(model_dataset)
+  
+  # overlay squared grid on data points 
+  gridded_dataset <- grid_up(model_dataset, grid_size, rnd_dist = TRUE)
+  
+  # get the cells occupied with at least one data point
+  occupied_cells <- unique(gridded_dataset$cell)
+  
+  # do bootstrapping and get the full training dataset
+  training_dataset <- do_boostrap(gridded_dataset)
+  
+  # get the cells occupied with at least one training point
+  occupied_cells_train <- unique(training_dataset$cell)
+  
+  # get the cells occupied with at least one validating point
+  occupied_cells_valid <- unique(occupied_cells[!occupied_cells %in% occupied_cells_train])
+  
+  # get the position (1/0) of the points in the training dataset
+  train_point_pos <- get_training_point_positions(no_data, training_dataset)
+  
+  # get the position (1/0) of the points in the validating dataset
+  valid_point_pos <- get_validating_point_positions(no_data, training_dataset)
+  
+  # get weights 
+  my_weights <- training_dataset$new_weight  
+  
+  # subset training dataset
+  training_dataset <- training_dataset[, c(dependent_variable, predictors)]
+  
+  
+  # ---------------------------------------- run core routine
+  
+  
+  ret <- spatial.cv.rf(
+    preds = predictors,
+    y_var = dependent_variable, 
+    train_set = training_dataset,
+    no_trees = no_trees, 
+    min_node_size = min_node_size,
+    x_data = x_data, 
+    y_data = y_data,
+    valid_points = valid_point_pos,
+    my_weights = my_weights)
+  
+  ret$train_point_pos <- train_point_pos
+  ret$valid_point_pos <- valid_point_pos
+  ret$occupied_cells_valid <- length(occupied_cells_valid)
+  ret
+}
+
+spatial.cv.rf <- function(
+  preds, y_var, train_set, 
+  no_trees, min_node_size, x_data, 
+  y_data, valid_points, my_weights){
+  
+  # fit a RF model
+  ranger_obj <- fit_RF(
+    dependent_variable = y_var, 
+    training_dataset = train_set, 
+    no_trees = no_trees, 
+    min_node_size = min_node_size,
+    my_weights = my_weights)
+  
+  # make predictions
+  predictions <- make_predictions(
+    mod_obj = ranger_obj, 
+    dataset = x_data, 
+    sel_preds = preds)
+  
+  # calculate sum of squared errors
+  sse <- calc_SSE(
+    y.data = y_data,
+    valid.set = valid_points,
+    predictions = predictions)
+  
+  list(obj = ranger_obj,
+       predictions = predictions,
+       sse = sse)
+}
