01d941a4074f15e7571ad4e545d41bd2b726fe16
Lorenzo Cattarino <lcattari@dide.local>
2019-09-08 15:36:44 +0100

    remove unused parts; rerun

--- scripts/4.Analysis/bootstrap_models/admin/predict_adm_with_bootstrap_03.R
+++ scripts/4.Analysis/bootstrap_models/admin/predict_adm_with_bootstrap_03.R
@@ -1,4 +1,4 @@
-# Calculates seroprevalence at some age
+# Calculates proportion of seronegative at some age
 
 
 source(file.path("R", "utility_functions.r"))
@@ -8,8 +8,8 @@ source(file.path("R", "create_parameter_list.R"))
 # define parameters -----------------------------------------------------------
 
 
-extra_prms  <- list(id = 4,
-                    age = 16)
+extra_prms  <- list(id = 2,
+                    age = c(9, 16))
 
 prediction_fl_nm <- "response_endemic.rds"
 
@@ -29,9 +29,7 @@ out_pt <- file.path("output",
 
 col_ids <- as.character(seq_len(parameters$no_samples))
 
-age <- parameters$age
-
-out_fl_nm <- sprintf("p%s%s", age, ".rds")
+ages <- parameters$age
 
 
 # load data ------------------------------------------------------------------- 
@@ -45,22 +43,21 @@ sqr_preds <- readRDS(file.path("output",
                                prediction_fl_nm))
 
 
-# calculate seroprevalence at age X -------------------------------------------
+# calculate proportion of seronegative at age X -------------------------------
 
 
-serop_var <- 100 * (1 - exp(-4 * age * sqr_preds[, col_ids])) # percentage
-
-base_info <- sqr_preds[, setdiff(names(sqr_preds), col_ids)]
+for (i in seq_along(ages)) {
   
-final_dts <- cbind(base_info, serop_var)
-
-write_out_rds(final_dts, out_pt, out_fl_nm)  
-
-false_positive_specifA <- 0.1 * (serop_var/100)
-false_positive_specifB <- 0.01 * (serop_var/100)
+  age <- ages[i] 
   
-final_dtsA <- cbind(base_info, false_positive_specifA * 100)
-final_dtsB <- cbind(base_info, false_positive_specifB * 100)
-
-write_out_rds(final_dtsA, out_pt, sprintf("p%s_FP_specifA%s", age, ".rds"))
-write_out_rds(final_dtsB, out_pt, sprintf("p%s_FP_specifB%s", age, ".rds"))
+  serop_var <- 100 * exp(-4 * age * sqr_preds[, col_ids]) # percentage
+  
+  base_info <- sqr_preds[, setdiff(names(sqr_preds), col_ids)]
+  
+  final_dts <- cbind(base_info, serop_var)
+  
+  out_fl_nm <- sprintf("p%s%s", age, ".rds")
+  
+  write_out_rds(final_dts, out_pt, out_fl_nm)  
+  
+}
