e09d059f0589a5f4a51a12e1cbf3f8154bb11470
Lorenzo Cattarino <lcattari@dide.local>
2019-09-08 15:57:32 +0100

    rerun

--- scripts/4.Analysis/bootstrap_models/admin/interventions_adm_bootstrap_03.R
+++ scripts/4.Analysis/bootstrap_models/admin/interventions_adm_bootstrap_03.R
@@ -16,7 +16,7 @@ source(file.path("R", "prepare_datasets", "calculate_mean_across_fits.R"))
 # define parameters ----------------------------------------------------------- 
 
 
-extra_prms <- list(id = 4,
+extra_prms <- list(id = 2,
                    R0_scenario = c(1, 2),
                    baseline_scenario_ids = 4,
                    intervention_name = "vaccine",
