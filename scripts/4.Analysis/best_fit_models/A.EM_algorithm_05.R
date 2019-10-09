d14c9c57800021cf70d3a93bbc316b2629dcfb01
Lorenzo Cattarino <lcattari@dide.local>
2019-10-09 09:03:58 +0100

    save EM diagnostic data

--- scripts/4.Analysis/best_fit_models/A.EM_algorithm_05.R
+++ scripts/4.Analysis/best_fit_models/A.EM_algorithm_05.R
@@ -8,12 +8,13 @@
 library(ggplot2)
 
 source(file.path("R", "plotting", "plot_EM_diagnostics.R"))
+source(file.path("R", "utility_functions.R"))
 
 
 # define parameters ----------------------------------------------------------- 
 
 
-parameters <- list(id = 2,
+parameters <- list(id = 1,
                    dependent_variable = "FOI")   
 
 
@@ -49,3 +50,30 @@ data_to_plot <- readRDS(file.path(diag_t_pth, "diagno_table.rds"))
 
 
 plot_EM_diagnostics(data_to_plot, figure_out_path, fig_file_tag)
+
+
+# save data plot --------------------------------------------------------------
+
+
+diagnostic_vars <- c("RF_ms_i", "ss_i", "ss_j", "r_av_sqr", "r_adm")
+
+data_to_plot_sub <- data_to_plot[, diagnostic_vars]
+  
+data_to_save <- as.data.frame(data_to_plot_sub)
+
+strip_labs <- c("mean square error", 
+                "1/6 degree sum of square", 
+                "admin unit sum of square",
+                "1/6 degree correlation",
+                "admin unit correlation") 
+
+names(data_to_save) <- strip_labs
+
+data_to_save$iter <- seq_len(nrow(data_to_save))
+
+data_to_save_long <- reshape2::melt(data_to_save, 
+                                    id.vars = "iter", 
+                                    measure.vars = strip_labs)
+
+write_out_csv(data_to_save_long, diag_t_pth, "diagnostic_data_plot.csv", row.names = FALSE)
+  
