a63047eae79c225faef63e0df1b47cc3f68a61ba
Lorenzo Cattarino <lcattari@dide.local>
2019-10-10 13:55:00 +0100

    define more theme elements

--- scripts/5.Post-processing/manuscript/plot_data_points.R
+++ scripts/5.Post-processing/manuscript/plot_data_points.R
@@ -7,12 +7,16 @@ library(ggplot2)
 # define parameters -----------------------------------------------------------
 
 
-parameters <- list(barwdt = 1.4,
+parameters <- list(barwdt = 1.5,
                    barhgt = 4.5,
+                   pol_brd_sz = 0.1,
+                   leg_pos_x = 0,
+                   leg_pos_y = 0,
+                   leg_txt_sz = 10,
+                   leg_ttl_sz = 12,
                    coord_limits = c(-130, 180, -60, 38),
                    adm1_i_want = c("BRA", "COL", "VEN", "MEX", "IND", "AUS"),
                    poly_fill = "grey80",
-                   poly_bd_sz = 0.1,
                    poly_bd_col = "grey40")
 
 
@@ -24,17 +28,25 @@ x2 <- parameters$coord_limits[2]
 y1 <- parameters$coord_limits[3]
 y2 <- parameters$coord_limits[4]
 
-adm1_i_want <- parameters$adm1_i_want
-  
-poly_fill <- parameters$poly_fill
-
-poly_bd_sz <- parameters$poly_bd_sz
-
-poly_bd_col <- parameters$poly_bd_col
-
 barwdt <- parameters$barwdt
 
 barhgt <- parameters$barhgt
+
+poly_bd_sz <- parameters$pol_brd_sz
+
+leg_pos_x <- parameters$leg_pos_x
+
+leg_pos_y <- parameters$leg_pos_y
+  
+leg_txt_sz <- parameters$leg_txt_sz
+
+leg_ttl_sz <- parameters$leg_ttl_sz 
+
+adm1_i_want <- parameters$adm1_i_want
+
+poly_fill <- parameters$poly_fill
+
+poly_bd_col <- parameters$poly_bd_col
 
 my_col <- colorRamps::matlab.like(100)
 
@@ -87,16 +99,19 @@ p <- ggplot() +
                                                  barwidth = barwdt, 
                                                  barheight = barhgt)) +
   coord_sf(xlim = c(x1, x2), ylim = c(y1, y2), expand = FALSE) +
-  theme(legend.justification = c(0, 0),
-        legend.position = c(0, 0),
-        plot.margin = unit(c(0, 0, 0, 0), "cm"),
+  theme(panel.background = element_rect(fill = "aliceblue"),
+        panel.border = element_rect(fill = NA, colour = "black"),
         axis.line = element_blank(),
-        axis.text.x = element_blank(),
-        axis.text.y = element_blank(),
+        axis.text = element_blank(),
         axis.ticks = element_blank(),
-        axis.title.x = element_blank(),
-        axis.title.y = element_blank(),
-        panel.background = element_blank())
+        axis.title = element_blank(),
+        legend.justification = c(0, 0),
+        legend.position = c(leg_pos_x, leg_pos_y),
+        plot.margin = unit(c(0, 0, 0, 0), "cm"),
+        legend.text = element_text(size = leg_txt_sz),
+        legend.title = element_text(face = "bold", size = leg_ttl_sz),
+        legend.box.background = element_rect(fill = "white", colour = "black"),
+        legend.box.margin = margin(0.025, 0.025, 0.025, 0.025, unit = "cm"))
 
 png(file.path("figures", "data", "dengue_points.png"), 
     width = 18.4, 
