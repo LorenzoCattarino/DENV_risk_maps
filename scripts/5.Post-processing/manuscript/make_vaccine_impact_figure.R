01920c8dffb03a8557415689f294e77bdb4633d7
Lorenzo Cattarino <lcattari@dide.local>
2019-10-10 10:21:17 +0100

    resize

--- scripts/5.Post-processing/manuscript/make_vaccine_impact_figure.R
+++ scripts/5.Post-processing/manuscript/make_vaccine_impact_figure.R
@@ -9,7 +9,7 @@ out_fig_path <- file.path("figures",
 scale_fill_vals <- c("lightskyblue1", "lightskyblue4", "deepskyblue")
 leg_title <- "Screening age"
 leg_labels <- c("9", "16", "Optimal")
-y_axis_title <- "Reduction in cases"
+y_axis_title <- "Cases reduction"
 screening_ages <- c(9, 16, 0)
 y_values <- seq(0, 0.45, 0.1)
 
@@ -39,7 +39,7 @@ p <- ggplot(summary_table_all, aes(x = treatment, y = mean, fill = treatment, ym
                     guide = guide_legend(title = leg_title,
                                          keywidth = 1.3,
                                          keyheight = 1.3,
-                                         label.theme = element_text(size = 12))) +
+                                         label.theme = element_text(size = 10))) +
   xlab(NULL) +
   scale_y_continuous(y_axis_title,
                      breaks = y_values,
@@ -51,7 +51,7 @@ p <- ggplot(summary_table_all, aes(x = treatment, y = mean, fill = treatment, ym
         axis.text.x = element_blank(),
         axis.ticks.x = element_blank(),
         axis.text.y = element_text(size = 12),
-        plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm"),
+        plot.margin = unit(c(0, 0, 0.05, 0), "cm"),
         strip.text.x = element_text(size = 10),
         legend.position = "bottom")
 
@@ -60,8 +60,8 @@ dir.create(out_fig_path, FALSE, TRUE)
 barplot_fl_nm <- paste0("proportional_reduction_in_", my_var_name, "_", intervention_name, ".png")
 
 png(file.path(out_fig_path, barplot_fl_nm),
-    width = 12,
-    height = 10,
+    width = 9,
+    height = 8,
     units = "cm",
     pointsize = 12,
     res = 300)
