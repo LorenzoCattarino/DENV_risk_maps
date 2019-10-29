7969d3c46420a8a15ed45c0eb6bfd4944891b4dd
Lorenzo Cattarino <lcattari@dide.local>
2019-10-29 11:52:59 +0000

    add a loop

--- scripts/4.Analysis/bootstrap_models/pixels/predict_squ_with_bootstrap_02.r
+++ scripts/4.Analysis/bootstrap_models/pixels/predict_squ_with_bootstrap_02.r
@@ -9,19 +9,20 @@ source(file.path("R", "prepare_datasets", "calculate_mean_across_fits.R"))
 # define parameters ----------------------------------------------------------- 
 
 
-extra_prms <- list(id = 8) 
-
-vars_to_average <- "response"
-# vars_to_average <- "transformed_1_wolbachia_4"
-# vars_to_average <- "transformed_2_wolbachia_4"
-# vars_to_average <- "C_pr_2_wolbachia_16"
-# vars_to_average <- "C_pr_2_wolbachia_32"
+extra_prms <- list(id = 2,
+                   vars_to_average = c("response",
+                                       "transformed_1_wolbachia_4",
+                                       "transformed_2_wolbachia_4",
+                                       "C_pr_2_wolbachia_16",
+                                       "C_pr_2_wolbachia_32"))
 
 
 # define variables ------------------------------------------------------------
 
 
 parameters <- create_parameter_list(extra_params = extra_prms)
+
+vars_to_average <- parameters$vars_to_average
 
 model_type <- paste0("model_", parameters$id)
 
@@ -36,16 +37,24 @@ in_path <- file.path("output",
 # -----------------------------------------------------------------------------
 
 
-dat <- readRDS(file.path(in_path, paste0(vars_to_average, ".rds")))
-
-dat <- as.data.frame(dat)
-
-ret <- average_boot_samples_dim2(dat[, col_names])
-
-base_info <- dat[, setdiff(names(dat), col_names)]
-
-ret2 <- cbind(base_info, ret)
-
-out_name <- paste0(vars_to_average, "_mean.rds")
-
-write_out_rds(ret2, in_path, out_name)
+for (i in seq_along(vars_to_average)) {
+  
+  var_to_average <- vars_to_average[i]
+  
+  message(var_to_average)
+  
+  dat <- readRDS(file.path(in_path, paste0(var_to_average, ".rds")))
+  
+  dat <- as.data.frame(dat)
+  
+  ret <- average_boot_samples_dim2(dat[, col_names], na.rm = TRUE)
+  
+  base_info <- dat[, setdiff(names(dat), col_names)]
+  
+  ret2 <- cbind(base_info, ret)
+  
+  out_name <- paste0(var_to_average, "_mean.rds")
+  
+  write_out_rds(ret2, in_path, out_name)
+  
+}
